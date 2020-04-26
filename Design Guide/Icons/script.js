9992012e3b95b784872e92385c1a48a716933c59
borntofrappe <borntofrappe@gmail.com>
2020-04-26 18:36:39 +0200

    describe fonts, colors and icons

---
+++ Design Guide/Icons/script.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const fs = require('fs');
+
+const icons = {};
+const extname = '.svg';
+
+function readFiles(folder = '.') {
+  const files = fs.readdirSync(folder, { withFileTypes: true });
+
+  files.forEach(file => {
+    const { name } = file;
+    if (file.isDirectory()) {
+      readFiles(`${folder}/${name}`);
+    } else {
+
+      if (path.extname(name) === extname) {
+        const key = path
+          .basename(name, extname)
+          .split('-')
+          .map((v, i) => (i === 0 ? v : `${v[0].toUpperCase()}${v.slice(1)}`))
+          .join('');
+
+        const value = fs.readFileSync(`${folder}/${name}`, {
+          encoding: 'utf-8',
+        });
+
+        if (!icons[key]) {
+          const syntax = value.replace(/[\r\n]/g, '').replace(/\>\s+\</g, '><');
+          const location = `${folder}/${name}`;
+
+          icons[key] = {
+            syntax,
+            location,
+          };
+        } else {
+          console.log(
+            `Duplicate icon: \x1b[33m${
+              icons[key].location
+            }\x1b[0m and \x1b[33m${folder}/${name}\x1b[0m`
+          );
+        }
+      }
+    }
+  });
+}
+readFiles();
+
+const entries = Object.entries(icons);
+
+// icons.js
+fs.writeFileSync(
+  'icons.js',
+  `const icons = {${entries
+    .map(([key, value]) => `\n\t${key}: \`${value.syntax}\`,`)
+    .join('')}\n}`,
+  'utf-8'
+);
+
+// index.html
+const markup = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Icons</title>
+    <style>
+      main {
+        width: 90vw;
+        max-width: 30rem;
+        margin: 1rem auto;
+        color: hsl(0, 0%, 5%);
+        text-align: center;
+      }
+      article {
+        display: inline-flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 1rem;
+      }
+      article h2 {
+        font-family: monospace;
+        margin-bottom: 0.5rem;
+      }
+      article svg {
+        width: 3rem;
+        height: auto;
+        display: block:
+      }
+    </style>
+  </head>
+  <body>
+      <main>
+        ${entries.map(([name, {syntax}]) => `
+          <article>
+            <h2>${name}</h2>
+            ${syntax}
+          </article>
+        `).join("")}
+      </main>
+  </body>
+</html>
+`.trim();
+
+fs.writeFileSync('index.html', markup);
--- Design Guide/README.md
+++ Design Guide/README.md
@@ -2,11 +2,45 @@
 
 Systems, patterns and assets used in the website.
 
+## Fonts
+
+| Choice          | Type       | Purpose                             |
+| --------------- | ---------- | ----------------------------------- |
+| Maven Pro       | sans-serif | navigation, footer, header, heading |
+| Crimson Pro     | serif      | body                                |
+| Source Code Pro | monospace  | code                                |
+
+Import with HTML
+
+```html
+<link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Maven+Pro:wght@500;600;700&family=Source+Code+Pro&display=swap" rel="stylesheet" />
+```
+
+Import with CSS
+
+```css
+@import url("https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Maven+Pro:wght@500;600;700&family=Source+Code+Pro&display=swap");
+```
+
+## Colors
+
+> the markup shows the picks side by side
+
+Scales of 11 choices, varying in hue, saturation, and most prominently lightness
+
+- primary
+
+- secondary
+
+- grey
+
+Set up with custom properties: `--primary-0`, `--primary-1`, where greater index values correspond to greater lightness.
+
 ## Icons
 
-Ultimately, the idea is to build a object describing the icons through property value pairs, and have the templating engine inject the syntax instead.
+> the script creates the object with the icons, _and_ the markup showing the assets
 
-In `_data` and `icons.js`:
+In `_data`, add `icons.js` describing the icons through key value pairs:
 
 ```js
 module.exports = {
@@ -15,7 +49,7 @@ module.exports = {
 };
 ```
 
-Through the templating language:
+Through the templating language, inject the icon where needed:
 
 ```njk
 {{ icons.rocket }}
