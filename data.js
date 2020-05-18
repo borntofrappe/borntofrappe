f07306b514258e4558c250aa24ecedae533a4512
borntofrappe <borntofrappe@gmail.com>
2020-05-18 09:29:35 +0200

    set up temporary landing page

---
+++ data.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        name: 'Mario Kart 8 Deluxe',
+        date: '2017-04-28',
+        value: 24.77
+    },
+    {
+        name: 'Super Smash Bros. Ultimate',
+        date: '2018-12-07',
+        value: 18.84
+    },
+    {
+        name: 'The Legend of Zelda: Breath of the Wild',
+        date: '2017-03-03',
+        value: 17.41
+    },
+    {
+        name: 'Super Mario Odyssey',
+        date: '2017-10-27',
+        value: 17.41
+    },
+    {
+        name: 'Pokémon Sword/Pokémon Shield',
+        date: '2019-11-16',
+        value: 17.37
+    },
+    {
+        name: 'Pokémon: Let\'s Go, Pikachu!/ Pokémon: Let\'s Go, Eevee!',
+        date: '2018-11-16',
+        value: 11.97
+    },
+    {
+        name: 'Animal Crossing: New Horizons',
+        date: '2020-03-20',
+        value: 11.77
+    },
+    {
+        name: 'Splatoon 2',
+        date: '2017-07-21',
+        value: 10.13
+    },
+    {
+        name: 'Super Mario Party',
+        date: '2018-10-05',
+        value: 10.10
+    },
+    {
+        name: 'New Super Mario Bros. U Deluxe',
+        date: '2019-01-11',
+        value: 6.60
+    },
+]
