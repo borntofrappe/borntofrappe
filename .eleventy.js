28a8981d6db5b300a157f1a3d81069f5e1252ba7
borntofrappe <borntofrappe@gmail.com>
2020-05-18 23:59:08 +0200

    add meta macro

--- .eleventy.js
+++ .eleventy.js
@@ -1,57 +1,58 @@
-const markdownIt = require('markdown-it');
-const icons = require('./src/_data/icons');
+const markdownIt = require("markdown-it");
+const icons = require("./src/_data/icons");
 
 module.exports = function (eleventyConfig) {
   const markdownLibrary = markdownIt();
-  markdownLibrary.renderer.rules.heading_open = function(tokens, index) {
+  markdownLibrary.renderer.rules.heading_open = function (tokens, index) {
     const { tag } = tokens[index];
     const { content } = tokens[index + 1];
-    const id = content.toLowerCase().replace(/ /g, '-');
-  
+    const id = content.toLowerCase().replace(/ /g, "-");
+
     return `<${tag} id="${id}" class="permalink">`;
   };
-  markdownLibrary.renderer.rules.heading_close = function(tokens, index) {
+  markdownLibrary.renderer.rules.heading_close = function (tokens, index) {
     const { tag } = tokens[index];
     const { content } = tokens[index - 1];
-    const id = content.toLowerCase().replace(/ /g, '-');
-  
+    const id = content.toLowerCase().replace(/ /g, "-");
+
     return `<a href="#${id}"><span class="visually-hidden">permalink</span>${icons.permalink}</a></${tag}>`;
   };
-  
-  markdownLibrary.renderer.rules.fence = function(tokens, index) {
+
+  markdownLibrary.renderer.rules.fence = function (tokens, index) {
     const token = tokens[index];
     const { info, content } = token;
-    let [name, lang] = info.split('.');
+    let [name, lang] = info.split(".");
     if (!lang) {
       lang = name;
     }
-    const icon = icons[lang] ? icons[lang] : '';
+    const icon = icons[lang] ? icons[lang] : "";
     const code = content.replace(/</g, "&lt;");
 
     return `<div class="code"><span>${icon} ${info}</span><pre><code>${code}</code></pre></div>`;
   };
 
-  eleventyConfig.setLibrary('md', markdownLibrary);
+  eleventyConfig.setLibrary("md", markdownLibrary);
 
-
-  eleventyConfig.addCollection('blog', function(collection) {
-    return collection.getFilteredByGlob('./src/blog/*.md').sort((a, b) => b.date - a.date);
+  eleventyConfig.addCollection("blog", function (collection) {
+    return collection
+      .getFilteredByGlob("./src/blog/*.md")
+      .sort((a, b) => b.date - a.date);
   });
 
-  eleventyConfig.addFilter("formatdate", function(date) {
+  eleventyConfig.addFilter("formatdate", function (date) {
     const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
     ];
     const year = date.getFullYear();
     const month = date.getMonth();
@@ -60,22 +61,22 @@ module.exports = function (eleventyConfig) {
     return `${months[month]} ${day}, ${year}`;
   });
 
-  eleventyConfig.addFilter("paths", function(url) {
+  eleventyConfig.addFilter("paths", function (url) {
     return url
       .slice(1)
       .split("/")
       .map((value, index, items) => ({
         value,
-        href: '/' + items.slice(0, index + 1).join("/")
+        href: "/" + items.slice(0, index + 1).join("/"),
       }));
   });
 
-  eleventyConfig.addPassthroughCopy('./src/static/');
+  eleventyConfig.addPassthroughCopy("./src/static/");
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
-    }
+      input: "src",
+      output: "dist",
+    },
   };
 };
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # borntofrappe
 
 A personal website. Coming soon: 2020-05-20.
+
+{% set title = "borntofrappe" %}
+{% set description = "Personal website for an ever changing character: aspiring developer, fledging designer, long-distance runner." %}
+
+{% set url = "borntofrappe / blog" %}
+{% set description = "A series of articles from a wannabe author. On HTML, CSS, JavaScript, SVG, and hopefully more." %}
