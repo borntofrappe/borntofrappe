ddcff8a06d767b08828265f407db62fcd68e3832
borntofrappe <borntofrappe@gmail.com>
2020-05-19 00:45:10 +0200

    restructure meta tags and macro

--- .eleventy.js
+++ .eleventy.js
@@ -63,8 +63,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("paths", function (url) {
     return url
-      .slice(1)
       .split("/")
+      .slice(1)
       .map((value, index, items) => ({
         value,
         href: "/" + items.slice(0, index + 1).join("/"),
--- src/blog/testing-testing.md
+++ src/blog/testing-testing.md
@@ -1,7 +1,6 @@
 ---
 title: testing-testing
 date: 2020-05-20
-brief: By this date I hope to publish the website, but again, this is a test
 keywords: ["rocket"]
 ---
 
