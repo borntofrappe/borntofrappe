ad39ba2f62e2ea8ece07abeda69ecbe8d4366faa
borntofrappe <borntofrappe@gmail.com>
2020-05-23 23:32:09 +0200

    don't run puppeteer if there aren't new articles

--- puppeteer.js
+++ puppeteer.js
@@ -32,33 +32,36 @@ const posts = files
     };
   });
 
-const width = 1000;
-const height = 500;
 const { length } = posts;
+  
+if(length > 0) {
+  const width = 1000;
+  const height = 500;
 
-const headless = true;
-(async () => {
-  const browser = await puppeteer.launch({ headless });
-  const page = await browser.newPage();
-  await page.setViewport({
-    width,
-    height: height * length,
-  });
-
-  const html = template(posts);
-  await page.setContent(html, { waitUntil: "networkidle0" });
-
-  for (const [index, post] of posts.entries()) {
-    const { name } = post;
-    const x = 0;
-    const y = index * height;
-
-    const outputPath = `${output.path}${name}${output.extension}`;
-    console.log(
-      `Writing ${outputPath} from ${input.path}${name}${input.extension}`
-    );
-    await page.screenshot({ path: outputPath, clip: { x, y, width, height } });
-  }
-  await browser.close();
-  console.log(`Wrote ${posts.length} files`);
-})();
+  const headless = true;
+  (async () => {
+    const browser = await puppeteer.launch({ headless });
+    const page = await browser.newPage();
+    await page.setViewport({
+      width,
+      height: height * length,
+    });
+  
+    const html = template(posts);
+    await page.setContent(html, { waitUntil: "networkidle0" });
+  
+    for (const [index, post] of posts.entries()) {
+      const { name } = post;
+      const x = 0;
+      const y = index * height;
+  
+      const outputPath = `${output.path}${name}${output.extension}`;
+      console.log(
+        `Writing ${outputPath} from ${input.path}${name}${input.extension}`
+      );
+      await page.screenshot({ path: outputPath, clip: { x, y, width, height } });
+    }
+    await browser.close();
+    console.log(`Wrote ${posts.length} files`);
+  })();
+}
