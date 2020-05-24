7d392b5c4f8f022b8956e1f21210b459ab9a9826
borntofrappe <borntofrappe@gmail.com>
2020-05-24 23:09:35 +0200

    add package.json files

--- playground/Utils/Markdown Parser/script-syntax.js
+++ playground/Utils/Markdown Parser/script-syntax.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
 const shiki = require('shiki');
-const { create } = require('domain');
 
 const input = 'blog';
 const output = 'pages';
