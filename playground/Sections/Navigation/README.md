b4c7493bec7caf755118060e0cd9753d3e4195ed
borntofrappe <borntofrappe@gmail.com>
2020-07-15 23:37:19 +0200

    add navigation, fix margin

---
+++ playground/Sections/Navigation/README.md
@@ -0,0 +1,13 @@
+# Navigation
+
+The component **Breadcrumb navigation** completes much of the logic behind the navigation included at the top of every page.
+
+Following the **Dark mode toggle** component however, it is necessary to incorporate the two next to one another.
+
+## SVG syntax
+
+The `viewBox` attribute is modified to have the sun and moon visuals featured more prominently in the wrapping SVG element. Consider it a zoom-in operation.
+
+## CSS transition
+
+The duration is considerably decreased to match value set for the `:hover` and `:focus` state on the anchor link elements.
---
+++ playground/Sections/Navigation/script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('nav button');
+button.removeAttribute('disabled');
+
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const colorScheme = window.localStorage.getItem('color-scheme');
+
+function setPreference(preference) {
+  document.body.setAttribute('data-preference', preference);
+  window.localStorage.setItem('color-scheme', preference);
+}
+
+if (colorScheme) {
+  setPreference(colorScheme);
+} else {
+  setPreference(mediaQuery.matches ? 'dark' : 'light');
+}
+
+button.addEventListener('click', () => setPreference(window.localStorage.getItem('color-scheme') === 'dark' ? 'light' : 'dark'));
+mediaQuery.addListener(({ matches }) => setPreference(matches ? 'dark' : 'light'));
+
+const timeout = setTimeout(() => {
+  document.body.setAttribute('data-transition', 'true');
+  clearTimeout(timeout);
+}, 0)
