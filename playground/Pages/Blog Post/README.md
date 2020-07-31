7b1ff2c493926dd736b2dee93d25cb9c2e7dbf6a
borntofrappe <borntofrappe@gmail.com>
2020-07-31 15:49:37 +0200

    update blog post pages

--- playground/Pages/Blog Post/README.md
+++ playground/Pages/Blog Post/README.md
@@ -1,4 +1,4 @@
-# Blog Post
+Page shown for articles in the blog, following the `/blog/post-title` path.
 
 ## Markup
 
@@ -6,19 +6,19 @@ Include the code from the following projects:
 
 - components/skip to content
 
-- components/breadcrumb navigation
+- sections/navigation
 
-- ???
+- sections/header blog post
 
-- sections/header blog header
+- content
 
 - components/footer
 
-## ???
+## content
 
-Between the header and footer, the project includes a `<main>` container for the content presumed from a series of articles.
+Include the markup presumed for a series of articles in a `<main>` element with an `id` of `content`.
 
-The markup is produced with a script using `marked` and `shiki`. Refer to the **Utils** folder and the **Markdown Parser** project for more information.
+The markup is produced with a script using `markdown-it` and `shiki`. Refer to _Utils/Markdown Parser_ and specifically `script-markdown-it` to see how the syntax is created from markdown documents.
 
 For the stylesheet, consider the code for the following components:
 
--- playground/Pages/Blog Post/script.js
+++ playground/Pages/Blog Post/script.js
@@ -1,32 +1,32 @@
 function setPreference(preference) {
-    document.body.setAttribute('data-preference', preference);
-    window.localStorage.setItem('color-scheme', preference);
+  document.body.setAttribute('data-preference', preference);
+  window.localStorage.setItem('color-scheme', preference);
+}
+
+if (window.CSS && CSS.supports('--primary-6: hotpink')) {
+  const button = document.querySelector('button');
+  button.removeAttribute('disabled');
+
+  const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+  const colorScheme = window.localStorage.getItem('color-scheme');
+
+  if (colorScheme) {
+    setPreference(colorScheme);
+  } else {
+    setPreference(prefersColorScheme.matches ? 'dark' : 'light');
   }
-  
-  if (window.CSS && CSS.supports('--primary-6: hotpink')) {
-    const button = document.querySelector('button');
-    button.removeAttribute('disabled');
-  
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    const colorScheme = window.localStorage.getItem('color-scheme');
-  
-    if (colorScheme) {
-      setPreference(colorScheme);
-    } else {
-      setPreference(mediaQuery.matches ? 'dark' : 'light');
-    }
-  
-    button.addEventListener('click', () =>
-      setPreference(
-        window.localStorage.getItem('color-scheme') === 'dark' ? 'light' : 'dark'
-      )
-    );
-    mediaQuery.addListener(({ matches }) =>
-      setPreference(matches ? 'dark' : 'light')
-    );
-  
-    const timeout = setTimeout(() => {
-      document.body.setAttribute('data-transition', 'true');
-      clearTimeout(timeout);
-    }, 0);
-  }
+
+  button.addEventListener('click', () =>
+    setPreference(
+      window.localStorage.getItem('color-scheme') === 'dark' ? 'light' : 'dark'
+    )
+  );
+  prefersColorScheme.addListener(({ matches }) =>
+    setPreference(matches ? 'dark' : 'light')
+  );
+
+  const timeout = setTimeout(() => {
+    document.body.setAttribute('data-transition', 'true');
+    clearTimeout(timeout);
+  }, 0);
+}
