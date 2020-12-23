c525b005d89f913d187de85a41f231d34c1eed37
borntofrappe <borntofrappe@gmail.com>
2020-12-23 15:39:47 +0100

    add syntax highlighting with highlight.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const highlighter = require('highlight.js')
 const markdownIt = require('markdown-it');
 const icons = require('./src/_data/icons');
 
@@ -7,8 +8,8 @@ const titleFilter = require('./src/filters/title-filter');
 const fileNameFilter = require('./src/filters/file-name-filter');
 
 module.exports = function (eleventyConfig) {
-
   const markdownLibrary = markdownIt();
+
   markdownLibrary.renderer.rules.heading_open = function (tokens, index) {
     const { tag } = tokens[index];
     const { content } = tokens[index + 1];
@@ -24,15 +25,10 @@ module.exports = function (eleventyConfig) {
     return `<a href="#${id}"><span class="visually-hidden">Permalink</span>${icons.permalink}</a></${tag}>`;
   };
 
-  markdownLibrary.renderer.rules.fence = function (tokens, index) {
-    const token = tokens[index];
-    const { info, content } = token;
-    const lang = info;
+  markdownLibrary.options.highlight = function(str, lang) {
     const icon = icons[lang] ? icons[lang] : '';
-    const code = content.replace(/</g, '&lt;');
-
-    return `<div class="code"><span aria-label="Language">${icon} ${info}</span><pre><code>${code}</code></pre></div>`;
-  };
+    return `<div class="code"><span aria-label="Language">${icon} ${lang}</span><pre><code>${highlighter.highlight(lang, str).value}</code></pre></div>`;
+  }
 
   eleventyConfig.setLibrary('md', markdownLibrary);
 
