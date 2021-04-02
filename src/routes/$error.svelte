8fb8c5c18aff6faf2abbe4f4a30fbe9663a92d87
borntofrappe <borntofrappe@gmail.com>
2021-04-02 23:54:55 +0200

    code | error page

---
+++ src/routes/$error.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let status;
+  export let error;
+</script>
+
+<main>
+  <h1>{status}</h1>
+  {#if status === 404}
+    <p>Page not found</p>
+  {:else}
+    <pre>{error.stack}</pre>
+  {/if}
+</main>
