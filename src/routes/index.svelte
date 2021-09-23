145f95593b004c70b718322619b1c41eefc449ef
borntofrappe <borntofrappe@gmail.com>
2021-09-23 22:08:40 +0200

    include global stylesheet and webfonts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
 <h1>Hello, World!</h1>
 <p>I'm building this website with <a href="https://kit.svelte.dev">SvelteKit</a>!</p>
 <p>Fingers crossed, the project is live and deployed through <a href="https://www.netlify.com/">Netlify</a></p>
+
+<h2>Fonts</h2>
+<p>This website louds <em>a few</em> fonts, among which <strong>Josefin Sans</strong> and <strong>Cantarell</strong>.</p>
+<p>Code snippets are rendered with <strong>Fira Code</strong>, but only if the browser detects the font locally. Otherwise there's a quite <strong><em>deep</em></strong> font stack.</p>
