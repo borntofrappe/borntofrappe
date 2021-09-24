76e5248313675b25f86bf2de3ef5b2b7ee65c02b
borntofrappe <borntofrappe@gmail.com>
2021-09-24 22:56:03 +0200

    fix path to icons and images

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -3,6 +3,8 @@
 
 	export let description;
 
+	const baseUrl = 'https://borntofrappe.netlify.app';
+
 	$: title = `borntofrappe${$page.path !== '/' ? $page.path.split('/').join(' / ') : ''}`;
 </script>
 
@@ -10,15 +12,15 @@
 	<title>{title}</title>
 	<meta name="description" content={description} />
 
-	<link rel="canonical" href="{$page.host}{$page.path}" />
+	<link rel="canonical" href="{baseUrl}{$page.path}" />
 
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:url" content="{$page.host}{$page.path}" />
+	<meta property="og:url" content="{baseUrl}{$page.path}" />
 	<meta property="og:locale" content="en_US" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="borntofrappe" />
-	<meta property="og:image" content="{$page.host}/images/borntofrappe.png" />
+	<meta property="og:image" content="{baseUrl}/images/borntofrappe.png" />
 	<meta property="og:image:alt" content={description} />
 
 	<meta property="twitter:card" content="summary_large_image" />
@@ -26,6 +28,6 @@
 	<meta property="twitter:description" content={description} />
 	<meta property="twitter:site" content="@borntofrappe" />
 	<meta property="twitter:creator" content="@borntofrappe" />
-	<meta property="twitter:image" content="{$page.host}/images/borntofrappe.png" />
+	<meta property="twitter:image" content="{baseUrl}/images/borntofrappe.png" />
 	<meta property="twitter:image:alt" content={description} />
 </svelte:head>
