35aac13b84e6ce75c04e43e0c02b47879d07e851
borntofrappe <borntofrappe@gmail.com>
2021-12-09 18:21:00 +0100

    update error page and header routes

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -10,6 +10,7 @@
 </script>
 
 <script>
+	import icons from '$lib/components/icons';
 	import Meta from '$lib/components/routes/Meta.svelte';
 	import Header from '$lib/components/routes/Header.svelte';
 	import RelativeLink from '$lib/components/routes/RelativeLink.svelte';
@@ -20,7 +21,10 @@
 
 <Meta title="{status} | borntofrappe" description={error} />
 
-<Header title="Oopsie" body="Something quite unexpected has happened." />
+<Header>
+	<h1>Oopsie</h1>
+	<p>Something quite unexpected has happened.</p>
+</Header>
 
 <main id="content">
 	<h2 class="visually-hidden">Status code {status}</h2>
@@ -30,29 +34,7 @@
 	<div>
 		<span>
 			{status}
-			<svg
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="-50 -50 100 100"
-				width="1em"
-				height="1em"
-			>
-				<g stroke="currentColor" stroke-linejoin="round" stroke-linecap="round">
-					<g transform="scale(0.8)">
-						<g fill="none" stroke-width="16">
-							<g transform="translate(0 13.5)">
-								<path d="M 34 0 h -68" />
-								<path transform="rotate(45)" d="M 40 0 h -80" />
-								<path transform="rotate(-45)" d="M 40 0 h -80" />
-							</g>
-						</g>
-						<g fill="currentColor" stroke-width="8">
-							<path d="M -0 -46 a 17 17 0 0 1 17 17 h -34 a 17 17 0 0 1 17 -17" />
-							<path d="M -0 -10 h 25 v 25 a 25 25 0 0 1 -50 0 v -25 z" />
-						</g>
-					</g>
-				</g>
-			</svg>
+			{@html icons.bug}
 		</span>
 		<pre><code>{error.message}</code></pre>
 	</div>
@@ -123,7 +105,7 @@
 		background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="-20 -20 240 240"%3E%3Cdefs%3E%3Ccircle id="dot" r="2"%3E%3C/circle%3E%3Cpath id="star" d="M 0 -5 a 5 5 0 0 0 5 5 5 5 0 0 0 -5 5 5 5 0 0 0 -5 -5 5 5 0 0 0 5 -5"%3E%3C/path%3E%3C/defs%3E%3Cg fill="hsl(185, 91%25, 17%25)" stroke="hsl(185, 91%25, 17%25)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"%3E%3Cuse href="%23dot" transform="translate(27.96 4.62) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(116.2 -9.49) scale(0.31) rotate(-4)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.57 15.18) scale(0.57)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(8.87 40.07) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.77 43.19) scale(0.28)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(78.77 44.35) scale(0.67)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(142.49 38.37) scale(0.3)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(189.24 46.54) scale(0.66)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(81.94 64.58) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(97.16 55.31) scale(0.35)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(182.15 65.83) scale(0.65)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(-1.64 70.38) scale(0.51) rotate(1)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(47.43 82.49) scale(0.59)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(73.87 83.41) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.75 82.16) scale(0.24)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(171.02 74.39) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(142.99 94.87) scale(0.45) rotate(-15)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.43 113.62) scale(0.23)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(153.46 125.55) scale(0.63)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(64.74 144.16) scale(0.55)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(143.3 140.55) scale(0.8) rotate(14)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(35.58 154.96) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(57.06 186.47) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(166.37 182.71) scale(0.73)"%3E%3C/use%3E%3C/g%3E%3C/svg%3E');
 	}
 
-	div span svg {
+	div span > :global(svg) {
 		width: 1.25em;
 		height: auto;
 	}
