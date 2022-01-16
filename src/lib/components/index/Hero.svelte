a23e0e6f81bb0781e3b9d1847ce5554412dcd2d2
borntofrappe <borntofrappe@gmail.com>
2022-01-16 22:30:37 +0100

    landing page

--- src/lib/components/index/Hero.svelte
+++ src/lib/components/index/Hero.svelte
@@ -1,39 +1,153 @@
 <script>
 	import icons from '$lib/icons';
+
+	const links = [
+		{
+			title: 'codepen',
+			desc: 'On CodePen I flex my muscles as a web developer, starting with little past HTML, CSS and JavaScript. The foundations of the web!',
+			href: 'https://codepen.io/borntofrappe',
+			icon: 'codepen'
+		},
+		{
+			title: 'freecodecamp',
+			desc: 'On freeCodeCamp I started my journey with HTML and CSS. This is where I earned a few certifications and where I continue growing as an endless learner.',
+			href: 'https://www.freecodecamp.org/borntofrappe',
+			icon: 'freecodecamp'
+		},
+		{
+			title: 'github',
+			desc: "On Github I share the code for my projects, and even this very website. Feel free to review, fork, and improve; I'm positive you'll find plenty of room for growth.",
+			href: 'https://github.com/borntofrappe',
+			icon: 'github'
+		},
+		{
+			title: 'twitter',
+			desc: 'On Twitter I try to share something helpful, entertaining, and perhaps even both. There are some who seem to appreciate.',
+			href: 'https://twitter.com/borntofrappe',
+			icon: 'twitter'
+		}
+	];
 </script>
 
-<footer>
-	Built with <a href="https://svelte.dev/">
-		<span class="visually-hidden">Svelte</span>
-		{@html icons.svelte}
-	</a> and much love
-</footer>
+<header>
+	<div>
+		<h1>Hello, World!</h1>
+		<p>I enjoy writing code.</p>
+		<p>Let me try and convince you I might be good at it as well.</p>
+	</div>
+
+	<nav aria-label="borntofrappe links">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="-225 -225 450 450" width="450" height="450">
+			<defs>
+				<path id="path" d="M 0 -32 a 32 32 0 0 1 0 64 32 32 0 0 1 0 -64" />
+				<path id="path-c" d="M 0 39 a 39 39 0 0 1 0 -78 39 39 0 0 1 0 78" />
+				<path id="path-cc" d="M 0 47 a 47 47 0 0 0 0 -94 47 47 0 0 0 0 94" />
+
+				<mask id="mask-text">
+					<rect x="-50" y="-50" width="100" height="100" fill="hsl(0, 0%, 100%)" />
+					<use href="#path" fill="hsl(0, 0%, 0%)" />
+				</mask>
+			</defs>
+
+			<g id="logo">
+				<g
+					fill="none"
+					stroke="currentColor"
+					stroke-width="8"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<circle r="46" />
+					<path
+						d="M -10 -13 q 0 -8 -8 -10"
+						stroke-dasharray="14.01 18.5"
+						stroke-dashoffset="-4.5"
+					/>
+					<path
+						d="M -10 -13 h 15 a 8 8 0 0 1 0 16 h -4 h 4 a 10 10 0 0 1 0 20 h -15 v -36"
+						stroke-dasharray="114.2 146.28"
+						stroke-dashoffset="-9.145"
+					/>
+				</g>
+				<circle r="46" opacity="0" />
+			</g>
+
+			{#each links as { title, desc, href, icon }, i}
+				<g
+					transform="rotate({(360 / links.length) * i}) translate(0 -150) rotate({(360 /
+						links.length) *
+						i *
+						-1})"
+				>
+					<a {href} aria-labelledby="title-{title}" aria-describedby="desc-{title}">
+						<title id="title-{title}">{title}</title>
+						<desc id="desc-{title}">{desc}</desc>
+
+						<g transform="translate(-75 -75)">
+							<svg viewBox="-50 -50 100 100" width="150" height="150">
+								<use href="#path" stroke="currentColor" stroke-width="6" fill="none" />
+								<g transform="rotate({(360 / links.length) * i})">
+									<g mask="url(#mask-text)">
+										<g class="text">
+											<text
+												aria-hidden="true"
+												fill="currentColor"
+												font-weight="bold"
+												letter-spacing="2"
+												text-anchor="middle"
+												font-size="12"
+												font-family="monospace"
+											>
+												<textPath
+													href={(360 / links.length) * i > 90 && (360 / links.length) * i < 270
+														? '#path-cc'
+														: '#path-c'}
+													startOffset="50%"
+												>
+													{title}
+												</textPath>
+											</text>
+										</g>
+									</g>
+								</g>
+								<g transform="translate(-16 -16)">
+									{@html icons[icon].replace(/1em/g, 32)}
+								</g>
+
+								<circle r="50" opacity="0" />
+							</svg>
+						</g></a
+					>
+				</g>
+			{/each}
+		</svg>
+	</nav>
+</header>
 
 <style>
-	:global(.webfonts) footer {
-		font-family: JosefinsansBold, sans-serif;
-		font-weight: 700;
-	}
-
-	footer {
-		--animation-duration: 1.5s;
-		--animation-timing-function: var(--ease-in-out-sine);
-		text-align: center;
-		padding: 2em 1em 1em;
+	header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-wrap: wrap;
+		gap: 0.5em;
+		padding: 2.5em 0;
 		background: hsl(330, 79%, 56%);
 		background: var(--accent-color);
 		background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="-20 -20 240 240"%3E%3Cdefs%3E%3Ccircle id="dot" r="2"%3E%3C/circle%3E%3Cpath id="star" d="M 0 -5 a 5 5 0 0 0 5 5 5 5 0 0 0 -5 5 5 5 0 0 0 -5 -5 5 5 0 0 0 5 -5"%3E%3C/path%3E%3C/defs%3E%3Cg fill="hsl(338, 100%25, 86%25)" stroke="hsl(338, 100%25, 86%25)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"%3E%3Cuse href="%23dot" transform="translate(27.96 4.62) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(116.2 -9.49) scale(0.31) rotate(-4)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.57 15.18) scale(0.57)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(8.87 40.07) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.77 43.19) scale(0.28)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(78.77 44.35) scale(0.67)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(142.49 38.37) scale(0.3)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(189.24 46.54) scale(0.66)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(81.94 64.58) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(97.16 55.31) scale(0.35)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(182.15 65.83) scale(0.65)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(-1.64 70.38) scale(0.51) rotate(1)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(47.43 82.49) scale(0.59)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(73.87 83.41) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.75 82.16) scale(0.24)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(171.02 74.39) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(142.99 94.87) scale(0.45) rotate(-15)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.43 113.62) scale(0.23)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(153.46 125.55) scale(0.63)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(64.74 144.16) scale(0.55)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(143.3 140.55) scale(0.8) rotate(14)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(35.58 154.96) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(57.06 186.47) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(166.37 182.71) scale(0.73)"%3E%3C/use%3E%3C/g%3E%3C/svg%3E');
 		background-size: 100px;
 		position: relative;
+		z-index: 0;
 	}
 
-	footer::before {
+	header::after {
 		content: '';
 		position: absolute;
-		left: 0%;
-		bottom: 100%;
+		top: 100%;
+		left: 50%;
 		width: 100%;
 		height: 20px;
+		transform: translateX(-50%) scaleY(-1);
 		background: inherit;
 		-webkit-mask-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="25 -20 100 40"%3E%3Cg fill="currentColor" stroke="none"%3E%3Cpath d="M 0 -15 c 50 -17.5 50 17.5 100 0 c 50 -17.5 50 17.5 100 0 v 30 c -50 17.5 -50 -17.5 -100 0 c -50 17.5 -50 -17.5 -100 0" /%3E%3C/g%3E%3C/svg%3E');
 		mask-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="25 -20 100 40"%3E%3Cg fill="currentColor" stroke="none"%3E%3Cpath d="M 0 -15 c 50 -17.5 50 17.5 100 0 c 50 -17.5 50 17.5 100 0 v 30 c -50 17.5 -50 -17.5 -100 0 c -50 17.5 -50 -17.5 -100 0" /%3E%3C/g%3E%3C/svg%3E');
@@ -41,96 +155,97 @@
 		mask-size: 100px 40px;
 		-webkit-mask-position: 50% 0;
 		mask-position: 50% 0;
+		z-index: -5;
 	}
 
 	@media (prefers-color-scheme: dark) {
-		:global(html:not([data-preference])) footer {
+		:global(html:not([data-preference])) header {
 			background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="-20 -20 240 240"%3E%3Cdefs%3E%3Ccircle id="dot" r="2"%3E%3C/circle%3E%3Cpath id="star" d="M 0 -5 a 5 5 0 0 0 5 5 5 5 0 0 0 -5 5 5 5 0 0 0 -5 -5 5 5 0 0 0 5 -5"%3E%3C/path%3E%3C/defs%3E%3Cg fill="hsl(185, 91%25, 17%25)" stroke="hsl(185, 91%25, 17%25)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"%3E%3Cuse href="%23dot" transform="translate(27.96 4.62) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(116.2 -9.49) scale(0.31) rotate(-4)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.57 15.18) scale(0.57)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(8.87 40.07) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.77 43.19) scale(0.28)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(78.77 44.35) scale(0.67)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(142.49 38.37) scale(0.3)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(189.24 46.54) scale(0.66)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(81.94 64.58) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(97.16 55.31) scale(0.35)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(182.15 65.83) scale(0.65)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(-1.64 70.38) scale(0.51) rotate(1)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(47.43 82.49) scale(0.59)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(73.87 83.41) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.75 82.16) scale(0.24)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(171.02 74.39) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(142.99 94.87) scale(0.45) rotate(-15)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.43 113.62) scale(0.23)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(153.46 125.55) scale(0.63)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(64.74 144.16) scale(0.55)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(143.3 140.55) scale(0.8) rotate(14)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(35.58 154.96) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(57.06 186.47) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(166.37 182.71) scale(0.73)"%3E%3C/use%3E%3C/g%3E%3C/svg%3E');
 		}
 	}
 
-	:global(html[data-preference='dark']) footer {
+	:global(html[data-preference='dark']) header {
 		background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="-20 -20 240 240"%3E%3Cdefs%3E%3Ccircle id="dot" r="2"%3E%3C/circle%3E%3Cpath id="star" d="M 0 -5 a 5 5 0 0 0 5 5 5 5 0 0 0 -5 5 5 5 0 0 0 -5 -5 5 5 0 0 0 5 -5"%3E%3C/path%3E%3C/defs%3E%3Cg fill="hsl(185, 91%25, 17%25)" stroke="hsl(185, 91%25, 17%25)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"%3E%3Cuse href="%23dot" transform="translate(27.96 4.62) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(116.2 -9.49) scale(0.31) rotate(-4)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.57 15.18) scale(0.57)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(8.87 40.07) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.77 43.19) scale(0.28)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(78.77 44.35) scale(0.67)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(142.49 38.37) scale(0.3)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(189.24 46.54) scale(0.66)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(81.94 64.58) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(97.16 55.31) scale(0.35)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(182.15 65.83) scale(0.65)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(-1.64 70.38) scale(0.51) rotate(1)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(47.43 82.49) scale(0.59)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(73.87 83.41) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(135.75 82.16) scale(0.24)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(171.02 74.39) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(142.99 94.87) scale(0.45) rotate(-15)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(58.43 113.62) scale(0.23)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(153.46 125.55) scale(0.63)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(64.74 144.16) scale(0.55)"%3E%3C/use%3E%3Cuse href="%23star" transform="translate(143.3 140.55) scale(0.8) rotate(14)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(35.58 154.96) scale(0.2)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(57.06 186.47) scale(0.8)"%3E%3C/use%3E%3Cuse href="%23dot" transform="translate(166.37 182.71) scale(0.73)"%3E%3C/use%3E%3C/g%3E%3C/svg%3E');
 	}
 
-	a {
-		padding: 0.25em 0;
-		text-decoration: none;
-		transition: none;
-		display: inline-flex;
-		justify-content: center;
-		align-items: center;
+	header > * {
+		width: 90vw;
+		width: var(--width);
+		max-width: 30rem;
 	}
 
-	a,
-	a:hover,
-	a:focus {
-		color: inherit;
+	header nav {
+		order: -1;
 	}
 
-	a::before,
-	a::after {
-		content: '';
-		position: absolute;
-		top: 0%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		margin-top: -8px;
+	nav {
+		--transition-duration: 0.5s;
+		padding: 0.5em;
 	}
 
-	a::before {
-		width: 3.5em;
-		height: 3.5em;
-		color: hsl(330, 79%, 56%);
-		color: var(--accent-color);
-		background: hsl(341, 100%, 95%);
-		background: var(--background-color);
-		border: 0.4em solid currentColor;
-		border-radius: 50%;
+	nav > svg {
+		width: 100%;
+		height: auto;
 	}
 
-	a::after {
-		transform: translate(-50%, -50%) scale(1);
-		width: 1em;
-		height: 1em;
-		background: hsl(330, 79%, 56%);
-		background: var(--accent-color);
-		border-radius: 50%;
+	nav > svg text {
+		font-family: Inconsolata, Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace;
 	}
 
-	@supports ((-webkit-mask-image: url()) or (mask-image: url())) {
-		a::after {
-			width: 1.4em;
-			height: 1.4em;
-			border-radius: initial;
-			-webkit-mask-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100"%3E%3Cg fill="currentColor" stroke="currentColor" stroke-width="10" stroke-linejoin="round" stroke-linecap="round"%3E%3Cpath d="M 0 -11.25 a 22.5 22.5 0 0 1 45 0 q 0 25 -45 50 q -45 -25 -45 -50 a 22.5 22.5 0 0 1 45 0" /%3E%3C/g%3E%3C/svg%3E');
-			mask-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100"%3E%3Cg fill="currentColor" stroke="currentColor" stroke-width="10" stroke-linejoin="round" stroke-linecap="round"%3E%3Cpath d="M 0 -11.25 a 22.5 22.5 0 0 1 45 0 q 0 25 -45 50 q -45 -25 -45 -50 a 22.5 22.5 0 0 1 45 0" /%3E%3C/g%3E%3C/svg%3E');
-		}
+	nav > svg #logo {
+		transition: transform 0.5s cubic-bezier(0.68, -0.5, 0.265, 1.55),
+			color 0.5s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+		transition: transform var(--transition-duration) var(--ease-in-out-back),
+			color var(--transition-duration) var(--ease-in-out-sine);
 	}
 
-	a::after {
-		animation: heart-beat 1.5s cubic-bezier(0.445, 0.05, 0.55, 0.95) infinite;
-		animation: heart-beat var(--animation-duration) var(--animation-timing-function) infinite;
-		animation-play-state: paused;
+	nav > svg #logo:hover {
+		color: hsl(210, 24%, 16%);
+		color: var(--heading-color);
+		transform: scale(1.4) rotate(-15deg);
 	}
 
-	@media (prefers-reduced-motion: no-preference) {
-		a:hover::after,
-		a:focus::after {
-			animation-play-state: running;
-		}
+	nav > svg a {
+		transition: transform 0.5s cubic-bezier(0.68, -0.5, 0.265, 1.55),
+			color 0.5s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+		transition: transform var(--transition-duration) var(--ease-in-out-back),
+			color var(--transition-duration) var(--ease-in-out-sine);
+		transform: scale(0.8);
 	}
 
-	@keyframes heart-beat {
-		15%,
-		45% {
-			transform: translate(-50%, -50%) scale(0.7);
-		}
+	nav > svg a .text {
+		transform: scale(0);
+		transition: transform 0.5s cubic-bezier(0.215, 0.61, 0.355, 1);
+		transition: transform var(--transition-duration) var(--ease-out-cubic);
+	}
 
-		0%,
-		30%,
-		60% {
-			transform: translate(-50%, -50%) scale(1);
-		}
+	nav > svg a:hover,
+	nav > svg a:focus {
+		color: hsl(210, 24%, 16%);
+		color: var(--heading-color);
+	}
+
+	nav > svg a:hover,
+	nav > svg a:focus,
+	nav > svg a:hover .text,
+	nav > svg a:focus .text {
+		transform: scale(1);
+	}
+
+	nav > svg a:focus {
+		outline: none;
+	}
+
+	div > * + * {
+		margin-top: 0.5em;
+	}
+
+	:global(.webfonts) p {
+		font-family: JosefinsansBold;
+		font-weight: 700;
+	}
+
+	p {
+		line-height: 1.5;
 	}
 </style>
