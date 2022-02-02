ee5f5cbb4f62b84a81b0b0f1fe4fd343bfda4cca
borntofrappe <borntofrappe@gmail.com>
2022-02-02 10:32:43 +0100

    add an error page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Meta from '$lib/components/routes/Meta.svelte';
+	import Header from '$lib/components/routes/Header.svelte';
 </script>
 
 <Meta
@@ -7,12 +8,26 @@
 	description="Personal website of one Gabriele Corti, online @borntofrappe."
 />
 
-<h1>Hello, world</h1>
+<Header>
+	<h1>Hello world!</h1>
+</Header>
 
-<p>
-	This is my website, and you are welcome<b>d</b>. It is built with <strong>SvelteKit</strong>, in
-	its latest
-	<code>1.0.0-next.253</code> version. It is deployed through <strong>Netlify</strong>, and
-	available on
-	<a href="https://borntofrappe.netlify.app">borntofrappe.netlify.app</a>.
-</p>
+<main>
+	<p>
+		This is my website, and you are welcome<b>d</b>. It is built with <strong>SvelteKit</strong>, in
+		its latest
+		<code>1.0.0-next.253</code> version. It is deployed through <strong>Netlify</strong>, and
+		available on
+		<a href="https://borntofrappe.netlify.app">borntofrappe.netlify.app</a>.
+	</p>
+</main>
+
+<style>
+	main {
+		max-width: 42rem;
+		max-width: var(--max-width);
+		width: 90vw;
+		width: var(--width);
+		margin: 2em auto 6em;
+	}
+</style>
