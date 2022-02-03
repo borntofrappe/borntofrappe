2f8ca42ddca52c7295721349112b9a2f6b3b4155
borntofrappe <borntofrappe@gmail.com>
2022-02-03 23:11:40 +0100

    fix base url
    use environmental variable from a local .env file or Netlify's read-only values. page.url.origin would work only on the client

--- src/lib/components/routes/Meta.svelte
+++ src/lib/components/routes/Meta.svelte
@@ -3,21 +3,23 @@
 
 	export let title = 'borntofrappe';
 	export let description = '';
+
+	const pageUrlOrigin = import.meta.env.VITE_URL || process.env.URL;
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
 
-	<link rel="canonical" href="{$page.url.origin}{$page.url.pathname}" />
+	<link rel="canonical" href="{pageUrlOrigin}{$page.url.pathname}" />
 
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:url" content="{$page.url.origin}{$page.url.pathname}" />
+	<meta property="og:url" content="{pageUrlOrigin}{$page.url.pathname}" />
 	<meta property="og:locale" content="en_US" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="borntofrappe" />
-	<meta property="og:image" content="{$page.url.origin}/images/borntofrappe.png" />
+	<meta property="og:image" content="{pageUrlOrigin}/images/borntofrappe.png" />
 	<meta property="og:image:alt" content="On pink days, on cyan nights, I code @borntofrappe" />
 
 	<meta property="twitter:card" content="summary_large_image" />
@@ -25,6 +27,9 @@
 	<meta property="twitter:description" content={description} />
 	<meta property="twitter:site" content="@borntofrappe" />
 	<meta property="twitter:creator" content="@borntofrappe" />
-	<meta property="twitter:image" content="{$page.url.origin}/images/borntofrappe.png" />
+	<meta
+		property="twitter:image"
+		content="{import.meta.env.VITE_URL || process.env.URL}/images/borntofrappe.png"
+	/>
 	<meta property="twitter:image:alt" content="On pink days, on cyan nights, I code @borntofrappe" />
 </svelte:head>
