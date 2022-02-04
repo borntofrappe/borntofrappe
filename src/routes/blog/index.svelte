7fecb0e3ab0445910a6028383a48714f18376b3a
borntofrappe <borntofrappe@gmail.com>
2022-02-04 22:24:27 +0100

    remove test articles
    remove .md and .svx documents used to test the blog routes

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -24,28 +24,22 @@
 <Header>
 	<h1>Blog</h1>
 	<p>
-		Monday to Friday I aspire to write about code. Over the weekend I sparingly splurge about games
+		Monday to Friday I aspire to write about code. Over the weekend I'd like to jabber about games
 		and anything which piques my interest. The hope is you'll find something of value in at least a
 		day a week.
 	</p>
 </Header>
 
 <main id="content">
-	{#if entries.length === 0}
-		<h2>Well, well, well</h2>
-		<p>It seems there are no articles at the time of writing. Isn't it funny.</p>
-		<p>At least we can all cherish how CSS handles layout with an unexpected set of elements.</p>
-	{:else}
-		{#each entries as { slug, title, datetime, date, brief }}
-			<article>
-				<h2>
-					<a sveltekit:prefetch href="/blog/{slug}">{title}</a>
-				</h2>
-				<time {datetime}>{formatDate(date)}</time>
-				<p>{brief}</p>
-			</article>
-		{/each}
-	{/if}
+	{#each entries as { slug, title, datetime, date, brief }}
+		<article>
+			<h2>
+				<a sveltekit:prefetch href="/blog/{slug}">{title}</a>
+			</h2>
+			<time {datetime}>{formatDate(date)}</time>
+			<p>{brief}</p>
+		</article>
+	{/each}
 </main>
 
 <style>
