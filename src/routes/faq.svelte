33c59a90f5175329002b406de9257ffb61b5872c
borntofrappe <borntofrappe@gmail.com>
2022-03-20 22:07:55 +0100

    feat: faq page

---
+++ src/routes/faq.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+	<title>FAQ | borntofrappe</title>
+	<meta name="description" content="What is the deal with this website." />
+</svelte:head>
+
+<h1>Questions and answers</h1>
+
+<dl>
+	<dt>What is this?</dt>
+	<dd>
+		This is a silly page on what is supposed to by my personal website. Signed, <em
+			>Gabriele Corti</em
+		>.
+	</dd>
+
+	<dt>How did you build it?</dt>
+	<dd>
+		<ul>
+			<li><a href="https://kit.svelte.dev">kit.svelte.dev</a></li>
+			<li><a href="https://www.netlify.com/">Netlify</a></li>
+		</ul>
+	</dd>
+
+	<dt>Where to go from here?</dt>
+	<dd>There's no place like <a href="/">home</a>.</dd>
+</dl>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,3 @@
 </svelte:head>
 
 <h1>borntofrappe</h1>
-
-<h2>Thankses to:</h2>
-<ul>
-	<li><a href="https://kit.svelte.dev">kit.svelte.dev</a></li>
-	<li><a href="https://www.netlify.com/">Netlify</a></li>
-</ul>
