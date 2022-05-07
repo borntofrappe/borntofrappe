65ed559de72d0fb2e9d07a1d2ae11c7f88e9a33d
borntofrappe <borntofrappe@gmail.com>
2022-05-07 21:31:43 +0200

    strucure smil articles

--- src/routes/smil/index.svelte
+++ src/routes/smil/index.svelte
@@ -8,7 +8,7 @@
 </svelte:head>
 
 <div>
-	<article>
+	<article style="--background-color: #f7717b; --outline-color: #b5d2c5;">
 		<Ladybug />
 	</article>
 </div>
@@ -17,17 +17,24 @@
 	div {
 		color: hsl(44, 30%, 17%);
 		background: hsl(41, 100%, 97%);
+		--outline-color: hsl(6, 100%, 74%);
+		--background-color: hsl(41, 100%, 97%);
 	}
 
 	article {
 		min-height: 100vh;
 		display: grid;
 		place-items: center;
+		background: inherit;
+		background: var(--background-color, inherit);
 	}
 
 	article > :global(svg) {
-		display: block;
-		max-width: 42rem;
-		height: auto;
+		max-width: 32rem;
+		border-radius: 0.25rem;
+		box-shadow: 0 -1rem 1rem hsla(0, 0%, 0%, 0.1), 0 0 0 0.15rem currentColor,
+			0 0 0 0.9rem hsl(6, 100%, 74%), 0 0 0 1rem currentColor;
+		box-shadow: 0 -1rem 1rem hsla(0, 0%, 0%, 0.1), 0 0 0 0.15rem currentColor,
+			0 0 0 0.9rem var(--outline-color), 0 0 0 1rem currentColor;
 	}
 </style>
