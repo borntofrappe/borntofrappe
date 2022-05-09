72ca9bbf5b095f3d0d5522a6473afb9346c3cec9
borntofrappe <borntofrappe@gmail.com>
2022-05-09 15:47:29 +0200

    revisit smil demo _Butterflies
    fix how the script elaborates the d attribute for the motion path; improve how the game starts and ends; remove nested markup and move elements repeatedly with adjacents animateTransform elements

--- src/routes/smil/_Butterflies.svelte
+++ src/routes/smil/_Butterflies.svelte
@@ -5,17 +5,23 @@
 
 	const getX = () => Math.floor(Math.random() * (width - size));
 	const getY = () => Math.floor(Math.random() * (height - size));
-	const getPath = ({ x, y, maxLength = 200 }) => {
+
+	const getPath = ({ translateX, translateY, thresholdLength = 200 }) => {
 		let d = `M 0 0`;
 		let length = 0;
 
-		while (length < maxLength) {
-			const nx = getX() - x;
-			const ny = getY() - y;
-			const dinstance = ((nx - x) ** 2 + (ny - y) ** 2) ** 0.5;
+		while (length < thresholdLength) {
+			const [px, py] = d
+				.match(/(-?\d+) (-?\d+)/)
+				.slice(1)
+				.map((c) => parseInt(c, 10));
 
-			d += ` ${nx} ${ny}`;
-			length += dinstance;
+			const x = getX() - translateX;
+			const y = getY() - translateY;
+			const distance = ((x - px) ** 2 + (y - py) ** 2) ** 0.5;
+
+			d += ` ${x} ${y}`;
+			length += distance;
 		}
 		return `${d} 0 0`;
 	};
@@ -26,123 +32,46 @@
 	const coords = [{ x, y }];
 
 	const ribbon = {
-		x: getX(),
-		y: getY()
+		x,
+		y
 	};
 
-	const butterflies = Array(2)
+	const butterflies = Array(3)
 		.fill()
 		.map(() => {
-			let nx = getX();
-			let ny = getY();
+			let x = getX();
+			let y = getY();
 			while (true) {
 				let overlaps = false;
 				for (const { x: cx, y: cy } of coords) {
-					if (nx + size > cx && nx < cx + size && ny + size > cy && ny < cy + size) {
+					if (x + size > cx && x < cx + size && y + size > cy && y < cy + size) {
 						overlaps = true;
 						break;
 					}
 				}
 				if (overlaps) {
-					nx = getX();
-					ny = getY();
+					x = getX();
+					y = getY();
 				} else {
 					coords.push({
-						x: nx,
-						y: ny
+						x,
+						y
 					});
 					break;
 				}
 			}
 
 			return {
-				x: nx,
-				y: ny,
-				d: getPath({ x: nx, y: ny })
+				x,
+				y,
+				path: getPath({ translateX: x, translateY: y })
 			};
 		});
-
-	const message = 'You dit it!';
-	const messageMarkup = Array(butterflies.length)
-		.fill()
-		.reduce(
-			(acc, _, i) => {
-				return `
-      <g transform="translate(0 -50)">
-        <animateTransform
-          attributeName="transform"
-          type="translate"
-          to="0 0"
-          dur="0.15s"
-          begin="butterfly${i}.begin"
-          fill="freeze"
-          calcMode="discrete"
-        />
-        ${acc}
-      </g>
-      `;
-			},
-			`
-      <rect width="80" height="50" opacity="0" />
-      <g transform="translate(40 25)">
-        <g
-          style:cursor="pointer"
-          font-size="7"
-          font-weight="bold"
-          font-family="sans-serif"
-          text-anchor="middle"
-          dominant-baseline="middle"
-          fill="#ffffff"
-          stroke="currentColor"
-          stroke-width="0.4"
-        >
-          <g>
-            <animateTransform
-              attributeName="transform"
-              type="translate"
-              to="-80 0"
-              dur="0.25s"
-              id="butterflyMessage"
-              begin="butterfly0.begin"
-              end="click"
-              repeatCount="indefinite"
-              calcMode="discrete"
-            />
-            <text y="-1">
-              ${message
-								.split('')
-								.map(
-									(l, i) => `
-                <tspan dx="0.5" dy=${i % 2 === 0 ? 0.5 : -0.5}>
-                  ${l}
-                </tspan>
-                `
-								)
-								.join('')}
-            </text>
-            <text x="80" y="-0.5">
-              ${message
-								.split('')
-								.map(
-									(l, i) => `
-                <tspan dx="0.5" dy=${i % 2 === 0 ? 0.5 : -0.5}>
-                  ${l}
-                </tspan>
-                `
-								)
-								.join('')
-								.trim()}
-            </text>
-          </g>
-        </g>
-      </g>
-    `
-		);
 </script>
 
 <svg viewBox="0 0 80 50">
 	<defs>
-		<pattern id="wall-of-bricks" viewBox="0 0 8 5" width="0.12" height="0.12">
+		<pattern id="butterflies-pattern-bricks" viewBox="0 0 8 5" width="0.12" height="0.12">
 			<rect fill="#bd4900" width="8" height="5" />
 			<g fill="none" stroke="currentColor" stroke-width="0.5" stroke-linecap="square">
 				<path d="M 7.75 0 v 2.375 h -7.75 m 3.875 0 v 2.375 m -3.875 0h 7.75" />
@@ -150,11 +79,10 @@
 		</pattern>
 	</defs>
 
-	<rect fill="url(#wall-of-bricks)" width="80" height="50" />
-
-	<g stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
-		<g transform="translate({ribbon.x} {ribbon.y})">
-			<g transform="translate(8 6) scale(0.8)">
+	<rect fill="url(#butterflies-pattern-bricks)" width="80" height="50" />
+	<g transform="translate({ribbon.x} {ribbon.y})">
+		<g transform="translate(8 6) scale(0.7)">
+			<g stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
 				<g fill="#2969bd">
 					<path d="M 0 1 c 0 2 1 8 2 8 1 -2 3 -1 3 -2z" />
 					<path transform="scale(-1 1)" d="M 0 1 c 0 2 1 8 2 8 1 -2 3 -1 3 -2z" />
@@ -168,39 +96,50 @@
 				<rect fill="#2969bd" x="-1.4" y="-0.6" width="2.8" height="3.2" rx="1" />
 			</g>
 		</g>
+	</g>
 
-		{#each butterflies as { x, y, d: path }, i}
-			<g style:cursor="pointer">
-				<animate
-					attributeName="opacity"
-					dur="0.1s"
-					id="butterfly{i}"
-					begin="click"
-					restart="never"
-				/>
-				<g transform="translate({x} {y})">
-					<g transform="translate(8 6) scale(0.8)">
-						<g>
-							<animateMotion
-								{path}
-								repeatCount="indefinite"
-								begin="butterfly{i}.begin"
-								end="butterflyMessage.end"
-								fill="freeze"
-								dur="10s"
-							/>
+	{#each butterflies as { x, y, path }, i}
+		<g transform="translate({x} {y})">
+			<g transform="translate(8 6) scale(0.7)">
+				<g style:cursor="pointer">
+					<animateTransform
+						attributeName="transform"
+						type="scale"
+						values="1; 1.02"
+						dur="0.2s"
+						fill="freeze"
+						begin="click"
+						id="butterfliesGameButterfly{i}"
+						restart="never"
+					/>
+					<g>
+						<animateMotion
+							{path}
+							dur="12s"
+							repeatCount="indefinite"
+							fill="freeze"
+							begin="butterfliesGameButterfly{i}.begin"
+							end="butterfliesGameEnd.end"
+						/>
+
+						<g
+							stroke="currentColor"
+							stroke-width="0.5"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
 							<g fill="#2969bd">
 								<g>
 									<animateTransform
 										attributeName="transform"
 										type="scale"
-										values="1 1; 0.6 1"
-										begin="butterfly{i}.begin"
-										end="butterflyMessage.end"
-										fill="freeze"
-										dur="0.22s"
+										values="0.6 1; 1 1"
+										dur="0.2s"
 										calcMode="discrete"
 										repeatCount="indefinite"
+										fill="freeze"
+										begin="butterfliesGameButterfly{i}.begin"
+										end="butterfliesGameEnd.end"
 									/>
 									<path
 										fill="currentColor"
@@ -208,20 +147,8 @@
 										d="M 0 0 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1"
 									/>
 									<path d="M 0 3 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1" />
-								</g>
-								<g transform="scale(-1 1)">
-									<g>
-										<animateTransform
-											attributeName="transform"
-											type="scale"
-											values="1 1; 0.6 1"
-											begin="butterfly{i}.begin"
-											end="butterflyMessage.end"
-											fill="freeze"
-											dur="0.22s"
-											calcMode="discrete"
-											repeatCount="indefinite"
-										/>
+
+									<g transform="scale(-1 1)">
 										<path
 											fill="currentColor"
 											transform="translate(0.5 3) scale(1.05 1.08)"
@@ -231,7 +158,9 @@
 									</g>
 								</g>
 							</g>
+
 							<path fill="#2969bd" d="M 0 0 c 2.5 3 3.5 7 0 9 -3.5 -2 -2.5 -6 0 -9" />
+
 							<g fill="none">
 								<path
 									transform="translate(0.5 -1.5) rotate(-40)"
@@ -254,8 +183,8 @@
 					</g>
 				</g>
 			</g>
-		{/each}
-	</g>
+		</g>
+	{/each}
 
 	<g transform="translate(40 25)">
 		<g
@@ -265,16 +194,16 @@
 			font-family="sans-serif"
 			text-anchor="middle"
 			dominant-baseline="middle"
-			fill="#ffffff"
+			fill="#f7f7f7"
 			stroke="currentColor"
-			stroke-width="0.4"
+			stroke-width="0.3"
 		>
 			<animate
 				attributeName="opacity"
-				values="1; 0"
+				to="0"
 				fill="freeze"
-				dur="0.15s"
-				begin="butterflyGame.begin"
+				dur="0.1s"
+				begin="butterfliesGameStart.begin"
 			/>
 			<text> Butterflies? </text>
 		</g>
@@ -282,14 +211,68 @@
 
 	<rect style:cursor="pointer" width="80" height="50" opacity="0">
 		<animate
-			id="butterflyGame"
+			id="butterfliesGameStart"
 			attributeName="display"
 			to="none"
 			fill="freeze"
-			dur="0.15s"
 			begin="click"
+			dur="0.1s"
+			restart="never"
 		/>
 	</rect>
 
-	{@html messageMarkup}
+	<g transform="translate({80 * butterflies.length * -1} 0)">
+		{#each butterflies as _, i}
+			<animateTransform
+				attributeName="transform"
+				type="translate"
+				by="80 0"
+				fill="freeze"
+				dur="0.1s"
+				calcMode="discrete"
+				begin="butterfliesGameButterfly{i}.begin"
+			/>
+		{/each}
+		<g transform="translate(40 25)">
+			<g
+				style="pointer-events: none;"
+				font-size="6"
+				font-weight="bold"
+				font-family="sans-serif"
+				text-anchor="middle"
+				dominant-baseline="middle"
+				fill="#f7f7f7"
+				stroke="currentColor"
+				stroke-width="0.3"
+			>
+				<animateTransform
+					attributeName="transform"
+					type="translate"
+					to="0 1"
+					dur="0.25s"
+					repeatCount="indefinite"
+					begin="butterfliesGameButterfly0.begin"
+					end="butterfliesGameEnd.end"
+					fill="freeze"
+					calcMode="discrete"
+				/>
+				<text>
+					{#each 'You found them!'.split('') as l, i}
+						<tspan dx="0.5" dy={i % 2 === 0 ? 0.5 : -0.5}>{l}</tspan>
+					{/each}
+				</text>
+			</g>
+		</g>
+		<rect style:cursor="pointer" width="80" height="50" opacity="0">
+			<animate
+				attributeName="display"
+				to="none"
+				fill="freeze"
+				begin="click"
+				restart="never"
+				dur="0.1s"
+				id="butterfliesGameEnd"
+			/>
+		</rect>
+	</g>
 </svg>
