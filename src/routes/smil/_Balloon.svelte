2a66416e215792006cb10c60065d9c33cf2a9fb1
borntofrappe <borntofrappe@gmail.com>
2022-05-09 15:43:07 +0200

    revisit smil demo _Ladybug.svelte
    refactor functions to create the mishapen outline of the ladybug; add missing line in the center of the insect; improve how the game starts and ends

--- src/routes/smil/_Balloon.svelte
+++ src/routes/smil/_Balloon.svelte
@@ -1,36 +1,40 @@
 <script>
-	const dBalloon = (r, o = 10, p = 30) => {
-		const points = Array(p % 2 === 0 ? p + 1 : p)
+	const dBalloon = ({ radius, protuberance = 10, points }) => {
+		let p = points || radius * 2;
+		if (p % 2 !== 0) p++;
+
+		const pts = Array(p)
 			.fill()
 			.map((_, i, { length }) => {
+				const distance = i % 2 !== 0 ? radius + Math.random() / 2 : radius - Math.random() / 2;
 				const angle = (((360 / length) * i) / 180) * Math.PI;
-				let ry = angle > 0 && angle < Math.PI ? r + o : r;
-				const x = Math.cos(angle) * r;
-				const y = Math.sin(angle) * ry;
+				const x = Math.cos(angle) * distance;
+				const y =
+					angle > 0 && angle < Math.PI
+						? Math.sin(angle) * (distance + protuberance)
+						: Math.sin(angle) * distance;
 				return {
 					x,
 					y
 				};
 			});
-		return (
-			points.reduce((acc, { x, y }, i) => `${acc} ${x} ${y}`, 'M') +
-			` L ${points[0].x} ${points[0].y}`
+
+		return [...pts, { x: pts[0].x, y: pts[0].y }].reduce(
+			(acc, { x, y }, i) => (i % 2 === 0 ? `${acc} ${x} ${y}` : `${acc} Q ${x} ${y}`),
+			'M'
 		);
 	};
 
-	const d = dBalloon(7, 1.5, 20);
+	const d = dBalloon({ radius: 7, protuberance: 2, points: 16 });
 
 	const angle = ((Math.floor(Math.random() * 180) + 180) / 180) * Math.PI;
 	const dx = Math.cos(angle) * 80;
 	const dy = Math.sin(angle) * 50;
-
-	const winMessage = 'You did it!';
-	const lossMessage = 'Too bad!';
 </script>
 
 <svg viewBox="0 0 80 50">
 	<defs>
-		<g id="cloud">
+		<g id="balloon-cloud">
 			<path
 				fill="#f7f7f7"
 				d="M -7 0 a 2 2 0 0 1 4 -2 3 3 0 0 1 6 0 2 2 0 0 1 4 2 2 2 0 0 1 0 4 1.85 1.85 0 0 1 -3.7 0 3.4 3.4 0 0 1 -6.6 0 1.85 1.85 0 0 1 -3.7 0 2 2 0 0 1 0 -4"
@@ -47,7 +51,7 @@
 		</g>
 
 		<pattern
-			id="wrapping-paper"
+			id="balloon-pattern-present"
 			viewBox="0 0 10 10"
 			width="4"
 			height="4"
@@ -60,25 +64,25 @@
 			/>
 		</pattern>
 
-		<ellipse
-			id="balloon-reflection"
-			stroke="none"
-			transform="translate(2.5 -2.75) rotate(-42)"
-			rx="2"
-			ry="2.5"
-		/>
+		<ellipse id="balloon-balloon-light" stroke="none" rx="2" ry="2.5" />
 
-		<g id="balloon">
-			<path d="M -1.25 10 h 2.5 q 0 -1.5 -1.25 -1.5 t -1.25 1.5" />
-
-			<path {d} />
-			<use fill="#f7d794" href="#balloon-reflection" />
-
-			<path fill="none" d="M 0 10 q -0.75 0.75 0 1.5 t 0 1.5" />
+		<g id="balloon-balloon">
+			<g transform="translate(0 -3)">
+				<path d="M -1.25 0 h 2.5 q 0 -1.5 -1.25 -1.5 t -1.25 1.5" />
+				<g transform="translate(0 -10)">
+					<path {d} />
+					<use
+						transform="translate(2 -2.5) rotate(-40)"
+						fill="#f7d794"
+						href="#balloon-balloon-light"
+					/>
+				</g>
+				<path fill="none" d="M 0 0 q -0.75 0.75 0 1.5 t 0 1.5" />
+			</g>
 		</g>
 
 		<pattern
-			id="balloon-shreds"
+			id="balloon-balloon-shreds"
 			viewBox="0 0 5 5"
 			width="1"
 			height="1"
@@ -90,138 +94,61 @@
 			</g>
 		</pattern>
 
-		<mask id="mask-balloon-shreds">
-			<use href="#balloon" fill="white" stroke="white" />
-			<g fill="url(#balloon-shreds)">
-				<use href="#balloon" stroke="white" />
-				<use href="#balloon-reflection" />
+		<mask id="balloon-mask-balloon-shreds">
+			<use href="#balloon-balloon" fill="white" stroke="white" />
+			<g fill="url(#balloon-balloon-shreds)">
+				<use href="#balloon-balloon" stroke="white" />
+				<use transform="translate(2 -15.5) rotate(-40)" href="#balloon-balloon-light" />
 			</g>
 		</mask>
 	</defs>
+
 	<rect fill="#10c2ce" width="80" height="50" />
 
 	<g stroke-width="0.5">
-		<use transform="translate(15 15) scale(0.8)" href="#cloud" />
-		<use transform="translate(65 35) scale(0.8)" href="#cloud" />
+		<use transform="translate(15 15) scale(0.8)" href="#balloon-cloud" />
+		<use transform="translate(65 35) scale(0.8)" href="#balloon-cloud" />
 	</g>
 
-	<g transform="translate(40 25)">
-		<g style:cursor="pointer" display="none">
-			<animate
-				id="balloonMessage"
-				attributeName="display"
-				to="initial"
+	<g transform="translate(40 28)">
+		<g>
+			<animateTransform
+				attributeName="transform"
+				type="translate"
+				to="{dx} {dy}"
+				dur="5s"
 				fill="freeze"
-				dur="0.1s"
-				begin="balloonFloat.end + 1s"
+				begin="balloonGameStart.begin"
+				end="balloonGamePopBalloon.begin"
+				id="balloonGameOver"
 			/>
-			<g
-				font-size="7"
-				font-weight="bold"
-				font-family="sans-serif"
-				text-anchor="middle"
-				dominant-baseline="middle"
-				fill="#ffffff"
-				stroke="currentColor"
-				stroke-width="0.4"
-			>
-				<g>
+			<g fill="#f70000" stroke="currentColor" stroke-width="0.5">
+				<g style:cursor="pointer">
 					<animateTransform
 						attributeName="transform"
 						type="translate"
-						to="-80 0"
-						dur="0.25s"
-						begin="balloonMessage.begin"
-						end="click"
-						repeatCount="indefinite"
+						values="160 0; 320 0"
+						dur="0.1s"
 						calcMode="discrete"
+						fill="freeze"
+						begin="click"
+						restart="never"
+						id="balloonGamePopBalloon"
 					/>
 					<g>
 						<animateTransform
 							attributeName="transform"
 							type="translate"
-							to="0 -50"
-							dur="0.1s"
-							begin="balloonPop.begin"
-							fill="freeze"
-							calcMode="discrete"
-						/>
-						<text y="-1">
-							{#each lossMessage.split('') as l, i}
-								<tspan dx="0.5" dy={i % 2 === 0 ? 0.5 : -0.5}>
-									{l}
-								</tspan>
-							{/each}
-						</text>
-						<text x="80" y="-0.5">
-							{#each lossMessage.split('') as l, i}
-								<tspan dx="0.5" dy={i % 2 === 0 ? 0.5 : -0.5}>
-									{l}
-								</tspan>
-							{/each}
-						</text>
-
-						<g transform="translate(0 50)">
-							<text y="-1">
-								{#each winMessage.split('') as l, i}
-									<tspan dx="0.5" dy={i % 2 === 0 ? 0.5 : -0.5}>
-										{l}
-									</tspan>
-								{/each}
-							</text>
-							<text x="80" y="-0.5">
-								{#each winMessage.split('') as l, i}
-									<tspan dx="0.5" dy={i % 2 === 0 ? 0.5 : -0.5}>
-										{l}
-									</tspan>
-								{/each}
-							</text>
-						</g>
-					</g>
-				</g>
-			</g>
-		</g>
-	</g>
-
-	<g transform="translate(40 18)">
-		<g>
-			<animateTransform
-				id="balloonFloat"
-				attributeName="transform"
-				type="translate"
-				to="{dx} {dy}"
-				dur="4.5s"
-				begin="balloonGame.begin"
-				end="balloonPop.begin"
-				fill="freeze"
-			/>
-			<g stroke="currentColor" stroke-width="0.6" stroke-linecap="round" stroke-linejoin="round">
-				<g>
-					<animateTransform
-						attributeName="transform"
-						type="translate"
-						to="0 0.8"
-						repeatCount="indefinite"
-						dur="0.7s"
-						calcMode="discrete"
-						begin="balloonGame.begin"
-						end="balloonPop.begin"
-					/>
-					<g style:cursor="pointer">
-						<animateTransform
-							id="balloonPop"
-							attributeName="transform"
-							type="translate"
-							values="-160 0; -320 0;"
-							dur="0.15s"
-							begin="click"
+							to="0 1"
+							dur="0.35s"
 							calcMode="discrete"
 							fill="freeze"
+							begin="balloonGameStart.begin"
+							repeatCount="indefinite"
+							id="balloonGamePopBalloon.end"
 						/>
-						<g fill="#f70000">
-							<use href="#balloon" />
-							<use transform="translate(160 0)" href="#balloon" mask="url(#mask-balloon-shreds)" />
-						</g>
+						<use href="#balloon-balloon" />
+						<use x="-160" href="#balloon-balloon" mask="url(#balloon-mask-balloon-shreds)" />
 					</g>
 				</g>
 			</g>
@@ -229,22 +156,20 @@
 				<animateTransform
 					attributeName="transform"
 					type="translate"
-					values="0 0; 0 50;"
-					dur="1s"
-					begin="balloonPop.begin"
+					to="0 100"
+					dur="1.5s"
 					fill="freeze"
+					begin="balloonGamePopBalloon.begin"
 					calcMode="spline"
 					keyTimes="0; 1"
-					keySplines="0.7 0 0.5 1"
+					keySplines="0.65 0 0.4 1"
 				/>
-				<g transform="translate(0 13)">
-					<g stroke="currentColor" stroke-width="0.6">
-						<g fill="url(#wrapping-paper)">
-							<rect y="4" x="-6" width="12" height="7" />
-							<rect x="-7" width="14" height="4" />
-						</g>
-						<rect fill="#73ce3a" x="-2" y="-0.5" width="4" height="11.5" />
+				<g stroke="currentColor" stroke-width="0.6">
+					<g fill="url(#balloon-pattern-present)">
+						<rect x="-6" y="4" width="12" height="7" />
+						<rect x="-7" width="14" height="4" />
 					</g>
+					<rect fill="#73ce3a" x="-2" y="-0.5" width="4" height="11.5" />
 				</g>
 			</g>
 		</g>
@@ -258,16 +183,16 @@
 			font-family="sans-serif"
 			text-anchor="middle"
 			dominant-baseline="middle"
-			fill="#ffffff"
+			fill="#f7f7f7"
 			stroke="currentColor"
-			stroke-width="0.4"
+			stroke-width="0.3"
 		>
 			<animate
 				attributeName="opacity"
-				values="1; 0;"
+				to="0"
 				fill="freeze"
-				dur="0.15s"
-				begin="balloonGame.begin"
+				dur="0.1s"
+				begin="balloonGameStart.begin"
 			/>
 			<text> Pop! </text>
 		</g>
@@ -275,12 +200,87 @@
 
 	<rect style:cursor="pointer" width="80" height="50" opacity="0">
 		<animate
-			id="balloonGame"
+			id="balloonGameStart"
 			attributeName="display"
 			to="none"
 			fill="freeze"
-			dur="0.15s"
 			begin="click"
+			dur="0.1s"
+			restart="never"
 		/>
 	</rect>
+
+	<g transform="translate(-80 0)">
+		<animateTransform
+			attributeName="transform"
+			by="80 0"
+			additive="sum"
+			calcMode="discrete"
+			fill="freeze"
+			begin="balloonGameOver.end + 1.5s"
+			dur="0.1s"
+			id="balloonGameMessage"
+		/>
+
+		<g transform="translate(40 25)">
+			<g
+				style="pointer-events: none;"
+				font-size="6"
+				font-weight="bold"
+				font-family="sans-serif"
+				text-anchor="middle"
+				dominant-baseline="middle"
+				fill="#f7f7f7"
+				stroke="currentColor"
+				stroke-width="0.3"
+			>
+				<animateTransform
+					attributeName="transform"
+					type="translate"
+					to="0 1"
+					dur="0.25s"
+					repeatCount="indefinite"
+					begin="balloonGameMessage.begin"
+					end="balloonGameEnd.end"
+					fill="freeze"
+					calcMode="discrete"
+				/>
+				<g>
+					<animateTransform
+						attributeName="transform"
+						type="translate"
+						to="0 50"
+						dur="0.1s"
+						calcMode="discrete"
+						fill="freeze"
+						begin="balloonGamePopBalloon.begin"
+					/>
+					<text>
+						{#each 'Too bad...'.split('') as l, i}
+							<tspan dx="0.5" dy={i % 2 === 0 ? -0.5 : 0.5}>{l}</tspan>
+						{/each}
+					</text>
+					<g transform="translate(0 -50)">
+						<text>
+							{#each 'You did it!'.split('') as l, i}
+								<tspan dx="0.5" dy={i % 2 === 0 ? 0.5 : -0.5}>{l}</tspan>
+							{/each}
+						</text>
+					</g>
+				</g>
+			</g>
+		</g>
+
+		<rect style:cursor="pointer" width="80" height="50" opacity="0">
+			<animate
+				attributeName="display"
+				to="none"
+				fill="freeze"
+				begin="click"
+				restart="never"
+				dur="0.1s"
+				id="balloonGameEnd"
+			/>
+		</rect>
+	</g>
 </svg>
