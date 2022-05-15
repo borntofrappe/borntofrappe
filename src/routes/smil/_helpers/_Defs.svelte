7f43b857a2356dc4362562b9fa4c520004361595
borntofrappe <borntofrappe@gmail.com>
2022-05-15 17:25:21 +0200

    add helper components

---
+++ src/routes/smil/_helpers/_Defs.svelte
@@ -0,0 +1,18 @@
+<svg aria-hidden="true" width="0" height="0">
+	<defs>
+		<linearGradient id="linear-gradient-text" x1="0" x2="0" y1="0" y2="1">
+			<stop stop-color="#f7f7f7" offset="0.33" />
+			<stop stop-color="#e6e7e6" offset="0.33" />
+			<stop stop-color="#e6e7e6" offset="0.67" />
+			<stop stop-color="#d6dbd6" offset="0.67" />
+		</linearGradient>
+	</defs>
+</svg>
+
+<style>
+	svg {
+		position: absolute;
+		width: 0;
+		height: 0;
+	}
+</style>
---
+++ src/routes/smil/_helpers/_Text.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let fontSize = 6;
+	export let fill = '#f7f7f7';
+	export let stroke = '#192d10';
+</script>
+
+<g
+	font-family="sans-serif"
+	font-weight="700"
+	font-size={fontSize}
+	text-anchor="middle"
+	dominant-baseline="middle"
+	{fill}
+	{stroke}
+	stroke-width={fontSize / 6}
+	paint-order="stroke"
+	letter-spacing={fontSize / 14}
+	stroke-linejoin="round"
+	stroke-linecap="round"
+>
+	<text>
+		<slot>Hello world</slot>
+	</text>
+</g>
