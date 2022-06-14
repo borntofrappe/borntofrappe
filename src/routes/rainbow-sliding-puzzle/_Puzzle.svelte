e2658dce15a44490467d736b4e8d9a674475fe71
borntofrappe <borntofrappe@gmail.com>
2022-06-14 21:54:10 +0200

    infinite sliding puzzle

--- src/routes/rainbow-sliding-puzzle/_Puzzle.svelte
+++ src/routes/rainbow-sliding-puzzle/_Puzzle.svelte
@@ -1,153 +1,30 @@
 <script>
-	import { getColorHsv as getColor } from './_utils.js';
 	import { tweened } from 'svelte/motion';
-	import { cubicInOut as easing } from 'svelte/easing';
-
-	const duration = 125;
-	const tween = tweened(0, { duration, easing });
+	import { cubicInOut, backOut } from 'svelte/easing';
+	import { Puzzle } from './_utils.js';
 
 	export let size = 4;
-	export let moves = 12;
+	export let moves = 20;
 	export let index = size ** 2 - 1;
 
-	const n = size * 2 - 1;
+	let puzzle = new Puzzle({ size, moves, index });
 
-	const hiddenCoords = [Math.floor(index / size), index % size];
-	let [hr, hc] = hiddenCoords;
-	const dr = Math.max(size - hr - 1, hr);
-	const dc = Math.max(size - hc - 1, hc);
-	const dn = dr + dc;
+	const sDuration = 125;
+	const slide = tweened(0, { duration: sDuration, easing: cubicInOut });
 
-	const animationDuration = 0.75;
-	const animationIterationCount = 5;
+	const tDuration = 400;
+	const tDelay = 50;
+	const reveal = tweened(0, { duration: tDuration, easing: cubicInOut });
+	const scale = tweened(1, { duration: tDuration, easing: cubicInOut });
 
-	const doColorsMatch = (grid, colors) => {
-		let colorsMatch = true;
-		for (let i = 0; i < colors.length; i++) {
-			for (let j = 0; j < colors[i].length; j++) {
-				if (colors[i][j] !== grid[i][j].color) {
-					colorsMatch = false;
-					break;
-				}
-			}
-			if (!colorsMatch) {
-				break;
-			}
-		}
-
-		return colorsMatch;
-	};
-
-	const colors = [];
-	const grid = Array(size)
-		.fill()
-		.map((_, r) => {
-			colors.push([]);
-			return Array(size)
-				.fill()
-				.map((_, c) => {
-					const color = getColor({ h: (360 * (c + r)) / n });
-					const animationDelay =
-						animationDuration - (animationDuration * (Math.abs(r - hr) + Math.abs(c - hc))) / dn;
-					colors[colors.length - 1].push(color);
-
-					return {
-						r,
-						c,
-						color,
-						animationDelay
-					};
-				});
-		});
-
-	const neighbors = [
-		[0, -1],
-		[1, 0],
-		[0, 1],
-		[-1, 0]
-	];
-
-	let nextNeighbors = neighbors.map(([r, c]) => [r, c]);
-
-	grid[hr][hc].hidden = true;
-
-	for (let i = 0; i < moves; i++) {
-		const availableNeighbors = nextNeighbors.filter(
-			([nr, nc]) => grid[hr + nr] && grid[hr + nr][hc + nc]
-		);
-		const [nr, nc] = availableNeighbors[Math.floor(Math.random() * availableNeighbors.length)];
-
-		const gr = hr + nr;
-		const gc = hc + nc;
-
-		grid[gr][gc].r = hr;
-		grid[gr][gc].c = hc;
-		grid[hr][hc].r = gr;
-		grid[hr][hc].c = gc;
-		const temp = grid[gr][gc];
-		grid[gr][gc] = grid[hr][hc];
-		grid[hr][hc] = temp;
-
-		hr = gr;
-		hc = gc;
-		nextNeighbors = neighbors.filter(([nnr, nnc]) => nnr !== nr * -1 || nnc !== nc * -1);
-	}
-
-	while (hr !== hiddenCoords[0]) {
-		const dr = hiddenCoords[0] - hr > 0 ? 1 : -1;
-		const gr = hr + dr;
-		const gc = hc;
-
-		grid[gr][gc].r = hr;
-		grid[gr][gc].c = hc;
-		grid[hr][hc].r = gr;
-		grid[hr][hc].c = gc;
-		const temp = grid[gr][gc];
-		grid[gr][gc] = grid[hr][hc];
-		grid[hr][hc] = temp;
-
-		hr = gr;
-	}
-	while (hc !== hiddenCoords[1]) {
-		const dc = hiddenCoords[1] - hc > 0 ? 1 : -1;
-		const gr = hr;
-		const gc = hc + dc;
-
-		grid[gr][gc].r = hr;
-		grid[gr][gc].c = hc;
-		grid[hr][hc].r = gr;
-		grid[hr][hc].c = gc;
-		const temp = grid[gr][gc];
-		grid[gr][gc] = grid[hr][hc];
-		grid[hr][hc] = temp;
-
-		hc = gc;
-	}
-
-	if (doColorsMatch(grid, colors)) {
-		const availableNeighbors = neighbors
-			.map(([nr, nc]) => [hr + nr, hc + nc])
-			.filter(([nr, nc]) => grid[nr] && grid[nr][nc]);
-
-		const [gr, gc] = availableNeighbors[Math.floor(Math.random() * availableNeighbors.length)];
-
-		grid[gr][gc].r = hr;
-		grid[gr][gc].c = hc;
-		grid[hr][hc].r = gr;
-		grid[hr][hc].c = gc;
-		const temp = grid[gr][gc];
-		grid[gr][gc] = grid[hr][hc];
-		grid[hr][hc] = temp;
-	}
-
-	let sliding;
 	let slid;
-	const updateGrid = ({ r, c }) => {
+	let sliding;
+	let animated;
+
+	const updateGrid = async ({ r, c }) => {
 		if (sliding) return;
 
-		const hiddenNeighbor = neighbors
-			.map(([nr, nc]) => [nr + r, nc + c])
-			.find(([nr, nc]) => grid[nr] && grid[nr][nc] && grid[nr][nc].hidden);
+		const hiddenNeighbor = puzzle.getHiddenNeighbor({ r, c });
 
 		if (hiddenNeighbor) {
 			const [hr, hc] = hiddenNeighbor;
@@ -156,37 +33,65 @@
 
 			sliding = true;
 
-			tween
-				.set(1, {
-					interpolate: (from, to) => (t) => {
-						const value = t * (to - from);
-						grid[r][c].r = r + dr * value;
-						grid[r][c].c = c + dc * value;
-						return value;
-					}
-				})
-				.then(() => {
-					grid[r][c].r = hr;
-					grid[r][c].c = hc;
-					grid[hr][hc].r = r;
-					grid[hr][hc].c = c;
-					const temp = grid[r][c];
-					grid[r][c] = grid[hr][hc];
-					grid[hr][hc] = temp;
+			await slide.set(1, {
+				interpolate: (from, to) => (t) => {
+					const value = t * (to - from);
+					puzzle.grid[r][c].r = r + dr * value;
+					puzzle.grid[r][c].c = c + dc * value;
+					return value;
+				}
+			});
 
-					if (doColorsMatch(grid, colors)) {
-						slid = true;
-					} else {
-						tween.set(0, { duration: 0 });
-						sliding = false;
-					}
-				});
+			puzzle.grid[r][c].r = hr;
+			puzzle.grid[r][c].c = hc;
+			puzzle.grid[hr][hc].r = r;
+			puzzle.grid[hr][hc].c = c;
+			const temp = puzzle.grid[r][c];
+			puzzle.grid[r][c] = puzzle.grid[hr][hc];
+			puzzle.grid[hr][hc] = temp;
+
+			slide.set(0, { duration: 0 });
+			sliding = false;
+
+			if (puzzle.doColorsMatch()) {
+				slid = true;
+			}
 		}
+	};
+
+	const getNewPuzzle = async ({ r, c }) => {
+		const [hr, hc] = puzzle.getHiddenNeighbor();
+		puzzle.grid[hr][hc].hidden = false;
+		puzzle.grid[r][c].hidden = true;
+		reveal.set(0);
+
+		await scale.set(0, { delay: tDelay });
+
+		const index = r * puzzle.size + c;
+		puzzle = new Puzzle({ size, moves, index });
+		slid = false;
+		animated = false;
+
+		scale.set(1);
+	};
+
+	const handleAnimation = async () => {
+		if (animated) return;
+
+		animated = true;
+
+		const { animationDuration: duration } = puzzle;
+
+		reveal.set(1, {
+			easing: backOut,
+			delay: (duration * 1000) / 2,
+			duration: (duration * 1000) / 2
+		});
 	};
 </script>
 
-<svg class:slid viewBox="0 0 {size} {size}">
-	{#each grid as row}
+<svg class:slid viewBox="-0.5 -0.5 {puzzle.size} {puzzle.size}">
+	{#each puzzle.grid as row}
 		{#each row as { r, c, color: fill, hidden, animationDelay }}
 			<g transform="translate({c} {r})">
 				<g
@@ -196,32 +101,37 @@
 						updateGrid({ r, c });
 					}}
 				>
-					<g transform="translate(0.5 0.5)">
-						<g
-							class:hidden
-							style={hidden
-								? `transform: scale(0); animation-duration: ${
-										animationDuration / 2
-								  }s; animation-delay: ${
-										animationDuration * animationIterationCount + animationDuration / 2
-								  }s`
-								: ''}
-						>
-							<rect
-								style="animation-duration: {animationDuration}s; animation-delay: {animationDelay}s; animation-iteration-count: {animationIterationCount};"
-								x="-0.45"
-								y="-0.45"
-								width="0.9"
-								height="0.9"
-								{fill}
-								rx="0.15"
-							/>
+					<g transform="scale({$scale})">
+						<g transform="scale({hidden ? $reveal : 1})">
+							<g on:animationend={handleAnimation}>
+								<rect
+									style="animation-duration: {puzzle.animationDuration}s; animation-delay: {animationDelay}s;"
+									x="-0.45"
+									y="-0.45"
+									width="0.9"
+									height="0.9"
+									{fill}
+									rx="0.15"
+								/>
+							</g>
 						</g>
 					</g>
 				</g>
 			</g>
 		{/each}
 	{/each}
+
+	{#if animated}
+		{#each puzzle.grid as row}
+			{#each row as { r, c }}
+				<g style:cursor="pointer" on:click|once={getNewPuzzle({ r, c })} opacity="0">
+					<g transform="translate({c} {r})">
+						<rect x="-0.45" y="-0.45" width="0.9" height="0.9" rx="0.15" />
+					</g>
+				</g>
+			{/each}
+		{/each}
+	{/if}
 </svg>
 
 <style>
@@ -230,25 +140,12 @@
 	}
 
 	.slid rect {
-		animation: pulse cubic-bezier(0.37, 0, 0.63, 1);
-	}
-
-	.slid .hidden {
-		animation: pop cubic-bezier(0.34, 1.56, 0.64, 1) both;
+		animation: pulse 5 cubic-bezier(0.37, 0, 0.63, 1);
 	}
 
 	@keyframes pulse {
 		50% {
 			transform: scale(0.8);
-		}
-	}
-
-	@keyframes pop {
-		0% {
-			transform: scale(0);
-		}
-		100% {
-			transform: scale(1);
 		}
 	}
 </style>
