8e651548b678be07ffdc6658073f178c62b4bb72
borntofrappe <borntofrappe@gmail.com>
2022-07-15 15:21:51 +0200

    feat: :wrench: set up markdown blog
    install mdsvex; add test .md and .svx documents; update config file to process the file extensions

---
+++ src/routes/blog/snake-svg-svelte.md
@@ -0,0 +1,7 @@
+---
+title: Snake, SVG, Svelte
+date: 2022-07-16T20:53
+brief: Let's build a game of Snake with vector graphics and a rather popular utility.
+---
+
+Snake is a game which needs very little introduction, except perhaps a demo.
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import { resolve } from 'path';
+import { mdsvex } from 'mdsvex';
+
+const mdsvexConfig = {
+	extensions: ['.md', '.svx']
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: mdsvex(mdsvexConfig),
 	kit: {
 		adapter: adapter(),
 		vite: {
