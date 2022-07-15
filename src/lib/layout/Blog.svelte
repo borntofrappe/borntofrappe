e124b3383626e561be9fc76211920a43ae88afb2
borntofrappe <borntofrappe@gmail.com>
2022-07-15 21:41:16 +0200

    feat: :sparkles: add component to update theme
    allow to change the layout file for documents in the blog folder in color and background. Use a custom component with an svg interactable to set a time of day. Use timeOfDay as a variable with three values to change the reading experience for morning/day/night

--- src/lib/layout/Blog.svelte
+++ src/lib/layout/Blog.svelte
@@ -1,11 +1,19 @@
 <script>
+	import Theme from '$lib/components/Theme.svelte';
+
 	export let title;
 	export let description;
-	export let datetime;
 
-	$: date = new Date(
-		...datetime.split(/[-T:]/).map((d, i) => (i === 1 ? parseInt(d, 10) - 1 : parseInt(d, 10)))
-	);
+	let timeOfDay;
+	let background;
+	let foreground;
+
+	const handleChange = (e) => {
+		const { detail: change } = e;
+		timeOfDay = change.timeOfDay;
+		background = change.background;
+		foreground = change.foreground;
+	};
 </script>
 
 <svelte:head>
@@ -15,19 +23,70 @@
 	<meta name="description" content={description} />
 </svelte:head>
 
-<header>
-	<h1>{title}</h1>
-	<time {datetime}>{date}</time>
-</header>
+<svg aria-hidden="true" style:position="absolute" style:width="0" style:height="0">
+	<defs>
+		<filter id="filter-outline">
+			<feFlood flood-color={foreground || 'currentColor'} result="ccolor" />
+			<feFlood flood-color="#63616b" result="ocolor" />
+			<feMorphology in="SourceAlpha" operator="dilate" radius="4" result="coutline" />
+			<feMorphology in="SourceAlpha" operator="dilate" radius="2" result="ooutline" />
+			<feComposite in="ccolor" in2="coutline" operator="in" result="ccolor-outline" />
+			<feComposite in="ocolor" in2="ooutline" operator="in" result="ocolor-outline" />
 
-<main>
-	<slot />
-</main>
+			<feMerge>
+				<feMergeNode in="ccolor-outline" />
+				<feMergeNode in="ocolor-outline" />
+				<feMergeNode in="SourceGraphic" />
+			</feMerge>
+		</filter>
+	</defs>
+</svg>
+
+<div class={timeOfDay}>
+	<header style:background>
+		<h1>{title}</h1>
+		<Theme {timeOfDay} on:change={handleChange} />
+	</header>
+
+	<main>
+		<slot />
+	</main>
+</div>
 
 <style>
+	div > :global(* + *) {
+		margin-top: 1em;
+	}
+
+	.morning {
+		color: hsl(0, 0%, 23%);
+		background: hsl(0, 0%, 93%);
+	}
+
+	.day {
+		color: hsl(0, 0%, 12%);
+		background: hsl(0, 0%, 97%);
+	}
+
+	.night {
+		color: hsl(0, 0%, 93%);
+		background: hsl(0, 0%, 12%);
+	}
+
 	header {
-		padding: 3rem 1rem;
+		padding: 2rem 1rem 0;
+	}
+
+	h1 {
+		color: hsl(0, 0%, 97%);
 		text-align: center;
+		filter: url(#filter-outline);
+	}
+
+	header > :global(svg) {
+		max-width: 36rem;
+		margin-left: auto;
+		margin-right: auto;
 	}
 
 	main {
