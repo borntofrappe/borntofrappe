1241335af8a86e139ea12ea27196f0e17c1b3afa
borntofrappe <borntofrappe@gmail.com>
2022-07-30 15:01:14 +0200

    feat: :tada: add smile-and-diy route
    create route to experiment with smil animation; add helper components to show text with a specific fill and stroke, always with SVG syntax

---
+++ src/routes/smile-and-diy/index.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Defs from './_helpers/Defs.svelte';
+	import Text from './_helpers/Text.svelte';
+	import AnimatedText from './_helpers/AnimatedText.svelte';
+</script>
+
+<Defs />
+<svg viewBox="0 0 80 40">
+	<g transform="translate(40 10)">
+		<Text fill="url(#linear-gradient-text)">Smile</Text>
+	</g>
+
+	<g transform="translate(40 20)">
+		<Text fill="url(#linear-gradient-text)">&</Text>
+	</g>
+
+	<g transform="translate(40 30)">
+		<AnimatedText fill="url(#linear-gradient-text)" text="D.I.Y." />
+	</g>
+</svg>
+
+<style>
+	svg {
+		max-width: 40rem;
+	}
+</style>
