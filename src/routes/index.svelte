72833a84575b0bbfa1b80c03be5f7c27856db414
borntofrappe <borntofrappe@gmail.com>
2022-08-15 21:36:42 +0200

    feat: :construction: add <head> elements
    add a custom svg icon in place of the png default; add a title and description to the landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,9 @@
+<svelte:head>
+	<title>borntofrappe</title>
+	<meta
+		name="description"
+		content="Personal website for one Gabriele Corti, built by one Gabriele Corti, coding online as borntofrappe."
+	/>
+</svelte:head>
+
 <h1>hello world</h1>
