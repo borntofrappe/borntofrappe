8e328653bf49b8352e5cc8fc439ff2981ce7b0d2
borntofrappe <borntofrappe@gmail.com>
2022-08-16 22:27:53 +0200

    build: :wrench: configure mdsvex to preprocess '.md' and '.svx' documents
    add mdsvex as a preprocessor to consider '.md' and '.svx' documents; configure sveltekit to consider both file extensions; add test article to showcase mdsvex and a layout file

---
+++ src/lib/layout/Blog.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let title;
+	export let description;
+</script>
+
+<svelte:head>
+	<title>
+		borntofrappe | blog - {title}
+	</title>
+	<meta name="description" content={description} />
+</svelte:head>
+
+<main>
+	<slot />
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
 
+const mdsvexExtensions = ['.md', '.svx'];
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
+	extensions: ['.svelte', ...mdsvexExtensions],
+	preprocess: mdsvex({
+		extensions: mdsvexExtensions,
+		layout: { blog: 'src/lib/layout/Blog.svelte' }
+	}),
 	kit: {
 		adapter: adapter()
 	}
