375aae343e269f8d743c01b2203a777e81554628
borntofrappe <borntofrappe@gmail.com>
2022-08-17 21:31:44 +0200

    style: :truck: create tangram component
    more tangram logic to a dedicated component in the lib/components subfolder

---
+++ src/lib/components/tangram/Tangram.svelte
@@ -0,0 +1,51 @@
+<script>
+	const pieces = [
+		{ d: 'M 0 0 l 2 2 2 -2z', fill: '#71c49a' },
+		{ d: 'M 0 0 l 2 2 2 -2z', fill: '#f6e48e' },
+		{ d: 'M 0 0 l 1 1 -1 1z', fill: '#ef847a' },
+		{ d: 'M 0 0 l 1 1 -1 1z', fill: '#ef847a' },
+		{ d: 'M 0 0 l 2 2 h-2z', fill: '#abdcc7' },
+		{ d: 'M 0 0 l 1 1 1 -1 -1 -1z', fill: '#71c49a' },
+		{ d: 'M 0 0 l 1 -1 h 2 l -1 1z', fill: '#f6e48e' }
+	];
+</script>
+
+<svg viewBox="0 0 {4} {4}">
+	{#each pieces as { d, fill }}
+		<path {d} {fill} />
+	{/each}
+</svg>
+
+<style>
+	svg {
+		display: block;
+	}
+
+	svg path:nth-of-type(1) {
+		transform: translate(0px, 0px) rotate(0deg);
+	}
+
+	svg path:nth-of-type(2) {
+		transform: translate(0px, 4px) rotate(270deg);
+	}
+
+	svg path:nth-of-type(3) {
+		transform: translate(4px, 2px) rotate(180deg);
+	}
+
+	svg path:nth-of-type(4) {
+		transform: translate(1px, 3px) rotate(270deg);
+	}
+
+	svg path:nth-of-type(5) {
+		transform: translate(2px, 4px) rotate(-90deg);
+	}
+
+	svg path:nth-of-type(6) {
+		transform: translate(2px, 2px) rotate(0deg);
+	}
+
+	svg path:nth-of-type(7) {
+		transform: translate(0px, 4px) rotate(0deg);
+	}
+</style>
--- src/routes/tangram/index.svelte
+++ src/routes/tangram/index.svelte
@@ -1,13 +1,5 @@
 <script>
-	const pieces = [
-		{ d: 'M 0 0 l 2 2 2 -2z', fill: '#71c49a' },
-		{ d: 'M 0 0 l 2 2 2 -2z', fill: '#f6e48e' },
-		{ d: 'M 0 0 l 1 1 -1 1z', fill: '#ef847a' },
-		{ d: 'M 0 0 l 1 1 -1 1z', fill: '#ef847a' },
-		{ d: 'M 0 0 l 2 2 h-2z', fill: '#abdcc7' },
-		{ d: 'M 0 0 l 1 1 1 -1 -1 -1z', fill: '#71c49a' },
-		{ d: 'M 0 0 l 1 -1 h 2 l -1 1z', fill: '#f6e48e' }
-	];
+	import Tangram from '$lib/components/tangram/Tangram.svelte';
 </script>
 
 <svelte:head>
@@ -20,11 +12,7 @@
 </svelte:head>
 
 <div>
-	<svg viewBox="0 0 {4} {4}">
-		{#each pieces as { d, fill }}
-			<path {d} {fill} />
-		{/each}
-	</svg>
+	<Tangram />
 </div>
 
 <style>
@@ -38,38 +26,8 @@
 		align-items: center;
 	}
 
-	svg {
-		display: block;
+	div > :global(svg) {
 		max-width: 30rem;
-		width: 100%;
 		height: auto;
-	}
-
-	svg path:nth-of-type(1) {
-		transform: translate(0px, 0px) rotate(0deg);
-	}
-
-	svg path:nth-of-type(2) {
-		transform: translate(0px, 4px) rotate(270deg);
-	}
-
-	svg path:nth-of-type(3) {
-		transform: translate(4px, 2px) rotate(180deg);
-	}
-
-	svg path:nth-of-type(4) {
-		transform: translate(1px, 3px) rotate(270deg);
-	}
-
-	svg path:nth-of-type(5) {
-		transform: translate(2px, 4px) rotate(-90deg);
-	}
-
-	svg path:nth-of-type(6) {
-		transform: translate(2px, 2px) rotate(0deg);
-	}
-
-	svg path:nth-of-type(7) {
-		transform: translate(0px, 4px) rotate(0deg);
 	}
 </style>
