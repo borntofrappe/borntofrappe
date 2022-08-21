2fe6255113cf85d0f59c7f025bf4657115b875cc
borntofrappe <borntofrappe@gmail.com>
2022-08-21 20:51:32 +0200

    style: :memo: update mouse interaction
    following mouse interaction scale the entire component to have the mascot bounce with more character; update viewbox to ensure the content is not cropped outside of the svg canvas

--- src/lib/components/mignon/Ray.svelte
+++ src/lib/components/mignon/Ray.svelte
@@ -2,12 +2,24 @@
 	import { onMount } from 'svelte';
 	import { spring } from 'svelte/motion';
 
-	let scale = spring(1, {
+	const rays = Array(24)
+		.fill()
+		.map((_, i, { length }) => {
+			const angle = (360 / length) * i;
+			const x = i % 2 === 0 ? 7 : 6;
+
+			return {
+				angle,
+				x
+			};
+		});
+
+	const scale = spring(1, {
 		stiffness: 0.1,
 		damping: 0.2
 	});
 
-	let offset = spring(
+	const offset = spring(
 		{ x: 0, y: 0 },
 		{
 			stiffness: 0.1,
@@ -28,8 +40,13 @@
 		h = height;
 	};
 
-	const handleStart = () => scale.set(1.5);
-	const handleEnd = () => scale.set(1);
+	const handleStart = () => {
+		scale.set(1.1);
+	};
+
+	const handleEnd = () => {
+		scale.set(1);
+	};
 
 	const handleOut = () => {
 		scale.set(1);
@@ -48,7 +65,7 @@
 
 <svg
 	xmlns="http://www.w3.org/2000/svg"
-	viewBox="-50 -50 100 100"
+	viewBox="-55 -55 110 110"
 	bind:this={svg}
 	on:mousedown={handleStart}
 	on:mouseup={handleEnd}
@@ -56,45 +73,41 @@
 	on:mousemove={handleMove}
 	on:click={handleMove}
 >
-	<g transform="translate({$offset.x * 0.3} {$offset.y * 0.3})">
-		<circle r="28" fill="#ffdb47" />
-	</g>
-
-	<g transform="translate({$offset.x * 0.2} {$offset.y * 0.2})">
-		<g fill="none" stroke="#ffdb47" stroke-width="3" stroke-linecap="round">
-			{#each Array(24)
-				.fill()
-				.map((_, i, { length }) => (360 / length) * i) as angle, i}
-				<path transform="rotate({angle})" d="M 35 0 h {i % 2 === 0 ? 7 : 6}" />
-			{/each}
+	<g transform="scale({$scale})">
+		<g transform="translate({$offset.x * 0.2} {$offset.y * 0.2})">
+			<g fill="none" stroke="#ffdb47" stroke-width="3" stroke-linecap="round">
+				{#each rays as { angle, x }}
+					<path transform="rotate({angle})" d="M 35 0 h {x}" />
+				{/each}
+			</g>
 		</g>
 	</g>
 
-	<g transform="translate({$offset.x} {$offset.y})">
-		<g fill="#ff877a">
-			<g transform="translate(-9 5)">
-				<circle r="4" />
-			</g>
-			<g transform="translate(9 5)">
-				<circle r="4" />
-			</g>
-			<g transform="translate(0 6)">
-				<g transform="scale({$scale})">
-					<g stroke="#ff877a" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
-						<path d="M -1.5 0 v 1.5 a 1.5 1.5 0 0 0 3 0 v -1.5z" />
+	<g transform="scale({$scale})">
+		<g transform="translate({$offset.x * 0.3} {$offset.y * 0.3})">
+			<circle r="28" fill="#ffdb47" />
+		</g>
+
+		<g transform="translate({$offset.x} {$offset.y})">
+			<g fill="#ff877a">
+				<g transform="translate(0 5)">
+					<circle r="4" cx="-9" />
+					<circle r="4" cx="9" />
+				</g>
+				<g transform="translate(0 6)">
+					<g transform="scale({$scale ** 2})">
+						<g stroke="#ff877a" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
+							<path d="M -1.5 0 v 1.5 a 1.5 1.5 0 0 0 3 0 v -1.5z" />
+						</g>
 					</g>
 				</g>
 			</g>
 		</g>
-	</g>
 
-	<g transform="translate({$offset.x * 1.1} {$offset.y * 1.1})">
-		<g fill="#38311e" stroke="#ffdb47" stroke-width="1">
-			<g transform="translate(-5 0)">
-				<circle r="2" />
-			</g>
-			<g transform="translate(5 0)">
-				<circle r="2" />
+		<g transform="translate({$offset.x * 1.1} {$offset.y * 1.1})">
+			<g fill="#38311e" stroke="#ffdb47" stroke-width="1">
+				<circle r="2" cx="-5" />
+				<circle r="2" cx="5" />
 			</g>
 		</g>
 	</g>
