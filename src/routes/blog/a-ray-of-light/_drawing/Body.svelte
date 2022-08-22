aced68a0b84f491e6252a6f7a4b8a1b3a555e7df
borntofrappe <borntofrappe@gmail.com>
2022-08-22 22:22:24 +0200

    docs: :memo: add drawing with svg section and components

---
+++ src/routes/blog/a-ray-of-light/_drawing/Body.svelte
@@ -0,0 +1,3 @@
+<svg viewBox="-50 -50 100 100">
+	<circle r="28" fill="#ffdb47" />
+</svg>
---
+++ src/routes/blog/a-ray-of-light/_drawing/Mouth.svelte
@@ -0,0 +1,14 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100">
+	<circle r="28" fill="#ffdb47" />
+	<g fill="#ff877a">
+		<g transform="translate(0 5)">
+			<circle r="4" cx="-9" />
+			<circle r="4" cx="9" />
+		</g>
+		<g transform="translate(0 6)">
+			<g stroke="#ff877a" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
+				<path d="M -1.5 0 v 1.5 a 1.5 1.5 0 0 0 3 0 v -1.5z" />
+			</g>
+		</g>
+	</g>
+</svg>
