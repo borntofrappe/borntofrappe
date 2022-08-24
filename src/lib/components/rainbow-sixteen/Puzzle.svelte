88c271cffb531ef8aafc4a161add8a3d0ca72b9d
borntofrappe <borntofrappe@gmail.com>
2022-08-24 18:07:07 +0200

    feat: :sparkles: reset puzzle
    allow to play multiple rounds by resetting the puzzle; consider refactoring the code with a puzzle class describing the grid and hidden neighbor; consider renaming hiddenNeighbor to hiddenTile

--- src/lib/components/rainbow-sixteen/Puzzle.svelte
+++ src/lib/components/rainbow-sixteen/Puzzle.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { tweened } from 'svelte/motion';
-	import { cubicInOut } from 'svelte/easing';
+	import { cubicInOut, backOut } from 'svelte/easing';
 
 	import { getColorHsv as getColor } from './utils.js';
 
@@ -8,83 +8,12 @@
 	export let index = size ** 2 - 1;
 	export let moves = 4;
 
-	const n = size * 2 - 1;
-	const puzzle = Array(size)
-		.fill()
-		.map((_, row) => {
-			return Array(size)
-				.fill()
-				.map((_, column) => {
-					const color = getColor({ h: (360 * (column + row)) / n });
-
-					return {
-						row,
-						column,
-						color,
-						hidden: false,
-						initial: {
-							row,
-							column
-						}
-					};
-				});
-		});
-
-	const hiddenRow = Math.floor(index / size);
-	const hiddenColumn = index % size;
-	puzzle[hiddenRow][hiddenColumn].hidden = true;
-
-	const neighbors = [
-		{ row: -1, column: 0 },
-		{ row: 0, column: 1 },
-		{ row: 1, column: 0 },
-		{ row: 0, column: -1 }
-	];
-
-	let nextNeighbors = neighbors.map((neighbor) => ({ ...neighbor }));
-
-	const hiddenNeighbor = {
-		row: hiddenRow,
-		column: hiddenColumn
-	};
-
-	for (let i = 0; i < moves; i++) {
-		const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
-
-		const availableNeighbors = nextNeighbors.filter(
-			({ row, column }) => puzzle[hiddenRow + row] && puzzle[hiddenRow + row][hiddenColumn + column]
-		);
-		const { row: neighborRow, column: neighborColumn } =
-			availableNeighbors[Math.floor(Math.random() * availableNeighbors.length)];
-
-		const row = hiddenRow + neighborRow;
-		const column = hiddenColumn + neighborColumn;
-
-		puzzle[row][column].row = hiddenRow;
-		puzzle[row][column].column = hiddenColumn;
-		puzzle[hiddenRow][hiddenColumn].row = row;
-		puzzle[hiddenRow][hiddenColumn].column = column;
-
-		[puzzle[row][column], puzzle[hiddenRow][hiddenColumn]] = [
-			puzzle[hiddenRow][hiddenColumn],
-			puzzle[row][column]
-		];
-
-		hiddenNeighbor.row = row;
-		hiddenNeighbor.column = column;
-
-		nextNeighbors = neighbors.filter(
-			({ row, column }) => row !== neighborRow * -1 || column !== neighborColumn * -1
-		);
-	}
-
 	const slideDuration = 125;
 	const slide = tweened(0, { duration: slideDuration, easing: cubicInOut });
 
-	const revealDuration = 400;
-	const reveal = tweened(0, { duration: revealDuration, easing: cubicInOut });
-
-	let isSliding = false;
+	const sizeDuration = 400;
+	const reveal = tweened(0, { duration: sizeDuration, easing: backOut });
+	const scale = tweened(1, { duration: sizeDuration, easing: cubicInOut });
 
 	const hasHiddenNeighbor = ({ row, column }) => {
 		const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
@@ -108,35 +37,126 @@
 		await slide.set(1, {
 			interpolate: (from, to) => (t) => {
 				const value = t * (to - from);
-				puzzle[row][column].row = row + offsetRow * value;
-				puzzle[row][column].column = column + offsetColumn * value;
+				grid[row][column].row = row + offsetRow * value;
+				grid[row][column].column = column + offsetColumn * value;
 				return value;
 			}
 		});
 
-		puzzle[row][column].row = hiddenRow;
-		puzzle[row][column].column = hiddenColumn;
-		puzzle[hiddenRow][hiddenColumn].row = row;
-		puzzle[hiddenRow][hiddenColumn].column = column;
+		grid[row][column].row = hiddenRow;
+		grid[row][column].column = hiddenColumn;
+		grid[hiddenRow][hiddenColumn].row = row;
+		grid[hiddenRow][hiddenColumn].column = column;
 
-		[puzzle[row][column], puzzle[hiddenRow][hiddenColumn]] = [
-			puzzle[hiddenRow][hiddenColumn],
-			puzzle[row][column]
+		[grid[row][column], grid[hiddenRow][hiddenColumn]] = [
+			grid[hiddenRow][hiddenColumn],
+			grid[row][column]
 		];
 
 		hiddenNeighbor.row = row;
 		hiddenNeighbor.column = column;
 
-		const hasWon = puzzle
+		const hasSolved = grid
 			.reduce((acc, curr) => [...acc, ...curr], [])
 			.every(({ row, column, initial }) => row === initial.row && column === initial.column);
 
-		if (hasWon) {
-			reveal.set(1);
+		if (hasSolved) {
+			await reveal.set(1);
+			isSolved = true;
 		} else {
 			slide.set(0, { duration: 0 });
 			isSliding = false;
 		}
+	};
+
+	const getPuzzle = ({ size, index, moves }) => {
+		const n = size * 2 - 1;
+		const grid = Array(size)
+			.fill()
+			.map((_, row) => {
+				return Array(size)
+					.fill()
+					.map((_, column) => {
+						const color = getColor({ h: (360 * (column + row)) / n });
+
+						return {
+							row,
+							column,
+							color,
+							hidden: false,
+							initial: {
+								row,
+								column
+							}
+						};
+					});
+			});
+
+		const hiddenRow = Math.floor(index / size);
+		const hiddenColumn = index % size;
+		grid[hiddenRow][hiddenColumn].hidden = true;
+
+		const neighbors = [
+			{ row: -1, column: 0 },
+			{ row: 0, column: 1 },
+			{ row: 1, column: 0 },
+			{ row: 0, column: -1 }
+		];
+
+		let nextNeighbors = neighbors.map((neighbor) => ({ ...neighbor }));
+
+		const hiddenNeighbor = {
+			row: hiddenRow,
+			column: hiddenColumn
+		};
+
+		for (let i = 0; i < moves; i++) {
+			const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
+
+			const availableNeighbors = nextNeighbors.filter(
+				({ row, column }) => grid[hiddenRow + row] && grid[hiddenRow + row][hiddenColumn + column]
+			);
+			const { row: neighborRow, column: neighborColumn } =
+				availableNeighbors[Math.floor(Math.random() * availableNeighbors.length)];
+
+			const row = hiddenRow + neighborRow;
+			const column = hiddenColumn + neighborColumn;
+
+			grid[row][column].row = hiddenRow;
+			grid[row][column].column = hiddenColumn;
+			grid[hiddenRow][hiddenColumn].row = row;
+			grid[hiddenRow][hiddenColumn].column = column;
+
+			[grid[row][column], grid[hiddenRow][hiddenColumn]] = [
+				grid[hiddenRow][hiddenColumn],
+				grid[row][column]
+			];
+
+			hiddenNeighbor.row = row;
+			hiddenNeighbor.column = column;
+
+			nextNeighbors = neighbors.filter(
+				({ row, column }) => row !== neighborRow * -1 || column !== neighborColumn * -1
+			);
+		}
+
+		return { grid, hiddenNeighbor };
+	};
+
+	const resetPuzzle = async ({ row, column }) => {
+		isSolved = false;
+		const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
+		grid[hiddenRow][hiddenColumn].hidden = false;
+		grid[row][column].hidden = true;
+		reveal.set(0, { duration: 300, easing: cubicInOut });
+		await scale.set(0, { duration: 500 });
+		const index = row * size + column;
+		const { grid: newGrid, hiddenNeighbor: newHiddenNeighbor } = getPuzzle({ size, index, moves });
+		grid = newGrid;
+		hiddenNeighbor = newHiddenNeighbor;
+		await scale.set(1);
+		isSliding = false;
+		slide.set(0, { duration: 0 });
 	};
 
 	const handleKeydown = ({ event, row, column }) => {
@@ -170,13 +190,20 @@
 			}
 		}
 	};
+
+	let { grid, hiddenNeighbor } = getPuzzle({ size, index, moves });
+	let isSliding = false;
+	let isSolved = false;
 </script>
 
 <svg
+	class:solved={isSolved}
 	viewBox="-0.55 -0.55 {size + 0.1} {size + 0.1}"
 	class="focusable"
 	tabindex="0"
-	aria-label="Slide the tiles so that the colors are in the correct order. Focus on a tile and press enter or one of the possible arrow keys to change its position."
+	aria-label={isSolved
+		? 'Slide the tiles so that the colors are in the correct order. Focus on a tile and press enter or one of the possible arrow keys to change its position.'
+		: 'Play a new round of rainbox sixteen. Focus on a tile and press enter to hide the matching color.'}
 >
 	<g class="focus" opacity="0">
 		<rect
@@ -190,31 +217,73 @@
 			stroke-width="0.05"
 		/>
 	</g>
-	{#each puzzle.reduce((acc, curr) => [...acc, ...curr], []) as { row, column, color: fill, hidden }}
+
+	{#each grid.reduce((acc, curr) => [...acc, ...curr], []) as { row, column, color: fill, hidden }}
 		<g transform="translate({column} {row})">
-			{#if hidden}
-				<g transform="scale({$reveal})">
-					<rect x="-0.45" y="-0.45" width="0.9" height="0.9" {fill} rx="0.2" />
-				</g>
-			{:else}
+			<g transform="scale({$scale})">
+				{#if hidden}
+					<g transform="scale({$reveal})">
+						<rect x="-0.45" y="-0.45" width="0.9" height="0.9" {fill} rx="0.2" />
+					</g>
+				{:else}
+					<g
+						style:cursor={!isSliding && hasHiddenNeighbor({ row, column }) ? 'pointer' : 'initial'}
+						on:click={() => {
+							if (!isSliding && hasHiddenNeighbor({ row, column })) {
+								updatePuzzle({ row, column });
+							}
+						}}
+						class="focusable"
+						role="button"
+						aria-label="Row {row + 1} and column {column + 1}. Color {fill}."
+						tabindex={!isSliding && hasHiddenNeighbor({ row, column }) ? '0' : '-1'}
+						on:keydown={(event) => {
+							if (!isSliding && hasHiddenNeighbor({ row, column })) {
+								handleKeydown({ event, row, column });
+							}
+						}}
+					>
+						<rect x="-0.45" y="-0.45" width="0.9" height="0.9" {fill} rx="0.15" />
+						<g class="focus" opacity="0">
+							<rect
+								x="-0.45"
+								y="-0.45"
+								width="0.9"
+								height="0.9"
+								rx="0.15"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="0.05"
+							/>
+						</g>
+					</g>
+				{/if}
+			</g>
+		</g>
+	{/each}
+
+	{#if isSolved}
+		{#each grid.reduce((acc, curr) => [...acc, ...curr], []) as { row, column, color: fill }}
+			<g transform="translate({column} {row})">
 				<g
-					style:cursor={!isSliding && hasHiddenNeighbor({ row, column }) ? 'pointer' : 'initial'}
+					style:cursor="pointer"
 					on:click={() => {
-						if (!isSliding && hasHiddenNeighbor({ row, column })) {
-							updatePuzzle({ row, column });
-						}
+						resetPuzzle({ row, column });
 					}}
 					class="focusable"
 					role="button"
 					aria-label="Row {row + 1} and column {column + 1}. Color {fill}."
-					tabindex={!isSliding && hasHiddenNeighbor({ row, column }) ? '0' : '-1'}
+					tabindex="0"
 					on:keydown={(event) => {
-						if (!isSliding && hasHiddenNeighbor({ row, column })) {
-							handleKeydown({ event, row, column });
+						const { key } = event;
+						if (key === 'Enter') {
+							event.preventDefault();
+
+							resetPuzzle({ row, column });
 						}
 					}}
 				>
-					<rect x="-0.45" y="-0.45" width="0.9" height="0.9" {fill} rx="0.15" />
+					<rect opacity="0" x="-0.45" y="-0.45" width="0.9" height="0.9" rx="0.15" />
 					<g class="focus" opacity="0">
 						<rect
 							x="-0.45"
@@ -228,9 +297,9 @@
 						/>
 					</g>
 				</g>
-			{/if}
-		</g>
-	{/each}
+			</g>
+		{/each}
+	{/if}
 </svg>
 
 <style>
