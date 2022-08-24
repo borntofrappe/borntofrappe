b2a3d3abb42f30791e9c27fb15b268ec431d05a1
borntofrappe <borntofrappe@gmail.com>
2022-08-24 18:26:42 +0200

    refactor: :art: export getPuzzle function
    create a function to initialize the puzzle and its two main parts: grid and hiddenTile; with svelte update grid and hiddenTile directly to modify the markup which follows

--- src/lib/components/rainbow-sixteen/Puzzle.svelte
+++ src/lib/components/rainbow-sixteen/Puzzle.svelte
@@ -1,22 +1,26 @@
 <script>
 	import { tweened } from 'svelte/motion';
-	import { cubicInOut, backOut } from 'svelte/easing';
+	import { cubicInOut as easing, backOut } from 'svelte/easing';
 
-	import { getColorHsv as getColor } from './utils.js';
+	import { getPuzzle } from './utils.js';
 
 	export let size = 4;
 	export let index = size ** 2 - 1;
 	export let moves = 4;
 
+	let { grid, hiddenTile } = getPuzzle({ size, index, moves });
+	let isSliding = false;
+	let isSolved = false;
+
 	const slideDuration = 125;
-	const slide = tweened(0, { duration: slideDuration, easing: cubicInOut });
+	const slide = tweened(0, { duration: slideDuration, easing });
 
 	const sizeDuration = 400;
-	const reveal = tweened(0, { duration: sizeDuration, easing: backOut });
-	const scale = tweened(1, { duration: sizeDuration, easing: cubicInOut });
+	const reveal = tweened(0, { duration: sizeDuration, easing });
+	const scale = tweened(1, { duration: sizeDuration, easing });
 
 	const hasHiddenNeighbor = ({ row, column }) => {
-		const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
+		const { row: hiddenRow, column: hiddenColumn } = hiddenTile;
 		return (
 			(Math.abs(row - hiddenRow) === 1 && column === hiddenColumn) ||
 			(Math.abs(column - hiddenColumn) === 1 && row === hiddenRow)
@@ -30,7 +34,7 @@
 
 		isSliding = true;
 
-		const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
+		const { row: hiddenRow, column: hiddenColumn } = hiddenTile;
 		const offsetRow = hiddenRow - row;
 		const offsetColumn = hiddenColumn - column;
 
@@ -53,15 +57,14 @@
 			grid[row][column]
 		];
 
-		hiddenNeighbor.row = row;
-		hiddenNeighbor.column = column;
+		hiddenTile = { row, column };
 
 		const hasSolved = grid
 			.reduce((acc, curr) => [...acc, ...curr], [])
 			.every(({ row, column, initial }) => row === initial.row && column === initial.column);
 
 		if (hasSolved) {
-			await reveal.set(1);
+			await reveal.set(1, { easing: backOut });
 			isSolved = true;
 		} else {
 			slide.set(0, { duration: 0 });
@@ -69,91 +72,20 @@
 		}
 	};
 
-	const getPuzzle = ({ size, index, moves }) => {
-		const n = size * 2 - 1;
-		const grid = Array(size)
-			.fill()
-			.map((_, row) => {
-				return Array(size)
-					.fill()
-					.map((_, column) => {
-						const color = getColor({ h: (360 * (column + row)) / n });
-
-						return {
-							row,
-							column,
-							color,
-							hidden: false,
-							initial: {
-								row,
-								column
-							}
-						};
-					});
-			});
-
-		const hiddenRow = Math.floor(index / size);
-		const hiddenColumn = index % size;
-		grid[hiddenRow][hiddenColumn].hidden = true;
-
-		const neighbors = [
-			{ row: -1, column: 0 },
-			{ row: 0, column: 1 },
-			{ row: 1, column: 0 },
-			{ row: 0, column: -1 }
-		];
-
-		let nextNeighbors = neighbors.map((neighbor) => ({ ...neighbor }));
-
-		const hiddenNeighbor = {
-			row: hiddenRow,
-			column: hiddenColumn
-		};
-
-		for (let i = 0; i < moves; i++) {
-			const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
-
-			const availableNeighbors = nextNeighbors.filter(
-				({ row, column }) => grid[hiddenRow + row] && grid[hiddenRow + row][hiddenColumn + column]
-			);
-			const { row: neighborRow, column: neighborColumn } =
-				availableNeighbors[Math.floor(Math.random() * availableNeighbors.length)];
-
-			const row = hiddenRow + neighborRow;
-			const column = hiddenColumn + neighborColumn;
-
-			grid[row][column].row = hiddenRow;
-			grid[row][column].column = hiddenColumn;
-			grid[hiddenRow][hiddenColumn].row = row;
-			grid[hiddenRow][hiddenColumn].column = column;
-
-			[grid[row][column], grid[hiddenRow][hiddenColumn]] = [
-				grid[hiddenRow][hiddenColumn],
-				grid[row][column]
-			];
-
-			hiddenNeighbor.row = row;
-			hiddenNeighbor.column = column;
-
-			nextNeighbors = neighbors.filter(
-				({ row, column }) => row !== neighborRow * -1 || column !== neighborColumn * -1
-			);
-		}
-
-		return { grid, hiddenNeighbor };
-	};
-
 	const resetPuzzle = async ({ row, column }) => {
 		isSolved = false;
-		const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
+		const { row: hiddenRow, column: hiddenColumn } = hiddenTile;
 		grid[hiddenRow][hiddenColumn].hidden = false;
 		grid[row][column].hidden = true;
-		reveal.set(0, { duration: 300, easing: cubicInOut });
+
+		reveal.set(0, { duration: 300 });
 		await scale.set(0, { duration: 500 });
+
 		const index = row * size + column;
-		const { grid: newGrid, hiddenNeighbor: newHiddenNeighbor } = getPuzzle({ size, index, moves });
-		grid = newGrid;
-		hiddenNeighbor = newHiddenNeighbor;
+		const puzzle = getPuzzle({ size, index, moves });
+		grid = puzzle.grid;
+		hiddenTile = puzzle.hiddenTile;
+
 		await scale.set(1);
 		isSliding = false;
 		slide.set(0, { duration: 0 });
@@ -171,29 +103,25 @@
 
 			updatePuzzle({ row, column });
 		} else {
-			const neighbors = {
+			const offsets = {
 				ArrowUp: { row: -1, column: 0 },
 				ArrowRight: { row: 0, column: 1 },
 				ArrowDown: { row: 1, column: 0 },
 				ArrowLeft: { row: 0, column: -1 }
 			};
 
-			const neighbor = neighbors[key];
+			const offset = offsets[key];
 
-			if (neighbor) {
+			if (offset) {
 				event.preventDefault();
-				const { row: neighborRow, column: neighborColumn } = neighbor;
-				const { row: hiddenRow, column: hiddenColumn } = hiddenNeighbor;
-				if (row === hiddenRow - neighborRow && column === hiddenColumn - neighborColumn) {
+				const { row: offsetRow, column: offsetColumn } = offset;
+				const { row: hiddenRow, column: hiddenColumn } = hiddenTile;
+				if (row === hiddenRow - offsetRow && column === hiddenColumn - offsetColumn) {
 					updatePuzzle({ row, column });
 				}
 			}
 		}
 	};
-
-	let { grid, hiddenNeighbor } = getPuzzle({ size, index, moves });
-	let isSliding = false;
-	let isSolved = false;
 </script>
 
 <svg
@@ -211,7 +139,7 @@
 			y="-0.5"
 			width={size}
 			height={size}
-			rx="0.15"
+			rx="0.2"
 			fill="none"
 			stroke="currentColor"
 			stroke-width="0.05"
@@ -223,7 +151,7 @@
 			<g transform="scale({$scale})">
 				{#if hidden}
 					<g transform="scale({$reveal})">
-						<rect x="-0.45" y="-0.45" width="0.9" height="0.9" {fill} rx="0.2" />
+						<rect x="-0.45" y="-0.45" width="0.9" height="0.9" {fill} rx="0.15" />
 					</g>
 				{:else}
 					<g
--- src/lib/components/rainbow-sixteen/utils.js
+++ src/lib/components/rainbow-sixteen/utils.js
@@ -6,3 +6,77 @@ export const getColorHsv = ({ h = 0, s = 0.55, v = 0.92 } = {}) => {
 	const sl = l === 0 || l === 1 ? 0 : (v - l) / Math.min(l, 1 - l);
 	return `hsl(${h}, ${sl * 100}%, ${l * 100}%)`;
 };
+
+export const getPuzzle = ({ size, index, moves }) => {
+	const n = size * 2 - 1;
+	const grid = Array(size)
+		.fill()
+		.map((_, row) => {
+			return Array(size)
+				.fill()
+				.map((_, column) => {
+					const color = getColorHsv({ h: (360 * (column + row)) / n });
+
+					return {
+						row,
+						column,
+						color,
+						hidden: false,
+						initial: {
+							row,
+							column
+						}
+					};
+				});
+		});
+
+	const hiddenRow = Math.floor(index / size);
+	const hiddenColumn = index % size;
+	grid[hiddenRow][hiddenColumn].hidden = true;
+
+	const offsets = [
+		{ row: -1, column: 0 },
+		{ row: 0, column: 1 },
+		{ row: 1, column: 0 },
+		{ row: 0, column: -1 }
+	];
+
+	let nextOffsets = [...offsets];
+
+	const hiddenTile = {
+		row: hiddenRow,
+		column: hiddenColumn
+	};
+
+	for (let i = 0; i < moves; i++) {
+		const { row: hiddenRow, column: hiddenColumn } = hiddenTile;
+
+		const availableOffsets = nextOffsets.filter(
+			({ row, column }) => grid[hiddenRow + row] && grid[hiddenRow + row][hiddenColumn + column]
+		);
+		const { row: neighborRow, column: neighborColumn } =
+			availableOffsets[Math.floor(Math.random() * availableOffsets.length)];
+
+		const row = hiddenRow + neighborRow;
+		const column = hiddenColumn + neighborColumn;
+
+		grid[row][column].row = hiddenRow;
+		grid[row][column].column = hiddenColumn;
+		grid[hiddenRow][hiddenColumn].row = row;
+		grid[hiddenRow][hiddenColumn].column = column;
+
+		[grid[row][column], grid[hiddenRow][hiddenColumn]] = [
+			grid[hiddenRow][hiddenColumn],
+			grid[row][column]
+		];
+
+		hiddenTile.row = row;
+		hiddenTile.column = column;
+
+		nextOffsets = [...offsets].filter(
+			({ row, column }) => row !== neighborRow * -1 || column !== neighborColumn * -1
+		);
+	}
+
+	return { grid, hiddenTile };
+};
