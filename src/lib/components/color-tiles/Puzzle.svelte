c0484af6b88ed40281961ba6e091823d4a0058c0
borntofrappe <borntofrappe@gmail.com>
2022-08-25 21:24:17 +0200

    feat: :sparkles: add two decks of tiles
    create two decks, one for each player and each on a side of the grid; the idea is to ultimately play multiple rounds and have the different color start first once

--- src/lib/components/color-tiles/Puzzle.svelte
+++ src/lib/components/color-tiles/Puzzle.svelte
@@ -10,15 +10,19 @@
 	const d = 4;
 	const strokeWidth = 0;
 
-	const whitespace = {
-		top: 0,
-		right: w,
-		bottom: 0,
-		left: w
-	};
+	const whitespaceX = w;
+	const whitespaceY = 0;
 
-	const width = w * 2 * size + (whitespace.left + whitespace.right);
-	const height = h * 2 * size + d + (whitespace.top + whitespace.bottom);
+	const gridWidth = w * 2 * size;
+	const gridHeight = h * 2 * size;
+
+	const viewBoxWidth = gridWidth + strokeWidth + whitespaceX * 2;
+	const viewBoxHeight = gridHeight + strokeWidth + d + whitespaceY * 2;
+	const viewBoxX = (viewBoxWidth / 2) * -1;
+	const viewBoxY = ((strokeWidth + whitespaceY * 2) / 2) * -1;
+
+	const defaultColor = colors[colors.length - 1];
+	const deckColors = Math.random() > 0.5 ? [colors[0], colors[1]] : [colors[1], colors[0]];
 
 	const grid = Array(size)
 		.fill()
@@ -38,19 +42,36 @@
 				})
 		);
 
-	let deck = Array(size ** 2)
+	const decks = Array(2)
 		.fill()
 		.map((_, i) => {
-			const color = i % 2 === 0 ? colors[0] : colors[1];
+			const deckColor = i === 0 ? deckColors[0] : deckColors[1];
+			return Array(size % 2 === 0 ? size ** 2 : size ** 2 + 1)
+				.fill()
+				.map((_, j) => {
+					let color;
+					if (j % 2 === 0) {
+						color = deckColor;
+					} else {
+						color = deckColor === colors[0] ? colors[1] : colors[0];
+					}
 
-			const x = (width / 2) * -1 + w / 2;
-			const y = height - ((i + 1) * d) / 2 - (h + d) / 2;
-			return {
-				x,
-				y,
-				color
-			};
+					const x =
+						i === 0
+							? (gridWidth / 2) * -1 + w / 2 - whitespaceX
+							: gridWidth / 2 - w / 2 + whitespaceX;
+					const y = gridHeight - (j * d) / 2 - h / 2;
+
+					return {
+						x,
+						y,
+						color
+					};
+				});
 		});
+
+	let iDeck = Math.floor(Math.random() * decks.length);
+	let deck = [...decks[iDeck]];
 
 	const tile = deck[deck.length - 1];
 	const { x, y } = tile;
@@ -67,8 +88,6 @@
 		},
 		{ duration, easing }
 	);
-
-	const defaultColor = colors[colors.length - 1];
 
 	const updateGrid = ({ row, column }) => {
 		grid[row][column].color = color;
@@ -90,8 +109,9 @@
 </script>
 
 <svg
-	viewBox="{((width + strokeWidth) / 2) * -1} {(strokeWidth / 2) * -1} {width +
-		strokeWidth} {height + strokeWidth}"
+	viewBox="{viewBoxX} {viewBoxY} {viewBoxWidth} {viewBoxHeight}"
+	class="focusable"
+	style:outline="none"
 	tabindex="0"
 	aria-label="Position tiles in the grid to create a line with the same color. Focus on a tile and press enter to position the piece."
 >
@@ -151,6 +171,16 @@
 			</g>
 		{/each}
 	</g>
+
+	<g style:pointer-events="none" class="focus" opacity="0">
+		<path
+			fill={defaultColor}
+			d="M {(gridWidth / 2) * -1} {gridHeight / 2} l 0 {d} {gridWidth / 2} {gridHeight /
+				2} {gridWidth / 2} {(gridHeight / 2) * -1} 0 {d * -1} {(gridWidth / 2) * -1} {gridHeight /
+				2}z"
+		/>
+	</g>
+
 	<g stroke="currentColor" stroke-width={strokeWidth}>
 		{#each deck as { x, y, color: fill }}
 			<g transform="translate({x} {y})">
@@ -171,14 +201,18 @@
 		display: block;
 	}
 
-	svg:focus:not(:focus-visible) {
-		outline: none;
+	.focusable:focus > .focus {
+		opacity: 1;
+	}
+	.focusable:not(:focus-visible):focus > .focus {
+		opacity: 0;
 	}
 
 	.tile {
 		cursor: pointer;
 	}
 
+	.focus,
 	.tile:hover,
 	.tile:focus {
 		filter: brightness(1.25);
