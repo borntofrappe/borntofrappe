ba239924260df95c8dc01dc5cc10a2f6a0979a8b
borntofrappe <borntofrappe@gmail.com>
2022-08-30 22:39:52 +0200

    feat: :tada: add minesweeper route
    create +page.svelte component for the minesweeper route; include a Puzzle component from the lib/components sub-folder

---
+++ src/lib/components/minesweeper/Puzzle.svelte
@@ -0,0 +1,1 @@
+<h1>Minesweeper</h1>
---
+++ src/routes/minesweeper/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Puzzle from '$lib/components/minesweeper/Puzzle.svelte';
+</script>
+
+<svelte:head>
+	<title>borntofrappe | Minesweeper</title>
+	<meta name="description" content="Play a nostalgia-filled version of a classic puzzle game." />
+	<link rel="icon" href="/icons/minesweeper.svg" type="image/svg+xml" />
+</svelte:head>
+
+<article>
+	<Puzzle />
+</article>
+
+<style>
+	article {
+		color: hsl(0, 0%, 2%);
+		background: hsl(0, 0%, 75%);
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	article > :global(svg) {
+		display: block;
+		width: 90vmin;
+		max-width: 36rem;
+	}
+</style>
