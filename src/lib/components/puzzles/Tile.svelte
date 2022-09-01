51bb7d7d854485093449afa4839f3a3e7c45de4f
borntofrappe <borntofrappe@gmail.com>
2022-09-01 15:02:16 +0200

    feat: :tada: add puzzles route
    create `/puzzles` route; introduce the page with a header and a custom heading element; the page is devoted to puzzles, loosely connected to the tiles of a board game; for the heading use a custom svg visual to break the text into tiles

---
+++ src/lib/components/puzzles/Tile.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let char = 't';
+	export let n = '';
+
+	export let width = 'auto';
+	export let height = 'auto';
+
+	export let tile = '#fdd4ce';
+	export let shadow = '#fea69c';
+
+	export let text = '#f2eeef';
+	export let outline = '#07093a';
+</script>
+
+<svg viewBox="0 0 1 1" {width} {height}>
+	<rect width="1" height="1" rx="0.1" fill={shadow} />
+	<rect width="1" height="0.9" rx="0.1" fill={tile} />
+
+	<text
+		fill={text}
+		stroke={outline}
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		paint-order="stroke"
+		stroke-width="0.07"
+		x="0.5"
+		y="0.45"
+		text-anchor="middle"
+		dominant-baseline="central"
+		font-weight="700"
+		font-size="0.45"
+	>
+		{char}
+	</text>
+
+	<text
+		style:user-select="none"
+		fill={outline}
+		x="0.925"
+		y="0.775"
+		text-anchor="end"
+		dominant-baseline="central"
+		font-weight="400"
+		font-size="0.12"
+	>
+		{n}
+	</text>
+</svg>
---
+++ src/routes/puzzles/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import TextTile from '$lib/components/puzzles/TextTile.svelte';
+</script>
+
+<svelte:head>
+	<title>borntofrappe | Puzzles</title>
+	<meta
+		name="description"
+		content="Enjoy a few interactive, infinitely replayable, inconsistently merciful puzzles."
+	/>
+	<link rel="icon" href="/icons/puzzles.svg" type="image/svg+xml" />
+</svelte:head>
+
+<div>
+	<header>
+		<TextTile element="h1" text="Puzzles" />
+	</header>
+</div>
+
+<style>
+	div {
+		color: hsl(238, 78%, 13%);
+		background: hsl(8, 83%, 54%);
+		font-family: Source Code Pro, Menlo, Consolas, Monaco, Liberation Mono, Lucida Console,
+			monospace;
+	}
+
+	div {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	header {
+		max-width: 52rem;
+	}
+</style>
