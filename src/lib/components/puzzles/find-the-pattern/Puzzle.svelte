c9d5117f2701c7082d8076d33c1dc7c1174a36ad
borntofrappe <borntofrappe@gmail.com>
2022-09-03 14:55:18 +0200

    feat: :sparkles: add player interaction
    allow to interact with the svg with mouse, keyboard, or again touch controls; with a mouse track when the mouse is down and when the mouse enters on a tile (here you'd check for a match); with keyboard track the enter key to set focus, the arrow keys to trace a pattern (use the focus event to remove the focused pattern, add preventDefault to mousedown to avoid the same for mouse events); with touch track only when a touch is registered, considering two successive touches as the start and end

--- src/lib/components/puzzles/find-the-pattern/Puzzle.svelte
+++ src/lib/components/puzzles/find-the-pattern/Puzzle.svelte
@@ -8,9 +8,100 @@
 	export let minimum = 5;
 
 	const puzzle = getPuzzle({ size, pattern, values, minimum });
+
+	let focus = null;
+
+	const handleStart = ({ row, column }) => {
+		focus = {
+			rowStart: row,
+			columnStart: column,
+			rowEnd: row,
+			columnEnd: column
+		};
+	};
+
+	const handleEnd = () => {
+		focus = null;
+	};
+
+	const handleIng = ({ row, column }) => {
+		if (!focus || (focus.rowEnd === row && focus.columnEnd === column)) return;
+
+		focus.rowEnd = row;
+		focus.columnEnd = column;
+		// TODO: highlight the possible match
+	};
+
+	const handleTouch = ({ row, column }) => {
+		if (!focus) {
+			focus = {
+				rowStart: row,
+				columnStart: column,
+				rowEnd: row,
+				columnEnd: column
+			};
+		} else {
+			const { rowEnd, columnEnd } = focus;
+			if (row !== rowEnd || column !== columnEnd) {
+				focus.rowEnd = row;
+				focus.columnEnd = column;
+
+				// TODO: highlight the possible match
+				focus = null;
+			}
+		}
+	};
+
+	const handleFocus = () => {
+		focus = null;
+	};
+
+	const handleKeydown = ({ event, row, column }) => {
+		const { key } = event;
+
+		if (key === 'Enter') {
+			event.preventDefault();
+
+			focus = focus
+				? null
+				: {
+						rowStart: row,
+						columnStart: column,
+						rowEnd: row,
+						columnEnd: column
+				  };
+		} else if (key === 'Escape') {
+			focus = null;
+		} else if (focus) {
+			const neighbors = {
+				ArrowUp: { row: -1, column: 0 },
+				ArrowRight: { row: 0, column: 1 },
+				ArrowDown: { row: 1, column: 0 },
+				ArrowLeft: { row: 0, column: -1 }
+			};
+
+			const neighbor = neighbors[key];
+
+			if (neighbor) {
+				event.preventDefault();
+
+				const { row: rowNeighbor, column: columnNeighbor } = neighbor;
+				const { rowEnd, columnEnd } = focus;
+				const row = rowEnd + rowNeighbor;
+				const column = columnEnd + columnNeighbor;
+
+				if (row >= 0 && row < size && column >= 0 && column < size) {
+					focus.rowEnd = row;
+					focus.columnEnd = column;
+
+					// TODO: highlight the possible match
+				}
+			}
+		}
+	};
 </script>
 
-<svg viewBox="-0.5 -0.5 {size} {size + 1}">
+<svg on:mouseleave={handleEnd} on:mouseup={handleEnd} viewBox="-0.5 -0.5 {size} {size + 1}">
 	<g>
 		<g transform="translate(-0.34 -0.34)">
 			<Tile
@@ -55,26 +146,75 @@
 	</g>
 
 	<g transform="translate(0 1)">
-		{#each puzzle.grid.reduce((acc, curr) => [...acc, ...curr], []) as { column, row, value }}
-			<g transform="translate({column} {row})">
-				<g transform="translate(-0.34 -0.34)">
-					<Tile
-						tile="var(--color-focus, hsl(345, 13%, 94%))"
-						shadow="var(--color-shadow, hsl(6, 98%, 80%))"
-						text="var(--color-focus, hsl(345, 13%, 94%))"
-						outline="var(--color-text, hsl(19, 56%, 12%))"
-						width={0.68}
-						height={0.68}
-						char={value.toString()}
-					/>
+		{#if focus}
+			<path
+				fill="none"
+				stroke="var(--color-focus, hsl(345, 13%, 94%))"
+				stroke-width="1"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M {focus.columnStart} {focus.rowStart} {focus.columnEnd} {focus.rowEnd}"
+				opacity="0.4"
+			/>
+		{/if}
+		<g>
+			{#each puzzle.grid.reduce((acc, curr) => [...acc, ...curr], []) as { column, row, value }}
+				<g transform="translate({column} {row})">
+					<g
+						style:cursor="pointer"
+						on:mousedown|preventDefault={() => {
+							handleStart({ column, row });
+						}}
+						on:mouseenter={() => {
+							handleIng({ column, row });
+						}}
+						on:touchstart|preventDefault={() => {
+							handleTouch({ column, row });
+						}}
+						tabindex="0"
+						role="button"
+						aria-label="Row {row + 1} and column {column + 1}, with a  value of {value}."
+						style:outline="none"
+						class="focusable"
+						on:focus={handleFocus}
+						on:keydown={(event) => {
+							handleKeydown({ event, column, row });
+						}}
+					>
+						<g class="focus" opacity="0">
+							<circle r="0.5" fill="var(--color-focus, hsl(345, 13%, 94%))" opacity="0.4" />
+						</g>
+
+						<g transform="translate(-0.34 -0.34)">
+							<Tile
+								tile="var(--color-focus, hsl(345, 13%, 94%))"
+								shadow="var(--color-shadow, hsl(6, 98%, 80%))"
+								text="var(--color-focus, hsl(345, 13%, 94%))"
+								outline="var(--color-text, hsl(19, 56%, 12%))"
+								width={0.68}
+								height={0.68}
+								char={value.toString()}
+							/>
+							<!-- <rect width="0.68" height="0.68" opacity="0" /> -->
+						</g>
+					</g>
 				</g>
-			</g>
-		{/each}
+			{/each}
+		</g>
 	</g>
 </svg>
 
 <style>
 	svg {
 		display: block;
+		user-select: none;
+	}
+
+	.focusable:focus > .focus {
+		opacity: 1;
+	}
+
+	.focusable:focus:not(:focus-visible) > .focus {
+		opacity: 0;
 	}
 </style>
