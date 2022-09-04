a518903590d490853cce4d16246ff115f09c06d7
borntofrappe <borntofrappe@gmail.com>
2022-09-04 21:41:58 +0200

    feat: :sparkles: design latin square
    add values in a latin square; each row and column must be unique, contain one instance of each value, avoid repeated values; create the square at random producing values in a while loop and add an "escape hatch" to avoid running the loop more than an arbitrary number of times

--- src/lib/components/puzzles/more-or-less/utils.js
+++ src/lib/components/puzzles/more-or-less/utils.js
@@ -1,3 +1,102 @@
+const latinSquares = {
+	3: [
+		[
+			[3, 2, 1],
+			[1, 3, 2],
+			[2, 1, 3]
+		],
+		[
+			[3, 2, 1],
+			[1, 3, 2],
+			[2, 1, 3]
+		],
+		[
+			[3, 1, 2],
+			[2, 3, 1],
+			[1, 2, 3]
+		],
+		[
+			[2, 1, 3],
+			[3, 2, 1],
+			[1, 3, 2]
+		],
+		[
+			[2, 3, 1],
+			[1, 2, 3],
+			[3, 1, 2]
+		]
+	],
+	4: [
+		[
+			[1, 4, 2, 3],
+			[4, 3, 1, 2],
+			[3, 2, 4, 1],
+			[2, 1, 3, 4]
+		],
+		[
+			[4, 1, 2, 3],
+			[3, 2, 1, 4],
+			[1, 4, 3, 2],
+			[2, 3, 4, 1]
+		],
+		[
+			[2, 1, 4, 3],
+			[4, 3, 2, 1],
+			[1, 2, 3, 4],
+			[3, 4, 1, 2]
+		],
+		[
+			[2, 4, 1, 3],
+			[1, 3, 2, 4],
+			[3, 2, 4, 1],
+			[4, 1, 3, 2]
+		],
+		[
+			[4, 2, 1, 3],
+			[2, 3, 4, 1],
+			[3, 1, 2, 4],
+			[1, 4, 3, 2]
+		]
+	],
+	5: [
+		[
+			[5, 3, 1, 4, 2],
+			[1, 5, 2, 3, 4],
+			[4, 2, 5, 1, 3],
+			[3, 1, 4, 2, 5],
+			[2, 4, 3, 5, 1]
+		],
+		[
+			[5, 3, 4, 2, 1],
+			[1, 4, 5, 3, 2],
+			[2, 5, 1, 4, 3],
+			[4, 2, 3, 1, 5],
+			[3, 1, 2, 5, 4]
+		],
+		[
+			[2, 5, 3, 1, 4],
+			[3, 2, 4, 5, 1],
+			[1, 4, 2, 3, 5],
+			[4, 1, 5, 2, 3],
+			[5, 3, 1, 4, 2]
+		],
+		[
+			[3, 5, 4, 2, 1],
+			[5, 3, 2, 1, 4],
+			[4, 1, 5, 3, 2],
+			[1, 2, 3, 4, 5],
+			[2, 4, 1, 5, 3]
+		],
+		[
+			[3, 1, 2, 5, 4],
+			[5, 2, 3, 4, 1],
+			[1, 3, 4, 2, 5],
+			[4, 5, 1, 3, 2],
+			[2, 4, 5, 1, 3]
+		]
+	]
+};
+
 const shuffle = (array) => {
 	const a = [...array];
 	let counter = a.length;
@@ -17,19 +116,71 @@ const shuffle = (array) => {
 };
 
 export const getPuzzle = ({ size = 4 }) => {
+	if (!latinSquares[size])
+		throw new Error(
+			`\`size\` not supported. Value must be one of the following: ${Object.keys(latinSquares).join(
+				', '
+			)}.`
+		);
+
 	const values = Array(size)
 		.fill()
 		.map((_, i) => i + 1);
 
-	const { length: valuesLength } = values;
+	const repeats = new RegExp(/(.)(.+)?\1/);
+	const duplicates = new RegExp(/(.)\1/g);
 
-	const grid = Array(size)
-		.fill()
-		.map((_, row) =>
+	let latinSquare = [];
+	let counter = 0;
+	const threshold = 1000;
+
+	while (latinSquare.length < size) {
+		counter++;
+		if (counter > threshold) {
+			latinSquare = latinSquares[size][Math.floor(Math.random() * latinSquares[size].length)];
+			break;
+		}
+
+		const row = shuffle(values);
+
+		if (latinSquare.length > 0 && latinSquare.find((d) => d.join('') === row.join(''))) {
+			continue;
+		}
+
+		if (
+			latinSquare.length > 0 &&
 			Array(size)
 				.fill()
-				.map((_, column) => values[Math.floor(Math.random() * valuesLength)])
-		);
+				.map((_, i) =>
+					Array(latinSquare.length + 1)
+						.fill()
+						.map((_, j) => [...latinSquare, row][latinSquare.length - j][i])
+						.join('')
+				)
+				.find((d) => repeats.test(d))
+		) {
+			continue;
+		}
 
-	return { grid };
+		if (
+			latinSquare.length === size - 1 &&
+			duplicates.test(
+				Array(size)
+					.fill()
+					.map((_, i) =>
+						Array(size)
+							.fill()
+							.map((_, j) => [...latinSquare, row][latinSquare.length - j][i])
+							.join('')
+					)
+					.join('')
+			)
+		) {
+			continue;
+		}
+
+		latinSquare.push(row);
+	}
+
+	return { grid: latinSquare };
 };
