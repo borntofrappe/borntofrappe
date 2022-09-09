90844b64711b948fe6d21717b8f6d6c52616fb88
Gabriele Corti <borntofrappe@gmail.com>
2022-09-09 14:48:47 +0200

    feat: initialize grid
    add two cells with either a 2 or 4 to introduce the game; use tiles array for the background

--- src/lib/components/puzzles/two-to-the-power/Puzzle.svelte
+++ src/lib/components/puzzles/two-to-the-power/Puzzle.svelte
@@ -2,24 +2,69 @@
 	import Tile from '../Tile.svelte';
 
 	export let size = 5;
+
+	const tiles = Array(size ** 2)
+		.fill()
+		.map((_, i) => ({
+			row: Math.floor(i / size),
+			column: i % size
+		}));
+
+	const getCell = (grid) => {
+		const availableTiles = tiles.filter(({ column, row }) =>
+			grid.every((cell) => cell.column !== column || cell.row !== row)
+		);
+
+		const { row, column } = availableTiles[Math.floor(Math.random() * availableTiles.length)];
+		const value = Math.random() > 0.5 ? 2 : 4;
+
+		return {
+			row,
+			column,
+			value
+		};
+	};
+
+	let grid = [];
+	for (let i = 0; i < 2; i++) {
+		grid = [...grid, getCell(grid)];
+	}
 </script>
 
 <svg viewBox="-0.5 -0.5 {size} {size}">
-	{#each Array(size ** 2) as _, i}
-		{@const column = i % size}
-		{@const row = Math.floor(i / size)}
-		<g transform="translate({column} {row})">
-			<g transform="translate(-0.35 -0.35)">
-				<Tile
-					tile="var(--color-focus, hsl(345, 13%, 94%))"
-					shadow="var(--color-shadow, hsl(6, 98%, 80%))"
-					text="var(--color-focus, hsl(345, 13%, 94%))"
-					outline="var(--color-text, hsl(19, 56%, 12%))"
-					width={0.7}
-					height={0.7}
-					char=""
-				/>
+	<g>
+		{#each tiles as { column, row }}
+			<g transform="translate({column} {row})">
+				<g transform="translate(-0.35 -0.35)">
+					<Tile
+						tile="var(--color-focus, hsl(345, 13%, 94%))"
+						shadow="var(--color-shadow, hsl(6, 98%, 80%))"
+						text="var(--color-focus, hsl(345, 13%, 94%))"
+						outline="var(--color-text, hsl(19, 56%, 12%))"
+						width={0.7}
+						height={0.7}
+						char=""
+					/>
+				</g>
 			</g>
-		</g>
-	{/each}
+		{/each}
+	</g>
+
+	<g>
+		{#each grid as { row, column, value }}
+			<g transform="translate({column} {row})">
+				<g transform="translate(-0.35 -0.35)">
+					<Tile
+						tile="var(--color-tile, hsl(8, 92%, 90%))"
+						shadow="var(--color-shadow, hsl(6, 98%, 80%))"
+						text="var(--color-focus, hsl(345, 13%, 94%))"
+						outline="var(--color-text, hsl(19, 56%, 12%))"
+						width={0.7}
+						height={0.7}
+						char={value.toString()}
+					/>
+				</g>
+			</g>
+		{/each}
+	</g>
 </svg>
