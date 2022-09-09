1e4a36ec6e9d6c6767baa3903cd35ba2dd7fbe72
Gabriele Corti <borntofrappe@gmail.com>
2022-09-09 14:54:30 +0200

    feat: animate tile intro
    use a custom js transition to animate the tiles in scale; it'd be possible to use a custom css transition, but at least on chrome the css causes a blur with respect to the text element

--- src/lib/components/puzzles/two-to-the-power/Puzzle.svelte
+++ src/lib/components/puzzles/two-to-the-power/Puzzle.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Tile from '../Tile.svelte';
+	import { scale } from './utils.js';
 
 	export let size = 5;
 
+	let id = 0;
 	const tiles = Array(size ** 2)
 		.fill()
 		.map((_, i) => ({
@@ -21,7 +23,8 @@
 		return {
 			row,
 			column,
-			value
+			value,
+			id: id++
 		};
 	};
 
@@ -29,7 +32,13 @@
 	for (let i = 0; i < 2; i++) {
 		grid = [...grid, getCell(grid)];
 	}
+
+	const addCell = () => {
+		grid = [...grid, getCell(grid)];
+	};
 </script>
+
+<button on:click={addCell}>Add</button>
 
 <svg viewBox="-0.5 -0.5 {size} {size}">
 	<g>
@@ -51,18 +60,20 @@
 	</g>
 
 	<g>
-		{#each grid as { row, column, value }}
+		{#each grid as { row, column, value, id } (id)}
 			<g transform="translate({column} {row})">
-				<g transform="translate(-0.35 -0.35)">
-					<Tile
-						tile="var(--color-tile, hsl(8, 92%, 90%))"
-						shadow="var(--color-shadow, hsl(6, 98%, 80%))"
-						text="var(--color-focus, hsl(345, 13%, 94%))"
-						outline="var(--color-text, hsl(19, 56%, 12%))"
-						width={0.7}
-						height={0.7}
-						char={value.toString()}
-					/>
+				<g in:scale>
+					<g transform="translate(-0.35 -0.35)">
+						<Tile
+							tile="var(--color-tile, hsl(8, 92%, 90%))"
+							shadow="var(--color-shadow, hsl(6, 98%, 80%))"
+							text="var(--color-focus, hsl(345, 13%, 94%))"
+							outline="var(--color-text, hsl(19, 56%, 12%))"
+							width={0.7}
+							height={0.7}
+							char={value.toString()}
+						/>
+					</g>
 				</g>
 			</g>
 		{/each}
