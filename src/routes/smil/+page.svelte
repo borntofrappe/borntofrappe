ac7e6a3a8181e9b8c1502c41efc23177181d44fe
Gabriele Corti <borntofrappe@gmail.com>
2022-09-16 15:30:27 +0200

    style: update page design
    frame the smil interaction with multiple box-shadows; update article's background-color with inline style and box shadow color with an inline custom property; give a fallback for the box-shadow to match the background color

--- src/routes/smil/+page.svelte
+++ src/routes/smil/+page.svelte
@@ -12,25 +12,32 @@
 <Defs />
 
 <div>
-	<article>
+	<article style:background-color="hsl(356, 89%, 71%)" style:--border-color="hsl(153, 24%, 77%)">
 		<PokeTheLadybug />
 	</article>
 </div>
 
 <style>
 	div {
-		color: #192d10;
-		background: #f7f7f7;
+		color: hsl(101, 48%, 12%);
+		background: hsl(0, 0%, 97%);
 	}
 
 	article {
 		min-height: 100vh;
 		display: grid;
 		place-items: center;
+		--_border-color: var(--border-color, currentColor);
 	}
 
 	article > :global(svg) {
 		display: block;
+		width: 90vmin;
+		width: calc(100vmin - 3rem);
 		max-width: 34rem;
+		height: auto;
+		border-radius: 0.25rem;
+		box-shadow: 0 -1em 1em hsla(0, 0%, 0%, 0.1), 0 0 0 0.05em hsl(0, 0%, 23%),
+			0 0 0 0.95em var(--_border-color), 0 0 0 1em hsl(0, 0%, 23%);
 	}
 </style>
