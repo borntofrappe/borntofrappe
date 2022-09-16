ec305b177942111f3483cec1b460bb6f9a093948
Gabriele Corti <borntofrappe@gmail.com>
2022-09-16 11:08:21 +0200

    feat: draw game visuals
    draw a background with a custom pattern; draw rocks with path elements and repeat the visual ith the use element; draw the ladybug overlapping circle elements

--- src/lib/components/smil/PokeTheLadybug.svelte
+++ src/lib/components/smil/PokeTheLadybug.svelte
@@ -1,3 +1,93 @@
 <svg viewBox="0 0 80 50">
-	<rect fill="#73d23a" width="80" height="50" />
+	<defs>
+		<pattern
+			id="poke-the-ladybug-pattern-grass"
+			viewBox="0 0 10 10"
+			patternUnits="userSpaceOnUse"
+			width="4"
+			height="4"
+		>
+			<rect fill="#73d23a" width="10" height="10" />
+
+			<g fill="#eef7ee">
+				<circle r="0.5" cx="1" cy="2" />
+				<circle r="0.5" cx="1.5" cy="4" />
+				<circle r="0.5" cx="1.5" cy="7.5" />
+				<circle r="0.5" cx="2.5" cy="8.5" />
+				<circle r="0.5" cx="4.5" cy="1" />
+				<circle r="0.5" cx="7" cy="2" />
+				<circle r="0.5" cx="5" cy="3.5" />
+				<circle r="0.5" cx="8" cy="0" />
+				<circle r="0.5" cx="8" cy="10" />
+				<circle r="0.5" cx="0" cy="3" />
+				<circle r="0.5" cx="10" cy="3" />
+				<circle r="0.5" cx="4" cy="5" />
+				<circle r="0.5" cx="6" cy="6" />
+				<circle r="0.5" cx="5" cy="8.5" />
+				<circle r="0.5" cx="8" cy="5" />
+				<circle r="0.5" cx="7.5" cy="9" />
+			</g>
+		</pattern>
+	</defs>
+	<rect fill="url(#poke-the-ladybug-pattern-grass)" width="80" height="50" />
+
+	<g>
+		<g transform="translate(12.5 12.5)">
+			<g id="poke-the-ladybug-rock">
+				<path
+					fill="#bdbebd"
+					stroke="#7b7e7b"
+					stroke-width="0.25"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M 0 0 2 -3 9 -5 12 -4 14 1 12 4 2 4z"
+				/>
+				<path fill="#7b7e7b" d="M 0 0 2 4 12 4 14 1 11.5 2.5 2.5 3" />
+
+				<g fill="#7b7e7b">
+					<circle r="0.2" cx="2" cy="-1" />
+					<circle r="0.2" cx="7" cy="-2" />
+					<circle r="0.2" cx="8" cy="-1.5" />
+					<circle r="0.2" cx="9" cy="-3.5" />
+					<circle r="0.2" cx="10.5" cy="-3.5" />
+					<circle r="0.2" cx="12" cy="-1" />
+					<circle r="0.2" cx="11" cy="0.5" />
+					<circle r="0.2" cx="10" />
+					<circle r="0.2" cx="6.5" />
+					<circle r="0.2" cx="7.5" cy="2" />
+					<circle r="0.2" cx="6" cy="1.5" />
+					<circle r="0.2" cx="3.5" cy="1" />
+				</g>
+			</g>
+		</g>
+		<use href="#poke-the-ladybug-rock" transform="translate(5 17.5)" />
+		<use href="#poke-the-ladybug-rock" transform="translate(60 15)" />
+		<use href="#poke-the-ladybug-rock" transform="translate(25 35) scale(-1 1)" />
+		<use href="#poke-the-ladybug-rock" transform="translate(70 35) scale(-1 1)" />
+		<use href="#poke-the-ladybug-rock" transform="translate(65 40) scale(-1 1)" />
+	</g>
+
+	<g transform="translate(40 25)">
+		<g transform="translate(0 -8)">
+			<circle fill="#192d10" r="6" />
+			<g transform="translate(0 -3.5)">
+				<g fill="#f7f7f7">
+					<circle r="1.25" cx="-2" />
+					<circle r="1.25" cx="2" />
+				</g>
+			</g>
+		</g>
+		<circle fill="#f70000" r="10" />
+		<g fill="#192d10">
+			<circle cx="-5" cy="-1" r="3.6" />
+			<circle cx="5" cy="1" r="3.6" />
+		</g>
+		<path
+			fill="none"
+			stroke="#192d10"
+			stroke-width="0.5"
+			stroke-linecap="square"
+			d="M 0 -9.75 v 19.5"
+		/>
+	</g>
 </svg>
