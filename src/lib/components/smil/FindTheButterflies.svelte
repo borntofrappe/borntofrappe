1b3d7c2d2f54a6ebc857bb6298ca7f88e572f368
Gabriele Corti <borntofrappe@gmail.com>
2022-09-17 15:08:00 +0200

    [refactor] update vector coordinates
    draw game assets from the center with a 16x16 rectangle in mind; allow to scale the visuals so that they occupy less space in the 80x50 viewBox (it would be possible to change the viewBox)

--- src/lib/components/smil/FindTheButterflies.svelte
+++ src/lib/components/smil/FindTheButterflies.svelte
@@ -1,7 +1,8 @@
 <script>
 	const width = 80;
 	const height = 50;
-	const size = 16;
+	const scale = 0.7;
+	const size = 16 * scale;
 
 	const targets = 3;
 
@@ -62,63 +63,70 @@
 
 	<rect fill="url(#find-the-butterflies-pattern-bricks)" width="80" height="50" />
 
-	<g transform="translate(8 5)">
-		<g transform="translate({ribbon.x} {ribbon.y})">
-			<g stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
-				<g fill="#2969bd">
-					<path id="find-the-butterflies-ribbon-bottom" d="M 0 1 c 0 2 1 8 2 9 1 -2 3 -1 3 -2z" />
-					<use transform="scale(-1 1)" href="#find-the-butterflies-ribbon-bottom" />
-					<path id="find-the-butterflies-ribbon-top" d="M 0 0 c 0 -4 10 -8 6 1 3 6 -6 6 -6 -1" />
-					<use transform="scale(-1 1)" href="#find-the-butterflies-ribbon-top" />
+	<g>
+		<g transform="translate({ribbon.x} {ribbon.y}) scale({scale})">
+			<g transform="translate(8 8)">
+				<g stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
+					<g fill="#2969bd">
+						<path
+							id="find-the-butterflies-ribbon-bottom"
+							d="M 0 -2 c 0 2 1 8 2 9 1 -2 3 -1 3 -2z"
+						/>
+						<use transform="scale(-1 1)" href="#find-the-butterflies-ribbon-bottom" />
+						<path id="find-the-butterflies-ribbon-top" d="M 0 -3 c 0 -4 10 -8 6 1 3 6 -6 6 -6 -1" />
+						<use transform="scale(-1 1)" href="#find-the-butterflies-ribbon-top" />
+					</g>
+					<g fill="none">
+						<path id="find-the-butterflies-ribbon-inset" d="M -3 0.25 c 1 -2 5 -2 3.8 0" />
+						<use transform="scale(-1 1)" href="#find-the-butterflies-ribbon-inset" />
+					</g>
+					<rect fill="#2969bd" x="-1.4" y="-3.6" width="2.8" height="3.2" rx="1" />
 				</g>
-				<g fill="none">
-					<path id="find-the-butterflies-ribbon-inset" d="M 0 0.25 c 1 -2 5 -2 3.8 0" />
-					<use transform="scale(-1 1)" href="#find-the-butterflies-ribbon-inset" />
-				</g>
-				<rect fill="#2969bd" x="-1.4" y="-0.6" width="2.8" height="3.2" rx="1" />
 			</g>
 		</g>
 
 		<g>
 			{#each butterflies as { x, y }}
-				<g transform="translate({x} {y})">
-					<g
-						stroke="currentColor"
-						stroke-width="0.5"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					>
-						<g fill="#2969bd">
-							<g id="find-the-butterflies-butterfly-wing">
-								<use
-									transform="scale(1.05 1.08)"
-									fill="currentColor"
-									href="#find-the-butterflies-butterfly-wing-front"
-								/>
-								<path
-									id="find-the-butterflies-butterfly-wing-front"
-									d="M 0 3 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1"
-								/>
+				<g transform="translate({x} {y}) scale({scale})">
+					<g transform="translate(8 8)">
+						<g
+							stroke="currentColor"
+							stroke-width="0.5"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<g fill="#2969bd">
+								<g id="find-the-butterflies-butterfly-wing">
+									<use
+										transform="scale(1.05 1.08)"
+										fill="currentColor"
+										href="#find-the-butterflies-butterfly-wing-front"
+									/>
+									<path
+										id="find-the-butterflies-butterfly-wing-front"
+										d="M 0 0 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1"
+									/>
+								</g>
+								<use transform="scale(-1 1)" href="#find-the-butterflies-butterfly-wing" />
 							</g>
-							<use transform="scale(-1 1)" href="#find-the-butterflies-butterfly-wing" />
-						</g>
-						<path fill="#2969bd" d="M 0 0 c 2.5 3 3.5 7 0 9 -3.5 -2 -2.5 -6 0 -9" />
-						<g fill="none">
-							<path
-								id="find-the-butterflies-butterfly-antenna"
-								transform="translate(0.5 -1) rotate(-40)"
-								d="M 0 0 c 1 -2 5 -2 3.8 0 t -0.25 -1"
-							/>
-							<use transform="scale(-1 1)" href="#find-the-butterflies-butterfly-antenna" />
-						</g>
-						<path fill="#2969bd" d="M 0 -1.5 c -1 0 -3 3 0 3 3 0 1 -3 0 -3" />
-						<g fill="none">
-							<path
-								id="find-the-butterflies-butterfly-eye"
-								transform="translate(1.25 -0.5) rotate(-40)"
-								d="M 0 -0.8 q 2 0.8 0 1.6z"
-							/>
-							<use transform="scale(-1 1)" href="#find-the-butterflies-butterfly-eye" />
+							<path fill="#2969bd" d="M 0 -3 c 2.5 3 3.5 7 0 9 -3.5 -2 -2.5 -6 0 -9" />
+							<g fill="none">
+								<path
+									id="find-the-butterflies-butterfly-antenna"
+									transform="translate(0.5 -4) rotate(-40)"
+									d="M 0 -0 c 1 -2 5 -2 3.8 0 t -0.25 -1"
+								/>
+								<use transform="scale(-1 1)" href="#find-the-butterflies-butterfly-antenna" />
+							</g>
+							<path fill="#2969bd" d="M 0 -4.5 c -1 0 -3 3 0 3 3 0 1 -3 0 -3" />
+							<g fill="none">
+								<path
+									id="find-the-butterflies-butterfly-eye"
+									transform="translate(1.25 -3.5) rotate(-40)"
+									d="M 0 -0.8 q 2 0.8 0 1.6z"
+								/>
+								<use transform="scale(-1 1)" href="#find-the-butterflies-butterfly-eye" />
+							</g>
 						</g>
 					</g>
 				</g>
