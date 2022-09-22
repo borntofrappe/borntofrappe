09f77880d83c8b70c2d66034388a61858c177b6e
Gabriele Corti <borntofrappe@gmail.com>
2022-09-22 09:17:59 +0200

    [feat] add clipping lens

--- src/lib/components/smil/TakeAPicture.svelte
+++ src/lib/components/smil/TakeAPicture.svelte
@@ -1,5 +1,5 @@
 <script>
-	const size = 12;
+	const size = 15;
 	const width = 80;
 	const height = 50;
 
@@ -55,45 +55,63 @@
 				</g>
 			</g>
 		</symbol>
+
+		<rect
+			id="take-a-picture-lens"
+			x={lens.x}
+			y={lens.y}
+			width={lens.width}
+			height={lens.height}
+			rx="2"
+		/>
+
+		<clipPath id="take-a-picture-clip-lens">
+			<use href="#take-a-picture-lens" />
+		</clipPath>
 	</defs>
 
 	<rect fill="currentColor" width="80" height="50" />
 
-	<rect width="80" height="50" fill="#f7f7f7" />
+	<g>
+		<use href="#take-a-picture-lens" fill="none" stroke="#f7f7f7" stroke-width="2.5" />
+		<use href="#take-a-picture-lens" fill="none" stroke="currentColor" stroke-width="1" />
 
-	<g fill="#dfdfdf">
-		<circle cx="0" cy="0" r="25" />
-		<circle cx="25" cy="0" r="25" />
-		<circle cx="50" cy="-5" r="25" />
-		<circle cx="80" cy="-10" r="25" />
-		<circle cx="15" cy="20" r="12" />
-		<circle cx="30" cy="20" r="12" />
-		<circle cx="45" cy="15" r="12" />
-		<circle cx="65" cy="10" r="12" />
-	</g>
+		<g clip-path="url(#take-a-picture-clip-lens)">
+			<rect width="80" height="50" fill="#f7f7f7" />
 
-	<g stroke-width="0.5" stroke="#dfdfdf">
-		<path
-			fill="#f7f7f7"
-			d="M 0 21.5 29.5 28.5 35 50 45 50 39 25 80 5 80 0 74 0 37 18 32.5 0 22.5 0 28 20 0 14"
-		/>
-		<g fill="none">
-			<path d="M 0 21.5 29.5 28.5" stroke-dasharray="8" transform="translate(2 -3.5)" />
-			<path d="M 29.5 27 35 50" stroke-dasharray="12" transform="translate(2 1)" />
-			<g stroke-dasharray="10">
-				<path d="M 45 50 39 25" transform="translate(-4 -2)" />
-				<path d="M 39 25 80 5" transform="translate(4 -5)" />
-				<path d="M 37 18 32.5 0" transform="translate(-3 -2)" />
-				<path d="M 22.5 0 28 20" transform="translate(2 -1)" />
+			<g fill="#dfdfdf">
+				<circle cx="0" cy="0" r="25" />
+				<circle cx="25" cy="0" r="25" />
+				<circle cx="50" cy="-5" r="25" />
+				<circle cx="80" cy="-10" r="25" />
+				<circle cx="15" cy="20" r="12" />
+				<circle cx="30" cy="20" r="12" />
+				<circle cx="45" cy="15" r="12" />
+				<circle cx="65" cy="10" r="12" />
+			</g>
+
+			<g stroke-width="0.5" stroke="#dfdfdf">
+				<path
+					fill="#f7f7f7"
+					d="M 0 21.5 29.5 28.5 35 50 45 50 39 25 80 5 80 0 74 0 37 18 32.5 0 22.5 0 28 20 0 14"
+				/>
+				<g fill="none">
+					<path d="M 0 21.5 29.5 28.5" stroke-dasharray="8" transform="translate(2 -3.5)" />
+					<path d="M 29.5 27 35 50" stroke-dasharray="12" transform="translate(2 1)" />
+					<g stroke-dasharray="10">
+						<path d="M 45 50 39 25" transform="translate(-4 -2)" />
+						<path d="M 39 25 80 5" transform="translate(4 -5)" />
+						<path d="M 37 18 32.5 0" transform="translate(-3 -2)" />
+						<path d="M 22.5 0 28 20" transform="translate(2 -1)" />
+					</g>
+				</g>
+			</g>
+
+			<g transform="translate(40 25)">
+				<svg x={-size / 2} y={-size / 2} width={size} height={size}>
+					<use href="#take-a-picture-sprite-1" />
+				</svg>
 			</g>
 		</g>
 	</g>
-
-	<svg width={size} height={size}>
-		<use href="#take-a-picture-sprite-0" />
-	</svg>
-
-	<svg y={size} width={size} height={size}>
-		<use href="#take-a-picture-sprite-1" />
-	</svg>
 </svg>
