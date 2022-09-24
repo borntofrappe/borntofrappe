1ed2346d3393425d186e64bbfc90bbde2b6c7267
Gabriele Corti <borntofrappe@gmail.com>
2022-09-24 15:07:40 +0200

    [feat] draw game assets
    draw crops as two distinct shapes, the bottom and the top; use the top for the different degrees of maturity and eventually the spoiled sprite

--- src/lib/components/smil/PickUpProduce.svelte
+++ src/lib/components/smil/PickUpProduce.svelte
@@ -53,6 +53,90 @@
 			id="pick-up-produce-grass"
 			d="M 0 34 l 1 -5 1 2 1 -5 1 5 1 1 0 -3 3 2 1 -2 2 2 0 -3 4 3 2 -2 2 3 2 -3 0 5z"
 		/>
+
+		<symbol id="pick-up-produce-crop" viewBox="-15 -21 30 47">
+			<g fill="none" stroke="#12984f" stroke-linecap="round" stroke-linejoin="round">
+				<path transform="rotate(0) translate(0 21)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(15) translate(0 20.5)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(-15) translate(0 20.5)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(-30) translate(0 20)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(30) translate(0 20)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+			</g>
+
+			<path fill="#ffdd9d" d="M 2 0 v 5 a 13 8 0 1 1 -4 0 v -5" />
+			<path fill="#76d0ad" d="M -2 -6 v 5 h 4 v -5" />
+
+			<g stroke="#c64e00" stroke-linecap="round" stroke-linejoin="round">
+				<g fill="#c64e00">
+					<path transform="translate(3.25 9)" d="M 0 0 l 1.1 1.7 -1.1 1.7 -1.1 -1.7z" />
+					<path transform="translate(-3.25 9)" d="M 0 0 l 1.1 1.7 -1.1 1.7 -1.1 -1.7z" />
+				</g>
+				<path fill="none" stroke="#c64e00" d="M -4 15 a 6 6 0 0 0 8 0" />
+			</g>
+
+			<path fill="#ffdd9d" d="M -1.5 -1 h 3 v 4 l -0.75 -0.75 -0.75 0.75 -0.75 -0.75 -0.75 0.75z" />
+			<path opacity="0.1" d="M -1.5 -1 h 3 v 4 l -0.75 -0.75 -0.75 0.75 -0.75 -0.75 -0.75 0.75z" />
+
+			<g stroke="#12984f" stroke-linecap="round" stroke-linejoin="round">
+				<path fill="none" stroke-width="1.25" d="M 2 -6 v 11 a 13 8 0 1 1 -4 0 v -11" />
+				<path fill="#ffdd9d" d="M -7 17 c -1 3 6 5 7 5 1 0 8 -2 7 -5" />
+			</g>
+		</symbol>
+
+		<symbol id="pick-up-produce-crop-top-0" viewBox="-15 -21 30 47">
+			<g transform="translate(0 -6)">
+				<g fill="#76d0ad" stroke="#12984f" stroke-linecap="round" stroke-linejoin="round">
+					<path d="M 0 0 a 9 9 0 0 1 0 -14 9 9 0 0 1 0 14" />
+					<path stroke-width="0.75" d="M 0 0 v -10" />
+					<path d="M 0.25 0 c 0 -5 0 -10 8 -14 5 7 -1 13 -5.5 14" />
+					<path transform="scale(-1 1)" d="M 0.25 0 c 0 -5 0 -10 8 -14 5 7 -1 13 -5.5 14" />
+					<g stroke-width="0.75">
+						<path d="M 0.25 0 q 3.5 -4 6.5 -9.5" />
+						<path transform="scale(-1 1)" d="M 0.25 0 q 3.5 -4 6.5 -9.5" />
+					</g>
+				</g>
+			</g>
+		</symbol>
+
+		<symbol id="pick-up-produce-crop-top-1" viewBox="-15 -21 30 47">
+			<g transform="translate(0 -6)">
+				<g fill="#76d0ad" stroke="#12984f" stroke-linecap="round" stroke-linejoin="round">
+					<path transform="rotate(10)" d="M 0 0 c 8 -5 -2 -18 -6 -11 7 5 -2 5 6 11" />
+					<path transform="rotate(-30)" d="M 0 0 c 8 -5 -2 -18 -6 -11 7 5 -2 5 6 11" />
+					<path transform="scale(-1 1) rotate(-30)" d="M 0 0 c 8 -5 -2 -18 -6 -11 7 5 -2 5 6 11" />
+					<g stroke-width="0.75">
+						<path d="M 0.25 -0.25 c 2 -3 3 -7 6 -7.5" />
+						<path transform="scale(-1 1)" d="M 0.25 -0.25 c 2 -3 3 -7 6 -7.5" />
+					</g>
+				</g>
+			</g>
+		</symbol>
+
+		<symbol id="pick-up-produce-crop-top-2" viewBox="-15 -21 30 47">
+			<g transform="translate(0 -6)">
+				<g fill="#76d0ad" stroke="#a5570c" stroke-linecap="round" stroke-linejoin="round">
+					<path d="M 0 0 c 8 -9 -3 -18 -6 -11 9 0 -4 5 6 11" />
+					<g stroke-width="0.75">
+						<path d="M 0 0 c -2 -4 2 -7 -1 -10" />
+					</g>
+					<path transform="scale(-1 1) rotate(-50)" d="M 0 -1 c 8 -9 -3 -18 -6 -11 9 0 -4 5 6 11" />
+					<path transform="rotate(-50)" d="M 0 -1 c 8 -9 -3 -18 -6 -11 9 0 -4 5 6 11" />
+				</g>
+			</g>
+		</symbol>
+
+		<symbol id="pick-up-produce-crop-top-spoiled" viewBox="-15 -21 30 47">
+			<g transform="translate(0 -6)">
+				<g fill="#ffdd9d" stroke="#a5570c" stroke-linecap="round" stroke-linejoin="round">
+					<path d="M 0 0 c 8 -9 -3 -18 -6 -11 9 0 -4 5 6 11" />
+					<g stroke-width="0.75">
+						<path d="M 0 0 c -2 -4 2 -7 -1 -10" />
+					</g>
+					<path transform="scale(-1 1) rotate(-50)" d="M 0 -1 c 8 -9 -3 -18 -6 -11 9 0 -4 5 6 11" />
+					<path transform="rotate(-50)" d="M 0 -1 c 8 -9 -3 -18 -6 -11 9 0 -4 5 6 11" />
+				</g>
+			</g>
+		</symbol>
 	</defs>
 
 	<rect width="80" height="50" fill="#85dde1" />
@@ -126,5 +210,18 @@
 				<path d="M 68 37 l -1 0 -1 0 0 2 1 1 1 0 2 -2 -4 0" />
 			</g>
 		</g>
+	</g>
+
+	<g transform="translate(0 10)">
+		{#each Array(3) as _, i}
+			<svg x={i * 20} width="20" height="20">
+				<use href="#pick-up-produce-crop" />
+				<use href="#pick-up-produce-crop-top-{i}" />
+			</svg>
+		{/each}
+		<svg x="60" width="20" height="20">
+			<use href="#pick-up-produce-crop" />
+			<use href="#pick-up-produce-crop-top-spoiled" />
+		</svg>
 	</g>
 </svg>
