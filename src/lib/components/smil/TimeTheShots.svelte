57c2cb612c758c7ed1077aa18a2d1bfcbdf7f874
Gabriele Corti <borntofrappe@gmail.com>
2022-09-24 09:17:42 +0200

    [init] add TimeTheShots.svelte

---
+++ src/lib/components/smil/TimeTheShots.svelte
@@ -0,0 +1,14 @@
+<svg viewBox="0 0 80 50">
+	<defs>
+		<pattern id="time-the-shots-pattern-sea" viewBox="0 0 8 5" width="0.1" height="0.1">
+			<rect width="8" height="5" fill="#286bc6" />
+			<g fill="none" stroke="#43b5f1" stroke-width="0.5">
+				<path d="M 1 2.5 h 0.5 m 0 -0.5 h0.5 m 0 0.5 h1" />
+				<path d="M 4.5 1 h 0.5 m 0 -0.5 h0.5 m 0 0.5 h1" />
+				<path d="M 3.5 4 h 0.5 m 0 -0.5 h0.5 m 0 0.5 h1" />
+			</g>
+		</pattern>
+	</defs>
+
+	<rect width="80" height="50" fill="url(#time-the-shots-pattern-sea)" />
+</svg>
