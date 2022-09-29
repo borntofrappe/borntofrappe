9073f68a8672fd1411b2dca84db22acaaaa8f793
Gabriele Corti <borntofrappe@gmail.com>
2022-09-29 21:38:22 +0200

    [design] add vector sprites

---
+++ src/blog/dragon-warrior/Pots.svelte
@@ -0,0 +1,96 @@
+<svg viewBox="0 0 16 16" width="1em" height="1em" shape-rendering="crispEdges">
+	<g fill="#0c1019">
+		<rect x="5" width="6" height="1" />
+		<rect x="4" y="1" width="1" height="1" />
+		<rect x="11" y="1" width="1" height="1" />
+		<rect x="3" y="2" width="1" height="1" />
+		<rect x="5" y="2" width="6" height="1" />
+		<rect x="12" y="2" width="1" height="1" />
+		<rect x="2" y="3" width="1" height="2" />
+		<rect x="13" y="3" width="1" height="2" />
+		<rect x="4" y="3" width="8" height="2" />
+		<rect x="3" y="5" width="1" height="1" />
+		<rect x="5" y="5" width="6" height="1" />
+		<rect x="12" y="5" width="1" height="1" />
+		<rect x="2" y="6" width="1" height="1" />
+		<rect x="4" y="6" width="1" height="1" />
+		<rect x="11" y="6" width="3" height="1" />
+		<rect x="1" y="7" width="1" height="1" />
+		<rect x="5" y="7" width="6" height="1" />
+		<rect x="12" y="7" width="3" height="1" />
+		<rect y="8" width="1" height="4" />
+		<rect x="11" y="8" width="5" height="1" />
+		<rect x="10" y="9" width="1" height="1" />
+		<rect x="12" y="9" width="1" height="1" />
+		<rect x="14" y="9" width="2" height="1" />
+		<rect x="11" y="10" width="1" height="1" />
+		<rect x="13" y="10" width="3" height="1" />
+		<rect x="10" y="11" width="1" height="1" />
+		<rect x="12" y="11" width="4" height="1" />
+		<rect x="1" y="12" width="1" height="1" />
+		<rect x="9" y="12" width="1" height="1" />
+		<rect x="11" y="12" width="1" height="1" />
+		<rect x="13" y="12" width="2" height="1" />
+		<rect x="2" y="13" width="1" height="1" />
+		<rect x="4" y="13" width="1" height="1" />
+		<rect x="6" y="13" width="1" height="1" />
+		<rect x="8" y="13" width="1" height="1" />
+		<rect x="10" y="13" width="1" height="1" />
+		<rect x="12" y="13" width="2" height="1" />
+		<rect x="3" y="14" width="3" height="1" />
+		<rect x="7" y="14" width="1" height="1" />
+		<rect x="9" y="14" width="4" height="1" />
+		<rect x="4" y="15" width="8" height="1" />
+	</g>
+	<g fill="#a4773e">
+		<rect x="5" y="1" width="1" height="1" />
+		<rect x="9" y="1" width="2" height="1" />
+		<rect x="11" y="2" width="1" height="1" />
+		<rect x="12" y="3" width="1" height="2" />
+		<rect x="3" y="4" width="1" height="1" />
+		<rect x="4" y="5" width="1" height="1" />
+		<rect x="11" y="5" width="1" height="1" />
+		<rect x="3" y="6" width="1" height="1" />
+		<rect x="5" y="6" width="6" height="1" />
+		<rect x="2" y="7" width="1" height="1" />
+		<rect x="4" y="7" width="1" height="1" />
+		<rect x="11" y="7" width="1" height="1" />
+		<rect x="1" y="8" width="1" height="4" />
+		<rect x="6" y="8" width="5" height="1" />
+		<rect x="3" y="9" width="1" height="1" />
+		<rect x="5" y="9" width="1" height="1" />
+		<rect x="7" y="9" width="1" height="1" />
+		<rect x="9" y="9" width="1" height="1" />
+		<rect x="11" y="9" width="1" height="1" />
+		<rect x="13" y="9" width="1" height="1" />
+		<rect x="2" y="10" width="1" height="1" />
+		<rect x="4" y="10" width="1" height="1" />
+		<rect x="6" y="10" width="5" height="1" />
+		<rect x="12" y="10" width="1" height="1" />
+		<rect x="2" y="11" width="8" height="1" />
+		<rect x="11" y="11" width="1" height="1" />
+		<rect x="2" y="12" width="7" height="1" />
+		<rect x="10" y="12" width="1" height="1" />
+		<rect x="12" y="12" width="1" height="1" />
+		<rect x="3" y="13" width="1" height="1" />
+		<rect x="5" y="13" width="1" height="1" />
+		<rect x="7" y="13" width="1" height="1" />
+		<rect x="9" y="13" width="1" height="1" />
+		<rect x="11" y="13" width="1" height="1" />
+		<rect x="6" y="14" width="1" height="1" />
+		<rect x="8" y="14" width="1" height="1" />
+	</g>
+	<g fill="#ffe489">
+		<rect x="6" y="1" width="3" height="1" />
+		<rect x="4" y="2" width="1" height="1" />
+		<rect x="3" y="3" width="1" height="1" />
+		<rect x="3" y="7" width="1" height="1" />
+		<rect x="2" y="8" width="4" height="1" />
+		<rect x="2" y="9" width="1" height="1" />
+		<rect x="4" y="9" width="1" height="1" />
+		<rect x="6" y="9" width="1" height="1" />
+		<rect x="8" y="9" width="1" height="1" />
+		<rect x="3" y="10" width="1" height="1" />
+		<rect x="5" y="10" width="1" height="1" />
+	</g>
+</svg>
