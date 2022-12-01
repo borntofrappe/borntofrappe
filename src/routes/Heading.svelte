98f413ea051316351f00ec13483fd087e91e0cd2
Gabriele Corti <borntofrappe@gmail.com>
2022-12-01 09:59:16 +0100

    explicit css animation

--- src/routes/Heading.svelte
+++ src/routes/Heading.svelte
@@ -9,6 +9,9 @@
 	const height = squircle * lines.length + inset;
 	const overlay = (width ** 2 + height ** 2) ** 0.5;
 	const id = text.toLowerCase().replace(/\s/, '-');
+
+	const duration = 1.2;
+	const delays = 0.2;
 </script>
 
 <svg viewBox="0 0 {width} {height}">
@@ -17,47 +20,80 @@
 		<symbol id="squircle-{id}" viewBox="0 0 1 1">
 			<path d="M 0 0.5 C 0 0 0 0 0.5 0 S 1 0 1 0.5 1 1 0.5 1 0 1 0 0.5" />
 		</symbol>
-		<linearGradient id="gradient-{id}" spreadMethod="reflect" x1="0" x2="0.5"
-			><stop stop-color="hsl(0, 75.97597597597598%, 66.7%)" offset="0" /><stop
-				stop-color="hsl(51.42857142857143, 75.97597597597598%, 66.7%)"
-				offset="0.142"
-			/><stop stop-color="hsl(51.42857142857143, 75.97597597597598%, 66.7%)" offset="0.284" /><stop
-				stop-color="hsl(102.85714285714286, 75.97597597597598%, 66.7%)"
-				offset="0.426"
-			/><stop stop-color="hsl(154.28571428571428, 75.97597597597598%, 66.7%)" offset="0.570" /><stop
-				stop-color="hsl(205.71428571428572, 75.97597597597598%, 66.7%)"
-				offset="0.714"
-			/><stop
-				stop-color="hsl(257.14285714285717, 75.97597597597598%, 66.7%)"
-				offset="0.856"
-			/></linearGradient
-		>
+		<linearGradient id="gradient-{id}" spreadMethod="reflect" x1="0" x2="0.5">
+			<stop stop-color="hsl(0, 75.97597597597598%, 66.7%)" offset="0" />
+			<stop stop-color="hsl(51.42857142857143, 75.97597597597598%, 66.7%)" offset="0.142" />
+			<stop stop-color="hsl(51.42857142857143, 75.97597597597598%, 66.7%)" offset="0.284" />
+			<stop stop-color="hsl(102.85714285714286, 75.97597597597598%, 66.7%)" offset="0.426" />
+			<stop stop-color="hsl(154.28571428571428, 75.97597597597598%, 66.7%)" offset="0.570" />
+			<stop stop-color="hsl(205.71428571428572, 75.97597597597598%, 66.7%)" offset="0.714" />
+			<stop stop-color="hsl(257.14285714285717, 75.97597597597598%, 66.7%)" offset="0.856" />
+		</linearGradient>
 
 		{#each lines as line, i}
-			<text
+			<g
 				id="line-{i}-{id}"
 				transform="translate({squircle / 2 + inset / 2} {squircle / 2 + inset / 2 + squircle * i})"
-				text-anchor="middle"
-				y={-squircle / 2 / 2}
-				dominant-baseline="hanging"
-				font-size={squircle / 2}
-				font-weight="700"
 			>
 				{#each line as letter, j}
-					<tspan
-						x={squircle * j}
-						class="fade"
-						style:animation-delay="{0.4 + 0.16 * (i + j)}s"
-						style:animation-duration="0.3s"
-					>
-						{letter}
-					</tspan>
+					<g transform="translate({squircle * j} 0)">
+						<g
+							class="transform"
+							style:animation-delay="{0.1 + delays * (i + j)}s"
+							style:animation-duration="{duration}s"
+						>
+							<g
+								class="fade"
+								style:animation-delay="{delays * (i + j)}s"
+								style:animation-duration="{duration}s"
+							>
+								<text
+									text-anchor="middle"
+									dominant-baseline="hanging"
+									font-size={squircle / 2}
+									y={-squircle / 2 / 2}
+									font-weight="700"
+								>
+									{letter}
+								</text>
+							</g>
+						</g>
+					</g>
 				{/each}
-			</text>
+			</g>
+		{/each}
+
+		{#each lines as line, i}
+			<g id="squircles-{i}-{id}">
+				<g
+					transform="translate({squircle / 2 + inset / 2} {squircle / 2 +
+						inset / 2 +
+						squircle * i})"
+				>
+					{#each line as _, j}
+						<g transform="translate({squircle * j} 0)">
+							<g
+								class="transform"
+								style:animation-delay="{delays * (i + j)}s"
+								style:animation-duration="{duration}s"
+							>
+								<use
+									transform="rotate(45)"
+									href="#squircle-{id}"
+									x={-squircle / 2}
+									y={-squircle / 2}
+									width={squircle}
+									height={squircle}
+								/>
+							</g>
+						</g>
+					{/each}
+				</g>
+			</g>
 		{/each}
 
 		<mask id="mask-{id}">
-			<rect width={width * 2} {height} />
+			<rect {width} {height} />
 			<g fill="white">
 				{#each lines as _, i}
 					<use href="#line-{i}-{id}" />
@@ -66,33 +102,14 @@
 		</mask>
 	</defs>
 
-	<g transform="translate({squircle / 2 + inset / 2} {squircle / 2 + inset / 2})">
-		{#each lines as line, i}
-			<g transform="translate(0 {squircle * i})">
-				{#each line as _, j}
-					<g transform="translate({squircle * j} 0)">
-						<g
-							class="transform"
-							style:animation-delay="{0.2 * (i + j)}s"
-							style:animation-duration="1s"
-						>
-							<use
-								transform="rotate(45)"
-								href="#squircle-{id}"
-								x={-squircle / 2}
-								y={-squircle / 2}
-								width={squircle}
-								height={squircle}
-								fill="currentColor"
-							/>
-						</g>
-					</g>
-				{/each}
-			</g>
+	<g>
+		{#each lines as _, i}
+			<use fill="currentColor" href="#squircles-{i}-{id}" />
+			<use href="#line-{i}-{id}" />
 		{/each}
 	</g>
 
-	<g mask="url(#mask-{id})" fill="url(#gradient-{id})">
+	<g mask="url(#mask-{id})" fill="url(#gradient-{id})" style:pointer-events="none">
 		<g transform="translate({width / 2} {height / 2})">
 			<g transform="rotate(40)">
 				<g transform="translate(0 0)">
@@ -111,12 +128,6 @@
 			</g>
 		</g>
 	</g>
-
-	<g fill="transparent">
-		{#each lines as _, i}
-			<use href="#line-{i}-{id}" />
-		{/each}
-	</g>
 </svg>
 
 <style>
@@ -126,10 +137,10 @@
 
 	@keyframes fade {
 		from {
-			fill-opacity: 0;
+			opacity: 0;
 		}
 		to {
-			fill-opacity: 1;
+			opacity: 1;
 		}
 	}
 
