bacbc5c6d5d7cb851cb1a2ffde2e6ee2f6a73b3d
Gabriele Corti <borntofrappe@gmail.com>
2022-12-14 10:38:47 +0100

    style page with chalk filter

--- src/routes/today/+page.svelte
+++ src/routes/today/+page.svelte
@@ -1,7 +1,34 @@
 <script>
 	import { format } from './utils.js';
+	import Defs from './Defs.svelte';
 
 	const date = new Date();
 </script>
 
-<h1>{format(date)}</h1>
+<Defs />
+
+<main>
+	<h1>{format(date)}</h1>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+		color: hsl(0, 0%, 92%);
+		background: hsl(0, 0%, 17%);
+	}
+
+	main ::selection {
+		background: hsl(0, 0%, 10%);
+	}
+
+	h1 {
+		text-align: center;
+		font-size: 4rem;
+		filter: url(#filter-chalkboard);
+	}
+</style>
---
+++ src/routes/today/Defs.svelte
@@ -0,0 +1,12 @@
+<svg
+	style="position: absolute; width: 0; height: 0;"
+	aria-hidden="true"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<defs>
+		<filter id="filter-chalkboard">
+			<feTurbulence type="turbulence" baseFrequency="0.6" numOctaves="3" result="turbulence" />
+			<feDisplacementMap in="SourceGraphic" in2="turbulence" scale="8" />
+		</filter>
+	</defs>
+</svg>
