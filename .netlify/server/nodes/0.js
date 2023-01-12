fe3e271360bc705757e34692737e4a7b1c39b223
Gabriele Corti <borntofrappe@gmail.com>
2023-01-12 14:53:51 +0100

    remove .netlify folder

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const file = '_app/immutable/components/layout.svelte-0d8c0898.js';
-export const imports = ["_app/immutable/components/layout.svelte-0d8c0898.js","_app/immutable/chunks/index-553c9e68.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .netlify/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/_page.svelte-461b09ba.js';
-export const imports = ["_app/immutable/components/pages/_page.svelte-461b09ba.js","_app/immutable/chunks/index-553c9e68.js"];
-export const stylesheets = ["_app/immutable/assets/_page-1462a3bc.css"];
-export const fonts = [];
