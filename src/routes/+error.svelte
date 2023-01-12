058e86fc62e8614a36b0a36fa8cd1c0e17e081cd
Gabriele Corti <borntofrappe@gmail.com>
2023-01-12 21:42:53 +0100

    add mascot to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,20 +1,21 @@
 <script>
 	import { page } from '$app/stores';
 
+	import Serena from './mignon/Serena.svelte';
 	import icons from '$lib/utils/icons.js';
 </script>
 
 <svelte:head>
 	<title>borntofrappe | {$page.status}</title>
 	<meta name="description" content={$page.error.message} />
-	<link rel="icon" href="icons/error.svg" type="image/svg+xml" />
 </svelte:head>
 
 <div id="root">
 	<main>
 		<h1>Oopsie</h1>
 		<p>
-			Something quite unexpected has happened.<br />Here's a possibly helpful message for context:
+			Something quite unexpected has happened, but let's try to be positive.<br />Here's a possibly
+			helpful message for context:
 		</p>
 		<div class="code">
 			<span>error {@html icons.bug}</span>
@@ -28,8 +29,12 @@
 			>.
 		</p>
 		<p>If you need some guidance, <em>home is always a good place to start.</em></p>
-		<p><a href="/">Go to home, page</a></p>
+		<p>Cheer up and <a href="/">go to home, page</a></p>
 	</main>
+
+	<div id="mascot">
+		<Serena />
+	</div>
 </div>
 
 <style>
@@ -41,7 +46,7 @@
 
 	#root ::selection {
 		color: hsl(210, 36%, 96%);
-		background: hsl(209, 23%, 60%);
+		background: hsl(205, 65%, 55%);
 	}
 
 	main {
@@ -101,5 +106,16 @@
 	.code pre::-webkit-scrollbar-thumb {
 		background: var(--accent);
 		border-radius: 0.2rem;
+	}
+
+	#mascot {
+		max-width: 32rem;
+		width: 100vmin;
+		margin-inline: auto;
+	}
+	#mascot > :global(svg) {
+		display: block;
+		width: 100%;
+		height: auto;
 	}
 </style>
