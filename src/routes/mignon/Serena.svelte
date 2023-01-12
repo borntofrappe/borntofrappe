cdfaaa4f53ca1cb448ca61f34ec3cefa23027019
Gabriele Corti <borntofrappe@gmail.com>
2023-01-12 18:50:39 +0100

    animate horizontal movement

--- src/routes/mignon/Serena.svelte
+++ src/routes/mignon/Serena.svelte
@@ -1,8 +1,18 @@
 <script>
+	import { onMount } from 'svelte';
 	import { tweened, spring } from 'svelte/motion';
 	import { elasticOut as easing } from 'svelte/easing';
 
 	let start;
+	let svg;
+	let l;
+	let w;
+
+	onMount(() => {
+		const { left, width } = svg.getBoundingClientRect();
+		l = left;
+		w = width;
+	});
 
 	const scale = spring(0, {
 		stiffness: 0.2,
@@ -12,6 +22,21 @@
 	const bounce = tweened(1, {
 		duration: 1000,
 		easing
+	});
+
+	const translate = spring(0, {
+		stiffness: 0.1,
+		damping: 0.8
+	});
+
+	const rotate = spring(0, {
+		stiffness: 0.1,
+		damping: 0.2
+	});
+
+	const skewX = spring(0, {
+		stiffness: 0.2,
+		damping: 0.2
 	});
 
 	const handleStart = () => {
@@ -24,6 +49,13 @@
 		scale.set(0);
 	};
 
+	const handleMove = ({ offsetX }) => {
+		const x = offsetX / w - 0.5;
+		rotate.set(x * 30);
+		translate.set(x * 30);
+		skewX.set(x * 10 * -1);
+	};
+
 	const handleMousedown = () => bounce.set(1.1);
 	const handleMouseup = () => bounce.set(1);
 </script>
@@ -31,12 +63,14 @@
 <svg
 	viewBox="-60 -30 120 130"
 	class:start
+	bind:this={svg}
 	on:mouseenter={handleStart}
 	on:mouseleave={handleEnd}
 	on:touchstart|preventDefault={handleStart}
 	on:touchend|preventDefault={handleEnd}
 	on:mousedown={handleMousedown}
 	on:mouseup={handleMouseup}
+	on:mousemove={handleMove}
 >
 	<defs>
 		<pattern id="p" viewBox="-8 -8 16 16" width="10" height="10" patternUnits="userSpaceOnUse">
@@ -49,7 +83,9 @@
 		<circle id="c" r="11" />
 		<mask id="m">
 			<use href="#c" fill="white" />
-			<use href="#c" y="-2" fill="black" />
+			<g transform="rotate({$rotate})">
+				<use href="#c" y="-2" fill="black" />
+			</g>
 		</mask>
 		<g id="cc">
 			<use href="#c" />
@@ -69,44 +105,47 @@
 			</g>
 		</g>
 	</defs>
-
-	<g class="raindrop">
-		<rect fill="url(#p)" x="-30" width="60" height="100" />
-	</g>
-
-	<g transform="scale({$bounce})">
-		<use href="#ccc" transform="scale(1.2)" />
-
-		<g fill="#f9a3c6">
-			<g transform="translate(12.5 3)">
-				<ellipse transform="scale({$scale})" rx="4" ry="2" />
-			</g>
-			<g transform="translate(-12.5 3)">
-				<ellipse transform="scale({$scale})" rx="4" ry="2" />
+	<g transform="translate({$translate} 0)">
+		<g transform="skewX({$skewX})">
+			<g class="raindrop">
+				<rect fill="url(#p)" x="-30" width="60" height="100" />
 			</g>
 		</g>
 
-		<g fill="#acacfa">
-			<g transform="translate(12.5 -2.5)">
-				<circle transform="scale({$bounce})" r="3" />
-			</g>
-			<g transform="translate(-12.5 -2.5)">
-				<circle transform="scale({$bounce})" r="3" />
-			</g>
-		</g>
+		<g transform="scale({$bounce})">
+			<use href="#ccc" transform="scale(1.2)" />
 
-		<g
-			fill="#acacfa"
-			stroke="#acacfa"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		>
-			<g transform="translate(0 10)">
-				<path d="M -2 0 h 4" />
-				<g transform="scale({$bounce})">
-					<g transform="scale({$scale})">
-						<path d="M -2 0 v 2 a 2 2 0 0 0 4 0 v -2z" />
+			<g fill="#f9a3c6">
+				<g transform="translate(12.5 3)">
+					<ellipse transform="scale({$scale})" rx="4" ry="2" />
+				</g>
+				<g transform="translate(-12.5 3)">
+					<ellipse transform="scale({$scale})" rx="4" ry="2" />
+				</g>
+			</g>
+
+			<g fill="#acacfa">
+				<g transform="translate(12.5 -2.5)">
+					<circle transform="scale({$bounce})" r="3" />
+				</g>
+				<g transform="translate(-12.5 -2.5)">
+					<circle transform="scale({$bounce})" r="3" />
+				</g>
+			</g>
+
+			<g
+				fill="#acacfa"
+				stroke="#acacfa"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<g transform="translate(0 10)">
+					<path d="M -2 0 h 4" />
+					<g transform="scale({$bounce})">
+						<g transform="scale({$scale})">
+							<path d="M -2 0 v 2 a 2 2 0 0 0 4 0 v -2z" />
+						</g>
 					</g>
 				</g>
 			</g>
