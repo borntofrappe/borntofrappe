ba5e3daf542e32d4d51e60509458bf727eac8697
Gabriele Corti <borntofrappe@gmail.com>
2023-01-12 18:23:24 +0100

    animate raindrop

--- src/routes/mignon/Serena.svelte
+++ src/routes/mignon/Serena.svelte
@@ -1,7 +1,37 @@
-<svg viewBox="-60 -30 120 130">
+<script>
+	import { spring } from 'svelte/motion';
+
+	let start;
+
+	const scale = spring(0, {
+		stiffness: 0.2,
+		damping: 0.5
+	});
+
+	const handleStart = () => {
+		start = true;
+		scale.set(1);
+	};
+
+	const handleEnd = () => {
+		start = false;
+		scale.set(0);
+	};
+</script>
+
+<svg
+	viewBox="-60 -30 120 130"
+	class:start
+	on:mouseenter={handleStart}
+	on:mouseleave={handleEnd}
+	on:touchstart|preventDefault={handleStart}
+	on:touchend|preventDefault={handleEnd}
+>
 	<defs>
 		<pattern id="p" viewBox="-8 -8 16 16" width="10" height="10" patternUnits="userSpaceOnUse">
-			<path fill="#35cce9" d="M 0 0 q 2 2 2 4 a 2 2 0 0 1 -4 0 q 0 -2 2 -4" />
+			<g transform="scale({$scale})">
+				<path fill="#35cce9" d="M 0 0 q 2 2 2 4 a 2 2 0 0 1 -4 0 q 0 -2 2 -4" />
+			</g>
 		</pattern>
 		<circle id="c" r="11" />
 		<mask id="m">
@@ -27,15 +57,19 @@
 		</g>
 	</defs>
 
-	<g class="translate">
+	<g class="raindrop">
 		<rect fill="url(#p)" x="-30" width="60" height="100" />
 	</g>
 
 	<use href="#ccc" transform="scale(1.2)" />
 
 	<g fill="#f9a3c6">
-		<ellipse cx="12.5" cy="3" rx="4" ry="2" />
-		<ellipse cx="-12.5" cy="3" rx="4" ry="2" />
+		<g transform="translate(12.5 3)">
+			<ellipse transform="scale({$scale})" rx="4" ry="2" />
+		</g>
+		<g transform="translate(-12.5 3)">
+			<ellipse transform="scale({$scale})" rx="4" ry="2" />
+		</g>
 	</g>
 
 	<g fill="#acacfa">
@@ -56,7 +90,27 @@
 	>
 		<g transform="translate(0 10)">
 			<path d="M -2 0 h 4" />
-			<path d="M -2 0 v 2 a 2 2 0 0 0 4 0 v -2z" />
+			<g transform="scale({$scale})">
+				<path d="M -2 0 v 2 a 2 2 0 0 0 4 0 v -2z" />
+			</g>
 		</g>
 	</g>
 </svg>
+
+<style>
+	svg .raindrop {
+		opacity: 1;
+		animation: raindrop 0.75s linear infinite;
+		animation-play-state: paused;
+	}
+
+	svg.start .raindrop {
+		animation-play-state: running;
+	}
+
+	@keyframes raindrop {
+		to {
+			transform: translateY(10px);
+		}
+	}
+</style>
