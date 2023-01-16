9f343a578f22463ed423c18dcc98bf98b7827453
Gabriele Corti <borntofrappe@gmail.com>
2023-01-16 22:07:38 +0100

    markup and style page

--- src/routes/today/+page.svelte
+++ src/routes/today/+page.svelte
@@ -4,4 +4,34 @@
 	const date = new Date();
 </script>
 
-{format(date)}
+<svelte:head>
+	<title>borntofrappe | Today</title>
+	<meta
+		name="description"
+		content="What day is today? Here's a direct answer to an often puzzling question."
+	/>
+</svelte:head>
+
+<main>
+	<h1>{format(date)}</h1>
+</main>
+
+<style>
+	main {
+		padding: 1rem;
+		display: flex;
+		gap: 0.5rem 0;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+		color: hsl(0, 0%, 92%);
+		background: hsl(0, 0%, 17%);
+		position: relative;
+	}
+
+	h1 {
+		text-align: center;
+		font-size: 4rem;
+	}
+</style>
