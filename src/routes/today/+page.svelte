8269cec576f84d641f9458aa4d3f20edca2872b4
Gabriele Corti <borntofrappe@gmail.com>
2023-01-16 22:12:57 +0100

    svg filter

--- src/routes/today/+page.svelte
+++ src/routes/today/+page.svelte
@@ -12,6 +12,18 @@
 	/>
 </svelte:head>
 
+<svg style="position: absolute; width: 0; height: 0;" aria-hidden="true">
+	<defs>
+		<filter id="filter-chalk">
+			<feTurbulence type="turbulence" baseFrequency="0.6" numOctaves="3" result="turbulence" />
+			<feDisplacementMap in="SourceGraphic" in2="turbulence" scale="8" />
+		</filter>
+		<filter id="filter-noise">
+			<feTurbulence type="fractalNoise" baseFrequency="0.7" numOctaves="10" stichTile="stitch" />
+		</filter>
+	</defs>
+</svg>
+
 <main>
 	<h1>{format(date)}</h1>
 </main>
@@ -30,8 +42,23 @@
 		position: relative;
 	}
 
+	main::before {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: hsl(0, 0%, 100%);
+		opacity: 0.35;
+		filter: url(#filter-noise);
+	}
+
+	main ::selection {
+		background: hsl(0, 0%, 17%);
+	}
+
 	h1 {
 		text-align: center;
 		font-size: 4rem;
+		filter: url(#filter-chalk);
 	}
 </style>
