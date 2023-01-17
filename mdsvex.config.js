d39d0c7535a3d92ecc1242e729e4dd18f9d95a2b
Gabriele Corti <borntofrappe@gmail.com>
2023-01-17 15:02:38 +0100

    syntax highlighting

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,31 @@
+import { escapeSvelte } from 'mdsvex';
+import { getHighlighter } from 'shiki';
+
+import icons from './src/lib/utils/icons.js';
+
+const extensions = ['.md', '.svx'];
+
+const smartypants = false;
+
+const layout = {
+	blog: 'src/lib/layout/Blog.svelte'
+};
+
+const highlighter = async (code, lang) => {
+	const shikiHighlighter = await getHighlighter({ theme: 'rose-pine-moon' });
+	const shikiCode = shikiHighlighter.codeToHtml(code, { lang });
+
+	return `<div class="code">
+    <span>${lang}${icons[lang] || icons.editor}</span>
+    ${escapeSvelte(shikiCode)}
+    </div>`;
+};
+
 export default {
-	extensions: ['.md', '.svx'],
-	smartypants: false,
-	layout: {
-		blog: 'src/lib/layout/Blog.svelte'
+	extensions,
+	smartypants,
+	layout,
+	highlight: {
+		highlighter
 	}
 };
