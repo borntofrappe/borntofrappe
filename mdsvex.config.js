ea3fafbb10d448f6dec114955cd6a80ababe63a9
Gabriele Corti <borntofrappe@gmail.com>
2023-01-17 12:03:44 +0100

    add mdsvex layout

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,4 +1,7 @@
 export default {
 	extensions: ['.md', '.svx'],
-	smartypants: false
+	smartypants: false,
+	layout: {
+		blog: 'src/lib/layout/Blog.svelte'
+	}
 };
---
+++ src/lib/layout/Blog.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let title;
+	export let description;
+</script>
+
+<svelte:head>
+	<title>
+		borntofrappe | {title}
+	</title>
+	<meta name="description" content={description} />
+</svelte:head>
+
+<header>
+	<h1>{title}</h1>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	header {
+		padding: 3rem 1rem;
+		text-align: center;
+	}
+
+	main {
+		max-width: 60ch;
+		margin-inline: auto;
+		padding: 1rem;
+	}
+
+	main > :global(* + *) {
+		margin-block-start: 1em;
+	}
+</style>
