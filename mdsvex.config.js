41618c59387569c2684b869c54138fb6ff972156
Gabriele Corti <borntofrappe@gmail.com>
2023-01-18 11:35:07 +0100

    clean up mdsvex config
    divide the config object into sections with the same structure; add note on the highlighter

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,6 +1,5 @@
 import { escapeSvelte } from 'mdsvex';
 import { getHighlighter } from 'shiki';
-
 import icons from './src/lib/utils/icons.js';
 
 const extensions = ['.md', '.svx'];
@@ -12,26 +11,26 @@ const layout = {
 };
 
 const theme = 'rose-pine-moon';
-let shikiHighlighter;
+// roundabout way to reduce the time it takes to highlight code blocks asynchronously
+// load the highlighter once
+let highlighter;
 (async () => {
-	shikiHighlighter = await getHighlighter({ theme });
+	highlighter = await getHighlighter({ theme });
 })();
+const highlight = {
+	highlighter: async (code, lang) => {
+		if (!highlighter) highlighter = await getHighlighter({ theme });
+		const html = highlighter.codeToHtml(code, { lang });
 
-const highlighter = async (code, lang) => {
-	if (!shikiHighlighter) shikiHighlighter = await getHighlighter({ theme });
-	const shikiCode = shikiHighlighter.codeToHtml(code, { lang });
-
-	return `<div class="code">
-    <span>${lang}${icons[lang] || icons.editor}</span>
-    ${escapeSvelte(shikiCode)}
-    </div>`;
+		return `<div class="code"><span>${lang}${icons[lang] || icons.editor}</span>${escapeSvelte(
+			html
+		)}</div>`;
+	}
 };
 
 export default {
 	extensions,
 	smartypants,
 	layout,
-	highlight: {
-		highlighter
-	}
+	highlight
 };
