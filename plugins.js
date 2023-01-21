7b85f1948d153d99d205770eb6587be03b26602b
Gabriele Corti <borntofrappe@gmail.com>
2023-01-21 18:02:37 +0100

    remove superfluous function

--- plugins.js
+++ plugins.js
@@ -4,33 +4,6 @@ import { unified } from 'unified';
 import rehypeParse from 'rehype-parse';
 import icons from './src/lib/utils/icons.js';
 
-export const remarkCode = () => async (tree) => {
-	const theme = 'rose-pine-moon';
-	const highlighter = await getHighlighter({ theme });
-
-	visit(tree, 'code', (node) => {
-		const { lang, value } = node;
-
-		const code = value
-			.replace(/&lt;/g, '<')
-			.replace(/&gt;/g, '>')
-			.replace(/&#123;/g, '{')
-			.replace(/&#125;/g, '}');
-
-		const string = highlighter
-			.codeToHtml(code, { lang })
-			.replace(/{/g, '&#123;')
-			.replace(/}/g, '&#125;');
-
-		const html = `<div class="code"><span>${lang || 'text'}${
-			icons[lang] || icons.editor
-		}</span>${string}</div>`;
-
-		node.type = 'html';
-		node.value = html;
-	});
-};
-
 export const rehypeCodeHighlight = () => async (tree) => {
 	const theme = 'rose-pine-moon';
 	const highlighter = await getHighlighter({ theme });
