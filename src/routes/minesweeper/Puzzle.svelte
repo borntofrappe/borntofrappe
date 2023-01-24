188821e62fc3c5b5d039ce173562f3d9193a9783
Gabriele Corti <borntofrappe@gmail.com>
2023-01-24 10:35:50 +0100

    function creating a custom store

--- src/routes/minesweeper/Puzzle.svelte
+++ src/routes/minesweeper/Puzzle.svelte
@@ -1,5 +1,24 @@
 <script>
 	import Display from './Display.svelte';
+	import { createTimer } from './utils.js';
+
+	const timer = createTimer();
 </script>
 
-<Display />
+<button
+	on:click={() => {
+		timer.start();
+	}}>Start</button
+>
+<button
+	on:click={() => {
+		timer.stop();
+	}}>Stop</button
+>
+<button
+	on:click={() => {
+		timer.reset();
+	}}>Reset</button
+>
+
+<Display value={$timer.toString()} />
