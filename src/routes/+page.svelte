43b4dddb2a18f43bd89c2d8d1bc6003ded69bfdd
Gabriele Corti <borntofrappe@gmail.com>
2023-01-24 15:18:52 +0100

    remove superfluous footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Header from './Header.svelte';
 	import Showcase from './Showcase.svelte';
-	import icons from '$lib/utils/icons.js';
 </script>
 
 <svelte:head>
@@ -11,14 +10,3 @@
 
 <Header />
 <Showcase />
-
-<footer>
-	<p>
-		Updated <time datetime="2023-01-22T18:14:31">January 22nd 2023 {@html icons.calendar}</time>.<br
-		/>Feel free to
-		<a
-			href="https://github.com/borntofrappe/borntofrappe/issues/new?title=Dated%20content&body=Please%20update%20the%20website"
-			>file an issue</a
-		> if the message becomes too old.
-	</p>
-</footer>
