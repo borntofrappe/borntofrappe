d7e83d9d172e5fde53d1e0b00172e067b962363d
Gabriele Corti <borntofrappe@gmail.com>
2023-01-31 22:53:58 +0100

    prerender route

--- src/routes/feed.xml/+server.js
+++ src/routes/feed.xml/+server.js
@@ -1,18 +1,34 @@
+export const prerender = true;
+
 import site from '$lib/utils/site.js';
 
-export async function GET({ fetch }) {
+export async function GET() {
 	const { title, description, origin, author } = site;
 	const { name, email } = author;
 
-	const response = await fetch('/blog');
-	const { blog } = await response.json();
+	const modules = import.meta.glob('$blog/**/*.{md,svx}');
+	const blog = await Promise.all(
+		Object.entries(modules).map(async ([path, module]) => {
+			const [slug] = path.split('/').pop().split('.');
+			const url = `${origin}/${slug}`;
+			const { metadata } = await module();
+
+			const date = new Date(metadata.date);
+
+			return {
+				...metadata,
+				url,
+				date
+			};
+		})
+	);
 
 	// prettier-ignore
 	const feed = `<?xml version="1.0" encoding="utf-8"?>
     <feed xmlns="http://www.w3.org/2005/Atom">
         <title>${title}</title>
         <subtitle>${description}</subtitle>
-        <link href="${origin}/feed" rel="self" />
+        <link href="${origin}/feed.xml" rel="self" />
         <link href="${origin}" />
         <updated>${new Date().toISOString()}</updated>
         <id>${origin}/</id>
@@ -21,11 +37,12 @@ export async function GET({ fetch }) {
         <email>${email}</email>
         </author>
         ${blog
-			.map(({ slug, title, date, description }) => `
+			.sort((a, b) => b.date - a.date)
+			.map(({ url, title, date, description }) => `
             <entry>
                 <title>${title}</title>
-                <link href="${origin}/${slug}"/>
-                <id>${origin}/${slug}</id>
+                <link href="${url}"/>
+                <id>${url}</id>
                 <updated>${new Date(date).toISOString()}</updated>
                 <summary>${description}</summary>
             </entry>`).join('')}
