b376ac9a5f71646193c7a682590d6ec5990ccbd6
Gabriele Corti <borntofrappe@gmail.com>
2023-01-31 21:24:35 +0100

    add site details

---
+++ src/lib/utils/site.js
@@ -0,0 +1,9 @@
+export default {
+	title: 'borntofrappe',
+	description: "Let's keep smiling.",
+	origin: 'https://borntofrappe.netlify.app',
+	author: {
+		name: 'Gabriele Corti',
+		email: 'borntofrappe@gmail.com'
+	}
+};
