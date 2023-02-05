db62fde1355fbe83725c87ec61f10c3263ca1391
Gabriele Corti <borntofrappe@gmail.com>
2023-02-05 18:49:01 +0100

    apply methodology to blog posts
    create blocks for permalink and skip to link elements

--- src/lib/layout/Blog.svelte
+++ src/lib/layout/Blog.svelte
@@ -40,7 +40,7 @@
 	<meta name="description" content={description} />
 </svelte:head>
 
-<svg aria-hidden="true" style:position="absolute" style:width="0" style:height="0">
+<svg aria-hidden="true" style:position="absolute" style:inline-size="0" style:block-size="0">
 	<defs>
 		<filter id="filter-outline">
 			<feFlood flood-color={foreground || 'currentColor'} result="l1-color" />
@@ -61,25 +61,27 @@
 
 <a href="#content" class="skip-to-content visually-hidden">Skip to content</a>
 
-<div class={timeOfDay}>
-	<header style:background>
-		<h1 style:filter="url(#filter-outline)">{title}</h1>
-		<Theme {timeOfDay} {colors} on:change={handleChange} />
+<div class="flow {timeOfDay}">
+	<header
+		class="box"
+		style:--color="#f7f7f7"
+		style:--background="#838ace"
+		style:--padding="var(--step-space-300) var(--step-space-200) 0"
+		style:background
+	>
+		<h1 style:text-align="center" style:filter="url(#filter-outline)">{title}</h1>
+		<div class="center" style:--measure="40rem">
+			<Theme {timeOfDay} {colors} on:change={handleChange} />
+		</div>
 	</header>
-	<main id="content">
-		<slot />
-	</main>
+	<div class="box" style:padding="var(--step-space-200) var(--step-space-100)">
+		<main id="content" class="center flow" style:--measure="60ch">
+			<slot />
+		</main>
+	</div>
 </div>
 
 <style>
-	div {
-		min-height: 100vh;
-	}
-
-	div > :global(* + *) {
-		margin-block-start: 1em;
-	}
-
 	.morning {
 		color: var(--grey-700);
 		background: var(--grey-100);
@@ -98,31 +100,31 @@
 		--shadow: var(--color-900);
 	}
 
-	header {
-		color: #f7f7f7;
-		background: #838ace;
-		padding: var(--step-space-400) var(--step-space-200) 0;
+	main :global(h2),
+	main :global(h3) {
+		--space: var(--step-space-400);
 	}
 
-	h1 {
-		color: inherit;
-		text-align: center;
+	main :global(h2 + *),
+	main :global(h3 + *) {
+		--space: var(--step-space-200);
 	}
 
-	header > :global(svg) {
-		max-width: 36rem;
-		margin-inline: auto;
+	main :global(blockquote),
+	main :global(blockquote + *),
+	main :global(figure),
+	main :global(figure + *) {
+		--space: var(--step-space-300);
 	}
 
-	main {
-		max-width: 70ch;
-		margin-inline: auto;
-		padding: var(--step-space-200);
+	main :global(table),
+	main :global(table + *) {
+		--space: var(--step-space-300);
 	}
 
-	main > :global(* + *) {
-		margin-block-start: 1em;
-		margin-block-start: var(--vertical-rhythm, 1em);
+	main :global(pre),
+	main :global(pre + *) {
+		--space: var(--step-space-300);
 	}
 
 	main :global(ul > * + *),
@@ -130,35 +132,8 @@
 		margin-block-start: 0.75em;
 	}
 
-	main :global(h2),
-	main :global(h3) {
-		--vertical-rhythm: var(--step-space-400);
-	}
-
-	main :global(h2 + *),
-	main :global(h3 + *) {
-		--vertical-rhythm: var(--step-space-100);
-	}
-
-	main :global(blockquote),
-	main :global(blockquote + *),
-	main :global(figure),
-	main :global(figure + *) {
-		--vertical-rhythm: var(--step-space-300);
-	}
-
-	main :global(table),
-	main :global(table + *) {
-		--vertical-rhythm: var(--step-space-300);
-	}
-
-	main :global(pre),
-	main :global(pre + *) {
-		--vertical-rhythm: var(--step-space-300);
-	}
-
 	main :global(blockquote) {
-		max-width: 50ch;
+		max-inline-size: 50ch;
 		margin-inline: auto;
 		font-style: italic;
 		font-weight: 700;
@@ -172,76 +147,6 @@
 		border-block-end: 0.2rem solid var(--shadow);
 	}
 
-	main :global(.code) {
-		display: flex;
-		flex-direction: column;
-		font-size: var(--step-size-200);
-	}
-
-	main :global(.code pre) {
-		margin-block-start: 0;
-		padding: var(--step-space-300);
-		border-radius: 1rem;
-		line-height: 1.75;
-		tab-size: 2;
-	}
-
-	main :global(.code pre),
-	main :global(.code > span) {
-		color: var(--theme-color, #e0def4);
-		background: var(--theme-background, #232136);
-	}
-
-	main :global(.code > span) {
-		align-self: flex-end;
-		display: inline-flex;
-		align-items: center;
-		gap: 0 var(--step-space-100);
-		border-start-start-radius: 0.5rem;
-		border-start-end-radius: 0.5rem;
-		margin-inline-end: var(--step-space-200);
-		padding: var(--step-space-000) var(--step-space-200);
-		font-weight: 700;
-	}
-
-	main :global(.code > span > svg) {
-		display: block;
-		height: 1em;
-		width: auto;
-	}
-
-	main :global(.code pre) {
-		overflow-x: auto;
-	}
-
-	main :global(.code pre::-webkit-scrollbar) {
-		height: 0.4rem;
-	}
-
-	main :global(.code pre::-webkit-scrollbar-thumb) {
-		background: var(--accent);
-		border-radius: 0.2rem;
-	}
-
-	main :global(.permalink) {
-		display: flex;
-		align-items: center;
-		gap: 0 var(--step-space-100);
-	}
-
-	main :global(.permalink a) {
-		width: 1rem;
-		height: 1rem;
-		border-radius: 0.5rem;
-		opacity: 0.3;
-		background: currentColor;
-		transition: opacity 0.25s cubic-bezier(0.37, 0, 0.63, 1);
-	}
-
-	main :global(.permalink a:focus) {
-		opacity: 1;
-	}
-
 	@supports ((-webkit-mask-image: url()) or (mask-image: url())) {
 		main :global(blockquote) {
 			padding-block: 1.5em;
@@ -249,8 +154,8 @@
 
 		main :global(blockquote::before) {
 			content: '';
-			width: 3em;
-			height: 3em;
+			inline-size: 3em;
+			block-size: 3em;
 			background: var(--accent);
 			float: left;
 			margin-inline-end: var(--step-space-100);
@@ -280,79 +185,6 @@
 			  <use href="%23star" transform="translate(-15 20) scale(0.4)"/>\
 			</g>\
 		  </svg>');
-		}
-
-		main :global(.permalink a) {
-			width: 1em;
-			height: 1em;
-			border-radius: initial;
-			position: relative;
-			-webkit-mask-image: url('data:image/svg+xml,\
-			  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100">\
-				  <g transform="rotate(-45)">\
-				  <g fill="none" stroke="currentColor" stroke-width="10" stroke-linejoin="round" stroke-linecap="round">\
-					  <path id="link" stroke-dasharray="140 26" stroke-dashoffset="-20" d="M -21 9 a 21 21 0 0 1 0 -42 h 26 a 21 21 0 0 1 0 42 h -26" />\
-					  <use href="%23link" transform="scale(-1 -1)" />\
-				  </g>\
-				  </g>\
-			  </svg>');
-			mask-image: url('data:image/svg+xml,\
-			  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100">\
-				  <g transform="rotate(-45)">\
-				  <g fill="none" stroke="currentColor" stroke-width="10" stroke-linejoin="round" stroke-linecap="round">\
-					  <path id="link" stroke-dasharray="140 26" stroke-dashoffset="-20" d="M -21 9 a 21 21 0 0 1 0 -42 h 26 a 21 21 0 0 1 0 42 h -26" />\
-					  <use href="%23link" transform="scale(-1 -1)" />\
-				  </g>\
-				  </g>\
-			  </svg>');
-		}
-	}
-
-	.skip-to-content {
-		font-size: var(--size-200);
-		color: var(--background);
-		background: var(--color);
-		padding: var(--space-100) var(--space-300);
-		border-radius: 2rem;
-		text-decoration: none;
-		position: absolute;
-		top: var(--step-space-100);
-		left: 50%;
-		transform: translateX(-50%);
-		outline: none;
-		z-index: 5;
-	}
-
-	@supports ((-webkit-mask-image: url()) or (mask-image: url())) {
-		.skip-to-content {
-			display: inline-flex;
-			align-items: center;
-			gap: 0 var(--step-space-100);
-		}
-
-		.skip-to-content::after {
-			content: '';
-			width: 1em;
-			height: 1em;
-			background: currentColor;
-			mask-image: url('data:image/svg+xml,\
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100">\
-		  <g fill="none" stroke="currentColor" stroke-width="10" stroke-linecap="round" stroke-linejoin="round">\
-			<path d="M 3 -20 h 22 a 20 20 0 0 1 0 40 h -22 l 10 -10 -10 10 10 10" />\
-			<path d="M -45 -20 h 28" />\
-			<path d="M -45 0 h 28" />\
-			<path d="M -45 20 h 28" />\
-		  </g>\
-		</svg>');
-			-webkit-mask-image: url('data:image/svg+xml,\
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100">\
-		  <g fill="none" stroke="currentColor" stroke-width="10" stroke-linecap="round" stroke-linejoin="round">\
-			<path d="M 3 -20 h 22 a 20 20 0 0 1 0 40 h -22 l 10 -10 -10 10 10 10" />\
-			<path d="M -45 -20 h 28" />\
-			<path d="M -45 0 h 28" />\
-			<path d="M -45 20 h 28" />\
-		  </g>\
-		</svg>');
 		}
 	}
 </style>
