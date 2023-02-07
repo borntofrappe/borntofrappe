2d28048a49b7622331ea09fec56189d2bef0b2fb
Gabriele Corti <borntofrappe@gmail.com>
2023-02-07 21:55:51 +0100

    animate transform transition

--- src/routes/tangram/+page.svelte
+++ src/routes/tangram/+page.svelte
@@ -21,6 +21,8 @@
 		...piece,
 		style: styles[shape][i]
 	}));
+
+	let tangram = true;
 </script>
 
 <svelte:head>
@@ -29,12 +31,13 @@
 	<link rel="icon" href="/icons/tangram.svg" type="image/svg+xml" />
 </svelte:head>
 
+<input bind:checked={tangram} type="checkbox" />
 <main
 	class="box centered"
 	style:--color="hsl(216, 33%, 97%)"
 	style:--background="hsl(210, 24%, 16%)"
 >
-	<svg viewBox="{padding * -1} {padding * -1} {4 + padding * 2} {4 + padding * 2}">
+	<svg class:tangram viewBox="{padding * -1} {padding * -1} {4 + padding * 2} {4 + padding * 2}">
 		{#each paths as { d, fill, style }}
 			<path {d} {fill} {style} />
 		{/each}
@@ -47,25 +50,29 @@
 		max-width: 42rem;
 	}
 
-	svg path:nth-of-type(1) {
-		transform: translate(0px, 0px);
+	svg path {
+		transition: transform 0.75s cubic-bezier(0.49, 0.11, 0.1, 1.32);
 	}
-	svg path:nth-of-type(2) {
-		transform: translate(0px, 0px);
+
+	svg.tangram path:nth-of-type(1) {
+		transform: translate(0px, 0px) rotate(0deg) !important;
 	}
-	svg path:nth-of-type(3) {
-		transform: translate(3px, 1px);
+	svg.tangram path:nth-of-type(2) {
+		transform: translate(0px, 0px) rotate(0deg) !important;
 	}
-	svg path:nth-of-type(4) {
-		transform: translate(1px, 3px);
+	svg.tangram path:nth-of-type(3) {
+		transform: translate(3px, 1px) rotate(0deg) !important;
 	}
-	svg path:nth-of-type(5) {
-		transform: translate(2px, 4px);
+	svg.tangram path:nth-of-type(4) {
+		transform: translate(1px, 3px) rotate(0deg) !important;
 	}
-	svg path:nth-of-type(6) {
-		transform: translate(2px, 2px);
+	svg.tangram path:nth-of-type(5) {
+		transform: translate(2px, 4px) rotate(0deg) !important;
 	}
-	svg path:nth-of-type(7) {
-		transform: translate(0px, 4px);
+	svg.tangram path:nth-of-type(6) {
+		transform: translate(2px, 2px) rotate(0deg) !important;
+	}
+	svg.tangram path:nth-of-type(7) {
+		transform: translate(0px, 4px) rotate(0deg) !important;
 	}
 </style>
