ea831490735e65c9e478518ab22a0fc7759db784
Gabriele Corti <borntofrappe@gmail.com>
2023-02-07 22:22:55 +0100

    +page.svelte markup
    wrap the checkbox and svg in a common label element; position the input above the svg but hide it through opacity; add assistive label

--- src/routes/tangram/+page.svelte
+++ src/routes/tangram/+page.svelte
@@ -17,51 +17,71 @@
 	<link rel="icon" href="/icons/tangram.svg" type="image/svg+xml" />
 </svelte:head>
 
-<input bind:checked={tangram} type="checkbox" />
 <main
-	class="box centered {tangram ? 'tangram' : ''}"
+	class="box centered"
 	style:--color="hsl(216, 33%, 97%)"
 	style:--background="hsl(210, 24%, 16%)"
 >
-	<!--
-	<svg class:tangram viewBox="{padding * -1} {padding * -1} {4 + padding * 2} {4 + padding * 2}">
-		{#each paths as { d, fill, style }}
-			<path {d} {fill} {style} />
-		{/each}
-	</svg>
--->
-	<Tangram {shape} />
+	<label class:tangram>
+		<span class="visually-hidden">
+			{tangram ? 'Position the pieces into a particular shape.' : 'Move the pieces back together.'}
+		</span>
+		<input bind:checked={tangram} type="checkbox" />
+		<Tangram {shape} padding={2} />
+	</label>
 </main>
 
 <style>
-	main > :global(svg) {
+	label {
+		--size: 100vmin;
+		--max-size: 52rem;
 		display: block;
-		max-width: 42rem;
+		background: none;
+		border: none;
+		block-size: var(--size);
+		inline-size: var(--size);
+		max-block-size: var(--max-size);
+		max-inline-size: var(--max-size);
+		aspect-ratio: 1/1;
+		position: relative;
 	}
 
-	main > :global(svg path) {
+	label input {
+		position: absolute;
+		top: 0;
+		left: 0;
+		block-size: 100%;
+		inline-size: 100%;
+		opacity: 0;
+	}
+
+	label:focus:not(:focus-visible) {
+		outline: none;
+	}
+
+	label > :global(svg path) {
 		transition: transform 0.75s cubic-bezier(0.49, 0.11, 0.1, 1.32);
 	}
 
-	main.tangram > :global(svg path:nth-of-type(1)) {
+	label.tangram > :global(svg path:nth-of-type(1)) {
 		transform: translate(0px, 0px) rotate(0deg) !important;
 	}
-	main.tangram > :global(svg path:nth-of-type(2)) {
+	label.tangram > :global(svg path:nth-of-type(2)) {
 		transform: translate(0px, 0px) rotate(0deg) !important;
 	}
-	main.tangram > :global(svg path:nth-of-type(3)) {
+	label.tangram > :global(svg path:nth-of-type(3)) {
 		transform: translate(3px, 1px) rotate(0deg) !important;
 	}
-	main.tangram > :global(svg path:nth-of-type(4)) {
+	label.tangram > :global(svg path:nth-of-type(4)) {
 		transform: translate(1px, 3px) rotate(0deg) !important;
 	}
-	main.tangram > :global(svg path:nth-of-type(5)) {
+	label.tangram > :global(svg path:nth-of-type(5)) {
 		transform: translate(2px, 4px) rotate(0deg) !important;
 	}
-	main.tangram > :global(svg path:nth-of-type(6)) {
+	label.tangram > :global(svg path:nth-of-type(6)) {
 		transform: translate(2px, 2px) rotate(0deg) !important;
 	}
-	main.tangram > :global(svg path:nth-of-type(7)) {
+	label.tangram > :global(svg path:nth-of-type(7)) {
 		transform: translate(0px, 4px) rotate(0deg) !important;
 	}
 </style>
