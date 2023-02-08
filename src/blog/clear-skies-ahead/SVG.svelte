5cfacaf423b05898f9e8d18d416ab4b2d7482026
Gabriele Corti <borntofrappe@gmail.com>
2023-02-08 23:01:06 +0100

    update helper components

--- src/blog/clear-skies-ahead/SVG.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import svg from './svg.js';
-	export let key;
-</script>
-
-{@html svg[key].trim()}
--- src/blog/why-svelte-why-svg/Center.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let maxWidth = '28rem';
-</script>
-
-<div style:max-width={maxWidth} style:margin-inline="auto">
-	<slot />
-</div>
