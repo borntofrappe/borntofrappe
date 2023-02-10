c75017018019299c1c14833541cfa234fe2a8450
Gabriele Corti <borntofrappe@gmail.com>
2023-02-10 14:39:21 +0100

    restructure footer
    update utils stylesheet to remove unnecessary class and change the name of the custom property devoted to the floating layout

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -8,14 +8,16 @@
 	style:--background="hsl(210, 24%, 16%)"
 	style:--selection-color="hsl(216, 33%, 97%)"
 	style:--selection-background="hsl(212, 34%, 30%)"
-	style:--padding="var(--step-space-300)"
+	style:--padding="var(--step-space-400) var(--step-space-300)"
 >
-	<blockquote class="center flow text-align:center" style:--space="0.5em">
-		<p>Designed with care, and considerably more than seven pieces.</p>
-		<p><cite>Gabriele Corti</cite></p>
-	</blockquote>
+	<figure class="center stack" style:--leading="0">
+		<figcaption class="flow" style:--space="0.5em">
+			<p>Crafted with considerably more than seven pieces, but always with care.</p>
+			<p><cite>Gabriele Corti</cite>, 2023</p>
+		</figcaption>
 
-	<div class="center" style:--measure="8rem" style:--leading="0">
-		<Tangram shape="heart" padding={1} />
-	</div>
+		<div style:max-inline-size="8rem" style:margin-inline="auto">
+			<Tangram shape="heart" padding={1} />
+		</div>
+	</figure>
 </footer>
