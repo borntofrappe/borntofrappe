4a63a7ead5c08f5a4e7a20c0ab984092acd5fc0d
Gabriele Corti <borntofrappe@gmail.com>
2023-02-15 11:31:51 +0100

    add helper group elements

--- src/routes/mignon/Mayo.svelte
+++ src/routes/mignon/Mayo.svelte
@@ -1,17 +1,18 @@
 <script>
-	// /	import Chicklet from './Chicklet.svelte'
+	let angle = 90;
+	let scale = 1;
 </script>
-
-<!-- <Chicklet /> -->
 
 <svg viewBox="-55 -60 110 100">
 	<defs>
 		<path
-			transform="rotate(90)"
+			transform="rotate({angle})"
 			id="w"
 			d="M 0 0 c 6 -5 14 -8 20 0 -5 5 -2 10 -12 10 -2 0 -8 0 -8 -10"
 		/>
-		<path transform="rotate(0)" id="l" d="M -3 0 v 7 m 0 0 l 7 7 -7 -4 -7 4z v8" />
+		<g id="l" transform="rotate({angle / 2})">
+			<path transform="rotate(-45)" id="l" d="M -3 0 v 7 m 0 0 l 7 7 -7 -4 -7 4z v8" />
+		</g>
 	</defs>
 	<g
 		fill="#f0d584"
@@ -44,7 +45,13 @@
 				<path d="M -20 0 c -18 24 58 24 40 0 a 20 20 0 0 0 -40 0" />
 			</g>
 			<g transform="translate(0 -49.5)">
-				<path stroke-width="3" d="M -5 0 c 5 0 11 -2 11 -5 0 5 2.5 5 5 3 0 5 -10 7 -16 2" />
+				<g transform="scale({scale})">
+					<path
+						transform="scale(1.2)"
+						stroke-width="3"
+						d="M -5 0 c 5 0 11 -2 11 -5 0 5 2.5 5 5 3 0 5 -10 7 -16 2"
+					/>
+				</g>
 			</g>
 		</g>
 	</g>
@@ -60,7 +67,9 @@
 				<path d="M -9 -2 c 6 6 12 6 18 0 c -7 -5 -11 -5 -18 0" />
 			</g>
 			<g fill="#fc531c" stroke="#fc531c" stroke-width="1">
-				<path d="M -6 -2 c 3 4 9 4 12 0 c -4 1 -8 1 -12 0" />
+				<g transform="scale({scale})">
+					<path d="M -6 -2 c 3 4 9 4 12 0 c -4 1 -8 1 -12 0" />
+				</g>
 			</g>
 		</g>
 	</g>
