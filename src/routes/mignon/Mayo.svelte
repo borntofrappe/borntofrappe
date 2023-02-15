4feabe7190824885481f7c4208e1408065aebc0a
Gabriele Corti <borntofrappe@gmail.com>
2023-02-15 11:46:45 +0100

    add interaction

--- src/routes/mignon/Mayo.svelte
+++ src/routes/mignon/Mayo.svelte
@@ -1,17 +1,66 @@
 <script>
-	let angle = 90;
-	let scale = 1;
+	import { onMount } from 'svelte';
+	import { spring } from 'svelte/motion';
+
+	const startAngle = 90;
+	const angle = spring(startAngle, {
+		stiffness: 0.1,
+		damping: 0.25
+	});
+
+	const scale = spring(0.8, {
+		stiffness: 0.1,
+		damping: 0.2
+	});
+
+	let svg;
+	let t;
+	let h;
+
+	onMount(() => {
+		handleSize();
+	});
+
+	const handleSize = () => {
+		const { top, height } = svg.getBoundingClientRect();
+		t = top;
+		h = height;
+	};
+
+	const handleMove = ({ offsetY }) => {
+		const y = 1 - offsetY / h;
+		scale.set(0.8 + y * 0.2);
+		angle.set(startAngle - y * startAngle);
+	};
 </script>
 
-<svg viewBox="-55 -60 110 100">
+<svelte:window on:resize={handleSize} />
+
+<svg
+	viewBox="-55 -60 110 100"
+	bind:this={svg}
+	on:mousemove={({ offsetY }) => {
+		handleMove({ offsetY });
+	}}
+	on:touchmove|preventDefault={(e) => {
+		const { pageY: y } = e.touches[0];
+		handleMove({
+			offsetY: y - t
+		});
+	}}
+>
 	<defs>
 		<path
-			transform="rotate({angle})"
+			transform="rotate({$angle})"
 			id="w"
 			d="M 0 0 c 6 -5 14 -8 20 0 -5 5 -2 10 -12 10 -2 0 -8 0 -8 -10"
 		/>
-		<g id="l" transform="rotate({angle / 2})">
-			<path transform="rotate(-45)" id="l" d="M -3 0 v 7 m 0 0 l 7 7 -7 -4 -7 4z v8" />
+		<g id="l" transform="rotate({$angle / 2})">
+			<path
+				transform="rotate({(startAngle * -1) / 2})"
+				id="l"
+				d="M -2 -1 v 7 m 0 0 l 7 7 -7 -4 -7 4z v8"
+			/>
 		</g>
 	</defs>
 	<g
@@ -21,19 +70,19 @@
 		stroke-linecap="round"
 		stroke-linejoin="round"
 	>
-		<g transform="translate(27 -10)">
+		<g transform="translate(25 -10)">
 			<use href="#w" />
 		</g>
-		<g transform="scale(-1 1) translate(27 -10)">
+		<g transform="scale(-1 1) translate(25 -10)">
 			<use href="#w" />
 		</g>
 	</g>
 
 	<g fill="none" stroke="#fd971b" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
-		<g transform="translate(14 17)">
+		<g transform="translate(12 18)">
 			<use href="#l" />
 		</g>
-		<g transform="scale(-1 1) translate(14 17)">
+		<g transform="scale(-1 1) translate(12 18)">
 			<use href="#l" />
 		</g>
 	</g>
@@ -45,7 +94,7 @@
 				<path d="M -20 0 c -18 24 58 24 40 0 a 20 20 0 0 0 -40 0" />
 			</g>
 			<g transform="translate(0 -49.5)">
-				<g transform="scale({scale})">
+				<g transform="scale({$scale})">
 					<path
 						transform="scale(1.2)"
 						stroke-width="3"
@@ -67,7 +116,7 @@
 				<path d="M -9 -2 c 6 6 12 6 18 0 c -7 -5 -11 -5 -18 0" />
 			</g>
 			<g fill="#fc531c" stroke="#fc531c" stroke-width="1">
-				<g transform="scale({scale})">
+				<g transform="scale({$scale})">
 					<path d="M -6 -2 c 3 4 9 4 12 0 c -4 1 -8 1 -12 0" />
 				</g>
 			</g>
