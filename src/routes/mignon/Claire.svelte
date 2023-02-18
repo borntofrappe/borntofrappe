287af80c4d854f20f97069907f8122365977450d
Gabriele Corti <borntofrappe@gmail.com>
2023-02-18 13:58:13 +0100

    somber face

--- src/routes/mignon/Claire.svelte
+++ src/routes/mignon/Claire.svelte
@@ -1,19 +1,58 @@
 <svg viewBox="-55 -55 110 110">
-	<circle r="28" fill="#f2ec64" />
-	<g fill="#dad35f">
-		<g transform="translate(0 -3)">
-			<circle r="4.5" cx="10" />
-			<circle r="4.5" cx="-10" />
+	<defs>
+		<g id="eyelash">
+			<g fill="none" stroke-linejoin="round" stroke-linecap="round">
+				<path stroke-width="1.5" d="M -4 0 a 4 4 0 0 0 8 0" />
+				<g stroke-width="1">
+					<path d="M 0 4 v 2" />
+					<path transform="rotate(60)" d="M 0 4 v 2" />
+					<path transform="rotate(-60)" d="M 0 4 v 2" />
+					<path transform="rotate(-30)" d="M 0 4 v 2" />
+					<path transform="rotate(30)" d="M 0 4 v 2" />
+				</g>
+			</g>
 		</g>
+	</defs>
 
-		<circle r="1.75" cx="19.75" cy="14" />
-		<circle r="1.5" cx="16.5" cy="18.5" />
-		<circle r="1" cx="13.25" cy="21.5" />
+	<g>
+		<circle r="28" fill="#dad35f" />
+		<g stroke="#bfb5f5">
+			<g transform="translate(0 -4)">
+				<use href="#eyelash" x="-10" />
+				<use href="#eyelash" x="10" />
+			</g>
+		</g>
+		<g fill="#bfb5f5">
+			<circle r="1.75" cx="19.75" cy="14" />
+			<circle r="1.5" cx="16.5" cy="18.5" />
+			<circle r="1" cx="13.25" cy="21.5" />
 
-		<g transform="translate(0 10)">
-			<g transform="scale(1.25)">
-				<g stroke="#dad35f" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
-					<path d="M -1.5 0 v 1.5 a 1.5 1.5 0 0 0 3 0 v -1.5z" />
+			<g transform="translate(0 10)">
+				<g transform="scale(1)">
+					<g stroke="#bfb5f5" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
+						<path d="M -1.5 0 v 1.5 a 1.5 1.5 0 0 0 3 0 v -1.5z" />
+					</g>
+				</g>
+			</g>
+		</g>
+	</g>
+	<g opacity="0">
+		<circle r="28" fill="#f2ec64" />
+		<g fill="#dad35f">
+			<g transform="translate(0 -3)">
+				<circle r="4.5" cx="10" />
+				<circle r="4.5" cx="-10" />
+			</g>
+
+			<circle r="1.75" cx="19.75" cy="14" />
+			<circle r="1.5" cx="16.5" cy="18.5" />
+			<circle r="1" cx="13.25" cy="21.5" />
+
+			<g transform="translate(0 10)">
+				<g transform="scale(1.25)">
+					<g stroke="#dad35f" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
+						<path d="M -1.5 0 v 1.5 a 1.5 1.5 0 0 0 3 0 v -1.5z" />
+					</g>
 				</g>
 			</g>
 		</g>
