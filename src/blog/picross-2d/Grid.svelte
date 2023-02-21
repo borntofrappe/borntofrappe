559b899f20c29e9a7bc8349a3caa4206b8c314a3
Gabriele Corti <borntofrappe@gmail.com>
2023-02-21 11:44:48 +0100

    add demos for the article

---
+++ src/blog/picross-2d/Grid.svelte
@@ -0,0 +1,33 @@
+<script>
+	const level = `xxxoo
+xxxox
+xxxox
+xxoox
+oooox
+oooox
+oxxox
+oxxox`;
+
+	const grid = level.split('\n').map((row) => row.split(''));
+	const height = grid.length;
+	const width = grid[0].length;
+</script>
+
+<svg role="img" viewBox="0 0 {width} {height}">
+	<title>An upstanding giraffe</title>
+	<g fill="currentColor">
+		{#each grid as row, y}
+			{#each row as cell, x}
+				{#if cell === 'o'}
+					<rect {x} {y} width="1" height="1" />
+				{/if}
+			{/each}
+		{/each}
+	</g>
+</svg>
+
+<style>
+	svg {
+		display: block;
+	}
+</style>
