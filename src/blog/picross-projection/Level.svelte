a987506c3cdb1a31a8aa1137369e9080b1b10624
Gabriele Corti <borntofrappe@gmail.com>
2023-02-22 21:16:37 +0100

    Level.svelte with padding and props

--- src/blog/picross-projection/Level.svelte
+++ src/blog/picross-projection/Level.svelte
@@ -1,4 +1,8 @@
 <script>
+	export let padding = 4;
+	export let offset = false;
+	export let reverse = false;
+
 	const level = `xxxoo
 xxxox
 xxxox
@@ -12,19 +16,30 @@ oxxox`;
 	const width = grid[0].length;
 	const height = grid.length;
 
-	const coordinates = grid.reduce((acc, curr, y) => {
-		return [
-			...acc,
-			...curr.reduce((a, c, x) => {
-				return c === 'o' ? [...a, { x, y }] : a;
-			}, [])
-		];
-	}, []);
+	const coordinates = offset
+		? grid.reduce((acc, curr, i) => {
+				return [
+					...acc,
+					...curr.reduce((a, c, j) => {
+						const x = j / 2;
+						const y = i / 2 - x / 2;
+						return c === 'o' ? [...a, { x, y }] : a;
+					}, [])
+				];
+		  }, [])
+		: grid.reduce((acc, curr, y) => {
+				return [
+					...acc,
+					...curr.reduce((a, c, x) => {
+						return c === 'o' ? [...a, { x, y }] : a;
+					}, [])
+				];
+		  }, []);
 </script>
 
-<svg viewBox="0 0 {width} {height}">
+<svg viewBox="{(padding / 2) * -1} {(padding / 2) * -1} {width + padding} {height + padding}">
 	<g fill="currentColor">
-		{#each coordinates as { x, y }}
+		{#each reverse ? [...coordinates].reverse() : coordinates as { x, y }}
 			<use {x} {y} href="#cube" />
 		{/each}
 	</g>
