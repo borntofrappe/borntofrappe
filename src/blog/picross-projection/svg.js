d6be668b78d7ee11562e8ab601d69c5cac753a6f
Gabriele Corti <borntofrappe@gmail.com>
2023-02-22 21:04:37 +0100

    add projection & snippets

--- src/blog/picross-projection/svg.js
+++ src/blog/picross-projection/svg.js
@@ -37,5 +37,72 @@ export default {
         <path opacity="0.25" d="M 0.5 0.5 l 0.5 -0.25 0 0.5 -0.5 0.25" />
       </g>
     </g>
+  </svg>`,
+	'isometric-grid': `<svg viewBox="0 0 6 4">
+    <defs>
+      <pattern
+        id="pattern-tile"
+        viewBox="0 0 1 0.5"
+        width="1"
+        height="0.5"
+        patternUnits="userSpaceOnUse"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          stroke-width="0.04"
+          d="M 0 0.25 l 0.5 0.25 0.5 -0.25 -0.5 -0.25 -0.5 0.25z"
+        />
+      </pattern>
+    </defs>
+    <rect width="6" height="4" fill="url(#pattern-tile)" stroke="currentColor" stroke-width="0.08" />
+  </svg>`,
+	cubes: `<svg viewBox="0 0 3 1">
+    <g fill="currentColor">
+      <use x="0" y="0" href="#cube" />
+      <use x="1" y="0" href="#cube" />
+      <use x="2" y="0" href="#cube" />
+    </g>
+  </svg>`,
+	'offset-x': `<svg viewBox="0 0 2 1">
+    <g fill="currentColor">
+      <use x="0" y="0" href="#cube" />
+      <use x="0.5" y="0" href="#cube" />
+      <use x="1" y="0" href="#cube" />
+    </g>
+  </svg>
+  `,
+	reverse: `<svg viewBox="0 0 2 1">
+    <g fill="currentColor">
+      <use x="1" y="0" href="#cube" />
+      <use x="0.5" y="0" href="#cube" />
+      <use x="0" y="0" href="#cube" />
+    </g>
+  </svg>
+  `,
+	'offsets-y': `<svg viewBox="0 -0.5 2 1.5">
+    <g fill="currentColor">
+      <use x="1" y="-0.5" href="#cube" />
+      <use x="0.5" y="-0.25" href="#cube" />
+      <use x="0" y="0" href="#cube" />
+    </g>
+  </svg>`,
+	rows: `<svg viewBox="0 -0.5 2 3.5">
+    <g fill="currentColor">
+      <use x="1" y="-0.5" href="#cube" />
+      <use x="0.5" y="-0.25" href="#cube" />
+      <use x="0" y="0" href="#cube" />
+      <use x="0" y="1" href="#cube" />
+      <use x="0" y="2" href="#cube" />
+    </g>
+  </svg>`,
+	offsets: `<svg viewBox="0 -0.5 2 2.5">
+    <g fill="currentColor">
+      <use x="0" y="1" href="#cube" />
+      <use x="0" y="0.5" href="#cube" />
+      <use x="1" y="-0.5" href="#cube" />
+      <use x="0.5" y="-0.25" href="#cube" />
+      <use x="0" y="0" href="#cube" />
+    </g>
   </svg>`
 };
