37812181b5fe2c7a7939d586c5e622f67003122d
Gabriele Corti <borntofrappe@gmail.com>
2023-02-22 21:34:28 +0100

    center components

--- src/blog/picross-projection/Level.svelte
+++ src/blog/picross-projection/Level.svelte
@@ -1,5 +1,4 @@
 <script>
-	export let padding = 4;
 	export let offset = false;
 	export let reverse = false;
 
@@ -35,6 +34,8 @@ oxxox`;
 					}, [])
 				];
 		  }, []);
+
+	const padding = 4;
 </script>
 
 <svg viewBox="{(padding / 2) * -1} {(padding / 2) * -1} {width + padding} {height + padding}">
