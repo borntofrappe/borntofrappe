14fd47b6726d6508c6538325ea7f15f901e77d01
Gabriele Corti <borntofrappe@gmail.com>
2023-02-23 10:13:57 +0100

    update demos to follow article

--- src/blog/picross-projection/Level.svelte
+++ src/blog/picross-projection/Level.svelte
@@ -1,45 +1,36 @@
 <script>
-	export let padding = 4;
-	export let offset = false;
-	export let reverse = false;
+	import levels from './levels';
 
-	const level = `xxxoo
-xxxox
-xxxox
-xxoox
-oooox
-oooox
-oxxox
-oxxox`;
+	export let key = 'giraffe';
+	export let color = 'currentColor';
 
-	const grid = level.split('\n').map((row) => row.split(''));
-	const width = grid[0].length;
-	const height = grid.length;
+	const grid = levels[key].split('\n').map((row) => row.split(''));
 
-	const coordinates = offset
-		? grid.reduce((acc, curr, i) => {
-				return [
-					...acc,
-					...curr.reduce((a, c, j) => {
-						const x = j / 2;
-						const y = i / 2 - x / 2;
-						return c === 'o' ? [...a, { x, y }] : a;
-					}, [])
-				];
-		  }, [])
-		: grid.reduce((acc, curr, y) => {
-				return [
-					...acc,
-					...curr.reduce((a, c, x) => {
-						return c === 'o' ? [...a, { x, y }] : a;
-					}, [])
-				];
-		  }, []);
+	const coordinates = grid.reduce((acc, curr, i) => {
+		return [
+			...acc,
+			...curr.reduce((a, c, j) => {
+				const x = j / 2;
+				const y = i / 2 - x / 2;
+				return c === 'o' ? [...a, { x, y }] : a;
+			}, [])
+		];
+	}, []);
+
+	const xs = coordinates.map(({ x }) => x).sort((a, b) => a - b);
+	const x0 = xs[0];
+	const x1 = xs[xs.length - 1];
+	const width = x1 - x0 + 1;
+
+	const ys = coordinates.map(({ y }) => y).sort((a, b) => a - b);
+	const y0 = ys[0];
+	const y1 = ys[ys.length - 1];
+	const height = y1 - y0 + 1;
 </script>
 
-<svg viewBox="{(padding / 2) * -1} {(padding / 2) * -1} {width + padding} {height + padding}">
+<svg style:color viewBox="{x0} {y0} {width} {height}">
 	<g fill="currentColor">
-		{#each reverse ? [...coordinates].reverse() : coordinates as { x, y }}
+		{#each [...coordinates].reverse() as { x, y }}
 			<use {x} {y} href="#cube" />
 		{/each}
 	</g>
@@ -48,5 +39,9 @@ oxxox`;
 <style>
 	svg {
 		display: block;
+	}
+
+	svg use:hover {
+		filter: brightness(2);
 	}
 </style>
