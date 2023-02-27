0fe84f619596cce3e93c7dad751daeb47b13b00c
Gabriele Corti <borntofrappe@gmail.com>
2023-02-27 22:23:54 +0100

    wording

--- src/blog/a-coy-introduction-to-smil/Koinobori.svelte
+++ src/blog/a-coy-introduction-to-smil/Koinobori.svelte
@@ -1,24 +1,19 @@
 <script>
 	let animate = null;
-	let pause = false;
+	let stop = false;
 
-	const handleAnimation = (pause) => {
+	const handleAnimation = (stop) => {
 		if (animate === null) return;
 
-		if (pause) {
+		if (stop) {
 			animate.endElement();
 		} else {
 			animate.beginElement();
 		}
 	};
 
-	$: handleAnimation(pause);
+	$: handleAnimation(stop);
 </script>
-
-<label>
-	<input type="checkbox" bind:checked={pause} />
-	<span>{pause ? 'Restart' : 'Take a break'}</span>
-</label>
 
 <svg viewBox="0 0 30 25">
 	<g
@@ -41,7 +36,8 @@
 						values="
 						M 0 0 q 2 0.5 4 0 m 1.5 -4 c 6 2 14 -2 20 0 q 2 0 -2 3 2 1 3 3 c -14 -0.5 -14 2 -21 1 q 4 -3.5 0 -7 -4 4 0 7 m 4.5 -5 a 1 1 0 0 0 0 2 1 1 0 0 0 0 -2 m 3 -1 q 1 3.5 0 5.5 m 2 -6 q 1 3.5 0.5 5.5 h 0.5 a 1.5 1.5 0 0 0 0 -3 1.5 1.5 0 0 0 0 -3 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3 m 0 -6 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3; 
 						M 0 0 q 3 0 4.5 -1 m 0.5 -4 c 6 -3 14 0 20 -1 q 2 0 -2 3.5 0 1 3 2.5 c -14 0.5 -10 -2 -20 2 q 3 -3.5 -1 -7 -3 4 1 7 m 4.5 -6.5 a 1 1 0 0 0 0 2 1 1 0 0 0 0 -2 m 3.5 -1.5 q 1.5 2 0 5.5 m 2 -5.5 q 1 2 0 5.5 h 1 a 1.3 1.3 0 0 0 0 -2.6 1.25 1.25 0 0 0 0 -2.5 h 2 a 1.3 1.3 0 0 1 0 2.6 h -1 h 0.5 a 1.3 1.3 0 0 1 0 2.7 m 0 -5.4 h 2 a 1.3 1.3 0 0 1 0 2.8 h -1.5 h 1.25 a 1.4 1.4 0 0 1 0 2.9; 
-						M 0 0 q 2 0.5 4 0 m 1.5 -4 c 6 2 14 -2 20 0 q 2 0 -2 3 2 1 3 3 c -14 -0.5 -14 2 -21 1 q 4 -3.5 0 -7 -4 4 0 7 m 4.5 -5 a 1 1 0 0 0 0 2 1 1 0 0 0 0 -2 m 3 -1 q 1 3.5 0 5.5 m 2 -6 q 1 3.5 0.5 5.5 h 0.5 a 1.5 1.5 0 0 0 0 -3 1.5 1.5 0 0 0 0 -3 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3 m 0 -6 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3"
+						M 0 0 q 2 0.5 4 0 m 1.5 -4 c 6 2 14 -2 20 0 q 2 0 -2 3 2 1 3 3 c -14 -0.5 -14 2 -21 1 q 4 -3.5 0 -7 -4 4 0 7 m 4.5 -5 a 1 1 0 0 0 0 2 1 1 0 0 0 0 -2 m 3 -1 q 1 3.5 0 5.5 m 2 -6 q 1 3.5 0.5 5.5 h 0.5 a 1.5 1.5 0 0 0 0 -3 1.5 1.5 0 0 0 0 -3 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3 m 0 -6 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3
+						"
 						repeatCount="indefinite"
 						calcMode="spline"
 						keySplines="0.5 0 0.5 1; 0.5 0 0.5 1;"
@@ -53,7 +49,21 @@
 	</g>
 </svg>
 
+<label>
+	<input type="checkbox" bind:checked={stop} />
+	<span>{stop ? 'Start from the top' : 'Stop animation'}</span>
+</label>
+
 <style>
+	label {
+		display: block;
+	}
+
+	input {
+		inline-size: 1em;
+		block-size: 1em;
+	}
+
 	svg {
 		display: block;
 	}
