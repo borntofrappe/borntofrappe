8269df0612842760cac7874d0cea9dd047ac7119
Gabriele Corti <borntofrappe@gmail.com>
2023-02-27 21:20:59 +0100

    allow to stop the animation

--- src/blog/a-coy-introduction-to-smil/Koinobori.svelte
+++ src/blog/a-coy-introduction-to-smil/Koinobori.svelte
@@ -1,3 +1,25 @@
+<script>
+	let animate = null;
+	let pause = false;
+
+	const handleAnimation = (pause) => {
+		if (animate === null) return;
+
+		if (pause) {
+			animate.endElement();
+		} else {
+			animate.beginElement();
+		}
+	};
+
+	$: handleAnimation(pause);
+</script>
+
+<label>
+	<input type="checkbox" bind:checked={pause} />
+	<span>{pause ? 'Restart' : 'Take a break'}</span>
+</label>
+
 <svg viewBox="0 0 30 25">
 	<g
 		fill="none"
@@ -13,6 +35,7 @@
 					d="M 0 0 q 2 0.5 4 0 m 1.5 -4 c 6 2 14 -2 20 0 q 2 0 -2 3 2 1 3 3 c -14 -0.5 -14 2 -21 1 q 4 -3.5 0 -7 -4 4 0 7 m 4.5 -5 a 1 1 0 0 0 0 2 1 1 0 0 0 0 -2 m 3 -1 q 1 3.5 0 5.5 m 2 -6 q 1 3.5 0.5 5.5 h 0.5 a 1.5 1.5 0 0 0 0 -3 1.5 1.5 0 0 0 0 -3 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3 m 0 -6 h 2 a 1.5 1.5 0 0 1 0 3 h -1 h 1 a 1.5 1.5 0 0 1 0 3"
 				>
 					<animate
+						bind:this={animate}
 						attributeName="d"
 						dur="2s"
 						values="
@@ -22,6 +45,7 @@
 						repeatCount="indefinite"
 						calcMode="spline"
 						keySplines="0.5 0 0.5 1; 0.5 0 0.5 1;"
+						fill="freeze"
 					/>
 				</path>
 			</g>
