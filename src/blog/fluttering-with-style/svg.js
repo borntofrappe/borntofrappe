d77e8041c823b035e7ba3581748814c99a02eae4
Gabriele Corti <borntofrappe@gmail.com>
2023-03-01 11:57:25 +0100

    composition wings

--- src/blog/fluttering-with-style/svg.js
+++ src/blog/fluttering-with-style/svg.js
@@ -60,7 +60,67 @@ export default {
 <circle r="2.75" cx="-12" />
 <circle r="2.75" cx="12" />
 </g>
-
+<g stroke-linecap="round" stroke-linejoin="round">
+<g fill="#fd971b" stroke="#fd971b" stroke-width="3">
+  <path d="M -9 8 c 6 6 12 6 18 0 -7 -5 -11 -5 -18 0" />
+</g>
+<g fill="#fc531c" stroke="#fc531c" stroke-width="1">
+<path d="M -6 8 c 3 4 9 4 12 0 -4 1 -8 1 -12 0" />
+</g>
+</g>
+</g>
+</svg>`,
+	wing: `<svg viewBox="-30 -30 60 60">
+<path fill="currentColor" d="M 0 0 c 6 -5 14 -8 20 0 -5 5 -2 10 -12 10 -2 0 -8 0 -8 -10" />
+</svg>`,
+	'wing-stroke': `<svg viewBox="-30 -30 60 60">
+  <path 
+    fill="#f0d584"
+    stroke="#f0d584"
+    stroke-width="8"
+    stroke-linecap="round"
+    stroke-linejoin="round" 
+    fill="currentColor" d="M 0 0 c 6 -5 14 -8 20 0 -5 5 -2 10 -12 10 -2 0 -8 0 -8 -10" />
+</svg>`,
+	'mayo-5': `<svg viewBox="-60 -60 120 120">
+<defs>
+<path id="mayo-5-leg" d="M 0 0 l 0 7 7 7 -5.5 -3 -1.5 3 -1.5 -3 -5.5 3 7 -7z" />
+<path id="mayo-5-wing" d="M 0 0 c 6 -5 14 -8 20 0 -5 5 -2 10 -12 10 -2 0 -8 0 -8 -10" />
+</defs>
+<g transform="translate(0 17)" fill="#fd971b" stroke="#fd971b" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+<use href="#mayo-5-leg" x="-12" />
+<use href="#mayo-5-leg" x="12" />
+</g>
+<g transform="translate(0 -10)">
+<g 
+	fill="#f0d584"
+	stroke="#f0d584"
+	stroke-width="8"
+	stroke-linecap="round"
+	stroke-linejoin="round">
+	
+    <g transform="translate(25 0)">
+        <use href="#mayo-5-wing" transform="rotate(90)">
+    </g>
+    <g transform="scale(-1 1) translate(25 0)">
+        <use href="#mayo-5-wing" transform="rotate(90)">
+    </g>
+</g>
+</g>
+<ellipse fill="#f9f5d0" rx="28" ry="25" />
+<g transform="translate(0 -30)">
+<path
+    d="M -20 0 c -18 24 58 24 40 0 a 20 20 0 0 0 -40 0"
+	fill="#f9f5d0"
+	stroke="#f9f5d0"
+	stroke-width="4"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+/>
+<g fill="#542b19">
+<circle r="2.75" cx="-12" />
+<circle r="2.75" cx="12" />
+</g>
 <g stroke-linecap="round" stroke-linejoin="round">
 <g fill="#fd971b" stroke="#fd971b" stroke-width="3">
   <path d="M -9 8 c 6 6 12 6 18 0 -7 -5 -11 -5 -18 0" />
