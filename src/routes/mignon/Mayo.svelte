cc2ac9fe896b3ada7c0014d4313aca9dcd61bf53
Gabriele Corti <borntofrappe@gmail.com>
2023-03-01 14:47:47 +0100

    clean up svg syntax

--- src/routes/mignon/Mayo.svelte
+++ src/routes/mignon/Mayo.svelte
@@ -67,26 +67,31 @@
 	on:touchend|preventDefault={handleEnd}
 >
 	<defs>
-		<path
-			transform="rotate({$angle})"
-			id="w"
-			d="M 0 0 c 6 -5 14 -8 20 0 -5 5 -2 10 -12 10 -2 0 -8 0 -8 -10"
-		/>
-		<g id="l" transform="rotate({$angle / 2})">
-			<path
-				transform="rotate({(startAngle * -1) / 2})"
-				id="l"
-				d="M 0 0 l 0 7 7 7 -5.5 -3 -1.5 3 -1.5 -3 -5.5 3 7 -7z"
-			/>
-		</g>
+		<path id="l" d="M 0 0 l 0 7 7 7 -5.5 -3 -1.5 3 -1.5 -3 -5.5 3 7 -7z" />
+		<path id="w" d="M 0 0 c 6 -5 14 -8 20 0 -5 5 -2 10 -12 10 -2 0 -8 0 -8 -10" />
 	</defs>
+
 	<g transform="scale({$bounce})">
-		<g fill="none" stroke="#fd971b" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
+		<g
+			fill="#fd971b"
+			stroke="#fd971b"
+			stroke-width="4"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
 			<g transform="translate(12 17)">
-				<use href="#l" />
+				<g transform="rotate({$angle / 2})">
+					<g transform="rotate({(startAngle * -1) / 2})">
+						<use href="#l" />
+					</g>
+				</g>
 			</g>
 			<g transform="scale(-1 1) translate(12 17)">
-				<use href="#l" />
+				<g transform="rotate({$angle / 2})">
+					<g transform="rotate({(startAngle * -1) / 2})">
+						<use href="#l" />
+					</g>
+				</g>
 			</g>
 		</g>
 
@@ -97,45 +102,52 @@
 			stroke-linecap="round"
 			stroke-linejoin="round"
 		>
-			<g transform="translate(25 -10)">
-				<use href="#w" />
-			</g>
-			<g transform="scale(-1 1) translate(25 -10)">
-				<use href="#w" />
+			<g transform="translate(0 -10)">
+				<g transform="translate(25 0)">
+					<use href="#w" transform="rotate({$angle})" />
+				</g>
+				<g transform="scale(-1 1) translate(25 0)">
+					<use href="#w" transform="rotate({$angle})" />
+				</g>
 			</g>
 		</g>
 
-		<g fill="#f9f5d0">
-			<ellipse rx="28" ry="25" />
-			<g stroke="#f9f5d0" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
-				<g transform="translate(0 -30)">
-					<path d="M -20 0 c -18 24 58 24 40 0 a 20 20 0 0 0 -40 0" />
-					<g transform="translate(0 -20)">
-						<g transform="scale({$scale})">
-							<path stroke-width="3" d="M 0 0 c 5 0 11 -2 11 -5 0 5 2.5 5 5 3 0 5 -10 7 -16 2" />
-						</g>
+		<ellipse fill="#f9f5d0" rx="28" ry="25" />
+
+		<g transform="translate(0 -30)">
+			<g
+				fill="#f9f5d0"
+				stroke="#f9f5d0"
+				stroke-width="4"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path d="M -20 0 c -18 24 58 24 40 0 a 20 20 0 0 0 -40 0" />
+				<g transform="translate(0 -20)">
+					<g transform="scale({$scale})">
+						<path stroke-width="3" d="M 0 0 c 5 0 11 -2 11 -5 0 5 2.5 5 5 3 0 5 -10 7 -16 2" />
 					</g>
 				</g>
 			</g>
-		</g>
 
-		<g fill="#542b19">
-			<g transform="translate(12 -30)">
-				<circle class="blink" r="2.75" />
-			</g>
-			<g transform="translate(-12 -30)">
-				<circle class="blink" r="2.75" />
-			</g>
-		</g>
-
-		<g transform="translate(0 -20)">
-			<g stroke-linecap="round" stroke-linejoin="round">
-				<g fill="#fd971b" stroke="#fd971b" stroke-width="3">
-					<path d="M -9 -2 c 6 6 12 6 18 0 c -7 -5 -11 -5 -18 0" />
+			<g fill="#542b19">
+				<g transform="translate(12 0)">
+					<circle class="blink" r="2.75" />
 				</g>
-				<g fill="#fc531c" stroke="#fc531c" stroke-width="1">
-					<g transform="scale({$scale})">
-						<path d="M -6 -2 c 3 4 9 4 12 0 c -4 1 -8 1 -12 0" />
+				<g transform="translate(-12 0)">
+					<circle class="blink" r="2.75" />
+				</g>
+			</g>
+
+			<g transform="translate(0 10)">
+				<g stroke-linecap="round" stroke-linejoin="round">
+					<g fill="#fd971b" stroke="#fd971b" stroke-width="3">
+						<path d="M -9 -2 c 6 6 12 6 18 0 c -7 -5 -11 -5 -18 0" />
+					</g>
+					<g fill="#fc531c" stroke="#fc531c" stroke-width="1">
+						<g transform="scale({$scale})">
+							<path d="M -6 -2 c 3 4 9 4 12 0 c -4 1 -8 1 -12 0" />
+						</g>
 					</g>
 				</g>
 			</g>
