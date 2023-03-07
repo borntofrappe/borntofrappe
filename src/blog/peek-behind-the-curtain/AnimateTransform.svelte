af36c65bedbd2cf740927a2bccd2ca347afcc02d
Gabriele Corti <borntofrappe@gmail.com>
2023-03-07 17:50:25 +0100

    redesign shapes with stroke

--- src/blog/peek-behind-the-curtain/AnimateTransform.svelte
+++ src/blog/peek-behind-the-curtain/AnimateTransform.svelte
@@ -8,7 +8,7 @@
 	onMount(() => {
 		if (repeatEvent === false) return;
 		svg.querySelector('animateTransform').addEventListener('repeatEvent', () => {
-			const baseLightness = Math.random() > 0.7 ? 5 : 85;
+			const baseLightness = Math.random() > 0.75 ? 5 : 85;
 			const lightness = baseLightness + Math.floor(Math.random() * 15);
 			const color = `hsl(0, 0%, ${lightness}%)`;
 			svg.style.color = color;
@@ -34,59 +34,88 @@
 </label>
 
 <svg bind:this={svg} viewBox="0 0 100 60">
-	<g transform="translate(-50 32.5)">
-		<g>
+	<g transform="translate(50 32.5)">
+		<g transform="translate(-100 0)">
 			<animateTransform
 				attributeName="transform"
 				id="translate"
 				type="translate"
-				to="200 0"
+				values="-100 0; 100 0"
 				dur="7s"
 				begin="indefinite"
 				repeatCount="indefinite"
 			/>
 			<g fill="none" stroke="#27241D" stroke-width="3.5" stroke-linecap="round">
-				<path d="M -11 4 l -3 5" />
-				<path d="M -7 6 l -3 5.5" />
-				<path d="M 8.5 5.5 l 3 4.5" />
-				<path d="M 11.5 3.5 l 3.5 3.5" />
+				<path d="M -12 3 l -3.5 5.5" />
+				<path d="M -7.5 5.5 l -3.5 6" />
+				<path d="M 8.5 5.5 l 3.5 5" />
+				<path d="M 11.5 3.5 l 4 4" />
 			</g>
-			<g fill="currentColor">
-				<circle r="8" />
-				<circle r="8" cx="4.5" cy="4.5" />
-				<circle r="8" cx="-4.5" cy="4.5" />
-				<circle r="8" cx="-4.5" cy="-4.5" />
-				<circle r="8" cx="4.5" cy="-5" />
-				<circle r="8" cx="10" />
-				<circle r="8" cx="-10" cy="1" />
+			<g>
+				<g fill="#27241D">
+					<circle r="9" />
+					<circle r="9" cx="4.5" cy="4.5" />
+					<circle r="9" cx="-4.5" cy="4.5" />
+					<circle r="9" cx="-4.5" cy="-4.5" />
+					<circle r="9" cx="4.5" cy="-5" />
+					<circle r="9" cx="10" />
+					<circle r="9" cx="-10" cy="1" />
+				</g>
+				<g fill="currentColor">
+					<circle r="8" />
+					<circle r="8" cx="4.5" cy="4.5" />
+					<circle r="8" cx="-4.5" cy="4.5" />
+					<circle r="8" cx="-4.5" cy="-4.5" />
+					<circle r="8" cx="4.5" cy="-5" />
+					<circle r="8" cx="10" />
+					<circle r="8" cx="-10" cy="1" />
+				</g>
 			</g>
 			<g transform="translate(8 -5) rotate(-10)">
-				<ellipse fill="#FACDCD" stroke="#FACDCD" rx="6.5" ry="7.5" />
+				<ellipse fill="#FACDCD" stroke="#27241D" stroke-width="0.75" rx="7" ry="8.5" />
 				<g stroke="#27241D" stroke-linecap="round" stroke-linejoin="round">
-					<path d="M 0 5 v 1 q 0.5 0.5 1 0 m -1 0 q -0.5 0.5 -1 0" fill="none" stroke-width="0.4" />
+					<path
+						d="M 0 5.5 v 1 q 0.75 0.75 1.5 0 m -1.5 0 q -0.75 0.75 -1.5 0"
+						fill="none"
+						stroke-width="0.75"
+					/>
 					<path
 						d="M -1 4.5 c 0.5 -0.5 1.5 -0.5 2 0 -0.5 0.75 -1.5 0.75 -2 0"
 						fill="#27241D"
-						stroke-width="0.5"
+						stroke-width="1"
 					/>
 				</g>
-				<circle fill="#27241D" r="1.25" cx="-3" cy="1.5" />
-				<circle fill="#27241D" r="1.25" cx="3" cy="1.5" />
-				<g fill="currentColor">
-					<circle r="4" cx="-5" cy="-2" />
-					<circle fill="black" opacity="0.025" r="4" cx="-5" cy="-2" />
-					<circle r="4" cx="-2.5" cy="-5.5" />
-					<circle fill="black" opacity="0.025" r="4" cx="-2.5" cy="-5.5" />
-					<circle r="4" cx="2.5" cy="-5.5" />
-					<circle fill="black" opacity="0.025" r="4" cx="2.5" cy="-5.5" />
-					<circle r="4" cy="-1.75" />
-					<circle fill="black" opacity="0.025" r="4" cy="-1.75" />
-					<circle r="4" cx="5" cy="-2" />
-					<circle fill="black" opacity="0.025" r="4" cx="5" cy="-2" />
+
+				<g fill="#27241D">
+					<circle r="1.25" cx="-2.75" cy="2" />
+					<circle fill="#27241D" r="1.25" cx="2.75" cy="2" />
+				</g>
+				<g>
+					<g fill="#27241D">
+						<circle r="4.75" cx="-5" cy="-2" />
+						<circle r="4.75" cx="-2.5" cy="-5.5" />
+						<circle r="4.75" cx="2.5" cy="-5.5" />
+						<circle r="4.75" cy="-1.75" />
+						<circle r="4.75" cx="5" cy="-2" />
+					</g>
+					<g fill="currentColor">
+						<circle r="4" cx="-5" cy="-2" />
+						<circle fill="black" opacity="0.025" r="4" cx="-5" cy="-2" />
+						<circle r="4" cx="-2.5" cy="-5.5" />
+						<circle fill="black" opacity="0.025" r="4" cx="-2.5" cy="-5.5" />
+						<circle r="4" cx="2.5" cy="-5.5" />
+						<circle fill="black" opacity="0.025" r="4" cx="2.5" cy="-5.5" />
+						<circle r="4" cy="-1.75" />
+						<circle fill="black" opacity="0.025" r="4" cy="-1.75" />
+						<circle r="4" cx="5" cy="-2" />
+						<circle fill="black" opacity="0.025" r="4" cx="5" cy="-2" />
+					</g>
 				</g>
 			</g>
 		</g>
 	</g>
+
+	<rect width="100" height="60" fill="none" stroke="#27241D" stroke-width="1.5" />
 </svg>
 
 <style>
