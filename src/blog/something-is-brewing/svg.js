79b151e26c28420c4ace8d731e6f8a6c75ddb134
Gabriele Corti <borntforappe@gmail.com>
2023-03-09 21:24:06 +0100

    svg cauldron

---
+++ src/blog/something-is-brewing/svg.js
@@ -0,0 +1,14 @@
+export default {
+	cauldron: `<svg style="display: block; outline: 1px solid" viewBox="-40 -15 80 70">
+	<g>
+	<g fill="#2e2e2c">
+		<rect x="-20" width="40" height="8" rx="4" />
+		<path d="M -16 4 a 22 22 0 1 0 32 0" />
+	</g>
+	<g fill="none" stroke="#2e2e2c" stroke-width="6" stroke-linecap="round">
+		<path d="M -5 20 l -10 20" />
+		<path d="M 5 20 l 10 20" />
+	</g>
+	</g>
+</svg>`
+};
