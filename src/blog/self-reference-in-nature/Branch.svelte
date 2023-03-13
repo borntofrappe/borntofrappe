d10bc8d9aba19d59a71906c4dd6655d78ef92208
Gabriele Corti <borntforappe@gmail.com>
2023-03-13 15:25:51 +0100

    add final demo

--- src/blog/self-reference-in-nature/Branch.svelte
+++ src/blog/self-reference-in-nature/Branch.svelte
@@ -2,17 +2,10 @@
 	export let strokeWidth = 1.2;
 	export let length = 10;
 	export let generation = 0;
-	export let animationDuration = 1;
 </script>
 
 <g stroke-width={strokeWidth}>
-	<path
-		class="line"
-		pathLength="1"
-		style="animation-duration: {animationDuration}s; animation-delay: {generation *
-			animationDuration}s"
-		d="M 0 0 v -{length}"
-	/>
+	<path class="line" pathLength="1" data-generation={generation} d="M 0 0 v -{length}" />
 	{#if strokeWidth > 0}
 		<g transform="translate(0 -{length})">
 			<g transform="rotate({Math.random() * 5})">
@@ -20,35 +13,19 @@
 					strokeWidth={strokeWidth - 0.3}
 					length={length - 2}
 					generation={generation + 1}
-					{animationDuration}
 				/>
 			</g>
 			<g transform="rotate(25)">
-				<svelte:self
-					strokeWidth={strokeWidth - 0.3}
-					generation={generation + 1}
-					{animationDuration}
-				/>
+				<svelte:self strokeWidth={strokeWidth - 0.3} generation={generation + 1} />
 			</g>
 
 			<g transform="rotate(-25)">
-				<svelte:self
-					strokeWidth={strokeWidth - 0.3}
-					generation={generation + 1}
-					{animationDuration}
-				/>
+				<svelte:self strokeWidth={strokeWidth - 0.3} generation={generation + 1} />
 			</g>
 		</g>
 	{:else}
 		<g transform="translate(0 -{length})">
-			<path
-				class="marker"
-				style="animation-duration: {animationDuration}s; animation-delay: {(generation + 1) *
-					animationDuration +
-					Math.random()}s"
-				d="M 0 0 l 0 0"
-				marker-end="url(#marker-branch)"
-			/>
+			<use class="blossom" data-generation={generation} href="#tree-blossom" />
 		</g>
 	{/if}
 </g>
--- src/blog/self-reference-in-nature/Tree.svelte
+++ src/blog/self-reference-in-nature/Tree.svelte
@@ -1,61 +1,86 @@
 <script>
 	import Branch from './Branch.svelte';
+
+	let svg = null;
+	const duration = 1000;
+	const handleAnimation = () => {
+		const lines = svg.querySelectorAll('.line');
+		const blossoms = svg.querySelectorAll('.blossom');
+
+		lines.forEach((line) => {
+			line.style.strokeDasharray = 1;
+			const generation = parseInt(line.getAttribute('data-generation'), 10);
+			line.animate(
+				{
+					strokeDashoffset: [1, 0]
+				},
+				{
+					duration,
+					delay: generation * duration,
+					fill: 'both',
+					easing: 'linear'
+				}
+			);
+		});
+
+		blossoms.forEach((blossom) => {
+			const generation = parseInt(blossom.getAttribute('data-generation'), 10);
+			blossom.animate(
+				{
+					transform: ['rotate(0turn) scale(0)', 'rotate(0.2turn) scale(1)']
+				},
+				{
+					duration: 700 + Math.random() * 1000,
+					delay: (generation + 1) * duration,
+					fill: 'both',
+					easing: 'cubic-bezier(0.33, 1, 0.68, 1)'
+				}
+			);
+		});
+	};
 </script>
 
-<svg viewBox="0 0 70 55">
+<button on:click={handleAnimation}> Animate </button>
+
+<svg bind:this={svg} viewBox="0 0 70 55">
 	<title>Spring has sprung</title>
 	<defs>
-		<marker
-			id="marker-branch"
-			viewBox="-25.44444465637207 -26 49.88888931274414 48.133331298828125"
-			markerWidth="3"
-			markerHeight="3"
-			markerUnits="userSpaceOnUse"
-			orient="auto"
-		>
+		<symbol id="blossom" viewBox="-25.44444465637207 -26 49.88888931274414 48.133331298828125">
 			<path
 				fill="#eb919b"
 				d="M 0 20 Q -21 28 -20 6 Q -34 -11 -12 -17 Q -1 -35 11 -17 Q 33 -11 19 6 Q 20 28 0 20"
 			/>
 			<circle fill="#fdfafb" r="8" />
-		</marker>
+		</symbol>
+		<use id="tree-blossom" href="#blossom" x="-1.5" y="-1.5" width="3" height="3" stroke="none" />
 	</defs>
 	<g transform="translate(35 54)">
 		<g fill="none" stroke="#8a6f68" stroke-linecap="square">
-			<Branch animationDuration={1} />
+			<Branch />
 		</g>
 	</g>
 </svg>
 
 <style>
+	button {
+		display: block;
+		margin-inline: auto;
+		background: none;
+		border: none;
+		border-radius: 2rem;
+		color: inherit;
+		font-weight: 700;
+		padding: 0.25rem 0.75rem;
+		color: hsl(0, 0%, 92%);
+		background: hsl(0, 0%, 10%);
+		transition: transform 0.1s ease-in-out;
+	}
+
+	button:active {
+		transform: scale(0.95);
+	}
+
 	svg {
 		display: block;
-	}
-
-	svg :global(path.line) {
-		stroke-dasharray: 1;
-		animation: line 1s linear both;
-	}
-
-	svg :global(path.marker) {
-		animation: show 0.7s cubic-bezier(0.33, 1, 0.68, 1) both;
-	}
-
-	@keyframes line {
-		from {
-			stroke-dashoffset: 1;
-		}
-		to {
-			stroke-dashoffset: 0;
-		}
-	}
-
-	@keyframes show {
-		from {
-			transform: rotate(0turn) scale(0);
-		}
-		to {
-			transform: rotate(0.2turn) scale(1);
-		}
 	}
 </style>
