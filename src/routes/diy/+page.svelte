ac8329cd2b804df0ab3785b1f98b23e4a6c9f4a6
Gabriele Corti <borntofrappe@gmail.com>
2023-03-19 22:08:46 +0100

    init /diy route

---
+++ src/routes/diy/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import site from '$lib/utils/site.js';
+	const { title } = site;
+</script>
+
+<svelte:head>
+	<title>D.I.Y. | {title}</title>
+	<meta
+		name="description"
+		content="Play a collection of touch-based games, crafted with care and animated, scalable vector graphics."
+	/>
+	<link rel="icon" href="/icons/diy.svg" type="image/svg+xml" />
+</svelte:head>
