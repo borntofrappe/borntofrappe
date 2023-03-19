ff03f7c58ec37810ca64e6a4b187ed3bea0ce313
Gabriele Corti <borntofrappe@gmail.com>
2023-03-19 21:22:23 +0100

    Party.svelte

---
+++ src/blog/dragon-warrior-iii/Party.svelte
@@ -0,0 +1,39 @@
+<script>
+	import svg from './svg.js';
+	import { animate } from './utils.js';
+
+	const party = [
+		{
+			ref: 'hero',
+			style: '--c0: #030f00; --c1: #070673; --c2: #eef2d9;'
+		},
+		{
+			ref: 'edo',
+			style: '--c0: #030f00; --c1: #00932d; --c2: #eef2d9;'
+		},
+		{
+			ref: 'ripa',
+			style: '--c0: #030f00; --c1: #1111c2; --c2: #eef2d9;'
+		},
+		{
+			ref: 'icin',
+			style: '--c0: #030f00; --c1: #db1051; --c2: #eef2d9;'
+		}
+	];
+</script>
+
+{@html svg['party']}
+
+<svg use:animate style="display: block;" viewBox="0 0 {party.length} 1">
+	{#each party as { ref, style }, x}
+		<use {x} {style} href="#party-{ref}-0" width="1" height="1">
+			<animate
+				attributeName="href"
+				values="#party-{ref}-0; #party-{ref}-1"
+				dur="0.4s"
+				begin="indefinite"
+				repeatCount="indefinite"
+			/>
+		</use>
+	{/each}
+</svg>
--- src/blog/dragon-warrior-iii/svg.js
+++ src/blog/dragon-warrior-iii/svg.js
@@ -22,5 +22,249 @@ export default {
         />
       </g>
     </g>
-  </svg>`
+  </svg>`,
+	party: `<svg aria-hidden="true" style="width: 0; height: 0; position: absolute;">
+  <defs>
+    <symbol
+      id="party-hero-0"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Hero</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 4 0 h 9 M 2 1 h 1 M 4 1 h 8 M 1 2 h 13 M 1 3 h 12 M 1 4 h 13 M 1 5 h 12 M 1 6 h 11 M 1 7 h 11 M 1 8 h 13 M 0 9 h 15 M 0 10 h 15 M 1 11 h 14 M 2 12 h 13 M 1 13 h 14 M 1 14 h 14 M 1 15 h 11"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 7 3 h 2 M 7 4 h 2 M 5 10 h 1 M 10 10 h 3 M 2 11 h 2 M 5 11 h 3 M 10 11 h 3 M 5 13 h 5 M 9 14 h 2"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 2 2 h 1 M 7 2 h 2 M 2 3 h 1 M 6 3 h 1 M 9 3 h 1 M 2 4 h 1 M 4 4 h 3 M 9 4 h 3 M 2 5 h 1 M 2 6 h 1 M 5 6 h 1 M 7 6 h 2 M 10 6 h 1 M 2 7 h 1 M 5 7 h 1 M 7 7 h 2 M 10 7 h 1 M 2 8 h 1 M 6 8 h 2 M 1 9 h 3 M 5 9 h 1 M 9 9 h 5 M 6 10 h 2 M 9 10 h 1 M 13 10 h 1 M 9 11 h 1 M 13 11 h 1 M 7 12 h 2 M 10 12 h 3 M 2 13 h 2 M 2 14 h 2 M 13 13 h 1 M 5 15 h 3"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-hero-1"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Hero</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 2 0 h 1 M 4 0 h 9 M 1 1 h 11 M 1 2 h 13 M 1 3 h 12 M 1 4 h 13 M 1 5 h 12 M 1 6 h 11 M 1 7 h 11 M 0 8 h 14 M 0 9 h 15 M 1 10 h 14 M 2 11 h 13 M 1 12 h 14 M 1 13 h 14 M 1 14 h 14 M 4 15 h 10"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 7 3 h 2 M 7 4 h 2 M 2 10 h 2 M 5 10 h 1 M 5 11 h 3 M 10 11 h 3 M 10 12 h 3 M 5 13 h 4 M 5 14 h 2"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 2 1 h 1 M 2 2 h 1 M 7 2 h 2 M 2 3 h 1 M 6 3 h 1 M 9 3 h 1 M 2 4 h 1 M 4 4 h 3 M 9 4 h 3 M 2 5 h 1 M 2 6 h 1 M 5 6 h 1 M 7 6 h 2 M 10 6 h 1 M 2 7 h 1 M 5 7 h 1 M 7 7 h 2 M 10 7 h 1 M 1 8 h 3 M 6 8 h 4 M 5 9 h 1 M 6 10 h 2 M 9 10 h 5 M 9 11 h 1 M 13 11 h 1 M 2 12 h 2 M 7 12 h 1 M 9 12 h 1 M 13 12 h 1 M 2 13 h 2 M 10 13 h 3 M 13 14 h 1 M 8 15 h 3"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-edo-0"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Edo</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 4 0 h 8 M 4 1 h 8 M 4 2 h 8 M 4 3 h 8 M 4 4 h 8 M 4 5 h 8 M 3 6 h 10 M 3 7 h 10 M 3 8 h 10 M 2 9 h 11 M 2 10 h 12 M 3 11 h 11 M 3 12 h 10 M 3 13 h 9 M 3 14 h 9 M 4 15 h 7"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 5 9 h 1 M 10 9 h 1 M 5 10 h 2 M 8 10 h 3 M 6 11 h 1 M 4 12 h 1 M 6 12 h 5 M 4 14 h 3 M 8 14 h 3"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 5 2 h 1 M 10 2 h 1 M 5 3 h 2 M 9 3 h 2 M 5 4 h 2 M 9 4 h 2 M 5 5 h 6 M 5 6 h 1 M 7 6 h 2 M 10 6 h 1 M 4 7 h 2 M 7 7 h 2 M 10 7 h 2 M 6 8 h 4 M 3 9 h 1 M 11 9 h 1 M 3 10 h 1 M 12 10 h 1 M 7 11 h 2 M 12 11 h 1 M 4 15 h 3"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-edo-1"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Edo</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 4 0 h 8 M 4 1 h 8 M 4 2 h 8 M 4 3 h 8 M 4 4 h 8 M 4 5 h 8 M 3 6 h 10 M 3 7 h 10 M 3 8 h 10 M 3 9 h 11 M 2 10 h 12 M 2 11 h 11 M 3 12 h 10 M 3 13 h 9 M 3 14 h 9 M 4 15 h 7"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 5 9 h 1 M 10 9 h 1 M 5 10 h 3 M 9 10 h 2 M 6 11 h 1 M 4 12 h 1 M 6 12 h 5 M 4 14 h 3 M 8 14 h 3"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 5 2 h 1 M 10 2 h 1 M 5 3 h 2 M 9 3 h 2 M 5 4 h 2 M 9 4 h 2 M 5 5 h 6 M 5 6 h 1 M 7 6 h 2 M 10 6 h 1 M 4 7 h 2 M 7 7 h 2 M 10 7 h 2 M 6 8 h 4 M 4 9 h 1 M 12 9 h 1 M 3 10 h 1  M 12 10 h 1 M 3 11 h 1 M 7 11 h 2 M 8 15 h 3"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-ripa-0"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Ripa</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 6 0 h 4 M 5 1 h 6 M 4 2 h 8 M 3 3 h 10 M 3 4 h 10 M 3 5 h 10 M 3 6 h 10 M 3 7 h 10 M 3 8 h 10 M 3 9 h 10 M 3 10 h 11 M 3 11 h 11 M 3 12 h 8 M 3 13 h 8 M 3 14 h 8 M 4 15 h 6"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 6 1 h 4 M 5 2 h 1 M 10 2 h 1 M 7 3 h 2 M 7 4 h 2 M 6 10 h 1 M 9 10 h 1 M 7 11 h 2 M 6 12 h 3 M 6 13 h 2"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M  6 3 h 1 M 9 3 h 1 M 5 4 h 2 M 9 4 h 2 M 5 6 h 1 M 7 6 h 2 M 10 6 h 1 M 4 7 h 2 M 7 7 h 2 M 10 7 h 2 M 6 8 h 4 M 11 9 h 1 M 4 10 h 1 M 7 10 h 2 M 12 10 h 1 M 5 12 h 1 M 9 12 h 1 M 5 13 h 1 M 9 13 h 1 M 5 14 h 2 M 6 15 h 1"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-ripa-1"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Ripa</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 6 0 h 4 M 5 1 h 6 M 4 2 h 8 M 3 3 h 10 M 3 4 h 10 M 3 5 h 10 M 3 6 h 10 M 3 7 h 10 M 3 8 h 10 M 3 9 h 10 M 2 10 h 11 M 2 11 h 11 M 5 12 h 7 M 5 13 h 7 M 5 14 h 7 M 6 15 h 5"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 6 1 h 4 M 5 2 h 1 M 10 2 h 1 M 7 3 h 2 M 7 4 h 2 M 6 10 h 1 M 9 10 h 1 M 7 11 h 2 M 7 12 h 3 M 8 13 h 2"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M  6 3 h 1 M 9 3 h 1 M 5 4 h 2 M 9 4 h 2 M 5 6 h 1 M 7 6 h 2 M 10 6 h 1 M 4 7 h 2 M 7 7 h 2 M 10 7 h 2 M 6 8 h 4 M 4 9 h 1 M 3 10 h 1 M 7 10 h 2 M 11 10 h 1 M 6 12 h 1 M 10 12 h 1 M 6 13 h 1 M 10 13 h 1 M 9 14 h 2 M 9 15 h 1"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-icin-0"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Icin</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 1 0 h 2 M 6 0 h 4 M 13 0 h 2 M 1 1 h 14 M 1 2 h 14 M 1 3 h 14 M 1 4 h 14 M 1 5 h 13 M 1 6 h 13 M 1 7 h 12 M 1 8 h 13 M 0 9 h 15 M 0 10 h 16 M 1 11 h 15 M 2 12 h 13 M 4 13 h 10 M 4 14 h 7 M 5 15 h 5"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 7 1 h 2 M 5 2 h 1 M 7 2 h 2 M 10 2 h 1 M 4 3 h 2 M 10 3 h 2 M 4 4 h 3 M 9 4 h 3 M 4 5 h 1 M 7 5 h 2 M 11 5 h 1 M 4 6 h 1 M 11 6 h 1 M 4 8 h 1 M 11 8 h 1 M 5 9 h 1 M 10 9 h 1 M 12 9 h 2 M 6 10 h 1 M 9 10 h 1 M 11 10 h 1 M 14 10 h 1 M 2 11 h 2 M 6 11 h 4 M 11 11 h 1 M 14 11 h 1 M 12 12 h 2 M 7 13 h 2 M 8 14 h 1 M 6 15 h 2"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 6 1 h 1 M 9 1 h 1 M 13 1 h 1 M 2 2 h 1 M 6 2 h 1 M 9 2 h 1 M 12 2 h 2 M 2 3 h 1 M 6 3 h 1 M 8 3 h 2 M 12 3 h 1 M 2 4 h 1 M 13 4 h 1 M 2 5 h 1 M 12 5 h 1 M 2 6 h 1 M 5 6 h 1 M 10 6 h 1 M 2 7 h 1 M 5 7 h 1 M 7 7 h 2 M 10 7 h 1 M 2 8 h 1 M 6 8 h 4 M 1 9 h 3 M 7 10 h 2 M 12 10 h 2 M 12 11 h 2 M 5 13 h 1 M 10 13 h 1 M 5 14 h 2"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-icin-1"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>Icin</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 1 0 h 2 M 6 0 h 4 M 13 0 h 2 M 1 1 h 14 M 1 2 h 14 M 1 3 h 14 M 1 4 h 14 M 1 5 h 13 M 1 6 h 13 M 1 7 h 12 M 0 8 h 13 M 0 9 h 14 M 1 10 h 14 M 2 11 h 14 M 4 12 h 12 M 4 13 h 11 M 4 14 h 10 M 5 15 h 6"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 7 1 h 2 M 5 2 h 1 M 7 2 h 2 M 10 2 h 1 M 4 3 h 2 M 10 3 h 2 M 4 4 h 3 M 9 4 h 3 M 4 5 h 1 M 7 5 h 2 M 11 5 h 1 M 4 6 h 1 M 11 6 h 1 M 11 8 h 1 M 5 9 h 1 M 10 9 h 1 M 2 10 h 2 M 6 10 h 1 M 9 10 h 1 M 12 10 h 2 M 6 11 h 4 M 11 11 h 1 M 14 11 h 1 M 11 12 h 1 M 14 12 h 1 M 7 13 h 2 M 12 13 h 2 M 7 14 h 1 M 8 15 h 2"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 2 1 h 1 M 6 1 h 1 M 9 1 h 1 M 13 1 h 1 M 2 2 h 1 M 6 2 h 1 M 9 2 h 1 M 12 2 h 2 M 2 3 h 1 M 6 3 h 1 M 8 3 h 2 M 12 3 h 1 M 2 4 h 1 M 13 4 h 1 M 2 5 h 1 M 12 5 h 1 M 2 6 h 1 M 5 6 h 1 M 10 6 h 1 M 2 7 h 1 M 5 7 h 1 M 7 7 h 2 M 10 7 h 1 M 1 8 h 3 M 6 8 h 4 M 11 9 h 1 M 7 10 h 2 M 12 11 h 2 M 12 12 h 2 M 5 13 h 1 M 10 13 h 1 M 9 14 h 2"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-king-0"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>King</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 3 0 h 1 M 12 0 h 1 M 2 1 h 3 M 6 1 h 4 M 11 1 h 3 M 2 2 h 12 M 3 3 h 10 M 3 4 h 10 M 3 5 h 10 M 3 6 h 10 M 3 7 h 10 M 2 8 h 13 M 1 9 h 15 M 1 10 h 15 M 1 11 h 12 M 3 12 h 10 M 3 13 h 10 M 4 14 h 9 M 5 15 h 7"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 3 1 h 1 M 12 1 h 1 M 4 2 h 1 M 6 2 h 1 M 9 2 h 1 M 11 2 h 1 M 5 3 h 1 M 10 3 h 1 M 4 4 h 3 M 9 4 h 3 M 5 5 h 1 M 10 5 h 1 M 4 6 h 2 M 7 6 h 2 M 10 6 h 2 M 5 7 h 1 M 10 7 h 1 M 3 8 h 1 M 12 8 h 1 M 2 9 h 4 M 10 9 h 4 M 3 10 h 1 M 11 10 h 1 M 13 10 h 1 M 4 11 h 1 M 11 11 h 1 M 4 12 h 1 M 11 12 h 1 M 4 13 h 2 M 10 13 h 2 M 5 14 h 2 M 8 14 h 4"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 3 2 h 1 M 12 2 h 1 M 6 7 h 1 M 9 7 h 1 M 6 8 h 4 M 14 9 h 1 M 2 10 h 1 M 5 10 h 1 M 7 10 h 2 M 10 10 h 1 M 5 11 h 2 M 9 11 h 2 M 5 12 h 6 M 6 13 h 4 M 9 15 h 2"
+          />
+        </g>
+      </g>
+    </symbol>
+    <symbol
+      id="party-king-1"
+      viewBox="0 -0.5 16 16"
+      shape-rendering="crispEdges"
+    >
+      <title>King</title>
+      <g stroke="currentColor">
+        <g stroke="var(--c0, hsl(0, 0%, 10%))">
+          <path
+            d="M 3 0 h 1 M 12 0 h 1 M 2 1 h 3 M 6 1 h 4 M 11 1 h 3 M 2 2 h 12 M 3 3 h 10 M 3 4 h 10 M 3 5 h 10 M 3 6 h 10 M 3 7 h 10 M 1 8 h 13 M 0 9 h 15 M 0 10 h 15 M 3 11 h 12 M 3 12 h 10 M 3 13 h 10 M 3 14 h 9 M 4 15 h 7"
+          />
+        </g>
+        <g stroke="var(--c1, hsl(0, 0%, 47%))">
+          <path
+            d="M 3 1 h 1 M 12 1 h 1 M 4 2 h 1 M 6 2 h 1 M 9 2 h 1 M 11 2 h 1 M 5 3 h 1 M 10 3 h 1 M 4 4 h 3 M 9 4 h 3 M 5 5 h 1 M 10 5 h 1 M 4 6 h 2 M 7 6 h 2 M 10 6 h 2 M 5 7 h 1 M 10 7 h 1 M 3 8 h 1 M 12 8 h 1 M 2 9 h 4 M 10 9 h 4 M 2 10 h 1 M 4 10 h 1 M 12 10 h 1  M 4 11 h 1 M 11 11 h 1 M 4 12 h 1 M 11 12 h 1 M 4 13 h 2 M 10 13 h 2 M 4 14 h 4 M 9 14 h 2"
+          />
+        </g>
+        <g stroke="var(--c2, hsl(0, 0%, 92%))">
+          <path
+            d="M 3 2 h 1 M 12 2 h 1 M 6 7 h 1 M 9 7 h 1 M 6 8 h 4 M 1 9 h 1 M 13 10 h 1 M 5 10 h 1 M 7 10 h 2 M 10 10 h 1 M 5 11 h 2 M 9 11 h 2 M 5 12 h 6 M 6 13 h 4 M 5 15 h 2"
+          />
+        </g>
+      </g>
+    </symbol>
+  </defs>
+</svg>`
 };
