b2848cffcb8e0cdd9417f1d1882a71cd96997098
Gabriele Corti <borntofrappe@gmail.com>
2023-03-19 21:15:12 +0100

    helmet.svg

---
+++ src/blog/dragon-warrior-iii/svg.js
@@ -0,0 +1,26 @@
+export default {
+	helmet: `<svg
+    style="display: block; --c0: #030f00; --c1: #d39854; --c2: #eef2d9;"
+    viewBox="0 -0.5 16 16"
+    shape-rendering="crispEdges"
+  >
+    <title>Helmet</title>
+    <g stroke="currentColor">
+      <g stroke="var(--c0, hsl(0, 0%, 10%))">
+        <path
+          d="M 7 2 h 2 M 6 3 h 4 M 0 4 h 2 M 5 4 h 6 M 14 4 h 2 M 0 5 h 16 M 0 6 h 16 M 0 7 h 16 M 1 8 h 14 M 1 9 h 14 M 1 10 h 14 M 1 11 h 14 M 1 12 h 14 M 1 13 h 2 M 13 13 h 2"
+        />
+      </g>
+      <g stroke="var(--c1, hsl(0, 0%, 47%))">
+        <path
+          d="M 7 3 h 2 M 5 5 h 1 M 10 5 h 1 M 3 6 h 3 M 10 6 h 3 M 3 7 h 2 M 6 7 h 1 M 9 7 h 1 M 11 7 h 2 M 3 8 h 1 M 7 8 h 2 M 12 8 h 1 M 2 9 h 2 M 12 9 h 2 M 2 10 h 1 M 13 10 h 1 M 2 11 h 1 M 13 11 h 1  M 2 12 h 1 M 6 12 h 4 M 13 12 h 1 "
+        />
+      </g>
+      <g stroke="var(--c2, hsl(0, 0%, 92%))">
+        <path
+          d="M 1 5 h 1 M 7 5 h 2 M 14 5 h 1 M 1 6 h 2 M 7 6 h 2 M 13 6 h 2 M 2 7 h 1 M 13 7 h 1"
+        />
+      </g>
+    </g>
+  </svg>`
+};
