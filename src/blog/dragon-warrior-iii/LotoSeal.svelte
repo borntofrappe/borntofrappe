49dc787aed63ab118fe8638c08b5e188fa88b20c
Gabriele Corti <borntofrappe@gmail.com>
2023-03-21 18:29:01 +0100

    update svg syntax

--- src/blog/dragon-warrior-iii/LotoSeal.svelte
+++ src/blog/dragon-warrior-iii/LotoSeal.svelte
@@ -5,7 +5,7 @@
 <svg
 	use:animate
 	style="display: block; --c0: #030f00; --c1: #a30f4f; --c2: #eef2d9;"
-	viewBox="0 -0.5 65 52"
+	viewBox="0 -0.5 63 49"
 	shape-rendering="crispEdges"
 >
 	<title>LotoSeal</title>
@@ -22,27 +22,19 @@
 			<rect width="65" height="52" y="-0.5" fill="black" />
 			<path
 				stroke="white"
-				d="M 1 1 h 3 M 61 1 h 3 M 1 2 h 5 M 59 2 h 5 M 1 3 h 8 M 56 3 h 8 M 1 4 h 9 M 55 4 h 9 M 1 5 h 11 M 53 5 h 11 M 2 6 h 13 M 50 6 h 13 M 4 7 h 13 M 48 7 h 13 M 5 8 h 15 M 32 8 h 1 M 45 8 h 15 M 7 9 h 15 M 32 9 h 1 M 43 9 h 15 M 9 10 h 14 M 31 10 h 3 M 42 10 h 14 M 3 11 h 4 M 14 11 h 10 M 31 11 h 3 M 41 11 h 10 M 58 11 h 4 M 3 12 h 6 M 16 12 h 8 M 31 12 h 3 M 41 12 h 8 M 56 12 h 6 M 3 13 h 12 M 19 13 h 6 M 30 13 h 5 M 40 13 h 6 M 50 13 h 12 M 4 14 h 21 M 29 14 h 7 M 40 14 h 21 M 4 15 h 21 M 29 15 h 7 M 40 15 h 21 M 5 16 h 21 M 29 16 h 7 M 39 16 h 21 M 6 17 h 21 M 28 17 h 9 M 38 17 h 21 M 8 18 h 49 M 11 19 h 43 M 15 20 h 35 M 19 21 h 27 M 7 22 h 51 M 7 23 h 51 M 7 24 h 51 M 8 25 h 49 M 8 26 h 49 M 10 27 h 45 M 21 28 h 23 M 14 29 h 5 M 22 29 h 21 M 46 29 h 5 M 12 30 h 41 M 11 31 h 43 M 11 32 h 43 M 11 33 h 14 M 26 33 h 13 M 40 33 h 14 M 13 34 h 11 M 27 34 h 11 M 41 34 h 11 M 17 35 h 8 M 28 35 h 9 M 40 35 h 8 M 18 36 h 7 M 28 36 h 3 M 34 36 h 3 M 40 36 h 7 M 19 37 h 6 M 28 37 h 3 M 34 37 h 3 M 40 37 h 6 M 20 38 h 5 M 28 38 h 3 M 34 38 h 3 M 40 38 h 5 M 20 39 h 5 M 28 39 h 3 M 34 39 h 3 M 40 39 h 5 M 20 40 h 4 M 28 40 h 3 M 34 40 h 3 M 41 40 h 4 M 20 41 h 4 M 28 41 h 3 M 34 41 h 3 M 41 41 h 4 M 21 42 h 2 M 28 42 h 3 M 34 42 h 3 M 42 42 h 2 M 28 43 h 3 M 34 43 h 3 M 28 44 h 3 M 34 44 h 3 M 28 45 h 3 M 34 45 h 3 M 28 46 h 3 M 34 46 h 3 M 28 47 h 3 M 34 47 h 3 M 28 48 h 3 M 34 48 h 3 M 28 49 h 3 M 34 49 h 3 M 29 50 h 1 M 35 50 h 1"
+				d="M 0 0 h 3 M 60 0 h 3 M 0 1 h 5 M 58 1 h 5 M 0 2 h 8 M 55 2 h 8 M 0 3 h 9 M 54 3 h 9 M 0 4 h 11 M 52 4 h 11 M 0 5 h 14 M 49 5 h 14 M 1 6 h 15 M 31 6 h 1 M 47 6 h 15 M 3 7 h 16 M 31 7 h 1 M 44 7 h 16 M 4 8 h 17 M 30 8 h 3 M 42 8 h 17 M 6 9 h 16 M 30 9 h 3 M 41 9 h 16 M 8 10 h 15 M 30 10 h 3 M 40 10 h 15 M 2 11 h 4 M 13 11 h 10 M 30 11 h 3 M 40 11 h 10 M 57 11 h 4 M 2 12 h 6 M 15 12 h 9 M 29 12 h 5 M 39 12 h 9 M 55 12 h 6 M 2 13 h 12 M 18 13 h 6 M 28 13 h 7 M 39 13 h 6 M 49 13 h 12 M 3 14 h 21 M 28 14 h 7 M 39 14 h 21 M 3 15 h 22 M 28 15 h 7 M 38 15 h 22 M 4 16 h 22 M 27 16 h 9 M 37 16 h 22 M 5 17 h 53 M 7 18 h 49 M 10 19 h 43 M 14 20 h 35 M 18 21 h 27 M 6 22 h 51 M 6 23 h 51 M 6 24 h 51 M 7 25 h 49 M 7 26 h 49 M 9 27 h 45 M 21 28 h 21 M 13 29 h 6 M 22 29 h 19 M 44 29 h 6 M 11 30 h 41 M 10 31 h 43 M 10 32 h 43 M 10 33 h 15 M 26 33 h 11 M 38 33 h 15 M 12 34 h 12 M 27 34 h 9 M 39 34 h 12 M 17 35 h 8 M 28 35 h 7 M 38 35 h 8 M 18 36 h 7 M 28 36 h 3 M 32 36 h 3 M 38 36 h 7 M 19 37 h 6 M 28 37 h 3 M 32 37 h 3 M 38 37 h 6 M 20 38 h 5 M 28 38 h 3 M 32 38 h 3 M 38 38 h 5 M 20 39 h 4 M 28 39 h 3 M 32 39 h 3 M 39 39 h 4 M 20 40 h 4 M 28 40 h 3 M 32 40 h 3 M 39 40 h 4 M 21 41 h 2 M 28 41 h 3 M 32 41 h 3 M 40 41 h 2 M 28 42 h 3 M 32 42 h 3 M 28 43 h 3 M 32 43 h 3 M 28 44 h 3 M 32 44 h 3 M 28 45 h 3 M 32 45 h 3 M 28 46 h 3 M 32 46 h 3 M 28 47 h 3 M 32 47 h 3 M 29 48 h 1 M 33 48 h 1"
 			/>
 		</mask>
 	</defs>
-
-	<path
-		opacity="0"
-		stroke="var(--c0, hsl(0, 0%, 10%))"
-		d="M 0 0 h 5 M 60 0 h 5 M 0 1 h 1 M 4 1 h 3 M 58 1 h 3 M 64 1 h 1 M 0 2 h 1 M 6 2 h 4 M 55 2 h 4 M 64 2 h 1 M 0 3 h 1 M 9 3 h 2 M 54 3 h 2 M 64 3 h 1 M 0 4 h 1 M 10 4 h 3 M 52 4 h 3 M 64 4 h 1 M 0 5 h 1 M 12 5 h 4 M 49 5 h 4 M 64 5 h 1 M 0 6 h 2 M 15 6 h 3 M 47 6 h 3 M 63 6 h 2 M 1 7 h 3 M 17 7 h 4 M 31 7 h 3 M 44 7 h 4 M 61 7 h 3 M 3 8 h 2 M 20 8 h 3 M 31 8 h 1 M 33 8 h 1 M 42 8 h 3 M 60 8 h 2 M 4 9 h 3 M 22 9 h 2 M 30 9 h 2 M 33 9 h 2 M 41 9 h 2 M 58 9 h 3 M 2 10 h 7 M 23 10 h 2 M 30 10 h 1 M 34 10 h 1 M 40 10 h 2 M 56 10 h 7 M 2 11 h 1 M 7 11 h 7 M 24 11 h 1 M 30 11 h 1 M 34 11 h 1 M 40 11 h 1 M 51 11 h 7 M 62 11 h 1 M 2 12 h 1 M 9 12 h 7 M 24 12 h 2 M 29 12 h 2 M 34 12 h 2 M 39 12 h 2 M 49 12 h 7 M 62 12 h 1 M 2 13 h 1 M 15 13 h 4 M 25 13 h 1 M 28 13 h 2 M 35 13 h 2 M 39 13 h 1 M 46 13 h 4 M 62 13 h 1 M 2 14 h 2 M 25 14 h 1 M 28 14 h 1 M 36 14 h 1 M 39 14 h 1 M 61 14 h 2 M 3 15 h 1 M 25 15 h 2 M 28 15 h 1 M 36 15 h 1 M 38 15 h 2 M 61 15 h 1 M 3 16 h 2 M 26 16 h 3 M 36 16 h 3 M 60 16 h 2 M 4 17 h 2 M 27 17 h 1 M 37 17 h 1 M 59 17 h 2 M 5 18 h 3 M 57 18 h 3 M 7 19 h 4 M 54 19 h 4 M 10 20 h 5 M 50 20 h 5 M 6 21 h 13 M 46 21 h 13 M 6 22 h 1 M 58 22 h 1 M 6 23 h 1 M 58 23 h 1 M 6 24 h 1 M 58 24 h 1 M 6 25 h 2 M 57 25 h 2 M 7 26 h 1 M 57 26 h 1 M 7 27 h 3 M 55 27 h 3 M 9 28 h 12 M 44 28 h 12 M 11 29 h 3 M 19 29 h 3 M 43 29 h 3 M 51 29 h 3 M 10 30 h 2 M 53 30 h 2 M 10 31 h 1 M 54 31 h 1 M 10 32 h 1 M 54 32 h 1 M 10 33 h 1 M 25 33 h 1 M 39 33 h 1 M 54 33 h 1 M 10 34 h 3 M 24 34 h 3 M 38 34 h 3 M 52 34 h 3 M 12 35 h 5 M 25 35 h 3 M 37 35 h 3 M 48 35 h 5 M 16 36 h 2 M 25 36 h 1 M 27 36 h 1 M 31 36 h 3 M 37 36 h 1 M 39 36 h 1 M 47 36 h 2 M 17 37 h 2 M 25 37 h 1 M 27 37 h 1 M 31 37 h 1 M 33 37 h 1 M 37 37 h 1 M 39 37 h 1 M 46 37 h 2 M 18 38 h 2 M 25 38 h 1 M 27 38 h 1 M 31 38 h 1 M 33 38 h 1 M 37 38 h 1 M 39 38 h 1 M 45 38 h 2 M 19 39 h 1 M 25 39 h 1 M 27 39 h 1 M 31 39 h 1 M 33 39 h 1 M 37 39 h 1 M 39 39 h 1 M 45 39 h 1 M 19 40 h 1 M 24 40 h 2 M 27 40 h 1 M 31 40 h 1 M 33 40 h 1 M 37 40 h 1 M 39 40 h 2 M 45 40 h 1 M 19 41 h 1 M 24 41 h 1 M 27 41 h 1 M 31 41 h 1 M 33 41 h 1 M 37 41 h 1 M 40 41 h 1 M 45 41 h 1 M 19 42 h 2 M 23 42 h 2 M 27 42 h 1 M 31 42 h 1 M 33 42 h 1 M 37 42 h 1 M 40 42 h 2 M 44 42 h 2 M 20 43 h 4 M 27 43 h 1 M 31 43 h 1 M 33 43 h 1 M 37 43 h 1 M 41 43 h 4 M 27 44 h 1 M 31 44 h 1 M 33 44 h 1 M 37 44 h 1 M 27 45 h 1 M 31 45 h 1 M 33 45 h 1 M 37 45 h 1 M 27 46 h 1 M 31 46 h 1 M 33 46 h 1 M 37 46 h 1 M 27 47 h 1 M 31 47 h 1 M 33 47 h 1 M 37 47 h 1 M 27 48 h 1 M 31 48 h 1 M 33 48 h 1 M 37 48 h 1 M 27 49 h 1 M 31 49 h 1 M 33 49 h 1 M 37 49 h 1 M 27 50 h 2 M 30 50 h 2 M 33 50 h 2 M 36 50 h 2 M 28 51 h 3 M 34 51 h 3"
-	/>
-
 	<rect
-		width="65"
-		height="1040"
+		width="63"
+		height="980"
 		y="-0.5"
 		mask="url(#loto-seal-mask)"
 		fill="url(#loto-seal-gradient)"
 	>
 		<animate begin="indefinite" attributeName="y" to="-988.5" dur="10s" />
 	</rect>
-
 	<g>
 		<animate
 			begin="indefinite"
@@ -52,16 +44,16 @@
 		/>
 		<g stroke="var(--c0, hsl(0, 0%, 10%))">
 			<path
-				d="M 31 22 h 3 M 30 23 h 5 M 29 24 h 7 M 28 25 h 9 M 28 26 h 9 M 28 27 h 9 M 29 28 h 7 M 30 29 h 5 M 31 30 h 3"
+				d="M 30 22 h 3 M 29 23 h 5 M 28 24 h 7 M 27 25 h 9 M 27 26 h 9 M 27 27 h 9 M 28 28 h 7 M 29 29 h 5 M 30 30 h 3"
 			/>
 		</g>
 		<g stroke="var(--c1, hsl(0, 0%, 47%))">
 			<path
-				d="M 31 23 h 3 M 30 24 h 2 M 34 24 h 1 M 29 25 h 3 M 34 25 h 2 M 29 26 h 7 M 29 27 h 7 M 30 28 h 2 M 34 28 h 1 M 31 29 h 3"
+				d="M 30 23 h 3 M 29 24 h 2 M 33 24 h 1 M 28 25 h 3 M 33 25 h 2 M 28 26 h 7 M 28 27 h 7 M 29 28 h 2 M 33 28 h 1 M 30 29 h 3"
 			/>
 		</g>
 		<g stroke="var(--c2, hsl(0, 0%, 92%))">
-			<path d="M 32 24 h 2 M 32 25 h 2 M 32 28 h 2" />
+			<path d="M 31 24 h 2 M 31 25 h 2 M 31 28 h 2" />
 		</g>
 		<g />
 	</g>
