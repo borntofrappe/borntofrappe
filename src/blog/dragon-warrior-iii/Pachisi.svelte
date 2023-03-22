fb0a9024c69a1252bfbdd09555a471281b4e41d2
Gabriele Corti <borntofrappe@gmail.com>
2023-03-22 22:01:27 +0100

    create board data structrue

--- src/blog/dragon-warrior-iii/Pachisi.svelte
+++ src/blog/dragon-warrior-iii/Pachisi.svelte
@@ -1,8 +1,69 @@
 <script>
 	import svg from './svg.js';
 
+	const getBoard = ({ columns = 4, rows = 3, ltr = true } = {}) => {
+		const actionPool = [1, 1, 1, 1, -1, -1, -1, -1, 2, 2, -2, -2, 3, -3];
+
+		const board = Array(rows * columns)
+			.fill()
+			.map((_) => {
+				const href = '#pachisi-grass';
+
+				return {
+					href,
+					ltr,
+					action: null
+				};
+			});
+
+		board[0].action = 'start';
+		board[board.length - 1].action = 'goal';
+		board[board.length - 1].href = '#pachisi-action-goal';
+
+		let pool = [...actionPool];
+		const { length } = board;
+		let step = 1;
+
+		while (step < length - 1) {
+			const i = Math.floor(Math.random() * pool.length);
+			const action = pool[i];
+			pool = [...pool.slice(0, i), ...pool.slice(i + 1)];
+
+			const stepAction = step + action;
+
+			if (
+				stepAction > 0 &&
+				stepAction < length &&
+				board[step].action === null &&
+				board[stepAction].action === null
+			) {
+				board[step].href = `#pachisi-action-${action}`;
+				board[step].action = action;
+				board[stepAction].action = 0;
+			}
+			step = Math.random() > 0.5 ? step + 2 : step + 3;
+		}
+
+		for (let i = 0; i < board.length; i++) {
+			const row = Math.floor(i / columns);
+			const even = row % 2 === 0;
+			const alt = even ? ltr : !ltr;
+			const column = alt ? i % columns : columns - 1 - (i % columns);
+			board[i].row = row;
+			board[i].column = column;
+		}
+
+		return board;
+	};
+
 	let dice = null;
 	let state = 'wait';
+	const columns = 4;
+	const rows = 3;
+	const ltr = true;
+
+	const board = getBoard({ columns, rows, ltr });
+	const { column, row } = board.find((d) => d.action === 'start');
 
 	const handleMove = (roll) => {
 		console.log(roll);
@@ -26,7 +87,6 @@
 		].join(';');
 
 		const dur = `${duration / 1000}s`;
-
 		dice.querySelector('use').setAttribute('href', value);
 
 		const animateTransform = dice.querySelector('animateTransform');
@@ -48,7 +108,7 @@
 {@html svg['dice']}
 {@html svg['board']}
 
-<svg viewBox="0 0 4 3">
+<svg viewBox="0 0 {columns} {rows}">
 	<defs>
 		<use
 			id="pachisi-grass"
@@ -58,7 +118,7 @@
 			height="1"
 		/>
 
-		<g id="pachisi-goal">
+		<g id="pachisi-action-goal">
 			<use
 				style="--c0: #030f00; --c1: #8c03ff; --c2: #eef2d9;"
 				href="#board-tile-action"
@@ -80,9 +140,38 @@
 		</g>
 
 		{#each Array(3) as _, i}
-			<g id="pachisi-forth-{i + 1}">
+			<g id="pachisi-action-{i + 1}">
 				<use
 					style="--c0: #030f00; --c1: #da3900; --c2: #eef2d9;"
+					href="#board-tile-action"
+					width="1"
+					height="1"
+				/>
+				<g transform="translate(0.5 0.5)">
+					<path
+						d="M 0 -0.25 l 0.075 0 0 0.1 0.05 0 0 0.05 -0.1 0.1 -0.05 0 -0.1 -0.1 0 -0.05 0.05 0 0 -0.1"
+						fill="#f3f6f5"
+					/>
+					<g
+						transform="translate(0 0.2)"
+						fill="#d39854"
+						stroke="#030f00"
+						paint-order="stroke"
+						stroke-width="0.06"
+						stroke-linejoin="bevel"
+						text-anchor="middle"
+						font-size="0.25"
+						word-spacing="-0.12"
+						font-family="monospace"
+					>
+						<text>+ {i + 1}</text>
+					</g>
+				</g>
+				<g />
+			</g>
+			<g id="pachisi-action-{(i + 1) * -1}">
+				<use
+					style="--c0: #030f00; --c1: #1348ee; --c2: #eef2d9;"
 					href="#board-tile-action"
 					width="1"
 					height="1"
@@ -105,80 +194,20 @@
 						word-spacing="-0.12"
 						font-family="monospace"
 					>
-						<text>+ {i + 1}</text>
-					</g>
-				</g>
-				<g />
-			</g>
-			<g id="pachisi-back-{i + 1}">
-				<use
-					style="--c0: #030f00; --c1: #1348ee; --c2: #eef2d9;"
-					href="#board-tile-action"
-					width="1"
-					height="1"
-				/>
-				<g transform="translate(0.5 0.5)">
-					<path
-						d="M 0 -0.25 l 0.075 0 0 0.1 0.05 0 0 0.05 -0.1 0.1 -0.05 0 -0.1 -0.1 0 -0.05 0.05 0 0 -0.1"
-						fill="#f3f6f5"
-					/>
-					<g
-						transform="translate(0 0.2)"
-						fill="#d39854"
-						stroke="#030f00"
-						paint-order="stroke"
-						stroke-width="0.06"
-						stroke-linejoin="bevel"
-						text-anchor="middle"
-						font-size="0.25"
-						word-spacing="-0.12"
-						font-family="monospace"
-					>
 						<text>- {i + 1}</text>
 					</g>
 				</g>
 			</g>
 		{/each}
 	</defs>
-	<g transform="translate(0 0)">
-		<use href="#pachisi-goal" />
-	</g>
-	<g transform="translate(1 0)">
-		<use href="#pachisi-grass" />
-	</g>
-	<g transform="translate(2 0)">
-		<use href="#pachisi-grass" />
-	</g>
-	<g transform="translate(3 0)">
-		<use href="#pachisi-grass" />
-	</g>
-	<g transform="translate(0 1)">
-		<use href="#pachisi-forth-1" />
-	</g>
-	<g transform="translate(1 1)">
-		<use href="#pachisi-forth-2" />
-	</g>
-	<g transform="translate(2 1)">
-		<use href="#pachisi-forth-3" />
-	</g>
-	<g transform="translate(3 1)">
-		<use href="#pachisi-grass" />
-	</g>
-	<g transform="translate(0 2)">
-		<use href="#pachisi-back-1" />
-	</g>
-	<g transform="translate(1 2)">
-		<use href="#pachisi-back-2" />
-	</g>
-	<g transform="translate(2 2)">
-		<use href="#pachisi-back-3" />
-	</g>
-	<g transform="translate(3 2)">
-		<use href="#pachisi-grass" />
-		<g />
-	</g>
 
-	<g transform="translate(3 2)">
+	{#each board as { column, row, href }}
+		<g transform="translate({column} {row})">
+			<use {href} />
+		</g>
+	{/each}
+
+	<g transform="translate({column} {row})">
 		<g transform="translate(0.5 0.5)">
 			<use
 				style="--c0: #030f00; --c1: #b15116; --c2: #eef2d9;"
