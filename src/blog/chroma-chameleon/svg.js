06fdd120dcf065de5db12aa8c35adb3c1f0883f1
Gabriele Corti <borntofrappe@gmail.com>
2023-03-23 21:04:21 +0100

    add chroma.svg

---
+++ src/blog/chroma-chameleon/svg.js
@@ -0,0 +1,36 @@
+export default {
+	chroma: `<svg style="display: block;" viewBox="-50 -40 100 100">
+	<g stroke-width="5.5" stroke-linecap="round" stroke-linejoin="round">
+		<path
+			fill="hsl(0, 0%, 26%)"
+			stroke="hsl(0, 0%, 26%)"
+			transform="translate(-2 -4)"
+			d="M 0 0 a 24 24 0 0 0 0 48 24 24 0 0 0 24 -24 a 1.5 1.5 0 0 0 -3 0 a 12.5 12.5 0 0 1 -25 0 12.5 12.5 0 0 1 12.5 -12.5 a 5.75 5.75 0 0 0 0 -11.5 h -8"
+		/>
+		<g fill="hsl(0, 0%, 20%)" stroke="hsl(0, 0%, 20%)">
+			<path
+				transform="translate(4 8) rotate(30)"
+				s
+				d="M 0 0 c 2 -1 8 -2 12 3 2 2 -8 -2 -16 2 1 0 4 -5 4 -5"
+			/>
+			<path
+				transform="translate(-10 22)"
+				d="M 0 0 c 4 -6 10 0 16 0 2 2 -6 2 -12 8 0 -2 0 -6  -4 -8"
+			/>
+		</g>
+		<path
+			transform="translate(0 -20)"
+			fill="hsl(0, 0%, 28%)"
+			stroke="hsl(0, 0%, 28%)"
+			d="M 0 0 a 20 20 0 0 1 26 20 l -6 0 a 2 2 0 0 0 0 6 l 4 0 c 2 2 -12 6 -20 0 -6 -4 -2 -10 -9 -18 -4 -4 3 -8 5 -8"
+		/>
+	</g>
+	<g transform="translate(10 -9)">
+		<g>
+			<circle fill="hsl(0, 0%, 50%)" r="8" />
+			<circle fill="hsl(0, 0%, 85%)" cx="-1.5" cy="-1.5" r="5" />
+			<circle fill="hsl(0, 0%, 90%)" cx="3.5" cy="4.5" r="1.5" />
+		</g>
+	</g>
+</svg>`
+};
