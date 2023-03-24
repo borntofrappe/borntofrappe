d3596ced77d381f5b14aa419d1035123cc341481
Gabriele Corti <borntofrappe@gmail.com>
2023-03-24 09:53:57 +0100

    svg syntax

--- src/blog/chroma-chameleon/svg.js
+++ src/blog/chroma-chameleon/svg.js
@@ -1,5 +1,5 @@
 export default {
-	chroma: `<svg style="display: block;" viewBox="0 0 100 100">
+	chameleon: `<svg style="display: block;" viewBox="0 0 100 100">
 	<g stroke-width="5.5" stroke-linecap="round" stroke-linejoin="round">
 		<path
 			transform="translate(48 36)"
@@ -32,6 +32,71 @@ export default {
 		<circle fill="hsl(0, 0%, 50%)" r="8" />
 		<circle fill="hsl(0, 0%, 85%)" cx="-1.5" cy="-1.5" r="5" />
 		<circle fill="hsl(0, 0%, 90%)" cx="3.5" cy="4.5" r="1.5" />
+	</g>
+</svg>`,
+	'linear-gradient': `<svg style="display: block" viewBox="0 0 100 100">
+	<defs>
+		<linearGradient id="linear-gradient-gradient">
+			<stop offset="0" stop-color="hsl(0, 85%, 72%)" />
+			<stop offset="0.2" stop-color="hsl(60, 85%, 72%)" />
+			<stop offset="0.4" stop-color="hsl(120, 85%, 72%)" />
+			<stop offset="0.6" stop-color="hsl(180, 85%, 72%)" />
+			<stop offset="0.8" stop-color="hsl(240, 85%, 72%)" />
+			<stop offset="1" stop-color="hsl(300, 85%, 72%)" />
+		</linearGradient>
+	</defs>
+	<rect fill="url(#linear-gradient-gradient)" width="100" height="100" />
+</svg>`,
+	'spread-method': `<svg style="display: block" viewBox="0 0 100 100">
+	<defs>
+		<linearGradient
+			id="spread-method-gradient"
+			x1="0"
+			x2="0.5"
+			y1="0"
+			y2="0"
+			spreadMethod="reflect"
+		>
+			<stop offset="0" stop-color="hsl(0, 85%, 72%)" />
+			<stop offset="0.2" stop-color="hsl(60, 85%, 72%)" />
+			<stop offset="0.4" stop-color="hsl(120, 85%, 72%)" />
+			<stop offset="0.6" stop-color="hsl(180, 85%, 72%)" />
+			<stop offset="0.8" stop-color="hsl(240, 85%, 72%)" />
+			<stop offset="1" stop-color="hsl(300, 85%, 72%)" />
+		</linearGradient>
+	</defs>
+	<rect fill="url(#spread-method-gradient)" width="100" height="100" />
+</svg>`,
+	path: `<svg style="display: block" viewBox="0 0 100 100">
+	<defs>
+		<linearGradient id="path-gradient" x1="0" x2="0.5" y1="0" y2="0" spreadMethod="reflect">
+			<stop offset="0" stop-color="hsl(0, 85%, 72%)" />
+			<stop offset="0.2" stop-color="hsl(60, 85%, 72%)" />
+			<stop offset="0.4" stop-color="hsl(120, 85%, 72%)" />
+			<stop offset="0.6" stop-color="hsl(180, 85%, 72%)" />
+			<stop offset="0.8" stop-color="hsl(240, 85%, 72%)" />
+			<stop offset="1" stop-color="hsl(300, 85%, 72%)" />
+		</linearGradient>
+	</defs>
+	<rect fill="url(#path-gradient)" width="100" height="100" />
+	<path fill="hsl(0, 0%, 28%)" d="M 10 70 a 40 40 0 0 1 80 0 l -20 0 a 20 20 0 0 0 -40 0z" />
+</svg>`,
+	'clip-path': `<svg style="display: block" viewBox="0 0 100 100">
+	<defs>
+		<linearGradient id="clip-path-gradient" x1="0" x2="0.5" y1="0" y2="0" spreadMethod="reflect">
+			<stop offset="0" stop-color="hsl(0, 85%, 72%)" />
+			<stop offset="0.2" stop-color="hsl(60, 85%, 72%)" />
+			<stop offset="0.4" stop-color="hsl(120, 85%, 72%)" />
+			<stop offset="0.6" stop-color="hsl(180, 85%, 72%)" />
+			<stop offset="0.8" stop-color="hsl(240, 85%, 72%)" />
+			<stop offset="1" stop-color="hsl(300, 85%, 72%)" />
+		</linearGradient>
+		<clipPath id="clip-path-clip">
+			<path d="M 10 70 a 40 40 0 0 1 80 0 l -20 0 a 20 20 0 0 0 -40 0z" />
+		</clipPath>
+	</defs>
+	<g clip-path="url(#clip-path-clip)">
+		<rect fill="url(#clip-path-gradient)" width="100" height="100" />
 	</g>
 </svg>`
 };
