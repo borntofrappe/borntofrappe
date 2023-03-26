b41c89df84ba9b653ec05430d0d82051aff2ec8e
Gabriele Corti <borntofrappe@gmail.com>
2023-03-26 21:44:42 +0200

    add background elements

--- src/routes/diy/PopTheBalloon.svelte
+++ src/routes/diy/PopTheBalloon.svelte
@@ -3,5 +3,28 @@
 </script>
 
 <svg style="display: block;" viewBox="0 0 80 50">
-	<Title fill="url(#linear-gradient-text)">Pop!</Title>
+	<defs>
+		<g id="pop-the-balloon-cloud">
+			<path
+				fill="#f7f7f7"
+				d="M -7 0 a 2 2 0 0 1 4 -2 3 3 0 0 1 6 0 2 2 0 0 1 4 2 2 2 0 0 1 0 4 1.85 1.85 0 0 1 -3.7 0 3.4 3.4 0 0 1 -6.6 0 1.85 1.85 0 0 1 -3.7 0 2 2 0 0 1 0 -4"
+			/>
+			<path
+				fill="#bdbebd"
+				d="M -7 0 a 2 2 0 0 0 0 4 1.85 1.85 0 0 0 3.7 0 3.4 3.4 0 0 0 6.6 0 1.85 1.85 0 0 0 3.7 0 2 2 0 0 0 0 -4 a 1.5 1.5 0 0 1 -0.5 2.5 a 1.4 1.4 0 0 1 -3 -0.5 a 3.6 3.6 0 0 1 -7 0 a 1.4 1.4 0 0 1 -3 0.5 a 1.5 1.5 0 0 1 -0.5 -2.5"
+			/>
+			<path
+				fill="none"
+				stroke="currentColor"
+				stroke-width="0.5"
+				stroke-linejoin="round"
+				stroke-linecap="round"
+				d="M -7 0 a 2 2 0 0 1 4 -2 3 3 0 0 1 6 0 2 2 0 0 1 4 2 2 2 0 0 1 0 4 1.85 1.85 0 0 1 -3.7 0 3.4 3.4 0 0 1 -6.6 0 1.85 1.85 0 0 1 -3.7 0 2 2 0 0 1 0 -4"
+			/>
+		</g>
+	</defs>
+	<rect fill="#10c2ce" width="80" height="50" />
+
+	<use transform="translate(15 15)" href="#pop-the-balloon-cloud" />
+	<use transform="translate(65 35)" href="#pop-the-balloon-cloud" />
 </svg>
