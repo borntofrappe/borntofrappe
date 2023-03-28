ac6d3da5ccd9c649e41c1010ef45de37e6e5a5cd
Gabriele Corti <borntofrappe@gmail.com>
2023-03-28 17:50:24 +0200

    add actors

--- src/routes/diy/FindTheButterflies.svelte
+++ src/routes/diy/FindTheButterflies.svelte
@@ -12,9 +12,72 @@
 				<path d="M 0 4.9 h 8" />
 			</g>
 		</pattern>
+
+		<symbol id="find-the-butterflies-ribbon" viewBox="-8 -8 16 16">
+			<g stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
+				<g fill="#2969bd">
+					<path d="M 0 -2 c 0 2 1 8 2 9 1 -2 3 -1 3 -2z" />
+					<path transform="scale(-1 1)" d="M 0 -2 c 0 2 1 8 2 9 1 -2 3 -1 3 -2z" />
+				</g>
+				<g fill="none">
+					<path d="M 0 -2 c 1 2 1 4 2.5 6.5" />
+					<path transform="scale(-1 1)" d="M 0 -2 c 1 2 1 4 2.5 6.5" />
+				</g>
+				<g fill="#2969bd">
+					<path d="M 0 -3 c 0 -4 10 -8 6 1 3 6 -6 6 -6 -1" />
+					<path transform="scale(-1 1)" d="M 0 -3 c 0 -4 10 -8 6 1 3 6 -6 6 -6 -1" />
+				</g>
+				<g fill="none">
+					<path d="M 0 -2.25 c 0 -2.5 5.5 -2.5 3.5 -0.25" />
+					<path transform="scale(-1 1)" d="M 0 -2.25 c 0 -2.5 5.5 -2.5 3.5 -0.25" />
+				</g>
+				<rect fill="#2969bd" x="-1.4" y="-3.6" width="2.8" height="3.2" rx="1" />
+			</g>
+		</symbol>
+
+		<symbol id="find-the-butterflies-butterfly" viewBox="-8 -8 16 16">
+			<g stroke="currentColor" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round">
+				<g fill="#2969bd">
+					<path transform="scale(1.05 1.08)" d="M 0 0 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1" />
+					<path d="M 0 0 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1" />
+					<g transform="scale(-1 1)">
+						<path transform="scale(1.05 1.08)" d="M 0 0 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1" />
+						<path d="M 0 0 c 0 -5 11.5 -9 5 1 3 6 -4 8 -5 -1" />
+					</g>
+				</g>
+
+				<path fill="#2969bd" d="M 0 -3 c 2.5 3 3.5 7 0 9 -3.5 -2 -2.5 -6 0 -9" />
+
+				<g fill="none">
+					<path transform="translate(0.5 -4) rotate(-40)" d="M 0 0 c 1 -2 5 -2 3.8 0 t -0.25 -1" />
+					<path
+						transform="scale(-1 1) translate(0.5 -4) rotate(-40)"
+						d="M 0 0 c 1 -2 5 -2 3.8 0 t -0.25 -1"
+					/>
+				</g>
+
+				<path fill="#2969bd" d="M 0 -4.5 c -1 0 -3 3 0 3 3 0 1 -3 0 -3" />
+
+				<g fill="none">
+					<path transform="translate(1.25 -3.5) rotate(-40)" d="M 0 -0.8 q 2 0.8 0 1.6z" />
+					<path
+						transform="scale(-1 1) translate(1.25 -3.5) rotate(-40)"
+						d="M 0 -0.8 q 2 0.8 0 1.6z"
+					/>
+				</g>
+			</g>
+		</symbol>
 	</defs>
 
 	<rect fill="url(#find-the-butterflies-pattern-bricks)" width="80" height="50" />
+
+	<g transform="translate(20 20)">
+		<use x="-8" y="-8" width="16" height="16" href="#find-the-butterflies-ribbon" />
+	</g>
+
+	<g transform="translate(60 20)">
+		<use x="-8" y="-8" width="16" height="16" href="#find-the-butterflies-butterfly" />
+	</g>
 
 	<g style:cursor="pointer">
 		<set begin="click" attributeName="display" to="none" fill="freeze" />
