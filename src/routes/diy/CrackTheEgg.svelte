d70cbdd2b7d90e9c2d241ccfee7a846fa9b25662
Gabriele Corti <borntofrappe@gmail.com>
2023-04-02 21:27:40 +0200

    add background

--- src/routes/diy/CrackTheEgg.svelte
+++ src/routes/diy/CrackTheEgg.svelte
@@ -1,3 +1,59 @@
 <svg style="display: block;" viewBox="0 0 80 50">
 	<title>Crack the egg!</title>
+
+	<defs>
+		<pattern
+			id="crack-the-egg-pattern-sky"
+			viewBox="0 0 2 2"
+			width="1"
+			height="1"
+			patternUnits="userSpaceOnUse"
+		>
+			<rect fill="#5bd2de" width="2" height="2" />
+			<g fill="#bdebe6">
+				<rect width="1" height="1" />
+				<rect x="1" y="1" width="1" height="1" />
+			</g>
+		</pattern>
+
+		<pattern
+			id="crack-the-egg-pattern-cloud"
+			viewBox="0 0 2 2"
+			width="1"
+			height="1"
+			patternUnits="userSpaceOnUse"
+		>
+			<rect fill="#e6e7e6" width="2" height="2" />
+			<g fill="#d6d2d6">
+				<rect width="1" height="1" />
+				<rect x="1" y="1" width="1" height="1" />
+			</g>
+		</pattern>
+	</defs>
+
+	<rect fill="url(#crack-the-egg-pattern-sky)" width="80" height="50" />
+
+	<g fill="url(#crack-the-egg-pattern-cloud)">
+		<g transform="translate(20 17.5)">
+			<circle r="9" />
+			<circle cx="-8" r="7" />
+			<circle cx="8" r="7" />
+		</g>
+
+		<g transform="translate(67.5 32.5)">
+			<circle r="9" />
+			<circle cx="-9" cy="3" r="6" />
+			<circle cx="9" cy="3" r="6" />
+		</g>
+
+		<g transform="translate(70 8)">
+			<ellipse rx="8" ry="5" />
+			<ellipse cx="-10" cy="4" rx="8" ry="5" />
+		</g>
+		<g transform="translate(5 40)">
+			<ellipse rx="9" ry="6" />
+			<ellipse cx="10" cy="2" rx="7" ry="4" />
+			/>
+		</g>
+	</g>
 </svg>
