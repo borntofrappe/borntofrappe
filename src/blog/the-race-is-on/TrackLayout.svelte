6cb3360ca4fffd98095dc4c78b6e262459bfedcd
Gabriele Corti <borntofrappe@gmail.com>
2023-04-13 14:21:07 +0200

    add useCurves prop
    show track layout with lines and curves

--- src/blog/the-race-is-on/TrackLayout.svelte
+++ src/blog/the-race-is-on/TrackLayout.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let accentColor = 'hotpink';
 	export let showPath = true;
+	export let useCurves = true;
 
 	const n = 12;
 	const offset = 40;
@@ -14,17 +15,31 @@
 			return [x, y];
 		});
 
-	const [x0, y0] = points[0];
-	const [x1, y1] = points[1];
-	const xm = (x0 + x1) / 2;
-	const ym = (y0 + y1) / 2;
+	let d = '';
 
-	let d = `M ${x0} ${y0} Q ${xm} ${ym} ${x1} ${y1} T`;
-	for (let i = 2; i < points.length; i++) {
-		const [x, y] = points[i];
-		d += `${x} ${y} `;
+	if (showPath) {
+		if (useCurves) {
+			const [x0, y0] = points[0];
+			const [x1, y1] = points[1];
+			const xm = (x0 + x1) / 2;
+			const ym = (y0 + y1) / 2;
+
+			d = `M ${x0} ${y0} Q ${xm} ${ym} ${x1} ${y1} T`;
+			for (let i = 2; i < points.length; i++) {
+				const [x, y] = points[i];
+				d += `${x} ${y} `;
+			}
+			d += `${x0} ${y0}z`;
+		} else {
+			const [x0, y0] = points[0];
+			d = `M ${x0} ${y0}`;
+			for (let i = 1; i < points.length; i++) {
+				const [x, y] = points[i];
+				d += ` L ${x} ${y}`;
+			}
+			d += ` L ${x0} ${y0}z`;
+		}
 	}
-	d += `${x0} ${y0}z`;
 </script>
 
 <svg viewBox="-50 -50 100 100">
