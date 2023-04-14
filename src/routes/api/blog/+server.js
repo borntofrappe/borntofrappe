cd34e8e5f3274f490181888f9ab731a3e0c6af4d
Gabriele Corti <borntofrappe@gmail.com>
2023-04-14 10:43:11 +0200

    search param 'limit'
    return a subset of articles from the api

--- src/routes/api/blog/+server.js
+++ src/routes/api/blog/+server.js
@@ -1,8 +1,14 @@
 import { json } from '@sveltejs/kit';
 import { content } from '$lib/search/content.server.js';
 
-export async function GET() {
-	const blog = await content();
+export async function GET({ url }) {
+	const limit = +url.searchParams.get('limit');
+
+	let blog = await content();
+
+	if (Number.isInteger(limit) && limit > 0) {
+		blog = blog.slice(0, limit);
+	}
 
 	return json({
 		blog
