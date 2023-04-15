be3327b9296d3c3ad525bb8697f552521760a17f
Gabriele Corti <borntofrappe@gmail.com>
2023-04-15 15:12:27 +0200

    add background

--- src/routes/diy/SmashTheRabbits.svelte
+++ src/routes/diy/SmashTheRabbits.svelte
@@ -1,3 +1,43 @@
 <svg style="display: block;" viewBox="0 0 80 50">
 	<title>Smash the rabbits!</title>
+
+	<defs>
+		<pattern
+			id="smash-the-rabbits-pattern-grass"
+			viewBox="-1 -1 6 6"
+			width="5"
+			height="5"
+			patternUnits="userSpaceOnUse"
+		>
+			<rect x="-1" y="-1" width="6" height="6" fill="#73d33e" />
+			<g fill="none" stroke="#34992f" stroke-width="0.5">
+				<path d="M 0.25 1 v 1.5" />
+				<path stroke="#98f463" d="M 0.25 1 v 0.5" />
+				<path d="M 4.75 3.5 v 1.5" />
+				<path stroke="#98f463" d="M 4.75 3.5 v 0.5" />
+				<path d="M 3.5 1 v 1.5" />
+				<path stroke="#96c636" d="M 3.5 1 v 0.5" />
+				<path d="M 4.5 0.5 v 0.5" />
+				<path d="M 2 1.5 v 0.5" />
+				<path d="M 0.5 3.5 v 0.5" />
+				<path d="M 1.5 4.5 v 0.5" />
+				<path d="M 2.5 2.75 v 0.5" />
+			</g>
+		</pattern>
+	</defs>
+
+	<rect fill="url(#smash-the-rabbits-pattern-grass)" width="80" height="50" />
+	<rect fill="#10c5ce" width="80" height="17" />
+	<g
+		transform="translate(0 17)"
+		fill="#73d33e"
+		stroke="#73d33e"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path d="M 0 0 l 5 -5 3 5 3 -8 3 5 3 -2 3 5z" />
+		<path transform="translate(40 0) scale(-1 1)" d="M 0 0 l 5 -5 3 5 3 -8 3 5 3 -2 3 5z" />
+		<path transform="translate(40 0)" d="M 0 0 l 5 -5 3 5 3 -8 3 5 3 -2 3 5z" />
+		<path transform="translate(80 0) scale(-1 1)" d="M 0 0 l 5 -5 3 5 3 -8 3 5 3 -2 3 5z" />
+	</g>
 </svg>
