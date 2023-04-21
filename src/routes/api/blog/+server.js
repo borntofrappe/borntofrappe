d8b6c8be06f6507865fffbf93760cb36cc36e650
Gabriele Corti <borntofrappe@gmail.com>
2023-04-21 15:48:27 +0200

    update search params
    +null is coerced into 0 meaning if you don't pass the search parameter this is interpreted as 0; this breaks [slug].svelte

--- src/routes/api/blog/+server.js
+++ src/routes/api/blog/+server.js
@@ -2,8 +2,8 @@ import { json } from '@sveltejs/kit';
 import { content } from '$lib/search/content.server.js';
 
 export async function GET({ url }) {
-	const page = +url.searchParams.get('page');
-	const limit = +url.searchParams.get('limit');
+	const page = url.searchParams.get('page') !== null && +url.searchParams.get('page');
+	const limit = url.searchParams.get('limit') !== null && +url.searchParams.get('limit');
 
 	let blog = await content();
 	const postsPerPage = 8;
