9b6903728eab191ec0e08d5eaf773db404bf7e53
Gabriele Corti <borntofrappe@gmail.com>
2023-04-23 22:20:20 +0200

    svg syntax
    add svg syntax to describe the new ladybug

--- src/blog/on-point/Coxy.svelte
+++ src/blog/on-point/Coxy.svelte
@@ -52,7 +52,7 @@
 		>
 			<g transform="rotate(90)">
 				<g transform="translate(0 -20)">
-					<g fill="none" stroke="hsl(37, 12%, 22%)" stroke-width="1.5" marker-end="url(#brianna-m)">
+					<g fill="none" stroke="hsl(37, 12%, 22%)" stroke-width="1.5">
 						<path d="M 0 0 l 12 -14" />
 						<path d="M 0 0 l -12 -14" />
 					</g>
@@ -62,23 +62,20 @@
 						<circle r="14" />
 					</g>
 				</g>
-
-				<g>
-					<circle fill="hsl(44, 92%, 63%)" r="25" />
-					<g fill="hsl(37, 12%, 22%)">
-						<circle cx="6" cy="-17" r="3" />
-						<circle cx="-6" cy="-17" r="3" />
-						<circle cx="15" cy="-9" r="2.5" />
-						<circle cx="-15" cy="-9" r="2.5" />
-						<circle cx="8" cy="-1" r="3.5" />
-						<circle cx="-8" cy="-1" r="3.5" />
-						<circle cx="16" cy="9" r="2.5" />
-						<circle cx="-16" cy="9" r="2.5" />
-						<circle cx="5" cy="10" r="3" />
-						<circle cx="-5" cy="10" r="3" />
-						<circle cx="9" cy="18" r="2" />
-						<circle cx="-9" cy="18" r="2" />
-					</g>
+				<circle fill="hsl(44, 92%, 63%)" r="25" />
+				<g fill="hsl(37, 12%, 22%)">
+					<circle cx="6" cy="-17" r="3" />
+					<circle cx="-6" cy="-17" r="3" />
+					<circle cx="15" cy="-9" r="2.5" />
+					<circle cx="-15" cy="-9" r="2.5" />
+					<circle cx="8" cy="-1" r="3.5" />
+					<circle cx="-8" cy="-1" r="3.5" />
+					<circle cx="16" cy="9" r="2.5" />
+					<circle cx="-16" cy="9" r="2.5" />
+					<circle cx="5" cy="10" r="3" />
+					<circle cx="-5" cy="10" r="3" />
+					<circle cx="9" cy="18" r="2" />
+					<circle cx="-9" cy="18" r="2" />
 				</g>
 			</g>
 		</marker>
