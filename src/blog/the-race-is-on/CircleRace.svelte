3291a7b0dbe5bb12f5b1346721bf8d3653e4896e
Gabriele Corti <borntofrappe@gmail.com>
2023-04-29 10:13:16 +0200

    update svg definitions
    draw the car in both components actually using it; remove now unnecessary svg.js definitions

--- src/blog/the-race-is-on/CircleRace.svelte
+++ src/blog/the-race-is-on/CircleRace.svelte
@@ -93,6 +93,81 @@
 	{/if}
 
 	<svg bind:this={svg} viewBox="-30 -30 60 60">
+		<defs>
+			<symbol id="circle-race-car" viewBox="0 0 100 70">
+				<mask id="circle-race-car-mask-cockpit">
+					<rect width="100" height="70" fill="white" />
+					<path d="M 66 0 v 15 a 20 20 0 0 0 0 40 v 15 h 34 v -70z" fill="black" />
+				</mask>
+				<mask id="circle-race-car-mask-spoiler">
+					<rect width="100" height="70" fill="white" />
+					<rect x="4" width="5" height="70" fill="black" />
+				</mask>
+
+				<g stroke="var(--c0, #220b08)">
+					<path stroke-width="5" d="M 24 0 v 70" />
+					<path stroke-width="4" d="M 73 20 v 30" />
+				</g>
+				<g fill="var(--c0, #220b08)">
+					<rect transform="translate(15 0)" rx="2" width="18" height="14" />
+					<rect transform="translate(15 56)" rx="2" width="18" height="14" />
+					<rect transform="translate(65 10)" rx="1.5" width="16" height="10" />
+					<rect transform="translate(65 50)" rx="1.5" width="16" height="10" />
+				</g>
+				<g stroke-linecap="round" stroke-linejoin="round">
+					<g stroke-width="2">
+						<path
+							fill="var(--c1, #273665)"
+							stroke="var(--c1, #273665)"
+							transform="translate(0 18)"
+							d="M 1 1 l 50 -1 l 8 5 q 30 2 40 12 q -10 10 -40 12 l -8 5 l -50 -1z"
+						/>
+						<path
+							fill="var(--c2, #4568a3)"
+							stroke="var(--c2, #4568a3)"
+							transform="translate(0 18)"
+							d="M 1 1 l 50 2 l 8 2 q 30 2 40 12 q -10 10 -40 12 l -8 2 l -50 2z"
+						/>
+
+						<path
+							fill="var(--c1, #273665)"
+							stroke="var(--c1, #273665)"
+							transform="translate(88 18)"
+							d="M 1 1 l 10 2 0 28 -10 2z"
+						/>
+						<path
+							fill="var(--c2, #4568a3)"
+							stroke="var(--c2, #4568a3)"
+							transform="translate(0 15)"
+							d="M 1 1 l 14 1 0 36 -14 1z"
+						/>
+						<g mask="url(#circle-race-car-mask-spoiler)">
+							<path
+								fill="var(--c1, #273665)"
+								stroke="var(--c1, #273665)"
+								transform="translate(0 15)"
+								d="M 1 1 l 14 1 0 36 -14 1z"
+							/>
+						</g>
+					</g>
+					<g>
+						<path
+							fill="var(--c0, #220b08)"
+							transform="translate(30 35)"
+							d="M 0 0 c 0 -15 40 -10 40 0 0 10 -40 15 -40 0"
+						/>
+						<g mask="url(#circle-race-car-mask-cockpit)">
+							<path
+								fill="var(--c1, #273665)"
+								transform="translate(30 35)"
+								d="M 0 0 c 0 -15 40 -10 40 0 0 10 -40 15 -40 0"
+							/>
+						</g>
+					</g>
+				</g>
+				<circle fill="var(--c3, #d5d3ca)" cx="52.5" cy="35" r="5.25" />
+			</symbol>
+		</defs>
 		<g fill="none">
 			<circle stroke="#160d05" y="0.3" stroke-width="7" r="25" />
 			<circle stroke="#3f3c32" stroke-width="7" r="25" />
@@ -112,7 +187,7 @@
 
 			<use
 				style="--c0: #160d05; --c1: #e36636; --c2: #f5ad37; --c3: #d5d3ca"
-				href="#the-race-is-on-car"
+				href="#circle-race-car"
 				x="-3.5"
 				y="-3.5"
 				width="7"
--- src/blog/the-race-is-on/LapRace.svelte
+++ src/blog/the-race-is-on/LapRace.svelte
@@ -40,6 +40,79 @@
 
 	<svg bind:this={svg} viewBox="0 0 80 40">
 		<defs>
+			<symbol id="lap-race-car" viewBox="0 0 100 70">
+				<mask id="lap-race-car-mask-cockpit">
+					<rect width="100" height="70" fill="white" />
+					<path d="M 66 0 v 15 a 20 20 0 0 0 0 40 v 15 h 34 v -70z" fill="black" />
+				</mask>
+				<mask id="lap-race-car-mask-spoiler">
+					<rect width="100" height="70" fill="white" />
+					<rect x="4" width="5" height="70" fill="black" />
+				</mask>
+
+				<g stroke="var(--c0, #220b08)">
+					<path stroke-width="5" d="M 24 0 v 70" />
+					<path stroke-width="4" d="M 73 20 v 30" />
+				</g>
+				<g fill="var(--c0, #220b08)">
+					<rect transform="translate(15 0)" rx="2" width="18" height="14" />
+					<rect transform="translate(15 56)" rx="2" width="18" height="14" />
+					<rect transform="translate(65 10)" rx="1.5" width="16" height="10" />
+					<rect transform="translate(65 50)" rx="1.5" width="16" height="10" />
+				</g>
+				<g stroke-linecap="round" stroke-linejoin="round">
+					<g stroke-width="2">
+						<path
+							fill="var(--c1, #273665)"
+							stroke="var(--c1, #273665)"
+							transform="translate(0 18)"
+							d="M 1 1 l 50 -1 l 8 5 q 30 2 40 12 q -10 10 -40 12 l -8 5 l -50 -1z"
+						/>
+						<path
+							fill="var(--c2, #4568a3)"
+							stroke="var(--c2, #4568a3)"
+							transform="translate(0 18)"
+							d="M 1 1 l 50 2 l 8 2 q 30 2 40 12 q -10 10 -40 12 l -8 2 l -50 2z"
+						/>
+
+						<path
+							fill="var(--c1, #273665)"
+							stroke="var(--c1, #273665)"
+							transform="translate(88 18)"
+							d="M 1 1 l 10 2 0 28 -10 2z"
+						/>
+						<path
+							fill="var(--c2, #4568a3)"
+							stroke="var(--c2, #4568a3)"
+							transform="translate(0 15)"
+							d="M 1 1 l 14 1 0 36 -14 1z"
+						/>
+						<g mask="url(#lap-race-car-mask-spoiler)">
+							<path
+								fill="var(--c1, #273665)"
+								stroke="var(--c1, #273665)"
+								transform="translate(0 15)"
+								d="M 1 1 l 14 1 0 36 -14 1z"
+							/>
+						</g>
+					</g>
+					<g>
+						<path
+							fill="var(--c0, #220b08)"
+							transform="translate(30 35)"
+							d="M 0 0 c 0 -15 40 -10 40 0 0 10 -40 15 -40 0"
+						/>
+						<g mask="url(#lap-race-car-mask-cockpit)">
+							<path
+								fill="var(--c1, #273665)"
+								transform="translate(30 35)"
+								d="M 0 0 c 0 -15 40 -10 40 0 0 10 -40 15 -40 0"
+							/>
+						</g>
+					</g>
+				</g>
+				<circle fill="var(--c3, #d5d3ca)" cx="52.5" cy="35" r="5.25" />
+			</symbol>
 			<path id="lap-race-track" d="M 40 5 h 20 a 15 15 0 0 1 0 30 h -40 a 15 15 0 0 1 0 -30 h 20" />
 		</defs>
 
@@ -66,7 +139,7 @@
 
 			<use
 				style="--c0: #160d05; --c1: #e36636; --c2: #f5ad37; --c3: #d5d3ca"
-				href="#the-race-is-on-car"
+				href="#lap-race-car"
 				x="-3.5"
 				y="-3.5"
 				width="7"
