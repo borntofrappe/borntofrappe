6dc866e61ffaab1c4e4b65fa397f826c268da8d6
Gabriele Corti <borntofrappe@gmail.com>
2023-05-02 21:59:12 +0200

    clip lens
    create an area in which the sprite (and the background elements) ought to be visible

--- src/routes/diy/TakeAPicture.svelte
+++ src/routes/diy/TakeAPicture.svelte
@@ -44,8 +44,52 @@
 				</g>
 			</g>
 		</symbol>
+
+		<rect id="take-a-picture-lens" x="25" y="11" width="30" height="28" rx="2" />
+
+		<clipPath id="take-a-picture-clip-lens">
+			<use href="#take-a-picture-lens" />
+		</clipPath>
 	</defs>
 
-	<use href="#take-a-picture-symbol-0" width="20" height="20" />
-	<use href="#take-a-picture-symbol-1" y="20" width="20" height="20" />
+	<rect fill="currentColor" width="80" height="50" />
+
+	<g fill="none">
+		<use href="#take-a-picture-lens" stroke="#f7f7f7" stroke-width="2.5" />
+		<use href="#take-a-picture-lens" stroke="currentColor" stroke-width="1" />
+	</g>
+
+	<g clip-path="url(#take-a-picture-clip-lens)">
+		<rect width="80" height="50" fill="#f7f7f7" />
+
+		<g fill="#dfdfdf">
+			<circle cx="0" cy="0" r="25" />
+			<circle cx="25" cy="0" r="25" />
+			<circle cx="50" cy="-5" r="25" />
+			<circle cx="80" cy="-10" r="25" />
+			<circle cx="15" cy="20" r="12" />
+			<circle cx="30" cy="20" r="12" />
+			<circle cx="45" cy="15" r="12" />
+			<circle cx="65" cy="10" r="12" />
+		</g>
+
+		<g stroke="#dfdfdf" stroke-width="0.75">
+			<path
+				fill="#f7f7f7"
+				d="M 0 21.5 29.5 28.5 35 50 45 50 39 25 80 5 80 0 74 0 37 18 32.5 0 22.5 0 28 20 0 14"
+			/>
+			<g fill="none">
+				<path d="M 0 21.5 29.5 28.5" stroke-dasharray="8" transform="translate(2 -3.5)" />
+				<path d="M 29.5 27 35 50" stroke-dasharray="12" transform="translate(2 1)" />
+				<g stroke-dasharray="10">
+					<path d="M 45 50 39 25" transform="translate(-4 -2)" />
+					<path d="M 39 25 80 5" transform="translate(4 -5)" />
+					<path d="M 37 18 32.5 0" transform="translate(-3 -2)" />
+					<path d="M 22.5 0 28 20" transform="translate(2 -1)" />
+				</g>
+			</g>
+		</g>
+
+		<use href="#take-a-picture-symbol-1" x="20" y="10" width="20" height="20" />
+	</g>
 </svg>
