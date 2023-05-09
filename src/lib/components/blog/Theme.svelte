69330b159cb94db9de80b2dc7650af3309dd7905
Gabriele Corti <borntofrappe@gmail.com>
2023-05-09 17:51:17 +0200

    refactor svg syntax
    prefer to define the details and marker in the defs block instead of using if, elseif, else blocks

--- src/lib/components/blog/Theme.svelte
+++ src/lib/components/blog/Theme.svelte
@@ -137,87 +137,98 @@
 
 <svg style:background bind:this={svg} viewBox="0 0 120 60">
 	<defs>
-		<g id="theme-day-cloud">
+		<g id="theme-cloud">
 			<circle r="4" />
 			<circle cx="3.8" r="3" />
 			<circle cx="-3.8" r="3" />
 		</g>
-		<path id="theme-day-ray" d="M 0 0 h -1 l 1 -1 1 1z" />
-		<path
-			id="theme-night-star"
-			d="M 0 0 a 1 1 0 0 0 1 -1 1 1 0 0 0 1 1 1 1 0 0 0 -1 1 1 1 0 0 0 -1 -1"
-		/>
+		<path id="theme-star" d="M 0 0 a 1 1 0 0 0 1 -1 1 1 0 0 0 1 1 1 1 0 0 0 -1 1 1 1 0 0 0 -1 -1" />
+
+		<g id="theme-details-morning">
+			<g fill="none" stroke="#a4bace" stroke-linecap="round" stroke-linejoin="round">
+				<g transform="translate(60 85)">
+					<g stroke-dasharray="15 30" stroke-width="2.5">
+						<path d="M -46 0 a 46 46 0 0 1 92 0" />
+						<path transform="rotate(-3)" d="M -45 0 a 45.5 45.5 0 0 1 91 0" />
+						<path transform="rotate(-4)" d="M -46.5 0 a 46.5 46.5 0 0 1 93 0" />
+					</g>
+					<g stroke-dasharray="20 26.5" stroke-width="2">
+						<path d="M -64 0 a 64 64 0 0 1 128 0" />
+						<path transform="rotate(-2)" d="M -65 0 a 65 65 0 0 1 130 0" />
+						<path transform="rotate(-4)" d="M -66 0 a 66 66 0 0 1 132 0" />
+					</g>
+				</g>
+			</g>
+		</g>
+		<g id="theme-details-day">
+			<g fill="#8cdbef" stroke="#8cdbef">
+				<g transform="translate(60 85)">
+					<use transform="translate(0 -52.5)" href="#theme-cloud" />
+					<use transform="rotate(45) translate(0 -55)" href="#theme-cloud" />
+					<use transform="rotate(-45) translate(0 -55)" href="#theme-cloud" />
+					<use transform="rotate(-22.5) translate(0 -70)" href="#theme-cloud" />
+					<use transform="rotate(22.5) translate(0 -70)" href="#theme-cloud" />
+				</g>
+			</g>
+		</g>
+		<g id="theme-details-night">
+			<g fill="#adc2d6">
+				<use x="30" y="52" href="#theme-star" />
+				<use href="#theme-star" x="43" y="42" />
+				<use href="#theme-star" x="63" y="42.5" />
+				<use href="#theme-star" x="81" y="42" />
+				<use href="#theme-star" x="90" y="54" />
+				<use href="#theme-star" x="98" y="44" />
+				<use href="#theme-star" x="74" y="27.5" />
+				<use href="#theme-star" x="52" y="26" />
+				<use href="#theme-star" x="32" y="32" />
+				<use href="#theme-star" x="14" y="46" />
+				<use href="#theme-star" x="14" y="46" />
+				<use href="#theme-star" x="13" y="26.5" />
+				<use href="#theme-star" x="33.5" y="14.5" />
+				<use href="#theme-star" x="61" y="10" />
+				<use href="#theme-star" x="89.5" y="18.5" />
+				<use href="#theme-star" x="102.5" y="26" />
+			</g>
+		</g>
+
+		<g id="theme-marker-morning">
+			<circle r="7" fill="#838ac5" />
+			<circle r="6" fill="#f7f7f7" stroke="#63616b" stroke-width="0.75" />
+			<g fill="#f7f752" stroke="#e6ca31" stroke-width="0.3">
+				<circle r="3" />
+			</g>
+		</g>
+		<g id="theme-marker-day">
+			<circle r="7" fill="#639aef" />
+			<circle r="6" fill="#f7f7f7" stroke="#63616b" stroke-width="0.75" />
+			<g fill="#f7f752" stroke="#e6ca31" stroke-width="0.3">
+				<circle r="3" />
+				<path d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+				<path transform="rotate(45)" d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+				<path transform="rotate(90)" d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+				<path transform="rotate(135)" d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+				<path transform="rotate(180)" d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+				<path transform="rotate(225)" d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+				<path transform="rotate(270)" d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+				<path transform="rotate(315)" d="M 0 -3.5 h -1 l 1 -1 1 1z" />
+			</g>
+		</g>
+		<g id="theme-marker-night">
+			<circle r="7" fill="#7369b5" />
+			<circle r="6" fill="#f7f7f7" stroke="#63616b" stroke-width="0.75" />
+			<g fill="#f7f752" stroke="#e6ca31" stroke-width="0.3">
+				<path transform="rotate(-45)" d="M 0 -3 a 3 3 0 1 1 -3 3 2 2 0 0 0 3 -3z" />
+			</g>
+		</g>
 	</defs>
 
 	<circle fill={background} r="60" cx="60" cy="60" />
-	{#if timeOfDay === 'morning'}
-		<g fill="none" stroke="#a4bace" stroke-linecap="round" stroke-linejoin="round">
-			<g transform="translate(60 85)">
-				<g stroke-dasharray="15 30" stroke-width="2.5">
-					<path d="M -46 0 a 46 46 0 0 1 92 0" />
-					<path transform="rotate(-3)" d="M -45 0 a 45.5 45.5 0 0 1 91 0" />
-					<path transform="rotate(-4)" d="M -46.5 0 a 46.5 46.5 0 0 1 93 0" />
-				</g>
-				<g stroke-dasharray="20 26.5" stroke-width="2">
-					<path d="M -64 0 a 64 64 0 0 1 128 0" />
-					<path transform="rotate(-2)" d="M -65 0 a 65 65 0 0 1 130 0" />
-					<path transform="rotate(-4)" d="M -66 0 a 66 66 0 0 1 132 0" />
-				</g>
-			</g>
-		</g>
-	{:else if timeOfDay === 'day'}
-		<g fill="#8cdbef" stroke="#8cdbef">
-			<g transform="translate(60 85)">
-				<use transform="translate(0 -52.5)" href="#theme-day-cloud" />
-				<use transform="rotate(45) translate(0 -55)" href="#theme-day-cloud" />
-				<use transform="rotate(-45) translate(0 -55)" href="#theme-day-cloud" />
-				<use transform="rotate(-22.5) translate(0 -70)" href="#theme-day-cloud" />
-				<use transform="rotate(22.5) translate(0 -70)" href="#theme-day-cloud" />
-			</g>
-		</g>
-	{:else}
-		<g fill="#adc2d6">
-			<use x="30" y="52" href="#theme-night-star" />
-			<use href="#theme-night-star" x="43" y="42" />
-			<use href="#theme-night-star" x="63" y="42.5" />
-			<use href="#theme-night-star" x="81" y="42" />
-			<use href="#theme-night-star" x="90" y="54" />
-			<use href="#theme-night-star" x="98" y="44" />
-			<use href="#theme-night-star" x="74" y="27.5" />
-			<use href="#theme-night-star" x="52" y="26" />
-			<use href="#theme-night-star" x="32" y="32" />
-			<use href="#theme-night-star" x="14" y="46" />
-			<use href="#theme-night-star" x="14" y="46" />
-			<use href="#theme-night-star" x="13" y="26.5" />
-			<use href="#theme-night-star" x="33.5" y="14.5" />
-			<use href="#theme-night-star" x="61" y="10" />
-			<use href="#theme-night-star" x="89.5" y="18.5" />
-			<use href="#theme-night-star" x="102.5" y="26" />
-		</g>
-	{/if}
+
+	<use href="#theme-details-{timeOfDay}" />
 
 	<g transform="translate({$position.x} {$position.y})">
-		<g>
-			<circle r="7" fill={foreground} />
-			<circle r="6" fill="#f7f7f7" stroke="#63616b" stroke-width="0.75" />
-			<g fill="#f7f752" stroke="#e6ca31" stroke-width="0.3">
-				{#if timeOfDay === 'morning'}
-					<circle r="3" />
-				{:else if timeOfDay === 'day'}
-					<circle r="3" />
-					<use href="#theme-day-ray" transform="translate(0 -3.5)" />
-					<use href="#theme-day-ray" transform="rotate(45) translate(0 -3.5)" />
-					<use href="#theme-day-ray" transform="rotate(90) translate(0 -3.5)" />
-					<use href="#theme-day-ray" transform="rotate(135) translate(0 -3.5)" />
-					<use href="#theme-day-ray" transform="rotate(180) translate(0 -3.5)" />
-					<use href="#theme-day-ray" transform="rotate(225) translate(0 -3.5)" />
-					<use href="#theme-day-ray" transform="rotate(270) translate(0 -3.5)" />
-					<use href="#theme-day-ray" transform="rotate(315) translate(0 -3.5)" />
-				{:else}
-					<path transform="rotate(-45)" d="M 0 -3 a 3 3 0 1 1 -3 3 2 2 0 0 0 3 -3" />
-				{/if}
-			</g>
-		</g>
+		<use href="#theme-marker-{timeOfDay}" />
 	</g>
 
 	<g
