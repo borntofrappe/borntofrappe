3028fac19b0c06c3c026d677dd131806f77f3f45
Gabriele Corti <borntofrappe@gmail.com>
2023-05-11 22:19:34 +0200

    microscope.svg
    add decoration to introduce the article

---
+++ src/blog/viewbox-focus/svg.js
@@ -0,0 +1,84 @@
+export default {
+	microscope: `<svg viewBox="-58.99136734008789 -110.89614868164062 112.49136734008789 125.89614868164062">
+	<rect
+		fill="hsl(0 0% 18%)"
+		x="-58.99136734008789"
+		y="-110.89614868164062"
+		width="112.49136734008789"
+		height="125.89614868164062"
+		rx="8"
+	/>
+
+	<g transform="translate(-8 -21)">
+		<g fill="hsl(0 0% 90%)">
+			<path d="M 0 4 h -1 a 25 25 0 0 1 0 -50 h 5 v 8 h -5 a 17 17 0 0 0 0 34 h 1" />
+		</g>
+
+		<g transform="translate(4 -42)">
+			<g transform="rotate(-30)">
+				<animateTransform
+					begin="3s"
+					attributeName="transform"
+					type="rotate"
+					values="-30; 2; 0"
+					dur="2s"
+					fill="freeze"
+					calcMode="spline"
+					keyTimes="0; 0.8; 1"
+					keySplines="0.4 0 0.6 1; 0.6 0 0.4 1"
+				/>
+				<g>
+					<path
+						fill="hsl(0 0% 90%)"
+						stroke="hsl(0 0% 90%)"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M 0 0 l 10 -20 h 10 v 30 h -10 v -10z"
+					/>
+					<g fill="hsl(0 0% 42%)">
+						<path d="M 15 -21 h 4 a 2 2 0 0 0 2 -2 h -12 a 2 2 0 0 0 2 2" />
+						<path d="M 15 -23 h 6 a 2 2 0 0 0 2 -2 v -2 h -16 v 2 a 2 2 0 0 0 2 2" />
+					</g>
+					<g fill="hsl(0 0% 42%)">
+						<path d="M 15 11 h 4 v 1 a 2 2 0 0 1 -2 2 h -4 a 2 2 0 0 1 -2 -2 v -1z" />
+						<path d="M 15 14 h 2 v 4 a 1 1 0 0 1 -1 1 h -2 a 1 1 0 0 1 -1 -1 v -4z" />
+					</g>
+				</g>
+
+				<circle fill="hsl(0 0% 90%)" r="7" />
+				<circle fill="hsl(0 0% 42%)" r="4" />
+			</g>
+		</g>
+
+		<g fill="hsl(0 0% 42%)">
+			<rect y="-2" width="28" height="4" />
+			<rect x="18" y="-12" width="2" height="10" />
+		</g>
+
+		<g transform="translate(19 -12)">
+			<g fill="hsl(0 0% 42%)">
+				<path d="M 0 0 h 5 a 2 2 0 0 0 2 -2 h -14 a 2 2 0 0 0 2 2z" />
+				<path d="M 0 -2 h 7 a 2 2 0 0 0 2 -2 v -2 h -18 v 2 a 2 2 0 0 0 2 2z" />
+			</g>
+			<rect fill="hsl(42 87% 55%)" x="-12" y="-7" width="24" height="1" />
+		</g>
+	</g>
+
+	<g fill="hsl(0 0% 90%)">
+		<path
+			stroke="hsl(0 0% 90%)"
+			stroke-width="4"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M -10 -2 v -25 l 20 25z"
+		/>
+		<path d="M -17.5 -5 v -1 a 5 5 0 0 1 5 -5 h 30 a 5 5 0 0 1 5 5 v 1z" />
+		<path d="M -22.5 0 v -1 a 5 5 0 0 1 5 -5 h 40 a 5 5 0 0 1 5 5 v 1z" />
+	</g>
+
+	<g transform="translate(-8 -21)">
+		<circle fill="hsl(0 0% 42%)" r="2.5" />
+	</g>
+</svg>`
+};
