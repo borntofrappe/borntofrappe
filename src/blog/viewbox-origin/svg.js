9df0384499df2279789d075ebcae6449cbccb504
Gabriele Corti <borntofrappe@gmail.com>
2023-05-12 10:50:16 +0200

    update helper visual
    match the improved design from the sibling project viewbox-focus

--- src/blog/viewbox-origin/svg.js
+++ src/blog/viewbox-origin/svg.js
@@ -1,36 +1,30 @@
 export default {
-	canvas: `<svg style="display: block;" viewBox="0 0 105 100">
+	'view-box': `<svg style="display: block; --accent: hsl(42 86% 86%);" viewBox="0 0 87.5 85">
+	<pattern id="view-box-pattern-grid" viewBox="0 0 1 1" width="0.05" height="0.05">
+		<g fill="none" stroke="currentColor" stroke-width="0.0025">
+			<rect width="1" height="1" />
+		</g>
+	</pattern>
 	<g transform="translate(10 10)">
-		<g
-			fill="none"
-			stroke="currentColor"
-			stroke-width="0.5"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		>
-			<path
-				transform="translate(70 0) rotate(90)"
-				d="M 0 -3.5 v 0 c 0 -7 35 0 35 -7 c 0 7 35 0 35 7"
-			/>
-			<path
-				transform="translate(70 70) rotate(180)"
-				d="M 0 -3.5 v 0 c 0 -7 35 0 35 -7 c 0 7 35 0 35 7"
-			/>
+		<g fill="none" stroke="currentColor" stroke-width="0.2">
+			<path transform="translate(62 0)" d="M 0 0 h 2 v 60 h -2" />
+			<path transform="translate(60 62) rotate(90)" d="M 0 0 h 2 v 60 h -2" />
 		</g>
-		<g font-family="monospace" fill="currentColor" font-size="5" letter-spacing="-0.25">
-			<text y="-3" text-anchor="middle"
-				>(<tspan font-weight="700">0</tspan>, <tspan font-weight="700">0</tspan>)</text
-			>
-			<text x="35" y="86.5" text-anchor="middle" font-weight="700">100</text>
-			<text x="82.5" y="36.5" font-weight="700">100</text>
+		<g font-family="monospace" fill="currentColor" font-size="5" letter-spacing="-0.1">
+			<text text-anchor="middle" y="-3">
+				(<tspan font-weight="700">0</tspan>, <tspan font-weight="700">0</tspan>)
+			</text>
+			<text text-anchor="middle" x="30" y="70" font-weight="700">100</text>
+			<text x="66" y="31" font-weight="700">100</text>
 		</g>
-		<svg viewBox="0 0 100 100" width="70" height="70">
+		<svg viewBox="0 0 100 100" width="60" height="60">
+			<rect x="0.375" y="0.375" width="99.25" height="99.25" fill="var(--accent, none)" />
 			<rect
 				x="0.375"
 				y="0.375"
 				width="99.25"
 				height="99.25"
-				fill="none"
+				fill="url(#view-box-pattern-grid)"
 				stroke="currentColor"
 				stroke-width="0.75"
 			/>
