84700ad1ab188d6ae3dc7b5c28be77e1ab70e8b7
Gabriele Corti <borntofrappe@gmail.com>
2023-05-15 18:36:13 +0200

    svg syntax
    add the svg syntax for the visuals pairing the article

---
+++ src/blog/room-for-growth/svg.js
@@ -0,0 +1,101 @@
+export default {
+	plant: `<svg style="display: block;" viewBox="0 0 62 89">
+	<rect fill="#f5f7fa" width="62" height="89" />
+	<svg style="color: #3e4c59;" viewBox="-11 -52 22 69" x="20" y="10" width="22" height="69">
+		<g
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linejoin="round"
+			stroke-linecap="round"
+		>
+			<path
+				d="M 0 0 v -6 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9 v -12 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9 v -12 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9 v -12 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9"
+			/>
+			<g transform="translate(-10 0)">
+				<rect width="20" height="6" rx="2" />
+				<path d="M 2 6 c 0.5 8 3 10 5 10 l 6 0 c 2 0 4.5 -2 5 -10" />
+			</g>
+		</g>
+	</svg>
+</svg>`,
+	elements: `<svg style="display: block; color: #3e4c59;" viewBox="0 0 100 89">
+	<rect fill="#f5f7fa" width="100" height="89" />
+	<g transform="translate(42 10)">
+		<g fill="none" stroke="currentColor" stroke-width="0.2">
+			<path d="M 0 40 h 15" />
+			<path d="M 1 55 h 14" />
+			<path d="M -2 65 h 17" />
+		</g>
+		<g transform="translate(15 0)">
+			<g fill="currentColor">
+				<circle r="0.75" cy="40" />
+				<circle r="0.75" cy="55" />
+				<circle r="0.75" cy="65" />
+			</g>
+			<g
+				transform="translate(3 1.2)"
+				fill="currentColor"
+				font-size="5"
+				font-weight="700"
+				font-family="monospace"
+			>
+				<text y="40">&lt;path /&gt;</text>
+				<text y="55">&lt;rect /&gt;</text>
+				<text y="65">&lt;path /&gt;</text>
+			</g>
+		</g>
+	</g>
+	<svg style="color: #3e4c59;" viewBox="-11 -52 22 69" x="20" y="10" width="22" height="69">
+		<g
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linejoin="round"
+			stroke-linecap="round"
+		>
+			<path
+				d="M 0 0 v -6 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9 v -12 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9 v -12 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9 v -12 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9"
+			/>
+			<g transform="translate(-10 0)">
+				<rect width="20" height="6" rx="2" />
+				<path d="M 2 6 c 0.5 8 3 10 5 10 l 6 0 c 2 0 4.5 -2 5 -10" />
+			</g>
+		</g>
+	</svg>
+</svg>`,
+	leaves: `<svg style="color: #3e4c59;" viewBox="-11 -52 22 69">
+	<g
+		fill="none"
+		stroke="currentColor"
+		stroke-width="2"
+		stroke-linejoin="round"
+		stroke-linecap="round"
+	>
+		<path d="M 0 0 v -6 v -12 v -12 v -12" />
+		<g transform="translate(0 -6)">
+			<path
+				transform="translate(0 0)"
+				d="M 0 0 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9"
+			/>
+			<path
+				transform="translate(0 -12)"
+				d="M 0 0 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9"
+			/>
+			<path
+				transform="translate(0 -24)"
+				d="M 0 0 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9"
+			/>
+			<path
+				transform="translate(0 -36)"
+				d="M 0 0 c 0 -7.5 2.5 -9 9 -9 c 0 6 -1 9 -9 9 c 0 -7.5 -2.5 -9 -9 -9 c 0 6 1 9 9 9"
+			/>
+		</g>
+		<path />
+		<g transform="translate(-10 0)">
+			<rect width="20" height="6" rx="2" />
+			<path d="M 2 6 c 0.5 8 3 10 5 10 l 6 0 c 2 0 4.5 -2 5 -10" />
+		</g>
+	</g>
+</svg>`
+};
