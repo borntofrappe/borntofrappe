755651df034b5283114dce0f6232a499f74b2088
Gabriele Corti <borntofrappe@gmail.com>
2023-05-17 21:47:46 +0200

    animate card flip

--- src/routes/diy/MatchInPairs.svelte
+++ src/routes/diy/MatchInPairs.svelte
@@ -46,6 +46,73 @@
 				d="M 9.510565162951535 3.090169943749474 Q 14.694631307311829 20.225424859373685 6.123233995736766e-16 10 Q -14.694631307311825 20.225424859373685 -9.510565162951535 3.090169943749475 Q -23.776412907378838 -7.725424859373693 -5.877852522924733 -8.090169943749473 Q -4.592425496802574e-15 -25 5.877852522924729 -8.090169943749476 Q 23.776412907378838 -7.725424859373692 9.510565162951535 3.090169943749474"
 			/>
 		</symbol>
+
+		<symbol id="match-in-pairs-card" viewBox="-0.25 -0.25 10.5 10.5">
+			<rect width="10" height="10" fill="#f7a629" stroke="#107c38" stroke-width="0.5" />
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flip" viewBox="0 0 1 1">
+			<use
+				transform="translate(0.5 0) scale(0.5 1) translate(-0.5 0)"
+				href="#match-in-pairs-card"
+			/>
+			<g fill="none" stroke="#f7f7f7" stroke-width="0.025" stroke-linecap="round">
+				<path d="M 0.12 0.4 0.4 0.1" />
+				<path transform="translate(0.05 0.1)" d="M 0.12 0.4 0.4 0.1" />
+			</g>
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flip-sun" viewBox="0 0 1 1">
+			<g transform="translate(0.5 0) scale(0.5 1) translate(-0.5 0)">
+				<use href="#match-in-pairs-card-flipped" />
+				<use href="#match-in-pairs-sun" />
+			</g>
+			<g fill="none" stroke="#f7f7f7" stroke-width="0.025" stroke-linecap="round">
+				<path d="M 0.12 0.4 0.4 0.1" />
+				<path transform="translate(0.05 0.1)" d="M 0.12 0.4 0.4 0.1" />
+			</g>
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flip-moon" viewBox="0 0 1 1">
+			<g transform="translate(0.5 0) scale(0.5 1) translate(-0.5 0)">
+				<use href="#match-in-pairs-card-flipped" />
+				<use href="#match-in-pairs-moon" />
+			</g>
+			<g fill="none" stroke="#f7f7f7" stroke-width="0.025" stroke-linecap="round">
+				<path d="M 0.12 0.4 0.4 0.1" />
+				<path transform="translate(0.05 0.1)" d="M 0.12 0.4 0.4 0.1" />
+			</g>
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flip-star" viewBox="0 0 1 1">
+			<g transform="translate(0.5 0) scale(0.5 1) translate(-0.5 0)">
+				<use href="#match-in-pairs-card-flipped" />
+				<use href="#match-in-pairs-star" />
+			</g>
+			<g fill="none" stroke="#f7f7f7" stroke-width="0.025" stroke-linecap="round">
+				<path d="M 0.12 0.4 0.4 0.1" />
+				<path transform="translate(0.05 0.1)" d="M 0.12 0.4 0.4 0.1" />
+			</g>
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flipped" viewBox="-0.25 -0.25 10.5 10.5">
+			<rect width="10" height="10" fill="#f7d794" stroke="#107c38" stroke-width="0.5" />
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flipped-sun" viewBox="0 0 1 1">
+			<use href="#match-in-pairs-card-flipped" />
+			<use href="#match-in-pairs-sun" />
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flipped-moon" viewBox="0 0 1 1">
+			<use href="#match-in-pairs-card-flipped" />
+			<use href="#match-in-pairs-moon" />
+		</symbol>
+
+		<symbol id="match-in-pairs-card-flipped-star" viewBox="0 0 1 1">
+			<use href="#match-in-pairs-card-flipped" />
+			<use href="#match-in-pairs-star" />
+		</symbol>
 	</defs>
 
 	<rect fill="#6bce31" width="80" height="60" />
@@ -64,7 +131,48 @@
 		<rect stroke="#bd4908" stroke-width="0.5" x="0.25" y="0.25" width="79.5" height="49.5" />
 	</g>
 
-	<use href="#match-in-pairs-sun" width="10" height="10" />
-	<use href="#match-in-pairs-moon" y="10" width="10" height="10" />
-	<use href="#match-in-pairs-star" y="20" width="10" height="10" />
+	<use href="#match-in-pairs-card" x="5" y="5" width="10" height="10">
+		<animate
+			begin="click"
+			attributeName="href"
+			values="
+				#match-in-pairs-card-flip;
+				#match-in-pairs-card-flip-sun;
+				#match-in-pairs-card-flipped-sun;
+			"
+			dur="0.2s"
+			fill="freeze"
+			calcMode="discrete"
+		/>
+	</use>
+
+	<use href="#match-in-pairs-card" x="25" y="5" width="10" height="10">
+		<animate
+			begin="click"
+			attributeName="href"
+			values="
+				#match-in-pairs-card-flip;
+				#match-in-pairs-card-flip-moon;
+				#match-in-pairs-card-flipped-moon;
+			"
+			dur="0.2s"
+			fill="freeze"
+			calcMode="discrete"
+		/>
+	</use>
+
+	<use href="#match-in-pairs-card" x="45" y="5" width="10" height="10">
+		<animate
+			begin="click"
+			attributeName="href"
+			values="
+				#match-in-pairs-card-flip;
+				#match-in-pairs-card-flip-star;
+				#match-in-pairs-card-flipped-star;
+			"
+			dur="0.2s"
+			fill="freeze"
+			calcMode="discrete"
+		/>
+	</use>
 </svg>
