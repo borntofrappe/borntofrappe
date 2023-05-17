cf09f71bc8bbd53103acbd43b79653e429c0eb0d
Gabriele Corti <borntofrappe@gmail.com>
2023-05-17 18:22:34 +0200

    MatchInPairs first commit
    add component and background elements

---
+++ src/routes/diy/MatchInPairs.svelte
@@ -0,0 +1,33 @@
+<svg viewBox="0 0 80 50">
+	<title>Match in pairs!</title>
+	<defs>
+		<pattern
+			id="match-in-pairs-pattern-background"
+			viewBox="0 0 2 2"
+			width="1"
+			height="1"
+			patternUnits="userSpaceOnUse"
+		>
+			<g fill="#009e3a">
+				<rect width="1" height="1" />
+				<rect x="1" y="1" width="1" height="1" />
+			</g>
+		</pattern>
+	</defs>
+
+	<rect fill="#6bce31" width="80" height="60" />
+	<g fill="none">
+		<rect
+			stroke="url(#match-in-pairs-pattern-background)"
+			stroke-width="3"
+			x="1.5"
+			y="1.5"
+			width="77"
+			height="47"
+		/>
+		<rect stroke="#eeae31" stroke-width="2" x="1" y="1" width="78" height="48" />
+		<rect stroke="#d66d19" stroke-width="1.5" x="0.75" y="0.75" width="77.5" height="47.5" />
+		<rect stroke="#c55108" stroke-width="1" x="0.5" y="0.5" width="79" height="49" />
+		<rect stroke="#bd4908" stroke-width="0.5" x="0.25" y="0.25" width="79.5" height="49.5" />
+	</g>
+</svg>
