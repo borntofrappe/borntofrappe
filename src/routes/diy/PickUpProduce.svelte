f1c4c4a0e763ae5e6544e53920af9104ae22e24d
Gabriele Corti <borntofrappe@gmail.com>
2023-05-22 22:12:44 +0200

    PickUpProduce first commit

---
+++ src/routes/diy/PickUpProduce.svelte
@@ -0,0 +1,21 @@
+<svg style="display: block;" viewBox="0 0 80 50">
+	<title>Pick up produce!</title>
+
+	<defs>
+		<pattern
+			id="pick-up-produce-pattern-overlay"
+			viewBox="0 0 2 2"
+			width="2"
+			height="2"
+			patternUnits="userSpaceOnUse"
+		>
+			<g opacity="0.2">
+				<rect width="1" height="1" />
+				<rect x="1" y="1" width="1" height="1" />
+			</g>
+		</pattern>
+	</defs>
+
+	<rect width="80" height="50" fill="#85dde1" />
+	<rect width="80" height="50" fill="url(#pick-up-produce-pattern-overlay)" />
+</svg>
