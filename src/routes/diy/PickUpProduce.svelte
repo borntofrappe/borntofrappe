55c03d811c85293360e0b5eff51586855ce5ff77
Gabriele Corti <borntofrappe@gmail.com>
2023-05-24 22:01:43 +0200

    add crop

--- src/routes/diy/PickUpProduce.svelte
+++ src/routes/diy/PickUpProduce.svelte
@@ -58,6 +58,47 @@
 				href="#pick-up-produce-tree-trunk-shape"
 			/>
 		</g>
+
+		<symbol id="pick-up-produce-crop" viewBox="-15 -21 30 47">
+			<g transform="translate(0 -6)">
+				<g fill="#76d0ad" stroke="#12984f" stroke-linecap="round" stroke-linejoin="round">
+					<path d="M 0 0 a 9 9 0 0 1 0 -14 9 9 0 0 1 0 14" />
+					<path stroke-width="0.75" d="M 0 0 v -10" />
+					<path d="M 0.25 0 c 0 -5 0 -10 8 -14 5 7 -1 13 -5.5 14" />
+					<path transform="scale(-1 1)" d="M 0.25 0 c 0 -5 0 -10 8 -14 5 7 -1 13 -5.5 14" />
+					<g stroke-width="0.75">
+						<path d="M 0.25 0 q 3.5 -4 6.5 -9.5" />
+						<path transform="scale(-1 1)" d="M 0.25 0 q 3.5 -4 6.5 -9.5" />
+					</g>
+				</g>
+			</g>
+			<g fill="none" stroke="#12984f" stroke-linecap="round" stroke-linejoin="round">
+				<path transform="rotate(0) translate(0 21)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(15) translate(0 20.5)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(-15) translate(0 20.5)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(-30) translate(0 20)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+				<path transform="rotate(30) translate(0 20)" d="M 0 0 a 1 1 0 0 0 0 2 1 1 0 0 1 0 2" />
+			</g>
+
+			<path fill="#ffdd9d" d="M 2 0 v 5 a 13 8 0 1 1 -4 0 v -5" />
+			<path fill="#76d0ad" d="M -2 -6 v 5 h 4 v -5" />
+
+			<g stroke="#c64e00" stroke-linecap="round" stroke-linejoin="round">
+				<g fill="#c64e00">
+					<path transform="translate(3.25 9)" d="M 0 0 l 1.1 1.7 -1.1 1.7 -1.1 -1.7z" />
+					<path transform="translate(-3.25 9)" d="M 0 0 l 1.1 1.7 -1.1 1.7 -1.1 -1.7z" />
+				</g>
+				<path fill="none" stroke="#c64e00" d="M -4 15 a 6 6 0 0 0 8 0" />
+			</g>
+
+			<path fill="#ffdd9d" d="M -1.5 -1 h 3 v 4 l -0.75 -0.75 -0.75 0.75 -0.75 -0.75 -0.75 0.75z" />
+			<path opacity="0.1" d="M -1.5 -1 h 3 v 4 l -0.75 -0.75 -0.75 0.75 -0.75 -0.75 -0.75 0.75z" />
+
+			<g stroke="#12984f" stroke-linecap="round" stroke-linejoin="round">
+				<path fill="none" stroke-width="1.25" d="M 2 -6 v 11 a 13 8 0 1 1 -4 0 v -11" />
+				<path fill="#ffdd9d" d="M -7 17 c -1 3 6 5 7 5 1 0 8 -2 7 -5" />
+			</g>
+		</symbol>
 	</defs>
 
 	<rect width="80" height="50" fill="#85dde1" />
@@ -74,52 +115,50 @@
 	</g>
 
 	<g>
-		<g>
-			<use href="#pick-up-produce-tree-back" x="-2" y="9" />
-			<use href="#pick-up-produce-tree-back" x="9" y="9" />
-			<use href="#pick-up-produce-tree-back" x="16" y="6" />
-		</g>
+		<use href="#pick-up-produce-tree-back" x="-2" y="9" />
+		<use href="#pick-up-produce-tree-back" x="9" y="9" />
+		<use href="#pick-up-produce-tree-back" x="16" y="6" />
 
 		<use href="#pick-up-produce-tree-trunk" x="5" y="34" />
 
-		<g>
-			<use href="#pick-up-produce-tree-front" x="-3" y="5" />
-			<use href="#pick-up-produce-tree-front" x="6" y="2" />
-			<use href="#pick-up-produce-tree-front" x="18" y="2" />
-			<use href="#pick-up-produce-tree-front" x="26" />
+		<use href="#pick-up-produce-tree-front" x="-3" y="5" />
+		<use href="#pick-up-produce-tree-front" x="6" y="2" />
+		<use href="#pick-up-produce-tree-front" x="18" y="2" />
+		<use href="#pick-up-produce-tree-front" x="26" />
+	</g>
+
+	<g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+		<g fill="#c64a00">
+			<path d="M 7 32 l 1 -1 3 0 1 1 0 1 -5 2 -1 -1z" />
+			<path d="M 25 32 l 1 0 1 1 2 0 0 2 -2 1 -1 0 -1 -2 0 -2z" />
+			<path d="M 45 32 l 1 -1 2 0 1 1 -1 1 -1 1 -2 -1z" />
+			<path d="M 52 33 l 0 -1 2 -2 2 0 1 1 -1 2 -4 0z" />
+			<path d="M  61 33 l 1 -2 1 0 1 1 -1 2 -1 0z" />
+			<path d="M  75 34 l 0 -1 1 -1 3 0 0 1 0 1 -4 0z" />
+
+			<path
+				d="M 0 33 l 1 0 2 -2 3 1 2 1 2 1 1 -1 2 0 1 -1 4 0 2 1 2 -1 3 1 1 2 3 -2 7 0 3 -1 1 0 2 1 5 0 1 -1 2 0 0 1 5 0 1 -2 5 0 1 2 2 -1 6 0 1 1 6 0 1 -1 1 0 H 81 V 51 H -1 V 33"
+			/>
+		</g>
+
+		<g fill="none">
+			<path d="M 3 38 l 1 -1 1 0 2 -1 2 1 -1 1 -2 0 -1 -1" />
+			<path d="M 9 37 l 2 0 0 -1 3 0 0 3 -5 0 0 -2" />
+			<path
+				d="M 6 47 l 0 -1 1 0 1 1 0 -2 2 0 1 1 2 -1 1 0 1 1 4 0 -1 -2 3 0 2 1 -1 2 -3 0 0 -1 0 0"
+			/>
+			<path
+				d="M 26 38 l -2 2 2 1 5 0 -1 -3 1 0 0 2 2 -1 1 2 2 -2 2 0 2 -2 2 0 1 2 2 0 0 1 2 -1 2 0"
+			/>
+			<path d="M  40 46 l 0 -2 1 0 3 0 1 1 -1 2 4 1 2 0 -2 -2 -3 0" />
+			<path d="M 55 48 l 2 0 0 -1 2 0 1 -2 1 1 3 0 0 -2 1 3 2 0 0 1" />
+			<path d="M  71 47 l 2 0 3 -3 -1 1 3 0 -3 0 -1 1 2 1 2 0" />
+			<path d="M 58 40 l 2 -1 2 0 1 -2 2 0 0 2" />
+			<path d="M 68 37 l -1 0 -1 0 0 2 1 1 1 0 2 -2 -4 0" />
 		</g>
 	</g>
 
 	<g>
-		<g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
-			<g fill="#c64a00">
-				<path d="M 7 32 l 1 -1 3 0 1 1 0 1 -5 2 -1 -1z" />
-				<path d="M 25 32 l 1 0 1 1 2 0 0 2 -2 1 -1 0 -1 -2 0 -2z" />
-				<path d="M 45 32 l 1 -1 2 0 1 1 -1 1 -1 1 -2 -1z" />
-				<path d="M 52 33 l 0 -1 2 -2 2 0 1 1 -1 2 -4 0z" />
-				<path d="M  61 33 l 1 -2 1 0 1 1 -1 2 -1 0z" />
-				<path d="M  75 34 l 0 -1 1 -1 3 0 0 1 0 1 -4 0z" />
-
-				<path
-					d="M 0 33 l 1 0 2 -2 3 1 2 1 2 1 1 -1 2 0 1 -1 4 0 2 1 2 -1 3 1 1 2 3 -2 7 0 3 -1 1 0 2 1 5 0 1 -1 2 0 0 1 5 0 1 -2 5 0 1 2 2 -1 6 0 1 1 6 0 1 -1 1 0 H 81 V 51 H -1 V 33"
-				/>
-			</g>
-
-			<g fill="none">
-				<path d="M 3 38 l 1 -1 1 0 2 -1 2 1 -1 1 -2 0 -1 -1" />
-				<path d="M 9 37 l 2 0 0 -1 3 0 0 3 -5 0 0 -2" />
-				<path
-					d="M 6 47 l 0 -1 1 0 1 1 0 -2 2 0 1 1 2 -1 1 0 1 1 4 0 -1 -2 3 0 2 1 -1 2 -3 0 0 -1 0 0"
-				/>
-				<path
-					d="M 26 38 l -2 2 2 1 5 0 -1 -3 1 0 0 2 2 -1 1 2 2 -2 2 0 2 -2 2 0 1 2 2 0 0 1 2 -1 2 0"
-				/>
-				<path d="M  40 46 l 0 -2 1 0 3 0 1 1 -1 2 4 1 2 0 -2 -2 -3 0" />
-				<path d="M 55 48 l 2 0 0 -1 2 0 1 -2 1 1 3 0 0 -2 1 3 2 0 0 1" />
-				<path d="M  71 47 l 2 0 3 -3 -1 1 3 0 -3 0 -1 1 2 1 2 0" />
-				<path d="M 58 40 l 2 -1 2 0 1 -2 2 0 0 2" />
-				<path d="M 68 37 l -1 0 -1 0 0 2 1 1 1 0 2 -2 -4 0" />
-			</g>
-		</g>
+		<use href="#pick-up-produce-crop" width="30" height="47" />
 	</g>
 </svg>
