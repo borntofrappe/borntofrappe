af1105e49f6e408d4bfd897ebe7cc2fd097a8f34
Gabriele Corti <borntofrappe@gmail.com>
2023-06-20 11:41:13 +0200

    C:/Program Files/Git/time
    add page to display the time per the readable store

---
+++ src/routes/time/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import site from '$lib/utils/site.js';
+	const { title } = site;
+
+	import { time } from '$lib/utils/stores.js';
+
+	$: datetime = `${$time.getHours()}:${$time.getMinutes()}`;
+</script>
+
+<svelte:head>
+	<title>Time | {title}</title>
+	<meta name="description" content="Time of day with a twelve hour clock." />
+</svelte:head>
+
+<h1>What time it is?</h1>
+<p><time {datetime}>{datetime}</time></p>
