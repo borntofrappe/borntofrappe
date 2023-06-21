07c2fc7dc7dacc421f5ee284c4e4920bb41fc592
Gabriele Corti <borntofrappe@gmail.com>
2023-06-21 22:27:25 +0200

    update svg syntax
    update code to match the necessities of the article (larger inline size, currentColor); include the finished version in the first visual as well, with the appropriate width and height to show only a portion

--- src/blog/aspect-a-surprise/svg.js
+++ src/blog/aspect-a-surprise/svg.js
@@ -1,7 +1,7 @@
 export default {
 	square: `<svg style="display: block;" viewBox="0 0 5 2.5">
 	<g transform="translate(2.5 0.8)" text-anchor="middle" font-size="0.45" font-family="monospace" font-weight="700">
-		<text>width = height</text>
+		<text fill="currentColor">width = height</text>
 	</g>
 	<g transform="translate(2 1.2)">
         <rect fill="none" stroke="currentColor" stroke-width="0.05" x="0.025" y="0.025" width="0.95" height="0.95" />
@@ -12,7 +12,7 @@ export default {
 </svg>`,
 	rectangle: `<svg style="display: block;" viewBox="0 0 5 2.5">
 	<g transform="translate(2.5 0.8)" text-anchor="middle" font-size="0.45" font-family="monospace" font-weight="700">
-		<text>width =  3 * height</text>
+		<text fill="currentColor">width =  3 * height</text>
 	</g>
 	<g transform="translate(1 1.2)">
         <rect fill="none" stroke="currentColor" stroke-width="0.05" x="0.025" y="0.025" width="2.95" height="0.95" />
@@ -22,13 +22,58 @@ export default {
 	</g>
 </svg>`,
 	painting: `<svg style="display: block;" viewBox="0 0 1 1">
-    <g stroke="#111111" stroke-width="0.04" stroke-linejoin="round" stroke-linecap="round">
-        <rect fill="#a57939" x="0.02" y="0.02" width="0.96" height="0.96" />
-        <rect fill="#92d3f5" x="0.14" y="0.14" width="0.72" height="0.72" />
-        <circle fill="#fcea2b" cx="0.3" cy="0.38" r="0.09" />
+    <g transform="translate(-1 0)">
+        <g transform="translate(0.5 0.5) scale(0.7) translate(-0.5 -0.5) translate(0 0.25)">
+            <g stroke="#111111" stroke-width="0.035" stroke-linejoin="round" stroke-linecap="round">
+                <path transform="translate(0.5 0.5)" fill="#a57939" d="M 0 -0.35 C 0.25 -0.35 0.3 -0.2 0.3 -0.1 C 0.3 0 0.475 -0.15 0.45 0.05 C 0.45 0.1 0.35 0.35 0 0.35 C -0.2 0.35 -0.42 0.25 -0.45 0.05 C -0.48 -0.1 -0.35 -0.35 0 -0.35" />
+                <ellipse fill="#ffffff" cx="0.57" cy="0.33" rx="0.08" ry="0.06" />
+                <circle fill="#61b2e4" cx="0.18" cy="0.48" r="0.06" />
+                <circle fill="#5c9e31" cx="0.3" cy="0.63" r="0.06" />
+                <circle fill="#fcea2b" cx="0.5" cy="0.7" r="0.06" />
+                <circle fill="#d22f27" cx="0.7" cy="0.65" r="0.06" />
+            </g>
+        </g>
     </g>
-    <path fill="#5c9e31" d="M 0.22 0.62 C 0.23 0.55 0.34 0.56 0.41 0.49 C 0.5 0.4 0.5 0.3 0.6 0.32 C 0.7 0.32 0.76 0.46 0.77 0.48 C 0.8 0.55 0.7 0.56 0.72 0.55 C 0.78 0.62 0.73 0.62 0.67 0.61 C 0.58 0.59 0.45 0.62 0.41 0.66 C 0.3 0.75 0.2 0.7 0.22 0.62" />
-    <path fill="none" stroke="#111111" stroke-width="0.04" stroke-linejoin="round" stroke-linecap="round" d="M 0.22 0.62 C 0.23 0.55 0.34 0.56 0.41 0.49 C 0.5 0.4 0.5 0.3 0.6 0.32 C 0.7 0.32 0.74 0.46 0.77 0.47" />
+
+    <g>
+        <g stroke="#111111" stroke-width="0.04" stroke-linejoin="round" stroke-linecap="round">
+            <rect fill="#a57939" x="0.02" y="0.02" width="0.96" height="0.96" />
+            <rect fill="#92d3f5" x="0.14" y="0.14" width="0.72" height="0.72" />
+            <circle fill="#fcea2b" cx="0.3" cy="0.38" r="0.09" />
+        </g>
+        <path fill="#5c9e31" d="M 0.22 0.62 C 0.23 0.55 0.34 0.56 0.41 0.49 C 0.5 0.4 0.5 0.3 0.6 0.32 C 0.7 0.32 0.76 0.46 0.77 0.48 C 0.8 0.55 0.7 0.56 0.72 0.55 C 0.78 0.62 0.73 0.62 0.67 0.61 C 0.58 0.59 0.45 0.62 0.41 0.66 C 0.3 0.75 0.2 0.7 0.22 0.62" />
+        <path fill="none" stroke="#111111" stroke-width="0.04" stroke-linejoin="round" stroke-linecap="round" d="M 0.22 0.62 C 0.23 0.55 0.34 0.56 0.41 0.49 C 0.5 0.4 0.5 0.3 0.6 0.32 C 0.7 0.32 0.74 0.46 0.77 0.47" />
+    </g>
+    
+    <g transform="translate(1 0)">
+        <g transform="translate(0.5 0.5) scale(0.75) translate(-0.5 -0.5) translate(0 0.1)">
+            <path stroke="#111111" stroke-width="0.035" stroke-linecap="round" d="M 0.12 0.855 H 0.61 M 0.82 0.85 H 0.88" /> 
+            <g transform="translate(0.45 0.85)">
+                <g fill="none" stroke="#111111" stroke-width="0.035" stroke-linejoin="round" stroke-linecap="round">
+                    <path d="M 0.12 0 V -0.13" />
+                    <path d="M 0 0 V -0.13" />
+                </g>
+                <path fill="#d22f27" d="M 0 -0.13 C 0.12 -0.13 0.2 -0.13 0.275 0.125 L 0.32 0.1 C 0.32 0.04 0.3 -0.13 0.12 -0.13" />
+                <g stroke="#111111" stroke-width="0.035" stroke-linejoin="round" stroke-linecap="round">
+                    <path fill="none" d="M 0 -0.13 C 0.12 -0.13 0.2 -0.13 0.275 0.125 L 0.32 0.1 Q 0.32 0.04 0.31 0.02" />
+                    <path fill="#d22f27" d="M 0 -0.13 C -0.175 -0.15 -0.175 -0.5 0 -0.53 C 0.125 -0.53 0.125 -0.68 0 -0.68 C 0.05 -0.75 0.3 -0.75 0.25 -0.5 L 0.25 -0.3 C 0.25 -0.13 0.1 -0.13 0 -0.13" />
+                    <path fill="#fcea2b" d="M 0 -0.68 C -0.1 -0.68 -0.15 -0.6 -0.15 -0.53 L 0 -0.53z" />
+                    <path fill="#ffffff" d="M 0 -0.53 C 0.125 -0.53 0.125 -0.68 0 -0.68 C -0.05 -0.65 -0.05 -0.53 0 -0.53 " />
+                </g>
+                <path fill="#d22f27" d="M 0.05 -0.45 C 0.1 -0.45 0.3 -0.4 0.275 -0.05 C -0.05 -0.05 -0.15 -0.45 0.05 -0.45" />
+                <path fill="#61b2e4" d="M 0.05 -0.45 C -0.15 -0.45 -0.05 -0.05 0.275 -0.05 C 0.1 -0.2 -0.15 -0.45 0.05 -0.45" />
+                <path fill="none" stroke="#111111" stroke-width="0.035" stroke-linejoin="round" stroke-linecap="round" d="M 0.05 -0.45 C 0.1 -0.45 0.3 -0.4 0.275 -0.05 C -0.05 -0.05 -0.15 -0.45 0.05 -0.45" />
+
+                <circle fill="#222222" cx="0.025" cy="-0.61" r="0.025" />
+            </g>
+            <g stroke="#111111" stroke-width="0.035" stroke-linejoin="round" stroke-linecap="round">
+                <g transform="translate(0.575 0.15)" >
+                    <path fill="#222222" d="M 0 0 L 0.1 0 L 0.075 -0.025 L 0.11 -0.05 C 0.07 -0.1 -0.07 -0.125 -0.11 -0.05 L -0.075 -0.025 L -0.1 0 L 0 0" />
+                    <path fill="none" d="M 0 -0.1 V -0.13" />
+                </g>
+            </g>
+        </g>
+    </g>
 </svg>`,
 	surprise: `<svg style="display: block;" viewBox="0 0 3 1">
     <svg width="3" height="1" viewBox="0 0 1 1">
