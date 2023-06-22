5227faab9fc99ffd77e5b19d519d05442e133803
Gabriele Corti <borntofrappe@gmail.com>
2023-06-22 16:17:02 +0200

    Intro.svelte
    introduce the article with the illustration in a float layout

---
+++ src/blog/aspect-a-surprise/Intro.svelte
@@ -0,0 +1,47 @@
+<script>
+	import svg from './svg.js';
+</script>
+
+<section>
+	<div>
+		{@html svg['painting']}
+	</div>
+
+	<p>
+		When you size an <code>&lt;svg&gt;</code> element with an existing <code>viewBox</code> you must
+		be wary of aspect ratio. The issue is more intuitive than
+		<a href="/meet-aspect-ratio">a certain article</a> would have you believe, but there are indeed a
+		few hiccups. And starting with the default behavior, you will soon discover you often get more than
+		you expect.
+	</p>
+</section>
+
+<style>
+	div {
+		max-inline-size: 20rem;
+	}
+
+	section > div {
+		float: right;
+		inline-size: 100%;
+		margin: 0 0 0.5rem 1rem;
+	}
+
+	@media screen and (max-width: 32rem) {
+		section > div {
+			margin: initial;
+		}
+
+		section {
+			display: flex;
+			flex-direction: column;
+		}
+
+		section > div {
+			margin-block-start: 1rem;
+			order: 1;
+			align-self: center;
+			inline-size: 100%;
+		}
+	}
+</style>
