82ad7658927d9807cc36aca2f34ba5eab35c73be
Gabriele Corti <borntofrappe@gmail.com>
2023-06-26 14:33:40 +0200

    mail.svg
    graphic introducing the article on an svg basic feature

---
+++ src/blog/z-overlap/svg.js
@@ -0,0 +1,83 @@
+export default {
+	mail: `<svg style="display: block;" viewBox="0 0 380 300">
+    <defs>
+        <symbol id="mail-sparkle" viewBox="-5 -5 10 10" x="-9" y="-9" width="18" height="18">
+            <path fill="currentColor" d="M -5 0 A 5 5 0 0 0 0 -5 A 5 5 0 0 0 5 0 A 5 5 0 0 0 0 5 A 5 5 0 0 0 -5 0 Z" />
+        </symbol>
+        <symbol id="mail-almond" viewBox="-6 -4 12 8" x="-12" y="-8" width="24" height="16">
+            <path fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" d="M -5 0 C -3.5 -2 -2 -3 0 -3 C 2 -3 3.5 -2 5 0 C 3.5 2 2 3 0 3 C -2 3 -3.5 2 -5 0 Z" />
+        </symbol>
+        <symbol id="mail-round" viewBox="-5 -5 10 10" x="-9" y="-9" width="18" height="18">
+            <circle fill="none" stroke="currentColor" stroke-width="2" r="4" />
+        </symbol>
+        <symbol id="mail-rhomboid" viewBox="-5 -5 10 10" x="-9" y="-9" width="18" height="18">
+            <path fill="none" stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" d="M -4 0 L 0 -4 L 4 0 L 0 4 Z" />
+        </symbol>
+        <symbol id="mail-lines" viewBox="-1.5 -19.5 19 21" x="-9.5" y="-10.5" width="19" height="21">
+            <g fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round">
+                <path d="M 0 0 L 16 -18" />
+                <path d="M 10 0 L 16 -6.75" />
+            </g>
+        </symbol>
+    </defs>
+    <g style="color: hsl(48 95% 76%);">
+        <use transform="translate(280 45) scale(1.6) rotate(-40)" href="#mail-almond" />
+
+        <use transform="translate(50 185)" href="#mail-round" />
+
+        <use transform="translate(65 210)" href="#mail-lines" />
+        <use transform="translate(230 40) rotate(180)" href="#mail-lines" />
+        <use transform="translate(310 230)" href="#mail-lines" />
+        <use transform="translate(325 105) rotate(180)" href="#mail-lines" />
+
+        <use transform="translate(340 180) scale(1.2)" href="#mail-rhomboid" />
+
+        <use transform="translate(85 70) scale(1.2)" href="#mail-sparkle" />
+        <use transform="translate(300 75)" href="#mail-sparkle" />
+        
+        <g fill="currentColor">
+            <circle transform="translate(135 60)" r="7" />
+            <circle transform="translate(260 80) scale(0.7)" r="7" />
+        </g>
+    </g>
+
+    <g style="color: hsl(36 77% 49%);" fill="currentColor">
+        <circle transform="translate(335 135) scale(1.5)" r="7" />
+    </g>
+
+    <g transform="translate(45 135)">
+        <g style="color: hsl(36 77% 49%);" fill="currentColor">
+            <path stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" d="M 5 -20 L 32 0 L 5 20 Z" />
+            <circle r="25" />
+        </g>
+        <g fill="hsl(48 100% 88%)">
+            <circle r="3.75" />
+            <circle cx="11" r="3.75" />
+            <circle cx="-11" r="3.75" />
+        </g>
+    </g>
+
+    <g transform="translate(190 150)">
+        <g stroke-linejoin="round" stroke-linecap="round">
+            <path style="color: hsl(42 87% 55%);" fill="currentColor" stroke="currentColor" stroke-width="30" d="M 0 -65 L 85 -15 L 85 65 A 20 20 0 0 1 65 85 L -65 85 A 20 20 0 0 1 -85 65 L -85 -15 L 0 -65 Z" />
+            <path style="color: hsl(48 100% 88%);" fill="currentColor" stroke="currentColor" stroke-width="30" d="M 0 -45 L 59.5 -10 L 0 20 L -59.5 -10 L 0 -45 Z" />
+            <g fill="none" stroke="hsl(36 77% 49%)" stroke-width="10">
+                <path d="M -26 -20 H 26" />
+                <path d="M -18 0 H 18" />
+            </g>
+        </g>
+    </g>
+
+    <g transform="translate(110 230)">
+        <g fill="none" stroke="hsl(36 77% 49%)" stroke-width="4" stroke-linecap="round">
+            <path d="M 0 0 L 14 -16" />
+            <path d="M 14 0 L 28 -16" />
+            <path d="M 28 0 L 42 -16" />
+        </g>
+    </g>
+    
+    <g transform="translate(245 225) rotate(-20)">
+        <path style="color: hsl(36 77% 49%);" fill="currentColor" stroke="currentColor" stroke-width="10" stroke-linejoin="round" stroke-linecap="round" d="M -15 0 C -10 -7 10 -7 15 0 C 10 7 -10 7 -15 0 Z" />
+    </g>
+</svg>`
+};
