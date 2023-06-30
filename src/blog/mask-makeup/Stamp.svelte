66f035670eda176bb2ef8f915544dcbe9f01009d
Gabriele Corti <borntofrappe@gmail.com>
2023-06-30 14:10:27 +0200

    refactor Stamp.svelte
    1. use a clipPath element to cheekily and effectively cut out the corners of the outer rectangle; 2. remove the unnecessary section container (prefer to size the elements from the parent scope); 3 (minor) rename selected to 'fill' to better describe the purpose of the variable

--- src/blog/mask-makeup/Stamp.svelte
+++ src/blog/mask-makeup/Stamp.svelte
@@ -9,105 +9,75 @@
 		'coral'
 	];
 
-	let selected = colors[0];
+	let [fill] = colors;
 </script>
 
-<section>
-	<div>
-		{#each colors as color}
-			<button
-				aria-current={selected === color}
-				style="background: {color}"
-				aria-label={color}
-				on:click={() => (selected = color)}
-			/>
-		{/each}
-	</div>
+<div>
+	{#each colors as color}
+		<button
+			aria-current={color === fill}
+			style="background: {color}"
+			aria-label={color}
+			on:click={() => (fill = color)}
+		/>
+	{/each}
+</div>
 
-	<svg style="display: block;" viewBox="0 0 100 100">
-		<defs>
-			<rect id="stamp-base" x="10" y="10" width="80" height="80" />
-			<mask id="stamp-mask">
-				<use fill="white" href="#stamp-base" />
-				<g transform="translate(50 50)">
-					<g id="stamp-scheme">
-						<g
-							fill="black"
-							stroke="black"
-							stroke-width="1"
-							stroke-linejoin="round"
-							stroke-linecap="round"
-						>
-							<path d="M 0 0 l -3 -3 v -4 l 3 4 3 -4 v 4z" />
-							<path
-								transform="rotate(45) translate(0 -20)"
-								d="M -3 0 v -4 l 3 -3 3 3 v 4 l -3 -3z"
-							/>
-							<path transform="translate(0 -25)" d="M 0 0 l -2 -3 2 -3 2 3z" />
-							<path
-								transform="translate(-3 -7) rotate(-30) translate(0 -25)"
-								d="M 0 0 l -2 -3 2 -3 2 3z"
-							/>
-							<path
-								transform="translate(3 -7) rotate(30) translate(0 -25)"
-								d="M 0 0 l -2 -3 2 -3 2 3z"
-							/>
-						</g>
+<svg style="display: block;" viewBox="0 0 100 100">
+	<defs>
+		<rect id="stamp-base" x="10" y="10" width="80" height="80" />
+		<mask id="stamp-mask">
+			<use fill="white" href="#stamp-base" />
+			<g transform="translate(50 50)">
+				<g id="stamp-scheme">
+					<g
+						fill="black"
+						stroke="black"
+						stroke-width="1"
+						stroke-linejoin="round"
+						stroke-linecap="round"
+					>
+						<path d="M 0 0 l -3 -3 v -4 l 3 4 3 -4 v 4z" />
+						<path
+							transform="rotate(45) translate(0 -20)"
+							d="M -3 3.5 v -4 l 3 -3 3 3 v 4 l -3 -3z"
+						/>
+						<path transform="translate(0 -20)" d="M -3 -4 l 3 4 3 -4 l -3 -4z" />
+						<path transform="rotate(45) translate(0 -35)" d="M -4 -3.5 l 4 -5 4 5 l -4 -2z" />
+						<path
+							transform="translate(-3 -7) rotate(-30) translate(0 -25)"
+							d="M 0 3 l -2 -3 2 -3 2 3z"
+						/>
+						<path
+							transform="translate(3 -7) rotate(30) translate(0 -25)"
+							d="M 0 3 l -2 -3 2 -3 2 3z"
+						/>
 					</g>
-					<use transform="rotate(90)" href="#stamp-scheme" />
-					<use transform="rotate(180)" href="#stamp-scheme" />
-					<use transform="rotate(270)" href="#stamp-scheme" />
 				</g>
-			</mask>
-			<pattern id="stamp-pattern-width" viewBox="0 0 2 2" width="0.1" height="1">
-				<circle fill="black" r="1" cx="1" />
-			</pattern>
-			<pattern id="stamp-pattern-height" viewBox="0 0 2 2" width="1" height="0.1">
-				<circle fill="black" r="1" cy="1" />
-			</pattern>
+				<use transform="rotate(90)" href="#stamp-scheme" />
+				<use transform="rotate(180)" href="#stamp-scheme" />
+				<use transform="rotate(270)" href="#stamp-scheme" />
+			</g>
+		</mask>
+		<clipPath id="stamp-clip">
+			<path
+				d="M 2.5 0 a 2.5 2.5 0 0 1 -2.5 2.5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 0 5 v 5 a 2.5 2.5 0 0 1 2.5 2.5 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 5 0 h 5 a 2.5 2.5 0 0 1 2.5 -2.5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 0 -5 v -5 a 2.5 2.5 0 0 1 -2.5 -2.5 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5 a 2.5 2.5 0 0 1 -5 -0 h -5z"
+			/>
+		</clipPath>
+	</defs>
 
-			<mask id="stamp-mask-pattern">
-				<rect fill="white" width="100" height="100" />
-				<rect fill="url(#stamp-pattern-width)" x="-5" width="110" height="5" />
-				<rect
-					fill="url(#stamp-pattern-width)"
-					transform="translate(100 100) rotate(180)"
-					x="-5"
-					width="110"
-					height="5"
-				/>
-				<rect fill="url(#stamp-pattern-height)" y="-5" width="5" height="110" />
-				<rect
-					fill="url(#stamp-pattern-height)"
-					transform="translate(100 100) rotate(180)"
-					y="-5"
-					width="5"
-					height="110"
-				/>
-			</mask>
-		</defs>
-
-		<rect mask="url(#stamp-mask-pattern)" fill="#fefefe" width="100" height="100" />
-		<rect fill={selected} x="5" y="5" width="90" height="90" />
-		<use fill="#ece5d9" mask="url(#stamp-mask)" href="#stamp-base" />
-	</svg>
-</section>
+	<rect clip-path="url(#stamp-clip)" fill="#fefefe" width="100" height="100" />
+	<rect {fill} x="5" y="5" width="90" height="90" />
+	<use mask="url(#stamp-mask)" fill="#ece5d9" href="#stamp-base" />
+</svg>
 
 <style>
-	section {
-		max-inline-size: 30rem;
-		margin-inline: auto;
-	}
-
-	section > * + * {
-		margin-block-start: 0.5rem;
-	}
-
 	div {
 		display: flex;
 		justify-content: center;
 		flex-wrap: wrap;
 		gap: 0.75rem;
+		padding: 0.5rem 0;
 	}
 
 	button {
@@ -117,5 +87,9 @@
 		border-radius: 50%;
 		border: none;
 		background: none;
+	}
+
+	button:focus {
+		outline-offset: 5px;
 	}
 </style>
