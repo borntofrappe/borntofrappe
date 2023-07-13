e3f193cd2f532ad26b785776495d03e1857d79f9
Gabriele Corti <borntofrappe@gmail.com>
2023-07-13 18:41:55 +0200

    add introduction
    introduce the article with the svg for the eagle

---
+++ src/blog/picross-pseudo-3d/Intro.svelte
@@ -0,0 +1,61 @@
+<script>
+	import svg from './svg.js';
+</script>
+
+<section>
+	<div>
+		{@html svg['eagle']}
+	</div>
+
+	<p>
+		This blog shares an affection for scalable vector graphics, but also picross, an intriguing
+		puzzle game where you fill a grid to conjure up interesting figures.
+	</p>
+
+	<p>
+		With SVG you are able to visualize solved puzzles quite effectively <a href="/picross-2d"
+			>in two dimensions</a
+		>, and exploring a different projection you can even try and break the mold, so to explore
+		<a href="/picross-projection">a third axis</a>.
+	</p>
+
+	<p>
+		The effect is quite convincing — there are certainly plenty of levels you can visualize in their
+		final, crisp state —, but with the benefit of a JavaScript library like Zdog you can go further,
+		and really explore deeper shapes. Better yet, you are able to visualize more complex pieces. Not
+		only squares, not only cubes.
+	</p>
+</section>
+
+<style>
+	div {
+		max-inline-size: 20rem;
+	}
+
+	section > p + p {
+		margin-block-start: 1rem;
+	}
+
+	section > div {
+		float: right;
+		inline-size: 100%;
+		margin: 0 0 0.5rem 1rem;
+	}
+
+	@media screen and (max-width: 32rem) {
+		section > div {
+			margin: initial;
+		}
+
+		section {
+			display: flex;
+			flex-direction: column;
+		}
+
+		section > div {
+			margin-block-end: 1rem;
+			align-self: center;
+			inline-size: 100%;
+		}
+	}
+</style>
