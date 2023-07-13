060d75762fb1bdbae7fdf45199ffeb527bba3215
Gabriele Corti <borntofrappe@gmail.com>
2023-07-13 09:00:42 +0200

    vocabulary.svg
    highlight shapes matching the alphabet

--- src/blog/picross-pseudo-3d/svg.js
+++ src/blog/picross-pseudo-3d/svg.js
@@ -1,5 +1,45 @@
 export default {
 	eagle: `<svg style="display: block;" viewBox="0 0 9 6">
     <path fill="currentColor" d="M 3 1 A 1 1 0 0 1 4 0 L 5 0 5 2 6 2 A 1 1 0 0 1 7 1 L 9 1 A 1 1 0 0 1 8 2 L 7 2 A 1 1 0 0 1 8 3 L 6 3 A 1 1 0 0 1 7 4 L 6 4 7 5 6 5 5 4 5 5 6 6 3 6 4 5 4 4 3 5 2 5 3 4 2 4 A 1 1 0 0 1 3 3 L 1 3 A 1 1 0 0 1 2 2 L 1 2 A 1 1 0 0 1 0 1 L 2 1 A 1 1 0 0 1 3 2 L 4 2 4 1 Z" />
+</svg>`,
+	vocabulary: `<svg style="display: block;" viewBox="-1.25 -1.299620859324932 13.422430038452148 7.624512195587158">
+    <g fill="currentColor">
+        <rect width="1" height="1" />
+        <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">A</text>
+
+        <g transform="translate(0 2)">
+            <path d="M 0 0 L 0 1 1 1 A 1 1 0 0 0 0 0 Z" />
+            <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">B</text>
+            <g transform="translate(3 0)">
+                <path d="M 0 0 L 1 0 A 1 1 0 0 1 0 1 Z" />
+                <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">C</text>
+            </g>
+            <g transform="translate(6 0)">
+                <path d="M 0 0 L 1 0 1 1 A 1 1 0 0 1 0 0 Z" />
+                <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">D</text>
+            </g>
+            <g transform="translate(9 0)">
+                <path d="M 1 0 L 1 1 0 1 A 1 1 0 0 1 1 0 Z" />
+                <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">E</text>
+            </g>
+        </g>
+
+        <g transform="translate(0 4)">
+            <path d="M 0 0 L 0 1 1 1 Z" />
+            <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">F</text>
+            <g transform="translate(3 0)">
+                <path d="M 0 0 L 1 0 0 1 Z" />
+                <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">G</text>
+            </g>
+            <g transform="translate(6 0)">
+                <path d="M 0 0 L 1 0 1 1 Z" />
+                <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">H</text>
+            </g>
+            <g transform="translate(9 0)">
+                <path d="M 1 0 L 1 1 0 1 Z" />
+                <text x="1.4" y="0.825" font-family="monospace" font-size="0.95" font-weight="700">I</text>
+            </g>
+        </g>
+    </g>	
 </svg>`
 };
