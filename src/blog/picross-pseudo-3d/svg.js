2b58781906170d743bf006fcb8de1d05f89fd79b
Gabriele Corti <borntofrappe@gmail.com>
2023-07-13 08:52:17 +0200

    eagle.svg
    add graphic introducing the article

---
+++ src/blog/picross-pseudo-3d/svg.js
@@ -0,0 +1,5 @@
+export default {
+	eagle: `<svg style="display: block;" viewBox="0 0 9 6">
+    <path fill="currentColor" d="M 3 1 A 1 1 0 0 1 4 0 L 5 0 5 2 6 2 A 1 1 0 0 1 7 1 L 9 1 A 1 1 0 0 1 8 2 L 7 2 A 1 1 0 0 1 8 3 L 6 3 A 1 1 0 0 1 7 4 L 6 4 7 5 6 5 5 4 5 5 6 6 3 6 4 5 4 4 3 5 2 5 3 4 2 4 A 1 1 0 0 1 3 3 L 1 3 A 1 1 0 0 1 2 2 L 1 2 A 1 1 0 0 1 0 1 L 2 1 A 1 1 0 0 1 3 2 L 4 2 4 1 Z" />
+</svg>`
+};
