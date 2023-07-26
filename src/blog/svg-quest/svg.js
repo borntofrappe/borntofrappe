4a25d8587aadddcaa21d5cbe76292e271b8fd811
Gabriele Corti <borntofrappe@gmail.com>
2023-07-26 09:35:39 +0200

    syntax preference
    add the instructions for entries 081-082 after the individual sprites

--- src/blog/svg-quest/svg.js
+++ src/blog/svg-quest/svg.js
@@ -1,82 +1,4 @@
 export default {
-	'entries-081-082': `<svg style="display: block;" viewBox="0 0 2.6 1.7">
-    <path fill="hsl(0 0% 80%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.6 A 0.1 0.1 0 0 1 1.1 1.7 L 0.1 1.7 A 0.1 0.1 0 0 1 0 1.6 Z" />
-    <path fill="hsl(0 0% 100%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.5 A 0.1 0.1 0 0 1 1.1 1.6 L 0.1 1.6 A 0.1 0.1 0 0 1 0 1.5 Z" />
-    <svg viewBox="0 0 100 100" x="0.1" y="0.35" width="1" height="1">
-        <title>Magnemite</title>
-        <defs>
-            <clipPath id="clip-entry">
-                <rect width="100" height="100" rx="8" />
-            </clipPath>
-        </defs>
-        <g clip-path="url(#clip-entry)">
-            <rect fill="hsl(195 36% 78%)" width="100" height="100" />
-            <rect fill="hsl(216 45% 98%)" x="25" y="12" width="50" height="50" />
-            <rect fill="hsl(300 1% 37%)" x="47" y="33.5" width="6" height="6" />
-            <g transform="translate(8 68)">
-                <rect fill="hsl(300 2% 79%)" width="20" height="20" />
-                <g fill="hsl(0 1% 55%)">
-                    <rect x="7" y="2" width="6" height="16" />	
-                    <rect x="2" y="7" width="16" height="6" />	
-                </g>
-            </g>
-            <g transform="translate(72 68)">
-                <rect fill="hsl(300 2% 79%)" width="20" height="20" />
-                <g fill="hsl(0 1% 55%)">
-                    <rect x="7" y="2" width="6" height="16" />	
-                    <rect x="2" y="7" width="16" height="6" />	
-                </g>
-            </g>
-        </g>
-    </svg>
-    <g fill="hsl(0 3% 13%)" font-family="sans-serif">
-        <text text-anchor="middle" x="0.25" y="0.2" font-size="0.2" font-weight="700">081</text>		
-        <text transform="translate(0 1.37)" text-anchor="middle" x="0.6" y="0.17" font-size="0.17">Magnemite</text>
-    </g>
-
-    <g transform="translate(1.4 0)">
-        <path fill="hsl(0 0% 80%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.6 A 0.1 0.1 0 0 1 1.1 1.7 L 0.1 1.7 A 0.1 0.1 0 0 1 0 1.6 Z" />
-        <path fill="hsl(0 0% 100%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.5 A 0.1 0.1 0 0 1 1.1 1.6 L 0.1 1.6 A 0.1 0.1 0 0 1 0 1.5 Z" />
-        <svg viewBox="0 0 100 100" x="0.1" y="0.35" width="1" height="1">
-            <title>Magneton</title>
-            <defs>
-                <clipPath id="clip-entry">
-                    <rect width="100" height="100" rx="8" />
-                </clipPath>
-                <symbol id="magnemite" viewBox="0 0 100 100">
-                    <title>Magnemite</title>
-                    <rect fill="hsl(195 36% 78%)" width="100" height="100" />
-                    <rect fill="hsl(216 45% 98%)" x="25" y="12" width="50" height="50" />
-                    <rect fill="hsl(300 1% 37%)" x="47" y="33.5" width="6" height="6" />
-                    <g transform="translate(8 68)">
-                        <rect fill="hsl(300 2% 79%)" width="20" height="20" />
-                        <g fill="hsl(0 1% 55%)">
-                            <rect x="7" y="2" width="6" height="16" />	
-                            <rect x="2" y="7" width="16" height="6" />	
-                        </g>
-                    </g>
-                    <g transform="translate(72 68)">
-                        <rect fill="hsl(300 2% 79%)" width="20" height="20" />
-                        <g fill="hsl(0 1% 55%)">
-                            <rect x="7" y="2" width="6" height="16" />	
-                            <rect x="2" y="7" width="16" height="6" />	
-                        </g>
-                    </g>
-                </symbol>
-            </defs>
-            <g clip-path="url(#clip-entry)">
-                <rect fill="hsl(44 100% 68%)" width="100" height="100" />
-                <use href="#magnemite" x="25" width="50" height="50" />
-                <use href="#magnemite" y="50" width="50" height="50" />
-                <use href="#magnemite" x="50" y="50" width="50" height="50" />
-            </g>
-        </svg>
-        <g fill="hsl(0 3% 13%)" font-family="sans-serif">
-            <text text-anchor="middle" x="0.25" y="0.2" font-size="0.2" font-weight="700">082</text>		
-            <text transform="translate(0 1.37)" text-anchor="middle" x="0.6" y="0.17" font-size="0.17">Magneton</text>
-        </g>
-    </g>
-</svg>`,
 	magnemite: `<svg style="display: block;" viewBox="0 0 100 100">
     <title>Magnemite</title>
     <defs>
@@ -214,6 +136,84 @@ export default {
             </g>
         </g>
     </svg>
+</svg>`,
+	'entries-081-082': `<svg style="display: block;" viewBox="0 0 2.6 1.7">
+    <path fill="hsl(0 0% 80%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.6 A 0.1 0.1 0 0 1 1.1 1.7 L 0.1 1.7 A 0.1 0.1 0 0 1 0 1.6 Z" />
+    <path fill="hsl(0 0% 100%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.5 A 0.1 0.1 0 0 1 1.1 1.6 L 0.1 1.6 A 0.1 0.1 0 0 1 0 1.5 Z" />
+    <svg viewBox="0 0 100 100" x="0.1" y="0.35" width="1" height="1">
+        <title>Magnemite</title>
+        <defs>
+            <clipPath id="clip-entry">
+                <rect width="100" height="100" rx="8" />
+            </clipPath>
+        </defs>
+        <g clip-path="url(#clip-entry)">
+            <rect fill="hsl(195 36% 78%)" width="100" height="100" />
+            <rect fill="hsl(216 45% 98%)" x="25" y="12" width="50" height="50" />
+            <rect fill="hsl(300 1% 37%)" x="47" y="33.5" width="6" height="6" />
+            <g transform="translate(8 68)">
+                <rect fill="hsl(300 2% 79%)" width="20" height="20" />
+                <g fill="hsl(0 1% 55%)">
+                    <rect x="7" y="2" width="6" height="16" />	
+                    <rect x="2" y="7" width="16" height="6" />	
+                </g>
+            </g>
+            <g transform="translate(72 68)">
+                <rect fill="hsl(300 2% 79%)" width="20" height="20" />
+                <g fill="hsl(0 1% 55%)">
+                    <rect x="7" y="2" width="6" height="16" />	
+                    <rect x="2" y="7" width="16" height="6" />	
+                </g>
+            </g>
+        </g>
+    </svg>
+    <g fill="hsl(0 3% 13%)" font-family="sans-serif">
+        <text text-anchor="middle" x="0.25" y="0.2" font-size="0.2" font-weight="700">081</text>		
+        <text transform="translate(0 1.37)" text-anchor="middle" x="0.6" y="0.17" font-size="0.17">Magnemite</text>
+    </g>
+
+    <g transform="translate(1.4 0)">
+        <path fill="hsl(0 0% 80%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.6 A 0.1 0.1 0 0 1 1.1 1.7 L 0.1 1.7 A 0.1 0.1 0 0 1 0 1.6 Z" />
+        <path fill="hsl(0 0% 100%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.5 A 0.1 0.1 0 0 1 1.1 1.6 L 0.1 1.6 A 0.1 0.1 0 0 1 0 1.5 Z" />
+        <svg viewBox="0 0 100 100" x="0.1" y="0.35" width="1" height="1">
+            <title>Magneton</title>
+            <defs>
+                <clipPath id="clip-entry">
+                    <rect width="100" height="100" rx="8" />
+                </clipPath>
+                <symbol id="magnemite" viewBox="0 0 100 100">
+                    <title>Magnemite</title>
+                    <rect fill="hsl(195 36% 78%)" width="100" height="100" />
+                    <rect fill="hsl(216 45% 98%)" x="25" y="12" width="50" height="50" />
+                    <rect fill="hsl(300 1% 37%)" x="47" y="33.5" width="6" height="6" />
+                    <g transform="translate(8 68)">
+                        <rect fill="hsl(300 2% 79%)" width="20" height="20" />
+                        <g fill="hsl(0 1% 55%)">
+                            <rect x="7" y="2" width="6" height="16" />	
+                            <rect x="2" y="7" width="16" height="6" />	
+                        </g>
+                    </g>
+                    <g transform="translate(72 68)">
+                        <rect fill="hsl(300 2% 79%)" width="20" height="20" />
+                        <g fill="hsl(0 1% 55%)">
+                            <rect x="7" y="2" width="6" height="16" />	
+                            <rect x="2" y="7" width="16" height="6" />	
+                        </g>
+                    </g>
+                </symbol>
+            </defs>
+            <g clip-path="url(#clip-entry)">
+                <rect fill="hsl(44 100% 68%)" width="100" height="100" />
+                <use href="#magnemite" x="25" width="50" height="50" />
+                <use href="#magnemite" y="50" width="50" height="50" />
+                <use href="#magnemite" x="50" y="50" width="50" height="50" />
+            </g>
+        </svg>
+        <g fill="hsl(0 3% 13%)" font-family="sans-serif">
+            <text text-anchor="middle" x="0.25" y="0.2" font-size="0.2" font-weight="700">082</text>		
+            <text transform="translate(0 1.37)" text-anchor="middle" x="0.6" y="0.17" font-size="0.17">Magneton</text>
+        </g>
+    </g>
 </svg>`,
 	'entries-050-051': `<svg style="display: block;" viewBox="0 0 2.6 1.7">
     <path fill="hsl(0 0% 80%)" d="M 0 0.1 A 0.1 0.1 0 0 1 0.1 0 L 0.4 0 A 0.1 0.1 0 0 1 0.5 0.1 L 0.5 0.25 L 1.1 0.25 A 0.1 0.1 0 0 1 1.2 0.35 L 1.2 1.6 A 0.1 0.1 0 0 1 1.1 1.7 L 0.1 1.7 A 0.1 0.1 0 0 1 0 1.6 Z" />
