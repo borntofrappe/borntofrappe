a76f16c27c2578118144f00c7bae5c22863983bb
Gabriele Corti <borntofrappe@gmail.com>
2023-08-01 21:57:14 +0200

    import footer component
    include the footer, similarly to the homepage, in the route dedicated to the blog posts

--- src/routes/[slug=slug]/+page.svelte
+++ src/routes/[slug=slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
+
 	export let data;
 </script>
 
@@ -6,4 +8,24 @@
 	<link rel="icon" href="/icons/blog.svg" type="image/svg+xml" />
 </svelte:head>
 
-<svelte:component this={data.Component} />
+<div
+	class="[ box ]"
+	style="
+		--box-color: var(--color);
+		--box-background: var(--background);
+		min-height: 100vh;
+		z-index: 1;
+		position: relative;
+	"
+>
+	<svelte:component this={data.Component} />
+</div>
+
+<div
+	style="
+		position: sticky;
+		bottom: 0;
+	"
+>
+	<Footer />
+</div>
