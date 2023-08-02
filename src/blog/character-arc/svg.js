b506d9d0ccf480be2a2c4f62c2bc947a450e9861
Gabriele Corti <borntofrappe@gmail.com>
2023-08-02 18:03:59 +0200

    rainbow.svg
    add graphic meant to introduce the article

---
+++ src/blog/character-arc/svg.js
@@ -0,0 +1,14 @@
+export default {
+	rainbow: `<svg aria-labeledby="rainbow-title rainbow-description" style="display: block;" viewBox="-0.5 -0.5 11 11">
+    <title id="rainbow-title">Colorful arcs</title>
+    <description id="rainbow-description">In French the word 'rainbow' is literally 'arc-en-ciel', arc in the sky. The word is apt to describe visual, specifically drawn with path elements and the 'A' character.</description>
+    <g fill="none" stroke-width="1" stroke-linecap="round">
+        <path stroke="hsl(245 93% 72%)" pathLength="1" stroke-dasharray="0.8 1.2" stroke-dashoffset="-0.08" d="M 10 5 A 5 5 0 0 0 5 10" />
+        <path stroke="hsl(191 100% 46%)" pathLength="1" stroke-dasharray="0.8 1.2" stroke-dashoffset="-0.12" d="M 10 4 A 6 6 0 0 0 4 10" />
+        <path stroke="hsl(81 59% 51%)" pathLength="1" stroke-dasharray="0.8 1.2" stroke-dashoffset="-0.08" d="M 10 3 A 7 7 0 0 0 3 10" />
+        <path stroke="hsl(51 95% 55%)" pathLength="1" stroke-dasharray="0.8 1.2" stroke-dashoffset="-0.12" d="M 10 2 A 8 8 0 0 0 2 10" />
+        <path stroke="hsl(33 100% 50%)" pathLength="1" stroke-dasharray="0.8 1.2" stroke-dashoffset="-0.08" d="M 10 1 A 9 9 0 0 0 1 10" />
+        <path stroke="hsl(15 100% 55%)" pathLength="1" stroke-dasharray="0.8 1.2" stroke-dashoffset="-0.12" d="M 10 0 A 10 10 0 0 0 0 10" />
+    </g>
+</svg>`
+};
