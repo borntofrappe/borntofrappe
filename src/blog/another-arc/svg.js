e660509930c3aced61c3ccd5413274c69fed1909
Gabriele Corti <borntofrappe@gmail.com>
2023-08-06 13:38:27 +0200

    add svg.js
    include file exporting the svg syntax for the project; add the graphic introducing the article

---
+++ src/blog/another-arc/svg.js
@@ -0,0 +1,19 @@
+export default {
+	'treasure-chest': `<svg style="display: block;" viewBox="0 0 100 65">
+    <title>A chest possibly packed with treasure</title>
+    <path fill="hsl(10 34% 54%)" d="M 68 0 A 25 25 0 0 1 93 25 L 87 25 A 19 19 0 0 0 68 6 A 19 19 0 0 0 47 25 L 41 25 A 25 25 0 0 1 62 0" />
+    <path fill="hsl(12 39% 63%)" d="M 65 3 A 22 22 0 0 1 87 25 L 57 25 A 22 22 0 0 0 35 3 Z" />
+    <path fill="hsl(43 92% 67%)" d="M 25 0 L 32 0 A 25 25 0 0 1 57 25 L 93 25 A 25 25 0 0 0 68 0 L 75 0 A 25 25 0 0 1 100 25 L 100 32 L 50 32 L 50 25 A 25 25 0 0 0 25 0" />
+    <path fill="hsl(34 72% 48%)" d="M 6 25 L 50 25 L 50 32 0 32 L 0 25 A 25 25 0 0 1 50 25 L 44 25 A 18.5 18.5 0 0 0 7 25 Z" />
+    <path fill="hsl(10 43% 43%)" d="M 44 25 A 18.5 18.5 0 0 0 7 25 Z" />
+    <path fill="hsl(8 48% 39%)" d="M 9 25 L 7 25 A 18.5 18.5 0 0 1 25.5 6.5 A 12.5 18.5 0 0 0 13 25" />
+    <path fill="hsl(10 34% 54%)" d="M 2 32 L 98 32 L 98 35 L 2 35 Z" />
+    <path fill="hsl(8 48% 39%)" d="M 2 32 L 48 32 L 48 35 L 2 35 Z" />
+    <path fill="hsl(34 72% 48%)" d="M 7 42 L 44 42 L 44 58 L 7 58 L 7 35 L 0 35 L 0 65 L 50 65 L 50 35 L 7 35 Z" />
+    <path fill="hsl(10 43% 43%)" d="M 7 42 L 44 42 L 44 58 L 7 58 L 7 35 Z" />
+    <path fill="hsl(8 48% 39%)" d="M 7 42 L 13 42 L 13 58 L 7 58" />
+    <path fill="hsl(43 92% 67%)" d="M 57 42 L 94 42 L 94 58 L 57 58 L 57 35 L 50 35 L 50 65 L 57 65 L 100 65 L 100 35 L 57 35 Z" />
+    <path fill="hsl(12 39% 63%)" d="M 57 42 L 94 42 L 94 58 L 57 58 L 57 35 Z" />
+    <path fill="hsl(10 34% 54%)" d="M 88 42 L 94 42 L 94 58 L 88 58" />
+</svg>`
+};
