f96d1424911ae5682083b69287bd3c4fe55cbd9e
Gabriele Corti <borntofrappe@gmail.com>
2023-08-09 21:58:59 +0200

    fix layout
    create .svelte component to house the illustrations side by side; use a div as a generic container and wrap the images in a figure element

---
+++ src/blog/svg-quest/Entries.svelte
@@ -0,0 +1,46 @@
+<script>
+	import svg from './svg.js';
+</script>
+
+<div>
+	<figure>
+		{@html svg['entries-050-051']}
+	</figure>
+	<figure>
+		{@html svg['entries-100-101']}
+	</figure>
+</div>
+
+<style>
+	div {
+		max-inline-size: 36rem;
+		margin-inline: auto;
+	}
+
+	div {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	div > figure {
+		flex-basis: 8rem;
+	}
+
+	@media (min-width: 36rem) {
+		div {
+			display: grid;
+			grid-template-columns: repeat(5, 1fr);
+			gap: 1rem;
+		}
+
+		div > figure:first-of-type {
+			grid-column: 1 / span 2;
+		}
+
+		div > figure:last-of-type {
+			grid-column: 4 / span 2;
+		}
+	}
+</style>
