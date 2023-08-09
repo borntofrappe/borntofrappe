5a88a7d165fc1e788330f1cde9bf4fda159d5ee7
Gabriele Corti <borntofrappe@gmail.com>
2023-08-09 09:38:36 +0200

    layout component
    include article visuals side by side; targeting a div in the .svx article would have the styles affected to _all_ elements, while the component scopes the styles to the single div

---
+++ src/blog/pseudo-3d-land/Pseudo3DLand.svelte
@@ -0,0 +1,23 @@
+<script>
+	import svg from './svg.js';
+	import Pseudo3D from './Pseudo3D.svelte';
+</script>
+
+<div>
+	{@html svg['2d']}
+
+	<Pseudo3D />
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+		justify-content: center;
+	}
+
+	div :global(> *) {
+		flex-basis: 300px;
+	}
+</style>
