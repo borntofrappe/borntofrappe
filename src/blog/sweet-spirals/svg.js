35f95d674e7360c6be1413ad809ce8370e967a33
Gabriele Corti <borntofrappe@gmail.com>
2023-08-14 09:26:05 +0200

    spiral.svg
    sraw a spiral with svg, a path element and arc commands

---
+++ src/blog/sweet-spirals/svg.js
@@ -0,0 +1,6 @@
+export default {
+	'spiral-arc': `<svg style="display: block;" viewBox="-25 -25 50 50">
+    <title>Arc spiral</title>
+    <path fill="none" stroke="currentColor" stroke-linecap="round" d="M 0 0 a 2 2 0 0 0 4 0 a 4 4 0 0 0 -8 0 a 6 6 0 0 0 12 0 a 8 8 0 0 0 -16 0 a 10 10 0 0 0 20 0 a 12 12 0 0 0 -24 0 a 14 14 0 0 0 28 0 a 16 16 0 0 0 -32 0 a 18 18 0 0 0 36 0" />
+</svg>`
+};
