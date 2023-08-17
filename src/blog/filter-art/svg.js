3ed4d33c4a044584aea2544b591f4468826376ab
Gabriele Corti <borntofrappe@gmail.com>
2023-08-17 10:21:17 +0200

    intro graphic
    add svg.js to export svg syntax; include vector graphic introducing the article

---
+++ src/blog/filter-art/svg.js
@@ -0,0 +1,21 @@
+export default {
+	'abstract-art': `<svg role="img" style="display: block;" viewBox="0 0 100 75">
+    <title>Abstract art</title>
+    <defs>
+        <pattern id="canvas-pattern" viewBox="0 0 2 2" patternUnits="userSpaceOnUse" width="1" height="1">
+            <rect fill="hsl(0 0% 89%)" width="2" height="2" />
+            <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+            <rect fill="hsl(0 0% 88%)" y="1" width="1" height="1" />
+        </pattern>
+    </defs>
+    <g stroke-linecap="round">
+        <rect fill="url(#canvas-pattern)" width="100" height="75" />
+        <path fill="none" stroke="hsl(0 93% 71%)" stroke-width="4" d="M 7 7 96 7 M 5 13 65 13 M 16 20 80 20 M 31 26 94 26 M 4 32 72 32 M 23 38 91 38 " />
+        <path fill="none" stroke="hsl(208 54% 49%)" stroke-width="5" d="M 4 46 94 46 M 9 54 93 54 M 5 62 90 62 M 11 69 90 69" />
+        <g style="color: hsl(63 91% 54%)">
+            <circle fill="currentColor" cx="50" cy="25" r="9" />
+            <path opacity="0.9" fill="none" stroke="currentColor" stroke-width="3" d="M 43 46 54 46 M 38 52 58 52 M 37 57 61 57 M 34 62 64 62 M 29 68 70 68" />
+        </g>
+    </g>
+</svg>`
+};
