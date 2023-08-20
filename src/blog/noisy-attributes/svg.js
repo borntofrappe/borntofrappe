f57eafdd442fd903854358145a1376730847988c
Gabriele Corti <borntofrappe@gmail.com>
2023-08-20 18:28:07 +0200

    card-5.svg
    final graphic usng the noise texture with feTurbulence to displace the pixels with feDisplacementMap

---
+++ src/blog/noisy-attributes/svg.js
@@ -0,0 +1,16 @@
+export default {
+	'card-5': `<svg style="display: block;" viewBox="-2 -2 13 13">
+    <title>A butterfly. Or a bat. Or...</title>
+    <defs>
+        <symbol id="card-5-painting" viewBox="-50 -50 100 100" width="10" height="10">
+            <rect fill="hsl(28 89% 96%)" x="-50" y="-50" width="100" height="100" />
+            <path fill="hsl(338 26% 18%)" stroke="hsl(338 26% 18%)" stroke-linecap="round" stroke-linejoin="round" d="M 0 -12.5 2 -12.5 4 -20.5 3 -11.5 4 -5.5 12 -7.5 15 -1.5 26 -2.5 27 8.5 43 19.5 32 16.5 40 22.5 31 20.5 26 21.5 14 15.5 15 13.5 9 12.5 6 9.5 2 13.5 3 20.5 0 15.5 -3 20.5 -2 13.5 -6 9.5 -9 12.5 -15 13.5 -14 15.5 -26 21.5 -31 20.5 -40 22.5 -32 16.5 -43 19.5 -27 8.5 -26 -2.5 -15 -1.5 -12 -7.5 -4 -5.5 -3 -11.5 -4 -20.5 -2 -12.5" />
+        </symbol>
+        <filter id="card-5-filter">
+            <feTurbulence numOctaves="3" baseFrequency="0.3" />
+            <feDisplacementMap in="SourceGraphic" scale="-3" />
+        </filter>
+    </defs>
+    <use filter="url(#card-5-filter)" href="#card-5-painting" />
+</svg>`
+};
