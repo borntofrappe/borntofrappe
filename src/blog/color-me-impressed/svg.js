f0bb9b451ffc5eaa1cf247c35625b81bae76d9a5
Gabriele Corti <borntofrappe@gmail.com>
2023-08-23 09:15:01 +0200

    remove unused graphic
    portrait.svg is already included inline in Tint.svelte

--- src/blog/color-me-impressed/svg.js
+++ src/blog/color-me-impressed/svg.js
@@ -43,14 +43,5 @@ export default {
     <use href="#hueRotate-sprite" />
     <use transform="translate(1 0)" filter="url(#hueRotate-filter-120)" href="#hueRotate-sprite" />
     <use transform="translate(2 0)" filter="url(#hueRotate-filter-240)" href="#hueRotate-sprite" />
-</svg>`,
-	portrait: `<svg style="display: block;" viewBox="0 -0.5 16 12">
-    <title>Retro-looking portrait</title>
-    <path stroke="hsl(86 25% 22%)" d="M 0 0 h 16 M 0 1 h 1 M 15 1 h 1 M 0 2 h 1 M 15 2 h 1 M 0 3 h 1 M 15 3 h 1 M 0 4 h 1 M 15 4 h 1 M 0 5 h 1 M 15 5 h 1 M 0 6 h 1 M 15 6 h 1 M 0 7 h 1 M 15 7 h 1 M 0 8 h 1 M 15 8 h 1 M 0 9 h 1 M 15 9 h 1 M 0 10 h 1 M 15 10 h 1 M 0 11 h 16" />
-    <path stroke="hsl(60 6% 90%)" d="M 1 1 h 14 M 1 2 h 1 M 14 2 h 1 M 1 3 h 1 M 14 3 h 1 M 1 4 h 1 M 14 4 h 1 M 1 5 h 1 M 14 5 h 1 M 1 6 h 1 M 14 6 h 1 M 1 7 h 1 M 14 7 h 1 M 1 8 h 1 M 14 8 h 1 M 1 9 h 1 M 14 9 h 1 M 1 10 h 14" />
-    <path stroke="hsl(85 6% 62%)" d="M 2 2 h 12 M 2 3 h 2 M 9 3 h 5 M 2 4 h 1 M 10 4 h 4 M 2 5 h 4 M 10 5 h 4 M 2 6 h 4 M 11 6 h 3 M 2 7 h 2 M 11 7 h 3 M 2 8 h 4 M 12 8 h 2 M 2 9 h 4 M 12 9 h 2" />
-    <path stroke="hsl(83 10% 42%)" d="M 4 3 h 1 M 3 4 h 1 M 8 6 h 2 M 4 7 h 7 M 6 8 h 5 M 6 9 h 6" />
-    <path stroke="hsl(30 5% 93%)" d="M 5 3 h 4 M 4 4 h 4 M 9 4 h 1 M 6 5 h 4 M 6 6 h 2 M 10 6 h 1 M 11 8 h 1" />
-    <path stroke="hsl(82 6% 63%)" d="M 8 4 h 1" />
 </svg>`
 };
