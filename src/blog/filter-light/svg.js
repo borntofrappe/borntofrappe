c83ad7790e9f4607a8780e6707864ede180c81db
Gabriele Corti <borntofrappe@gmail.com>
2023-08-31 22:31:14 +0200

    svg.js
    introductory graphic giving a sample of flat, 2d designs in svg

---
+++ src/blog/filter-light/svg.js
@@ -0,0 +1,90 @@
+export default {
+	'flat-design': `<svg style="display: block;" viewBox="0 0 260 80">
+    <title>A stylish sample of flat design</title>
+    <svg width="80" height="80" viewBox="-40 -40 80 80">
+        <title>Sunrise</title>
+        <defs>
+            <clipPath id="sunrise-clip">
+                <rect x="-40" y="-40" width="80" height="80" rx="10" />
+            </clipPath>
+        </defs>
+        <g clip-path="url(#sunrise-clip)">
+            <rect fill="hsl(2 100% 72%)" x="-40" y="-40" width="80" height="80" />
+            <path fill="hsl(32 100% 64%)" d=" M 24 0 C 28.531695488854606 9.270509831248422 28.531695488854606 9.270509831248422 19.41640786499874 14.106846055019355 C 17.633557568774194 24.270509831248425 17.633557568774194 24.270509831248425 7.416407864998739 22.825356391083684 C 1.83697019872103e-15 30 1.83697019872103e-15 30 -7.416407864998736 22.825356391083687 C -17.63355756877419 24.270509831248425 -17.63355756877419 24.270509831248425 -19.416407864998735 14.106846055019357 C -28.531695488854606 9.270509831248425 -28.531695488854606 9.270509831248425 -24 2.9391523179536475e-15 C -28.531695488854606 -9.270509831248432 -28.531695488854606 -9.270509831248432 -19.416407864998742 -14.106846055019354 C -17.633557568774197 -24.27050983124842 -17.633557568774197 -24.27050983124842 -7.4164078649987415 -22.825356391083684 C -5.510910596163089e-15 -30 -5.510910596163089e-15 -30 7.4164078649987335 -22.825356391083687 C 17.633557568774187 -24.270509831248425 17.633557568774187 -24.270509831248425 19.416407864998735 -14.10684605501936 C 28.531695488854606 -9.27050983124843 28.531695488854606 -9.27050983124843 24 0" />
+            <circle fill="hsl(48 96% 66%)" r="20" />
+            <path fill="hsl(209 95% 67%)" d="M -43.75 14 C -39.375 14 -39.375 12 -35 12 S -30.625 14 -26.25 14 S -21.875 12 -17.5 12 S -13.125 14 -8.75 14 S -4.375 12 0 12 S 4.375 14 8.75 14 S 13.125 12 17.5 12 S 21.875 14 26.25 14 S 30.625 12 35 12 S 39.375 14 43.75 14 L 43.75 41.20633888244629 L -43.75 41.20633888244629 Z" />
+            <path stroke="hsl(0 0% 99%)" stroke-width="3" stroke-linecap="round" d="M -22 23 22 23 M -10 31 10 31" />
+        </g>
+    </svg>
+    <svg x="90" width="80" height="80" viewBox="-40 -40 80 80">
+        <title>Cityscape at dusk</title>
+        <defs>
+            <clipPath id="cityscape-dusk-clip">
+                <rect x="-40" y="-40" width="80" height="80" rx="10" />
+            </clipPath>
+            <rect id="cityscape-dusk-window" x="-3" y="-3" width="6" height="6" rx="1.5" />
+        </defs>
+        <g clip-path="url(#cityscape-dusk-clip)">
+            <rect fill="hsl(279 34% 63%)" x="-40" y="-40" width="80" height="80" />
+            <path fill="hsl(250 20% 41%)" d="M -40 14 L -35 14 -35 -20 A 5 5 0 0 1 -30 -25 L -20 -25 -20 -27.5 A 5 5 0 0 1 -15 -32.5 L -10 -32.5 A 5 5 0 0 1 -5 -27.5 L -5 5 30 5 A 5 5 0 0 1 35 10 L 35 20 40 20 40 40 -40 40 Z" />
+            <g fill="hsl(251 21% 53%)">
+                <path d="M -40 0 L -30 0 A 5 5 0 0 1 -25 5 L -25 40 -30 40 -40 40 Z" />
+                <path d="M -20 40 L -20 -10 A 5 5 0 0 1 -15 -15 L -1.5 -15 A 5 5 0 0 1 3.5 -10 L 3.5 25 10 25 A 5 5 0 0 1 15 30 L 15 40 Z" />
+                <path d="M 20 40 L 20 20 A 5 5 0 0 1 25 15 L 40 15 40 30 40 40 Z" />
+            </g>
+            <g fill="hsl(276 23% 28%)">
+                <use transform="translate(-32.5 7)" href="#cityscape-dusk-window" />
+                <use transform="translate(-32.5 27)" href="#cityscape-dusk-window" />
+                <use transform="translate(-12.5 12)" href="#cityscape-dusk-window" />
+                <use transform="translate(-4 22)" href="#cityscape-dusk-window" />
+                <use transform="translate(-4 -8)" href="#cityscape-dusk-window" />
+            </g>
+            <g fill="hsl(55 100% 77%)">
+                <use transform="translate(-32.5 17)" href="#cityscape-dusk-window" />
+                <use transform="translate(-12.5 -8)" href="#cityscape-dusk-window" />
+                <use transform="translate(-12.5 2)" href="#cityscape-dusk-window" />
+                <use transform="translate(-12.5 22)" href="#cityscape-dusk-window" />
+                <use transform="translate(-4 2)" href="#cityscape-dusk-window" />
+                <use transform="translate(-4 12)" href="#cityscape-dusk-window" />
+                <rect transform="translate(25 20)" width="30" height="5" rx="2.5" />
+                <rect transform="translate(25 30)" width="30" height="5" rx="2.5" />
+            </g>
+        </g>
+    </svg>
+    <svg x="180" width="80" height="80" viewBox="-40 -40 80 80">
+        <title>Shooting star</title>
+        <defs>
+            <clipPath id="shooting-star-clip">
+                <rect x="-40" y="-40" width="80" height="80" rx="10" />
+            </clipPath>
+            <symbol id="shooting-star-star-symbol" viewBox="-25.160249710083008 -26.331560134887695 50.320499420166016 48.120750427246094">
+                <path fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M 8.572527594031472e-16 14C -17.63355756877419 24.270509831248425 -17.63355756877419 24.270509831248425 -13.31479122813215 4.326237921249265C -28.531695488854606 -9.270509831248432 -28.531695488854606 -9.270509831248432 -8.228993532094625 -11.326237921249263C -5.510910596163089e-15 -30 -5.510910596163089e-15 -30 8.22899353209462 -11.326237921249266C 28.531695488854606 -9.27050983124843 28.531695488854606 -9.27050983124843 13.314791228132151 4.326237921249261C 17.633557568774222 24.270509831248404 17.633557568774222 24.270509831248404 8.572527594031472e-16 14" />
+            </symbol>
+            <use id="shooting-star-star" href="#shooting-star-star-symbol" x="-22" y="-22" width="44" height="44" />
+            <path id="shooting-star-tail" d="M 0 -20 L 100 -40 100 10 12 21.2" />
+            <linearGradient id="shooting-star-gradient" x1="0" x2="1" y1="0" y2="0">
+                <stop stop-color="hsl(0 0% 40%)" offset="0" />
+                <stop stop-color="hsl(0 0% 0%)" offset="1" />
+            </linearGradient>
+            <mask id="shooting-star-mask">
+                <use fill="url(#shooting-star-gradient)" href="#shooting-star-tail" />
+            </mask>
+        </defs>
+        <g clip-path="url(#shooting-star-clip)">
+            <rect fill="hsl(277 21% 39%)" x="-40" y="-40" width="80" height="80" />
+            <g fill="hsl(265 33% 90%)">
+                <circle transform="translate(-20 -34)" r="1" />
+                <circle transform="translate(-28 -25)" r="1" />
+                <circle transform="translate(-32 23)" r="1" />
+                <circle transform="translate(25 14)" r="1" />
+                <circle transform="translate(20 25)" r="1" />
+                <circle transform="translate(30 30)" r="1" />
+            </g>
+            <g style="color: hsl(48 96% 66%);" transform="translate(-12.5 8) rotate(-30)">
+                <use mask="url(#shooting-star-mask)" fill="currentColor" href="#shooting-star-tail" />
+                <use href="#shooting-star-star" />
+            </g>
+        </g>
+    </svg>
+</svg>`
+};
