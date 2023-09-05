06e4058a35a1d86885f48d652aa56c55cd222f02
Gabriele Corti <borntofrappe@gmail.com>
2023-09-05 21:45:08 +0200

    showcase graphic
    add visual with 'abstract art', svgs using now uppercase, now lowercase commands for the path element

---
+++ src/blog/absolutely-relative/svg.js
@@ -0,0 +1,24 @@
+export default {
+	'abstract-art': `<svg style="display: block;" viewBox="0 0 120 80">
+    <title>Abstract art</title>
+    <rect fill="hsl(213 15% 12%)" width="120" height="80" />
+    <g transform="translate(10 0)">
+        <svg width="40" height="40" viewBox="-20 -20 40 40">
+            <path fill="hsl(33 75% 65%)" d="M -20 0 C -10 -20 -10 20 -0 0 S 10 20 20 0" />
+            <path fill="hsl(205 30% 48%)" d="M -20 0 c -10 -20 -10 20 -0 0 s 10 20 20 0" />
+        </svg>
+    </g>
+    <g transform="translate(40 40)">
+        <svg width="40" height="40" viewBox="-20 -20 40 40">
+            <path fill="hsl(33 75% 65%)" d="M 0 0 A 10 10 0 0 0 20 0 A 10 10 0 0 0 -20 0" />
+            <path fill="hsl(3 72% 59%)" d="M 0 0 a 10 10 0 0 0 20 0 a 10 10 0 0 0 -20 0" />
+        </svg>
+    </g>
+    <g transform="translate(80 10)">
+        <svg width="40" height="40" viewBox="-20 -20 40 40">
+            <path fill="hsl(33 75% 65%)" d="M 0 -20 Q -20 -10 0 0 T 0 20" />
+            <path fill="hsl(205 30% 48%)" d="m 0 -20 q -20 -10 0 0 t 0 20" />
+        </svg>
+    </g>
+</svg>`
+};
