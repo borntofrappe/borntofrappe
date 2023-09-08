1dbe46895ef137690b05530c1cd48ba693fc7bd8
Gabriele Corti <borntofrappe@gmail.com>
2023-09-08 21:03:42 +0200

    update labels and titles

--- src/blog/filter-region/svg.js
+++ src/blog/filter-region/svg.js
@@ -1,6 +1,6 @@
 export default {
 	'filter-blur': `<svg style="display: block;" viewBox="0 0 2.4 1.3">
-    <title>...</title>
+    <title>A blurred circle</title>
     <svg width="1" height="1" viewBox="-50 -50 100 100">
         <circle fill="hsl(36 99% 65%)" stroke="hsl(228 6% 16%)" stroke-width="3" r="40" />
     </svg>
@@ -18,26 +18,26 @@ export default {
         <text fill="currentColor" font-size="0.1" font-family="monospace" text-anchor="middle" x="0.5" y="-0.02">filter="<tspan font-weight="700">url(#blur)</tspan>"</text>
     </g>
 </svg>`,
-	'filter-blur-region': `<svg style="display: block;" viewBox="0 0 2.4 1.3">
-    <title>...</title>
+	'filter-region': `<svg style="display: block;" viewBox="0 0 2.4 1.3">
+    <title>Parts of a blurred circle</title>
     <svg width="1" height="1" viewBox="-50 -50 100 100">
         <defs>
-            <filter id="filter-blur-region-blur">
+            <filter id="filter-region-blur">
                 <feGaussianBlur stdDeviation="2" />
             </filter>
         </defs>
-        <g filter="url(#filter-blur-region-blur)">
+        <g filter="url(#filter-region-blur)">
             <circle fill="hsl(36 99% 65%)" stroke="hsl(228 6% 16%)" stroke-width="3" r="40" />
         </g>
     </svg>
     <g transform="translate(1.25 0.3)">
         <svg width="1" height="1" viewBox="-50 -50 100 100">
             <defs>
-                <filter id="filter-blur-region-filter" x="0.35">
+                <filter id="filter-region-filter" x="0.35">
                     <feGaussianBlur stdDeviation="2" />
                 </filter>
             </defs>
-            <g filter="url(#filter-blur-region-filter)">
+            <g filter="url(#filter-region-filter)">
                 <circle fill="hsl(36 99% 65%)" stroke="hsl(228 6% 16%)" stroke-width="3" r="40" />
             </g>
         </svg>
