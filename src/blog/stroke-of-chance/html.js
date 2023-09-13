7cc9d552c5449d2cdcee4ab14238fcee2172bc47
Gabriele Corti <borntofrappe@gmail.com>
2023-09-13 14:32:00 +0200

    outline-stroke.svg
    highlight a different way to strok text elements in svg elements

--- src/blog/stroke-of-chance/html.js
+++ src/blog/stroke-of-chance/html.js
@@ -17,7 +17,7 @@ export default {
 	'outline-html': `<div>
     <svg aria-hidden="true" style="position: absolute; width: 0; height: 0;">
         <defs>
-            <filter id="html-filter">
+            <filter id="outline-html-filter">
                     <feMorphology in="SourceGraphic" result="dilation" operator="dilate" radius="4" />
                     <feFlood result="color" flood-color="hsl(0 0% 25%)" />
                     <feComposite in="color" in2="dilation" operator="in" />
@@ -29,8 +29,11 @@ export default {
             </filter>
         </defs>
     </svg>
-    <p style="filter: url(#html-filter); color: hsl(0 0% 99%); font-family: sans-serif; font-size: 3rem; font-weight: 700;">
+    <p style="filter: url(#outline-html-filter); color: hsl(0 0% 99%); font-family: sans-serif; font-size: 3rem; font-weight: 700;">
         On HTML element and even on longer sentences, daring to break in multiple rows.
     </p>
-</div>`
+</div>`,
+	'outline-stroke': `<svg style="display: block;" viewBox="-2.5 -16.962017059326172 83.21540832519531 22.93290138244629">
+    <text fill="hsl(0 0% 99%)" stroke="hsl(0 0% 25%)" stroke-width="2" paint-order="stroke" font-family="sans-serif" font-size="16" font-weight="700">Sforzando</text>
+</svg>`
 };
