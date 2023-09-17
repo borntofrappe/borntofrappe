650c3aafe136a41093f257026f6f7da8f95c8b6f
Gabriele Corti <borntofrappe@gmail.com>
2023-09-17 18:12:32 +0200

    helper visual
    add graphic to highlight how you draw dashes in SVG and Zdog respectively
    
    - stroke-dasharray
    - a series of multiple, separated lines (since in Zdog there's no such thing as stroke-dasharray)

---
+++ src/blog/long-way-round/svg.js
@@ -0,0 +1,28 @@
+export default {
+	'stroke-dashes': `<svg style="display: block;" viewBox="8.999999046325684 0.14330196380615234 82 58.85669708251953">
+    <title>Dashes in SVG and with Zdog</title>
+    <g stroke-linecap="round" stroke-linejoin="round">
+        <text fill="currentColor" stroke="currentColor" stroke-width="0.4" font-family="sans-serif" font-size="12" font-weight="700" x="10" y="12" letter-spacing="1">SVG</text>
+        <path transform="translate(10 22)" fill="none" stroke="currentColor" stroke-width="2" stroke-dasharray="4" d="M 0 0 80 0" />
+        <g transform="translate(0 36)">
+            <text fill="currentColor" stroke="currentColor" stroke-width="0.4" font-family="sans-serif" font-size="12" font-weight="700" text-anchor="end" x="90" y="12" letter-spacing="1">Zdog</text>
+            <g transform="translate(9 21)">
+                <svg width="82" height="2" viewBox="-41 -1 82 2">
+                    <g fill="none" stroke-width="2" stroke="currentColor">
+                        <path d="M-40 0 L -36 0" />
+                        <path d="M-32 0 L -28 0" />
+                        <path d="M-24 0 L -20 0" />
+                        <path d="M-16 0 L -12 0" />
+                        <path d="M-8 0 L -4 0" />
+                        <path d="M0 0 L 4 0" />
+                        <path d="M8 0 L 12 0" />
+                        <path d="M16 0 L 20 0" />
+                        <path d="M24 0 L 28 0" />
+                        <path d="M32 0 L 36 0" />
+                    </g>
+                </svg>
+            </g>
+        </g>
+    </g>
+</svg>`
+};
