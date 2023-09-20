141f7581ae47af6966f95fd6fdda1500cc5507a0
Gabriele Corti <borntofrappe@gmail.com>
2023-09-20 14:27:16 +0200

    highlight feTile issue
    the snippet produces different results in different browsers (chrome, edge, opera vs firefox, safari)

---
+++ src/blog/over-tiling-issues/svg.js
@@ -0,0 +1,20 @@
+export default {
+	'fe-tile': `<svg style="display: block;" viewBox="0 0 5 5">
+    <title>...</title>
+    <defs>
+        <filter id="fe-tile-filter" x="0" y="0" width="1" height="1">
+            <feTile width="5" height="5" />
+        </filter>
+    </defs>
+    <g filter="url(#fe-tile-filter)">
+        <g fill="hsl(0 0% 97%)">
+            <rect x="0.5" width="0.5" height="0.5" />
+            <rect y="0.5" width="0.5" height="0.5" />
+        </g>
+        <g fill="hsl(0 0% 70%)">
+            <rect width="0.5" height="0.5" />
+            <rect x="0.5" y="0.5" width="0.5" height="0.5" />
+        </g>
+    </g>
+</svg>`
+};
