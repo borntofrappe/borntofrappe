3a93b3edab2716de16a7a68de3a0096810fdecb2
Gabriele Corti <borntofrappe@gmail.com>
2023-09-22 17:28:57 +0200

    intro graphic
    create svg.js to export markup resulting in the accompanying visuals
    
    add graphic introducing the article with an instance of feMorphology

---
+++ src/blog/convolute-features/svg.js
@@ -0,0 +1,18 @@
+export default {
+	'fe-morphology': `<svg style="display: block;" viewBox="0 0 6 1.4">
+    <title>Cryptic runes</title>
+    <defs>
+        <filter id="fe-morphology-filter">
+            <feMorphology operator="erode" radius="0.02" />
+        </filter>
+    </defs>
+    <g filter="url(#fe-morphology-filter)" fill="none" stroke="currentColor" stroke-width="0.1" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M 0.2 1.3 0.2 0.1 0.8 0.55 0.8 1.3" />
+        <path transform="translate(1 0)" d="M 0.5 1.3 0.5 0.1 M 0.1 0.2 0.5 0.55 0.9 0.2" />
+        <path transform="translate(2 0)" d="M 0.1 1.3 0.9 0.5 0.5 0.1 0.1 0.5 0.9 1.3" />
+        <path transform="translate(3 0)" d="M 0.2 1.3 0.2 0.1 M 0.2 0.55 0.8 0.15 M 0.2 0.9 0.7 0.57" />
+        <path transform="translate(4 0)" d="M 0.2 1.3 0.8 0.7 0.2 0.1 M 0.8 1.3 0.2 0.7 0.8 0.1" />
+        <path transform="translate(5 0)" d="M 0.2 1.3 0.2 0.1 0.65 0.45 0.2 0.8 M 0.4 0.65 0.8 0.9 0.2 1.3" />
+    </g>
+</svg>`
+};
