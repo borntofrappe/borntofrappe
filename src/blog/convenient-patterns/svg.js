18a107446802c0085621f27d00f8ae99e4dc3c0f
Gabriele Corti <borntofrappe@gmail.com>
2023-10-04 09:48:38 +0200

    final graphic
    additional tile+pattern to illustrate patternTransform further

--- src/blog/convenient-patterns/svg.js
+++ src/blog/convenient-patterns/svg.js
@@ -2,14 +2,14 @@ export default {
 	'diamond-tile': `<svg style="display: block:" viewBox="0 0 1 1">
     <title>Diamond tile</title>
     <rect fill="hsl(0 0% 90%)" width="1" height="1" />
-    <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+    <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
 </svg>`,
 	'diamond-pattern': `<svg style="display: block:" viewBox="0 0 100 100">
     <title>Diamond pattern</title>
     <defs>
         <pattern id="diamond-pattern" width="0.2" height="0.2" viewBox="0 0 1 1">
             <rect fill="hsl(0 0% 90%)" width="1" height="1" />
-            <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+            <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
         </pattern>
     </defs>
     <rect fill="url(#diamond-pattern)" width="100" height="100" />
@@ -17,7 +17,7 @@ export default {
 	'repeated-tile': `<svg style="display: block:" viewBox="0 0 1 1">
     <title>Diamond tile repeated and offset</title>
     <rect fill="hsl(0 0% 90%)" width="1" height="1" />
-    <path id="repeated-tile" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+    <path id="repeated-tile" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
     <use x="-0.5" y="-0.5" href="#repeated-tile" />
     <use x="0.5" y="-0.5" href="#repeated-tile" />
     <use x="0.5" y="0.5" href="#repeated-tile" />
@@ -28,7 +28,7 @@ export default {
     <defs>
         <pattern id="transform-pattern" width="0.2" height="0.2" viewBox="0 0 1 1" patternTransform="rotate(45)">
             <rect fill="hsl(0 0% 90%)" width="1" height="1" />
-            <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+            <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
         </pattern>
     </defs>
     <rect fill="url(#transform-pattern)" width="100" height="100" />
@@ -36,16 +36,35 @@ export default {
 	'transform-tile': `<svg style="display: block:" viewBox="0 0 1 1">
     <title>Rotated diamond tile — a square</title>
     <rect fill="hsl(0 0% 90%)" width="1" height="1" />
-    <path transform="rotate(45, 0.5 0.5)" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+    <path transform="rotate(45, 0.5 0.5)" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
 </svg>`,
 	'checkboard-pattern': `<svg style="display: block:" viewBox="0 0 100 100">
     <title>Checkboard pattern</title>
     <defs>
         <pattern id="checkboard-pattern" width="0.2" height="0.2" viewBox="0 0 1 1" patternTransform="rotate(45)">
         <rect fill="hsl(0 0% 90%)" width="1" height="1" />
-        <path transform="rotate(-45, 0.5 0.5)" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+        <path transform="rotate(-45, 0.5 0.5)" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
         </pattern>
     </defs>
     <rect fill="url(#checkboard-pattern)" width="100" height="100" />
+</svg>`,
+	'gaps-tile': `<svg style="display: block:" viewBox="0 0 1 1">
+    <title>Rotated tile with gaps</title>
+    <g transform="rotate(45, 0.5 0.5)">
+        <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+        <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
+    </g>
+</svg>`,
+	'gaps-pattern': `<svg style="display: block:" viewBox="0 0 100 100">
+    <title>Checkboard pattern with holes</title>
+    <defs>
+        <pattern id="gaps-pattern" width="0.2" height="0.2" viewBox="0 0 1 1" patternTransform="rotate(45)">
+            <g transform="rotate(-45, 0.5 0.5)">
+                <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+                <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5 Z" />
+            </g>
+        </pattern>
+    </defs>
+    <rect fill="url(#gaps-pattern)" width="100" height="100" />
 </svg>`
 };
