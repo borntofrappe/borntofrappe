93de7eb5de373776b945201ff95c4a71a04269e3
Gabriele Corti <borntofrappe@gmail.com>
2023-10-04 09:45:01 +0200

    helper graphics
    svg illustrating the process of designing tile and pattern, plus a alternative way to achieve the checkboard sequence

--- src/blog/convenient-patterns/svg.js
+++ src/blog/convenient-patterns/svg.js
@@ -1,4 +1,43 @@
 export default {
+	'diamond-tile': `<svg style="display: block:" viewBox="0 0 1 1">
+    <title>Diamond tile</title>
+    <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+    <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+</svg>`,
+	'diamond-pattern': `<svg style="display: block:" viewBox="0 0 100 100">
+    <title>Diamond pattern</title>
+    <defs>
+        <pattern id="diamond-pattern" width="0.2" height="0.2" viewBox="0 0 1 1">
+            <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+            <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+        </pattern>
+    </defs>
+    <rect fill="url(#diamond-pattern)" width="100" height="100" />
+</svg>`,
+	'repeated-tile': `<svg style="display: block:" viewBox="0 0 1 1">
+    <title>Diamond tile repeated and offset</title>
+    <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+    <path id="repeated-tile" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+    <use x="-0.5" y="-0.5" href="#repeated-tile" />
+    <use x="0.5" y="-0.5" href="#repeated-tile" />
+    <use x="0.5" y="0.5" href="#repeated-tile" />
+    <use x="-0.5" y="0.5" href="#repeated-tile" />
+</svg>`,
+	'transform-pattern': `<svg style="display: block:" viewBox="0 0 100 100">
+    <title>Rotated pattern</title>
+    <defs>
+        <pattern id="transform-pattern" width="0.2" height="0.2" viewBox="0 0 1 1" patternTransform="rotate(45)">
+            <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+            <path fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+        </pattern>
+    </defs>
+    <rect fill="url(#transform-pattern)" width="100" height="100" />
+</svg>`,
+	'transform-tile': `<svg style="display: block:" viewBox="0 0 1 1">
+    <title>Rotated diamond tile — a square</title>
+    <rect fill="hsl(0 0% 90%)" width="1" height="1" />
+    <path transform="rotate(45, 0.5 0.5)" fill="hsl(0 0% 30%)" d="M 0.5 0.2 0.8 0.5 0.5 0.8 0.2 0.5Z" />
+</svg>`,
 	'checkboard-pattern': `<svg style="display: block:" viewBox="0 0 100 100">
     <title>Checkboard pattern</title>
     <defs>
