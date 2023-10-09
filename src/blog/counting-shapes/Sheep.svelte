2f276d48f1ee95022cec77346a316f1bd99289f5
Gabriele Corti <borntofrappe@gmail.com>
2023-10-09 10:04:13 +0200

    remove whitespace

--- src/blog/counting-shapes/Sheep.svelte
+++ src/blog/counting-shapes/Sheep.svelte
@@ -12,19 +12,19 @@
 					light: 'hsl(210 9% 31%)',
 					dark: 'hsl(214 14% 10%)'
 				},
-				path: 'M 5 2 h 1 M 7 2 h 2 M 10 2 h 2 M 2 3 h 3 M 6 3 h 1 M 9 3 h 1 M 12 3 h 1 M 1 4 h 1 M 12 4 h 2 M 1 5 h 1 M 8 5 h 2 M 14 5 h 1 M 0 6 h 1 M 7 6 h 1 M 10 6 h 1 M 14 6 h 1 M 0 7 h 1 M 6 7 h 3 M 11 7 h 1 M 15 7 h 1 M 0 8 h 1 M 5 8 h 1 M 8 8 h 1 M 11 8 h 1 M 15 8 h 1 M 0 9 h 1 M 5 9 h 1 M 7 9 h 2 M 11 9 h 2 M 14 9 h 1 M 0 10 h 1 M 6 10 h 1 M 10 10 h 1 M 12 10 h 1 M 14 10 h 1 M 0 11 h 1 M 7 11 h 3 M 14 11 h 1 M 1 12 h 1 M 13 12 h 1 M 1 13 h 1 M 6 13 h 2 M 10 13 h 3 M 2 14 h 1 M 4 14 h 2 M 7 14 h 2 M 10 14 h 2 M 2 15 h 3 M 8 15 h 3'
+				path: 'M 5 0 h 1 M 7 0 h 2 M 10 0 h 2 M 2 1 h 3 M 6 1 h 1 M 9 1 h 1 M 12 1 h 1 M 1 2 h 1 M 12 2 h 2 M 1 3 h 1 M 8 3 h 2 M 14 3 h 1 M 0 4 h 1 M 7 4 h 1 M 10 4 h 1 M 14 4 h 1 M 0 5 h 1 M 6 5 h 3 M 11 5 h 1 M 15 5 h 1 M 0 6 h 1 M 5 6 h 1 M 8 6 h 1 M 11 6 h 1 M 15 6 h 1 M 0 7 h 1 M 5 7 h 1 M 7 7 h 2 M 11 7 h 2 M 14 7 h 1 M 0 8 h 1 M 6 8 h 1 M 10 8 h 1 M 12 8 h 1 M 14 8 h 1 M 0 9 h 1 M 7 9 h 3 M 14 9 h 1 M 1 10 h 1 M 13 10 h 1 M 1 11 h 1 M 6 11 h 2 M 10 11 h 3 M 2 12 h 1 M 4 12 h 2 M 7 12 h 2 M 10 12 h 2 M 2 13 h 3 M 8 13 h 3'
 			},
 			{
 				colors: {
 					main: 'hsl(210 16% 93%)'
 				},
-				path: 'M 5 3 h 1 M 7 3 h 2 M 10 3 h 2 M 2 4 h 10 M 2 5 h 3 M 6 5 h 2 M 10 5 h 4 M 1 6 h 1 M 3 6 h 4 M 11 6 h 3 M 1 7 h 5 M 12 7 h 3 M 1 8 h 4 M 14 8 h 1 M 1 9 h 2 M 4 9 h 1 M 13 9 h 1 M 1 10 h 5 M 11 10 h 1 M 13 10 h 1 M 1 11 h 6 M 11 11 h 3 M 2 12 h 8 M 11 12 h 2 M 2 13 h 4 M 8 13 h 2'
+				path: 'M 5 1 h 1 M 7 1 h 2 M 10 1 h 2 M 2 2 h 10 M 2 3 h 3 M 6 3 h 2 M 10 3 h 4 M 1 4 h 1 M 3 4 h 4 M 11 4 h 3 M 1 5 h 5 M 12 5 h 3 M 1 6 h 4 M 14 6 h 1 M 1 7 h 2 M 4 7 h 1 M 13 7 h 1 M 1 8 h 5 M 11 8 h 1 M 13 8 h 1 M 1 9 h 6 M 11 9 h 3 M 2 10 h 8 M 11 10 h 2 M 2 11 h 4 M 8 11 h 2'
 			},
 			{
 				colors: {
 					main: 'hsl(210 7% 56%)'
 				},
-				path: 'M 5 5 h 1 M 2 6 h 1 M 8 6 h 2 M 9 7 h 2 M 6 8 h 2 M 9 8 h 2 M 12 8 h 2 M 3 9 h 1 M 6 9 h 1 M 9 9 h 2 M 7 10 h 3 M 10 11 h 1 M 10 12 h 1 M 3 14 h 1 M 9 14 h 1'
+				path: 'M 5 3 h 1 M 2 4 h 1 M 8 4 h 2 M 9 5 h 2 M 6 6 h 2 M 9 6 h 2 M 12 6 h 2 M 3 7 h 1 M 6 7 h 1 M 9 7 h 2 M 7 8 h 3 M 10 9 h 1 M 10 10 h 1 M 3 12 h 1 M 9 12 h 1'
 			}
 		].map(({ colors, path }) => {
 			const boxes = path
@@ -64,7 +64,7 @@
 			addTo: root,
 			translate: {
 				x: -8,
-				y: -9
+				y: -6.5
 			}
 		});
 
--- src/blog/counting-shapes/svg.js
+++ src/blog/counting-shapes/svg.js
@@ -1,107 +1,107 @@
 export default {
-	paths: `<svg style="display: block;" viewBox="0 -0.5 16 16">
+	paths: `<svg style="display: block;" viewBox="0 -0.5 16 14">
     <title>Pixelated sheep</title>
-    <path stroke="hsl(210 10% 23%)" d="M 5 2 h 1 M 7 2 h 2 M 10 2 h 2 M 2 3 h 3 M 6 3 h 1 M 9 3 h 1 M 12 3 h 1 M 1 4 h 1 M 12 4 h 2 M 1 5 h 1 M 8 5 h 2 M 14 5 h 1 M 0 6 h 1 M 7 6 h 1 M 10 6 h 1 M 14 6 h 1 M 0 7 h 1 M 6 7 h 3 M 11 7 h 1 M 15 7 h 1 M 0 8 h 1 M 5 8 h 1 M 8 8 h 1 M 11 8 h 1 M 15 8 h 1 M 0 9 h 1 M 5 9 h 1 M 7 9 h 2 M 11 9 h 2 M 14 9 h 1 M 0 10 h 1 M 6 10 h 1 M 10 10 h 1 M 12 10 h 1 M 14 10 h 1 M 0 11 h 1 M 7 11 h 3 M 14 11 h 1 M 1 12 h 1 M 13 12 h 1 M 1 13 h 1 M 6 13 h 2 M 10 13 h 3 M 2 14 h 1 M 4 14 h 2 M 7 14 h 2 M 10 14 h 2 M 2 15 h 3 M 8 15 h 3" />
-    <path stroke="hsl(210 16% 93%)" d="M 5 3 h 1 M 7 3 h 2 M 10 3 h 2 M 2 4 h 10 M 2 5 h 3 M 6 5 h 2 M 10 5 h 4 M 1 6 h 1 M 3 6 h 4 M 11 6 h 3 M 1 7 h 5 M 12 7 h 3 M 1 8 h 4 M 14 8 h 1 M 1 9 h 2 M 4 9 h 1 M 13 9 h 1 M 1 10 h 5 M 11 10 h 1 M 13 10 h 1 M 1 11 h 6 M 11 11 h 3 M 2 12 h 8 M 11 12 h 2 M 2 13 h 4 M 8 13 h 2" />
-    <path stroke="hsl(210 7% 56%)" d="M 5 5 h 1 M 2 6 h 1 M 8 6 h 2 M 9 7 h 2 M 6 8 h 2 M 9 8 h 2 M 12 8 h 2 M 3 9 h 1 M 6 9 h 1 M 9 9 h 2 M 7 10 h 3 M 10 11 h 1 M 10 12 h 1 M 3 14 h 1 M 9 14 h 1" />
+    <path stroke="hsl(210 10% 23%)" d="M 5 0 h 1 M 7 0 h 2 M 10 0 h 2 M 2 1 h 3 M 6 1 h 1 M 9 1 h 1 M 12 1 h 1 M 1 2 h 1 M 12 2 h 2 M 1 3 h 1 M 8 3 h 2 M 14 3 h 1 M 0 4 h 1 M 7 4 h 1 M 10 4 h 1 M 14 4 h 1 M 0 5 h 1 M 6 5 h 3 M 11 5 h 1 M 15 5 h 1 M 0 6 h 1 M 5 6 h 1 M 8 6 h 1 M 11 6 h 1 M 15 6 h 1 M 0 7 h 1 M 5 7 h 1 M 7 7 h 2 M 11 7 h 2 M 14 7 h 1 M 0 8 h 1 M 6 8 h 1 M 10 8 h 1 M 12 8 h 1 M 14 8 h 1 M 0 9 h 1 M 7 9 h 3 M 14 9 h 1 M 1 10 h 1 M 13 10 h 1 M 1 11 h 1 M 6 11 h 2 M 10 11 h 3 M 2 12 h 1 M 4 12 h 2 M 7 12 h 2 M 10 12 h 2 M 2 13 h 3 M 8 13 h 3" />
+    <path stroke="hsl(210 16% 93%)" d="M 5 1 h 1 M 7 1 h 2 M 10 1 h 2 M 2 2 h 10 M 2 3 h 3 M 6 3 h 2 M 10 3 h 4 M 1 4 h 1 M 3 4 h 4 M 11 4 h 3 M 1 5 h 5 M 12 5 h 3 M 1 6 h 4 M 14 6 h 1 M 1 7 h 2 M 4 7 h 1 M 13 7 h 1 M 1 8 h 5 M 11 8 h 1 M 13 8 h 1 M 1 9 h 6 M 11 9 h 3 M 2 10 h 8 M 11 10 h 2 M 2 11 h 4 M 8 11 h 2" />
+    <path stroke="hsl(210 7% 56%)" d="M 5 3 h 1 M 2 4 h 1 M 8 4 h 2 M 9 5 h 2 M 6 6 h 2 M 9 6 h 2 M 12 6 h 2 M 3 7 h 1 M 6 7 h 1 M 9 7 h 2 M 7 8 h 3 M 10 9 h 1 M 10 10 h 1 M 3 12 h 1 M 9 12 h 1" />
 </svg>`,
-	rects: `<svg style="display: block;" viewBox="0 0 16 16">
+	rects: `<svg style="display: block;" viewBox="0 0 16 14" viewBox="0 0 16 14">
     <title>Rectangular sheep</title>
     <g fill="hsl(210 10% 23%)">
-        <rect x="5" y="2" width="1" height="1" />
-        <rect x="7" y="2" width="2" height="1" />
-        <rect x="10" y="2" width="2" height="1" />
-        <rect x="2" y="3" width="3" height="1" />
-        <rect x="6" y="3" width="1" height="1" />
-        <rect x="9" y="3" width="1" height="1" />
-        <rect x="12" y="3" width="1" height="1" />
-        <rect x="1" y="4" width="1" height="1" />
-        <rect x="12" y="4" width="2" height="1" />
-        <rect x="1" y="5" width="1" height="1" />
-        <rect x="8" y="5" width="2" height="1" />
-        <rect x="14" y="5" width="1" height="1" />
+        <rect x="5" y="0" width="1" height="1" />
+        <rect x="7" y="0" width="2" height="1" />
+        <rect x="10" y="0" width="2" height="1" />
+        <rect x="2" y="1" width="3" height="1" />
+        <rect x="6" y="1" width="1" height="1" />
+        <rect x="9" y="1" width="1" height="1" />
+        <rect x="12" y="1" width="1" height="1" />
+        <rect x="1" y="2" width="1" height="1" />
+        <rect x="12" y="2" width="2" height="1" />
+        <rect x="1" y="3" width="1" height="1" />
+        <rect x="8" y="3" width="2" height="1" />
+        <rect x="14" y="3" width="1" height="1" />
+        <rect x="0" y="4" width="1" height="1" />
+        <rect x="7" y="4" width="1" height="1" />
+        <rect x="10" y="4" width="1" height="1" />
+        <rect x="14" y="4" width="1" height="1" />
+        <rect x="0" y="5" width="1" height="1" />
+        <rect x="6" y="5" width="3" height="1" />
+        <rect x="11" y="5" width="1" height="1" />
+        <rect x="15" y="5" width="1" height="1" />
         <rect x="0" y="6" width="1" height="1" />
-        <rect x="7" y="6" width="1" height="1" />
-        <rect x="10" y="6" width="1" height="1" />
-        <rect x="14" y="6" width="1" height="1" />
+        <rect x="5" y="6" width="1" height="1" />
+        <rect x="8" y="6" width="1" height="1" />
+        <rect x="11" y="6" width="1" height="1" />
+        <rect x="15" y="6" width="1" height="1" />
         <rect x="0" y="7" width="1" height="1" />
-        <rect x="6" y="7" width="3" height="1" />
-        <rect x="11" y="7" width="1" height="1" />
-        <rect x="15" y="7" width="1" height="1" />
+        <rect x="5" y="7" width="1" height="1" />
+        <rect x="7" y="7" width="2" height="1" />
+        <rect x="11" y="7" width="2" height="1" />
+        <rect x="14" y="7" width="1" height="1" />
         <rect x="0" y="8" width="1" height="1" />
-        <rect x="5" y="8" width="1" height="1" />
-        <rect x="8" y="8" width="1" height="1" />
-        <rect x="11" y="8" width="1" height="1" />
-        <rect x="15" y="8" width="1" height="1" />
+        <rect x="6" y="8" width="1" height="1" />
+        <rect x="10" y="8" width="1" height="1" />
+        <rect x="12" y="8" width="1" height="1" />
+        <rect x="14" y="8" width="1" height="1" />
         <rect x="0" y="9" width="1" height="1" />
-        <rect x="5" y="9" width="1" height="1" />
-        <rect x="7" y="9" width="2" height="1" />
-        <rect x="11" y="9" width="2" height="1" />
+        <rect x="7" y="9" width="3" height="1" />
         <rect x="14" y="9" width="1" height="1" />
-        <rect x="0" y="10" width="1" height="1" />
-        <rect x="6" y="10" width="1" height="1" />
-        <rect x="10" y="10" width="1" height="1" />
-        <rect x="12" y="10" width="1" height="1" />
-        <rect x="14" y="10" width="1" height="1" />
-        <rect x="0" y="11" width="1" height="1" />
-        <rect x="7" y="11" width="3" height="1" />
-        <rect x="14" y="11" width="1" height="1" />
-        <rect x="1" y="12" width="1" height="1" />
-        <rect x="13" y="12" width="1" height="1" />
-        <rect x="1" y="13" width="1" height="1" />
-        <rect x="6" y="13" width="2" height="1" />
-        <rect x="10" y="13" width="3" height="1" />
-        <rect x="2" y="14" width="1" height="1" />
-        <rect x="4" y="14" width="2" height="1" />
-        <rect x="7" y="14" width="2" height="1" />
-        <rect x="10" y="14" width="2" height="1" />
-        <rect x="2" y="15" width="3" height="1" />
-        <rect x="8" y="15" width="3" height="1" />
+        <rect x="1" y="10" width="1" height="1" />
+        <rect x="13" y="10" width="1" height="1" />
+        <rect x="1" y="11" width="1" height="1" />
+        <rect x="6" y="11" width="2" height="1" />
+        <rect x="10" y="11" width="3" height="1" />
+        <rect x="2" y="12" width="1" height="1" />
+        <rect x="4" y="12" width="2" height="1" />
+        <rect x="7" y="12" width="2" height="1" />
+        <rect x="10" y="12" width="2" height="1" />
+        <rect x="2" y="13" width="3" height="1" />
+        <rect x="8" y="13" width="3" height="1" />
     </g>
     <g fill="hsl(210 16% 93%)">
-        <rect x="5" y="3" width="1" height="1" />
-        <rect x="7" y="3" width="2" height="1" />
-        <rect x="10" y="3" width="2" height="1" />
-        <rect x="2" y="4" width="10" height="1" />
-        <rect x="2" y="5" width="3" height="1" />
-        <rect x="6" y="5" width="2" height="1" />
-        <rect x="10" y="5" width="4" height="1" />
-        <rect x="1" y="6" width="1" height="1" />
-        <rect x="3" y="6" width="4" height="1" />
-        <rect x="11" y="6" width="3" height="1" />
-        <rect x="1" y="7" width="5" height="1" />
-        <rect x="12" y="7" width="3" height="1" />
-        <rect x="1" y="8" width="4" height="1" />
-        <rect x="14" y="8" width="1" height="1" />
-        <rect x="1" y="9" width="2" height="1" />
-        <rect x="4" y="9" width="1" height="1" />
-        <rect x="13" y="9" width="1" height="1" />
-        <rect x="1" y="10" width="5" height="1" />
-        <rect x="11" y="10" width="1" height="1" />
-        <rect x="13" y="10" width="1" height="1" />
-        <rect x="1" y="11" width="6" height="1" />
-        <rect x="11" y="11" width="3" height="1" />
-        <rect x="2" y="12" width="8" height="1" />
-        <rect x="11" y="12" width="2" height="1" />
-        <rect x="2" y="13" width="4" height="1" />
-        <rect x="8" y="13" width="2" height="1" />
+        <rect x="5" y="1" width="1" height="1" />
+        <rect x="7" y="1" width="2" height="1" />
+        <rect x="10" y="1" width="2" height="1" />
+        <rect x="2" y="2" width="10" height="1" />
+        <rect x="2" y="3" width="3" height="1" />
+        <rect x="6" y="3" width="2" height="1" />
+        <rect x="10" y="3" width="4" height="1" />
+        <rect x="1" y="4" width="1" height="1" />
+        <rect x="3" y="4" width="4" height="1" />
+        <rect x="11" y="4" width="3" height="1" />
+        <rect x="1" y="5" width="5" height="1" />
+        <rect x="12" y="5" width="3" height="1" />
+        <rect x="1" y="6" width="4" height="1" />
+        <rect x="14" y="6" width="1" height="1" />
+        <rect x="1" y="7" width="2" height="1" />
+        <rect x="4" y="7" width="1" height="1" />
+        <rect x="13" y="7" width="1" height="1" />
+        <rect x="1" y="8" width="5" height="1" />
+        <rect x="11" y="8" width="1" height="1" />
+        <rect x="13" y="8" width="1" height="1" />
+        <rect x="1" y="9" width="6" height="1" />
+        <rect x="11" y="9" width="3" height="1" />
+        <rect x="2" y="10" width="8" height="1" />
+        <rect x="11" y="10" width="2" height="1" />
+        <rect x="2" y="11" width="4" height="1" />
+        <rect x="8" y="11" width="2" height="1" />
     </g>
     <g fill="hsl(210 7% 56%)">
-        <rect x="5" y="5" width="1" height="1" />
-        <rect x="2" y="6" width="1" height="1" />
-        <rect x="8" y="6" width="2" height="1" />
+        <rect x="5" y="3" width="1" height="1" />
+        <rect x="2" y="4" width="1" height="1" />
+        <rect x="8" y="4" width="2" height="1" />
+        <rect x="9" y="5" width="2" height="1" />
+        <rect x="6" y="6" width="2" height="1" />
+        <rect x="9" y="6" width="2" height="1" />
+        <rect x="12" y="6" width="2" height="1" />
+        <rect x="3" y="7" width="1" height="1" />
+        <rect x="6" y="7" width="1" height="1" />
         <rect x="9" y="7" width="2" height="1" />
-        <rect x="6" y="8" width="2" height="1" />
-        <rect x="9" y="8" width="2" height="1" />
-        <rect x="12" y="8" width="2" height="1" />
-        <rect x="3" y="9" width="1" height="1" />
-        <rect x="6" y="9" width="1" height="1" />
-        <rect x="9" y="9" width="2" height="1" />
-        <rect x="7" y="10" width="3" height="1" />
-        <rect x="10" y="11" width="1" height="1" />
-        <rect x="10" y="12" width="1" height="1" />
-        <rect x="3" y="14" width="1" height="1" />
-        <rect x="9" y="14" width="1" height="1" />
+        <rect x="7" y="8" width="3" height="1" />
+        <rect x="10" y="9" width="1" height="1" />
+        <rect x="10" y="10" width="1" height="1" />
+        <rect x="3" y="12" width="1" height="1" />
+        <rect x="9" y="12" width="1" height="1" />
     </g>
 </svg>`
 };
