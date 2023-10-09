ad80eb614f75c323f80f40d37696d35125020229
Gabriele Corti <borntofrappe@gmail.com>
2023-10-09 09:34:45 +0200

    match darkest color
    repeat the same hsl color found in Sheep.svelte

--- src/blog/counting-shapes/svg.js
+++ src/blog/counting-shapes/svg.js
@@ -1,13 +1,13 @@
 export default {
 	paths: `<svg style="display: block;" viewBox="0 -0.5 16 16">
     <title>Pixelated sheep</title>
-    <path stroke="hsl(210 9% 31%)" d="M 5 2 h 1 M 7 2 h 2 M 10 2 h 2 M 2 3 h 3 M 6 3 h 1 M 9 3 h 1 M 12 3 h 1 M 1 4 h 1 M 12 4 h 2 M 1 5 h 1 M 8 5 h 2 M 14 5 h 1 M 0 6 h 1 M 7 6 h 1 M 10 6 h 1 M 14 6 h 1 M 0 7 h 1 M 6 7 h 3 M 11 7 h 1 M 15 7 h 1 M 0 8 h 1 M 5 8 h 1 M 8 8 h 1 M 11 8 h 1 M 15 8 h 1 M 0 9 h 1 M 5 9 h 1 M 7 9 h 2 M 11 9 h 2 M 14 9 h 1 M 0 10 h 1 M 6 10 h 1 M 10 10 h 1 M 12 10 h 1 M 14 10 h 1 M 0 11 h 1 M 7 11 h 3 M 14 11 h 1 M 1 12 h 1 M 13 12 h 1 M 1 13 h 1 M 6 13 h 2 M 10 13 h 3 M 2 14 h 1 M 4 14 h 2 M 7 14 h 2 M 10 14 h 2 M 2 15 h 3 M 8 15 h 3" />
+    <path stroke="hsl(210 10% 23%)" d="M 5 2 h 1 M 7 2 h 2 M 10 2 h 2 M 2 3 h 3 M 6 3 h 1 M 9 3 h 1 M 12 3 h 1 M 1 4 h 1 M 12 4 h 2 M 1 5 h 1 M 8 5 h 2 M 14 5 h 1 M 0 6 h 1 M 7 6 h 1 M 10 6 h 1 M 14 6 h 1 M 0 7 h 1 M 6 7 h 3 M 11 7 h 1 M 15 7 h 1 M 0 8 h 1 M 5 8 h 1 M 8 8 h 1 M 11 8 h 1 M 15 8 h 1 M 0 9 h 1 M 5 9 h 1 M 7 9 h 2 M 11 9 h 2 M 14 9 h 1 M 0 10 h 1 M 6 10 h 1 M 10 10 h 1 M 12 10 h 1 M 14 10 h 1 M 0 11 h 1 M 7 11 h 3 M 14 11 h 1 M 1 12 h 1 M 13 12 h 1 M 1 13 h 1 M 6 13 h 2 M 10 13 h 3 M 2 14 h 1 M 4 14 h 2 M 7 14 h 2 M 10 14 h 2 M 2 15 h 3 M 8 15 h 3" />
     <path stroke="hsl(210 16% 93%)" d="M 5 3 h 1 M 7 3 h 2 M 10 3 h 2 M 2 4 h 10 M 2 5 h 3 M 6 5 h 2 M 10 5 h 4 M 1 6 h 1 M 3 6 h 4 M 11 6 h 3 M 1 7 h 5 M 12 7 h 3 M 1 8 h 4 M 14 8 h 1 M 1 9 h 2 M 4 9 h 1 M 13 9 h 1 M 1 10 h 5 M 11 10 h 1 M 13 10 h 1 M 1 11 h 6 M 11 11 h 3 M 2 12 h 8 M 11 12 h 2 M 2 13 h 4 M 8 13 h 2" />
     <path stroke="hsl(210 7% 56%)" d="M 5 5 h 1 M 2 6 h 1 M 8 6 h 2 M 9 7 h 2 M 6 8 h 2 M 9 8 h 2 M 12 8 h 2 M 3 9 h 1 M 6 9 h 1 M 9 9 h 2 M 7 10 h 3 M 10 11 h 1 M 10 12 h 1 M 3 14 h 1 M 9 14 h 1" />
 </svg>`,
 	rects: `<svg style="display: block;" viewBox="0 0 16 16">
     <title>Rectangular sheep</title>
-    <g fill="hsl(210 9% 31%)">
+    <g fill="hsl(210 10% 23%)">
         <rect x="5" y="2" width="1" height="1" />
         <rect x="7" y="2" width="2" height="1" />
         <rect x="10" y="2" width="2" height="1" />
