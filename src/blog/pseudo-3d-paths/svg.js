0db33b34ffd328aac7bde041fc8553c4bc831ea9
Gabriele Corti <borntofrappe@gmail.com>
2023-10-18 11:33:53 +0200

    svg triangle

---
+++ src/blog/pseudo-3d-paths/svg.js
@@ -0,0 +1,5 @@
+export default {
+	path: `<svg style="display: block;" viewBox="-5 -5 10 10">
+    <path d="M -4 3 0 -3 4 3 Z" fill="hsl(0 0% 70%)" stroke="hsl(0 0% 70%)" stroke-linecap="round" stroke-linejoin="round" />
+</svg>`
+};
