93d71720e0ce5bd8ffabb3fc1745433e1b5b10e8
Gabriele Corti <borntofrappe@gmail.com>
2023-10-30 10:26:36 +0100

    list "Zdog monday" projects
    create a component to list the projects in the "Zdog monday" series and link to every instance beside the current one
    
    ideally, the component would look at the blog folder and extract the values at build time, but currently, the projects are included in a hard-coded array
    
    currently, the component is also unstyled, focusing on the necessary markup

---
+++ src/lib/components/blog/ZdogMonday.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let current = '';
+	const mondays = [
+		{
+			title: 'Beauty sleep',
+			slug: 'beauty-sleep'
+		},
+		{
+			title: 'Counting shapes',
+			slug: 'counting-shapes'
+		},
+		{
+			title: 'Step through frames',
+			slug: 'step-through-frames'
+		},
+		{
+			title: 'Spinning change',
+			slug: 'spinning-change'
+		},
+		{
+			title: 'One out of six',
+			slug: 'one-out-of-six'
+		}
+	];
+</script>
+
+<h2>What's this?</h2>
+
+<p>
+	Welcome to <em>"Zdog monday"</em>, a series devoted to spiffy pseudo-3D graphics created with
+	<a href="https://zzz.dog/">the Zdog library</a> in the early stages of the week.
+</p>
+
+<p>
+	There is <a href="https://codepen.io/collection/PYyrNq">a Codepen collection</a>
+	housing every project, but if you need a bit of context, and a few delightful twists, I care to introduce
+	every visual with a few words:
+</p>
+
+<ol>
+	{#each mondays as { title, slug }}
+		<li>
+			{#if slug === current}
+				{title}
+			{:else}
+				<a href="/{slug}">{title}</a>
+			{/if}
+		</li>
+	{/each}
+</ol>
