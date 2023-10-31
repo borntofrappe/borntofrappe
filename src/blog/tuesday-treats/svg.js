e62441cf41b186ef004f13cfbbab4b2c3b6e514a
Gabriele Corti <borntofrappe@gmail.com>
2023-10-31 14:11:17 +0100

    match Treats.svelte colors

--- src/blog/tuesday-treats/svg.js
+++ src/blog/tuesday-treats/svg.js
@@ -3,9 +3,9 @@ export default {
     <defs>
         <path id="cookie-quarter" d="M 0 0 0 -5 4 -5 A 1 1 0 0 1 5 -4 L 5 0 Z" />
     </defs>
-    <use fill="hsl(8 39% 27%)" href="#cookie-quarter" />
+    <use fill="hsl(8 39% 30%)" href="#cookie-quarter" />
     <use transform="rotate(90)" fill="hsl(30 84% 74%)" href="#cookie-quarter" />
-    <use transform="rotate(180)" fill="hsl(8 39% 27%)" href="#cookie-quarter" />
+    <use transform="rotate(180)" fill="hsl(8 39% 30%)" href="#cookie-quarter" />
     <use transform="rotate(270)" fill="hsl(30 84% 74%)" href="#cookie-quarter" />
 </svg>`
 };
