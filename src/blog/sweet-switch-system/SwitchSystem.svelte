ecb1f4ae22f5dd735c2c3c9eee427b5886b85b2e
Gabriele Corti <borntofrappe@gmail.com>
2023-11-22 11:48:24 +0100

    update svg syntax
    fill="currentColor"
    
    sans-serif to ensure the text is visible in the chosen viewBox

--- src/blog/sweet-switch-system/SwitchSystem.svelte
+++ src/blog/sweet-switch-system/SwitchSystem.svelte
@@ -1,23 +1,22 @@
 <script>
-    import data from "./data.js";
-  </script>
-  
-  <svg viewBox="-2.5 -24.087947845458984 202.7579345703125 31.522336959838867">
-    <switch>
-      {#each data as { systemLanguage, flag, dessert }}
-        <g {systemLanguage}>
-          <text font-size="20">
-            <tspan font-size="10">{flag}</tspan>
-            {dessert}
-          </text>
-        </g>
-      {/each}
-      <g>
-        <text font-size="20">
-          <tspan font-size="10">🇪🇺</tspan>
-          Sweet Europe
-        </text>
-      </g>
-    </switch>
-  </svg>
-  
+	import data from './data.js';
+</script>
+
+<svg viewBox="-2.5 -19.04204750061035 146.1881103515625 24.337772369384766">
+	<switch>
+		{#each data as { systemLanguage, flag, dessert }}
+			<g {systemLanguage}>
+				<text font-family="sans-serif" font-size="20" fill="currentColor">
+					<tspan font-size="10">{flag}</tspan>
+					{dessert}
+				</text>
+			</g>
+		{/each}
+		<g>
+			<text font-family="sans-serif" font-size="20" fill="currentColor">
+				<tspan font-size="10">🇪🇺</tspan>
+				Sweet Europe
+			</text>
+		</g>
+	</switch>
+</svg>
