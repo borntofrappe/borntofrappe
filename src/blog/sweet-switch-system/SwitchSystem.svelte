1ef7a9090ccd29924ae412c2954db00cbfdd15b0
Gabriele Corti <borntofrappe@gmail.com>
2023-11-22 11:34:57 +0100

    switch element demo

---
+++ src/blog/sweet-switch-system/SwitchSystem.svelte
@@ -0,0 +1,23 @@
+<script>
+    import data from "./data.js";
+  </script>
+  
+  <svg viewBox="-2.5 -24.087947845458984 202.7579345703125 31.522336959838867">
+    <switch>
+      {#each data as { systemLanguage, flag, dessert }}
+        <g {systemLanguage}>
+          <text font-size="20">
+            <tspan font-size="10">{flag}</tspan>
+            {dessert}
+          </text>
+        </g>
+      {/each}
+      <g>
+        <text font-size="20">
+          <tspan font-size="10">🇪🇺</tspan>
+          Sweet Europe
+        </text>
+      </g>
+    </switch>
+  </svg>
+  
