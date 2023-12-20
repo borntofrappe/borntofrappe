6cbdea42d680b7beefc9912e7d90d9a3f666d00f
Gabriele Corti <borntofrappe@gmail.com>
2023-12-20 21:09:14 +0100

    add wrapper container
    rely on the wrapping div to set the gap between the label and svg

--- src/blog/system-shift/Ruler.svelte
+++ src/blog/system-shift/Ruler.svelte
@@ -12,52 +12,58 @@
 		.map((_, i) => i / 10 + min);
 </script>
 
-<label>
-	Start from <output for="origin">{value}</output>
-	<input id="origin" type="range" list="markers" bind:value {min} {max} />
-</label>
+<div>
+	<label>
+		Start from <output for="origin">{value}</output>
+		<input id="origin" type="range" list="markers" bind:value {min} {max} />
+	</label>
 
-<svg
-	style="display: block; outline: 1px solid transparent;"
-	viewBox="{value - 0.3} -0.365 {width + 0.6} 2"
->
-	<path
-		transform="translate({value} 0)"
-		fill="none"
-		stroke="currentColor"
-		stroke-width="0.03"
-		d="M 0 -0.175 0 -0.35 {width} -0.35 {width} -0.175"
-	/>
-	<g fill="none" stroke="currentColor" stroke-width="0.008">
-		{#each ticks as i}
-			<path d="M {i} 0 V 0.3" />
-		{/each}
-	</g>
-
-	<g fill="none" stroke="currentColor" stroke-width="0.02">
-		{#each coordinates as i}
-			<path d="M {i} 0 V 0.5" />
-		{/each}
+	<svg
+		style="display: block; outline: 1px solid transparent;"
+		viewBox="{value - 0.3} -0.365 {width + 0.6} 2"
+	>
 		<path
-			d="M {min - 0.29} 0 {max + width + 0.29} 0 {max + width + 0.29} 1.55 {min - 0.29} 1.55 Z"
+			transform="translate({value} 0)"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="0.03"
+			d="M 0 -0.175 0 -0.35 {width} -0.35 {width} -0.175"
 		/>
-	</g>
+		<g fill="none" stroke="currentColor" stroke-width="0.008">
+			{#each ticks as i}
+				<path d="M {i} 0 V 0.3" />
+			{/each}
+		</g>
 
-	<g fill="currentColor" font-family="monospace" font-size="0.3" text-anchor="middle">
-		{#each coordinates as i}
-			<text x={i} y="1.4">{i}</text>
+		<g fill="none" stroke="currentColor" stroke-width="0.02">
+			{#each coordinates as i}
+				<path d="M {i} 0 V 0.5" />
+			{/each}
+			<path
+				d="M {min - 0.29} 0 {max + width + 0.29} 0 {max + width + 0.29} 1.55 {min - 0.29} 1.55 Z"
+			/>
+		</g>
+
+		<g fill="currentColor" font-family="monospace" font-size="0.3" text-anchor="middle">
+			{#each coordinates as i}
+				<text x={i} y="1.4">{i}</text>
+			{/each}
+		</g>
+		<circle fill="currentColor" r="0.07" />
+	</svg>
+
+	<datalist id="markers">
+		{#each { length: Math.abs(min) + max + 1 } as _, i}
+			<option value={i + min} />
 		{/each}
-	</g>
-	<circle fill="currentColor" r="0.07" />
-</svg>
-
-<datalist id="markers">
-	{#each { length: Math.abs(min) + max + 1 } as _, i}
-		<option value={i + min} />
-	{/each}
-</datalist>
+	</datalist>
+</div>
 
 <style>
+	div > * + * {
+		margin-block-start: 0.5rem;
+	}
+
 	label {
 		display: block;
 	}
