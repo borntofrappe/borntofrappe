1a5167cefd905e6e42be6b70d2d892ad61b9cbbf
Gabriele Corti <borntofrappe@gmail.com>
2023-12-22 15:27:23 +0100

    helper graphic
    svg drawing an icon for HTML and one for XML. In between, two shapes to symbolize SVG

---
+++ src/blog/sensitive-syntax/svg.js
@@ -0,0 +1,27 @@
+export default {
+	'svg-icons': `<svg style="display: block; --c0: hsl(184 65% 59%); --c1: hsl(185 62% 45%); --c2: hsl(185 81% 29%);" viewBox="0 0 3.4 1">
+    <svg width="1" height="1" style="display: block;" viewBox="-15 -15 30 30">
+        <path fill="var(--c0, hsl(0 0% 60%))" d="M -10 -15 7.5 -15 L 15 -7.5 L 15 10 A 5 5 0 0 1 10 15 L -10 15 A 5 5 0 0 1 -15 10 L -15 -10 A 5 5 0 0 1 -10 -15" />
+        <path fill="var(--c1, hsl(0 0% 40%))" d="M 7.5 -15 A 1 1 0 0 0 6.5 -14 L 6.5 -7.5 A 1 1 0 0 0 7.5 -6.5 L 14 -6.5 A 1 1 0 0 0 15 -7.5" />
+        <g fill="none" stroke="var(--c2, hsl(0 0% 30%))" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M 1 -4 -1 4 Z" />
+            <path d="M -3.5 -3 -6.5 0 -3.5 3 M 3.5 -3 6.5 0 3.5 3" />
+        </g>
+    </svg>
+    <svg x="1.2" width="1" height="1" style="display: block;" viewBox="-15 -15 30 30">
+        <g fill="var(--c2, hsl(0 0% 30%))" stroke="var(--c2, hsl(0 0% 30%))" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M 4 -5 A 9 9 0 0 0 -14 -5 A 9 9 0 0 0 -5 4 L -5 -5 Z" />
+            <rect x="-1" y="-1" width="15" height="15" />
+        </g>
+    </svg>
+    <svg x="2.4" width="1" height="1" style="display: block;" viewBox="-15 -15 30 30">
+        <path fill="var(--c0, hsl(0 0% 60%))" d="M -10 -15 7.5 -15 L 15 -7.5 L 15 10 A 5 5 0 0 1 10 15 L -10 15 A 5 5 0 0 1 -15 10 L -15 -10 A 5 5 0 0 1 -10 -15" />
+        <path fill="var(--c1, hsl(0 0% 40%))" d="M 7.5 -15 A 1 1 0 0 0 6.5 -14 L 6.5 -7.5 A 1 1 0 0 0 7.5 -6.5 L 14 -6.5 A 1 1 0 0 0 15 -7.5" />
+        <circle fill="var(--c2, hsl(0 0% 30%))" cx="-3.5" cy="3.5" r="1" />
+        <g fill="none" stroke="var(--c2, hsl(0 0% 30%))" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M -3.5 0 A 3.5 3.5 0 0 1 0 3.5" />
+            <path d="M -3.5 -3.5 A 7 7 0 0 1 3.5 3.5" />
+        </g>
+    </svg>
+</svg>`
+};
