c531daa33256ab39d90911def7192ce303813e7a
Gabriele Corti <borntofrappe@gmail.com>
2024-01-18 17:47:44 +0100

    helper graphic
    vector spelling out the elements used to draw the plant

--- src/blog/growing-up-with-svelte/svg.js
+++ src/blog/growing-up-with-svelte/svg.js
@@ -5,5 +5,22 @@ export default {
         <rect x="-10" width="20" height="6" />
         <path transform="translate(0 6)" d="M -9 0 A 9 9 0 0 0 9 0 Z" />
     </g>
+</svg>`,
+	elements: `<svg style="display: block;" viewBox="-40 -30 60 50">
+    <g fill="none" stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
+        <path d="M 0 0 C 0 -7 2 -9 9 -9 C 9 -3 8 0 0 0 Z" />
+        <rect x="-10" width="20" height="6" />
+        <path transform="translate(0 6)" d="M -9 0 A 9 9 0 0 0 9 0 Z" />
+    </g>
+    <g fill="none" stroke="currentColor" stroke-width="0.3" stroke-linejoin="round" stroke-linecap="round" stroke-dasharray="0.5 2.5">
+        <path d="M 0 0 -15 -20" />
+        <path d="M -10 0 -27.5 -7.5" />
+        <path d="M 0 15 -25 10" />
+    </g>
+    <g fill="currentColor" font-size="3" font-family="monospace">
+        <text x="-15" y="-22" text-anchor="middle">&lt;path /&gt;</text>
+        <text x="-27.5" y="-9.5" text-anchor="middle">&lt;rect /&gt;</text>
+        <text x="-25" y="8" text-anchor="middle">&lt;path /&gt;</text>
+    </g>
 </svg>`
 };
