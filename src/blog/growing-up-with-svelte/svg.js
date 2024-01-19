c6edec9169c19a18f918f6b4ebd3d824193be284
Gabriele Corti <borntofrappe@gmail.com>
2024-01-19 11:02:46 +0100

    replace static svg
    assuming the clipboard api is available allow to copy the markup inside the svg element
    
    include the HTML in a `svg` element with a larger `viewBox`, to make space for additional elements

--- src/blog/growing-up-with-svelte/svg.js
+++ src/blog/growing-up-with-svelte/svg.js
@@ -1,11 +1,4 @@
 export default {
-	plant: `<svg style="display: block;" viewBox="-20 -20 40 40">
-    <g fill="none" stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
-        <path d="M 0 0 C 0 -7 2 -9 9 -9 C 9 -3 8 0 0 0 Z" />
-        <rect x="-10" width="20" height="6" />
-        <path transform="translate(0 6)" d="M -9 0 A 9 9 0 0 0 9 0 Z" />
-    </g>
-</svg>`,
 	elements: `<svg style="display: block;" viewBox="-40 -30 60 50">
     <g fill="none" stroke="currentColor" stroke-width="2" stroke-linejoin="round" stroke-linecap="round">
         <path d="M 0 0 C 0 -7 2 -9 9 -9 C 9 -3 8 0 0 0 Z" />
