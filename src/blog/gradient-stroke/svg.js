e15a755fcb5cd72a6a86a9141ff4cf2fc9e231a9
Gabriele Corti <borntofrappe@gmail.com>
2024-01-24 17:58:32 +0100

    gradient stroke issue
    show the issue of using a gradient for the stroke of horizontal  / vertical lines

---
+++ src/blog/gradient-stroke/svg.js
@@ -0,0 +1,15 @@
+export default {
+	stroke: `<svg style="display: block;" viewBox="-5 -7 30 14">
+    <defs>
+        <linearGradient id="stroke-gradient">
+            <stop stop-color="hsl(188 100% 50%)" offset="0" />
+            <stop stop-color="hsl(276 100% 46%)" offset="1" />
+        </linearGradient>
+    </defs>	
+    <rect fill="hsl(0 0% 90%)" x="-5" y="-7" width="30" height="14" />
+    <path fill="none" stroke="url(#stroke-gradient)" d="M 0 0 H 20" />
+    <text fill="hsl(215 40% 7%)" font-size="1.75" font-family="monospace" font-style="italic" x="23.5" y="5.5" text-anchor="end">
+        stroke
+    </text>
+</svg>`
+};
