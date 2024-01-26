29d81a716586824e1bffe65003076d06c3cb5d6a
Gabriele Corti <borntofrappe@gmail.com>
2024-01-26 09:50:51 +0100

    helper graphic
    show how the percentage value is not relative to the length of the line

--- src/blog/confounding-dashes/svg.js
+++ src/blog/confounding-dashes/svg.js
@@ -19,5 +19,19 @@ export default {
     <text fill="currentColor" font-size="1.25" font-family="monospace" font-style="italic" x="23.5" y="5.5" text-anchor="end">
         stroke-width="<tspan font-weight="700">5</tspan>"
     </text>
+</svg>`,
+	'percentage-length-1': `<svg style="display: block; color: hsl(215 40% 7%);" viewBox="-5 -7 30 14">
+    <rect fill="hsl(0 0% 90%)" x="-5" y="-7" width="30" height="14" />
+    <line fill="none" stroke="currentColor" stroke-dasharray="2%" x1="0" x2="20" />
+    <text fill="currentColor" font-size="1.25" font-family="monospace" font-style="italic" x="23.5" y="5.5" text-anchor="end">
+        x2="<tspan font-weight="700">20</tspan>"
+    </text>
+</svg>`,
+	'percentage-length-2': `<svg style="display: block; color: hsl(215 40% 7%);" viewBox="-5 -7 30 14">
+    <rect fill="hsl(0 0% 90%)" x="-5" y="-7" width="30" height="14" />
+    <line fill="none" stroke="currentColor" stroke-dasharray="2%" x1="0" x2="10" />
+    <text fill="currentColor" font-size="1.25" font-family="monospace" font-style="italic" x="23.5" y="5.5" text-anchor="end">
+        x2="<tspan font-weight="700">10</tspan>"
+    </text>
 </svg>`
 };
