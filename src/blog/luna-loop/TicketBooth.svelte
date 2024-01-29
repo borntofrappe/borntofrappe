793fb3c3706fb4994a575fa02e08d3776e19f8d9
Gabriele Corti <borntofrappe@gmail.com>
2024-01-29 15:15:46 +0100

    update colors

--- src/blog/luna-loop/TicketBooth.svelte
+++ src/blog/luna-loop/TicketBooth.svelte
@@ -28,14 +28,14 @@
 
 <svg class:open style="display: block;" viewBox="-42 -36 84 84">
 	<g stroke-linecap="round" stroke-linejoin="round">
-		<path fill="hsl(227 42% 51%)" d="M -36 0 A 36 36 0 0 1 36 0" />
-		<path fill="hsl(221 78% 86%)" d="M -29 0 A 29 29 0 0 1 29 0" />
-		<rect fill="hsl(210 36% 96%)" x="-38" width="76" height="4" rx="2" />
-		<rect fill="hsl(221 78% 86%)" x="-35" y="4" width="70" height="40" />
-		<rect fill="hsl(227 42% 51%)" x="-36" y="44" width="72" height="4" rx="2" />
+		<path fill="hsl(3 76% 74%)" d="M -36 0 A 36 36 0 0 1 36 0" />
+		<path fill="hsl(14 84% 85%)" d="M -29 0 A 29 29 0 0 1 29 0" />
+		<rect fill="hsl(18 38% 95%)" x="-38" width="76" height="4" rx="2" />
+		<rect fill="hsl(18 38% 95%)" x="-35" y="4" width="70" height="40" />
+		<rect fill="hsl(3 76% 74%)" x="-36" y="44" width="72" height="4" rx="2" />
 		<rect
-			fill="hsl(224 67% 76%)"
-			stroke="hsl(225 57% 67%)"
+			fill="hsl(4 87% 90%)"
+			stroke="hsl(4 82% 80%)"
 			stroke-width="2"
 			x="-15"
 			y="13"
@@ -45,9 +45,9 @@
 		<rect
 			class="window"
 			opacity="0"
-			fill="hsl(45 90% 88%)"
-			fill-opacity="0.8"
-			stroke="hsl(45 97% 54%)"
+			fill="hsl(18 38% 95%)"
+			fill-opacity="0.5"
+			stroke="hsl(18 38% 99%)"
 			stroke-width="2"
 			pathLength="20"
 			stroke-dasharray="0 0.5"
@@ -58,18 +58,18 @@
 		/>
 		<g transform="translate(-35 4)">
 			<path
-				fill="hsl(227 42% 51%)"
+				fill="hsl(13 79% 75%)"
 				d="M 0 0 0 14 A 5 5 0 0 0 10 14 L 10 0 M 20 0 20 8 A 5 5 0 0 0 30 8 L 30 0 M 40 0 40 8 A 5 5 0 0 0 50 8 L 50 0 M 60 0 60 14 A 5 5 0 0 0 70 14 L 70 0"
 			/>
 			<path
 				transform="translate(10 0)"
-				fill="hsl(210 36% 96%)"
+				fill="hsl(14 84% 85%)"
 				d="M 0 0 0 10 A 5 5 0 0 0 10 10 L 10 0 M 20 0 20 6 A 5 5 0 0 0 30 6 L 30 0 M 40 0 40 10 A 5 5 0 0 0 50 10 L 50 0"
 			/>
 		</g>
 		<text
-			fill="hsl(209 34% 30%)"
-			stroke="hsl(209 34% 30%)"
+			fill="hsl(31 100% 20%)"
+			stroke="hsl(31 100% 20%)"
 			stroke-width="0.5"
 			font-family="serif"
 			font-size="8"
