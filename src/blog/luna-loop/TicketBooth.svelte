d645fd26eb7b6e8adfbb6a4f825a8bbf298f1327
Gabriele Corti <borntofrappe@gmail.com>
2024-01-29 09:26:51 +0100

    intro graphic
    vector drawing a tickets booth and showing the animated window only when the media query for prefers-reduced-motion does not match

---
+++ src/blog/luna-loop/TicketBooth.svelte
@@ -0,0 +1,110 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let open = false;
+
+	onMount(() => {
+		const listener = (e) => {
+			if (e.matches) {
+				open = false;
+			} else {
+				open = true;
+			}
+		};
+
+		const reducedMotion = matchMedia('(prefers-reduced-motion: reduce)');
+
+		if (!reducedMotion.matches) {
+			open = true;
+		}
+
+		reducedMotion.addEventListener('change', listener);
+
+		return () => {
+			reducedMotion.removeEventListener('change', listener);
+		};
+	});
+</script>
+
+<svg class:open style="display: block;" viewBox="-42 -36 84 84">
+	<g stroke-linecap="round" stroke-linejoin="round">
+		<path fill="hsl(227 42% 51%)" d="M -36 0 A 36 36 0 0 1 36 0" />
+		<path fill="hsl(221 78% 86%)" d="M -29 0 A 29 29 0 0 1 29 0" />
+		<rect fill="hsl(210 36% 96%)" x="-38" width="76" height="4" rx="2" />
+		<rect fill="hsl(221 78% 86%)" x="-35" y="4" width="70" height="40" />
+		<rect fill="hsl(227 42% 51%)" x="-36" y="44" width="72" height="4" rx="2" />
+		<rect
+			fill="hsl(224 67% 76%)"
+			stroke="hsl(225 57% 67%)"
+			stroke-width="2"
+			x="-15"
+			y="13"
+			width="30"
+			height="30"
+		/>
+		<rect
+			class="window"
+			opacity="0"
+			fill="hsl(45 90% 88%)"
+			fill-opacity="0.8"
+			stroke="hsl(45 97% 54%)"
+			stroke-width="2"
+			pathLength="20"
+			stroke-dasharray="0 0.5"
+			x="-15"
+			y="13"
+			width="30"
+			height="30"
+		/>
+		<g transform="translate(-35 4)">
+			<path
+				fill="hsl(227 42% 51%)"
+				d="M 0 0 0 14 A 5 5 0 0 0 10 14 L 10 0 M 20 0 20 8 A 5 5 0 0 0 30 8 L 30 0 M 40 0 40 8 A 5 5 0 0 0 50 8 L 50 0 M 60 0 60 14 A 5 5 0 0 0 70 14 L 70 0"
+			/>
+			<path
+				transform="translate(10 0)"
+				fill="hsl(210 36% 96%)"
+				d="M 0 0 0 10 A 5 5 0 0 0 10 10 L 10 0 M 20 0 20 6 A 5 5 0 0 0 30 6 L 30 0 M 40 0 40 10 A 5 5 0 0 0 50 10 L 50 0"
+			/>
+		</g>
+		<text
+			fill="hsl(209 34% 30%)"
+			stroke="hsl(209 34% 30%)"
+			stroke-width="0.5"
+			font-family="serif"
+			font-size="8"
+			font-weight="700"
+			letter-spacing="1"
+			style="text-transform: uppercase;"
+			text-anchor="middle"
+			y="-6"
+		>
+			Tickets
+		</text>
+	</g>
+</svg>
+
+<style>
+	svg .window {
+		opacity: 0;
+		stroke-dashoffset: 0;
+		animation: offset 1s steps(3) infinite reverse paused;
+	}
+
+	svg.open .window {
+		opacity: 1;
+		animation-play-state: running;
+	}
+
+	@keyframes offset {
+		0% {
+			stroke-dashoffset: 0;
+		}
+		50% {
+			stroke-dashoffset: 5;
+		}
+		100% {
+			stroke-dashoffset: 10;
+		}
+	}
+</style>
