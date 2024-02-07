7281bc5e932801bff3b8b33d3a61f782e5d3d733
Gabriele Corti <borntofrappe@gmail.com>
2024-02-07 08:55:16 +0100

    palette icon

---
+++ src/blog/dry-paint/svg.js
@@ -0,0 +1,13 @@
+export default {
+	palette: `<svg aria-hidden="true" role="img" width="1em" height="1em" viewBox="-18.472135543823242 -27.944271087646484 48 46.944271087646484">
+    <g stroke-linecap="round" stroke-linejoin="round">
+        <path fill="hsl(20 76% 84%)" stroke="hsl(20 76% 84%)" stroke-width="18" d="M 20 0 A 15 15 0 1 0 0 10 A 20 20 0 0 1 20 0" />
+        <g fill="none" stroke-width="10">
+            <path stroke="hsl(203 51% 68%)" pathLength="1" stroke-dasharray="0 1" stroke-dashoffset="0.2" d="M 20 0 A 15 15 0 1 0 0 10" />
+            <path stroke="hsl(37 71% 69%)" pathLength="1" stroke-dasharray="0 1" stroke-dashoffset="0.4" d="M 20 0 A 15 15 0 1 0 0 10" />
+            <path stroke="hsl(9 67% 65%)" pathLength="1" stroke-dasharray="0 1" stroke-dashoffset="0.6" d="M 20 0 A 15 15 0 1 0 0 10" />
+            <path stroke="hsl(138 18% 67%)" pathLength="1" stroke-dasharray="0 1" stroke-dashoffset="0.8" d="M 20 0 A 15 15 0 1 0 0 10" />
+        </g>
+    </g>
+</svg>`
+};
