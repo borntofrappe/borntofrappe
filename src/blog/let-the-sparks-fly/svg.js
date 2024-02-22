c4513ea2d8fa996b077e4f34613120229752ce3e
Gabriele Corti <borntofrappe@gmail.com>
2024-02-22 18:12:39 +0100

    helper graphic
    chart the progress of the steps function with an arbitrary number of steps and the default value for the steps' position

--- src/blog/let-the-sparks-fly/svg.js
+++ src/blog/let-the-sparks-fly/svg.js
@@ -11,5 +11,44 @@ export default {
     <g filter="url(#viewBox-filter)" opacity="0.5">
         <path transform="translate(150 50)" fill="currentColor" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M 0 -12 A 12 12 0 0 0 12 0 12 12 0 0 0 0 12 12 12 0 0 0 -12 0 12 12 0 0 0 0 -12 Z" />
     </g>
+</svg>`,
+	'step-end': `<svg style="display: block;" viewBox="-15 -5 120 117.5">
+    <defs>
+        <marker id="step-end-marker" viewBox="-1 -1 2 2" markerWidth="3" markerHeight="3">
+            <circle fill="currentColor" r="1" />
+        </marker>
+    </defs>
+    <g fill="none" stroke="currentColor" >
+        <g stroke-width="0.5">
+            <path d="M 0 0 0 100 100 100" />
+            <g stroke-linecap="square">
+                <path transform="translate(0 100)" d="M 0 0 0 2 M 25 0 25 2 M 50 0 50 2 M 75 0 75 2 M 100 0 100 2" />
+                <path d="M 0 0 -2 0 M 0 25 -2 25 M 0 50 -2 50 M 0 75 -2 75 M 0 100 -2 100" />
+            </g>
+            <path pathLength="1" stroke-dasharray="0.0125 0.025" stroke-dashoffset="-0.0125" d="M 25 100 25 75 M 50 75 50 50 M 75 50 75 25 M 100 25 100 0" />
+        </g>
+        <g marker-start="url(#step-end-marker)" stroke-width="1.5">
+            <path d="M 0 100 25 100" />
+            <path d="M 25 75 50 75" />
+            <path d="M 50 50 75 50" />
+            <path d="M 75 25 100 25" />
+            <path d="M 100 0 Z" />
+        </g>
+    </g>
+    <g fill="currentColor" font-family="monospace" font-size="6">
+        <text x="-4" y="109" text-anchor="end">0</text>
+        <g transform="translate(0 109)" text-anchor="middle">
+            <text x="25">0.25</text>
+            <text x="50">0.50</text>
+            <text x="75">0.75</text>
+            <text x="100">1</text>
+        </g>
+        <g transform="translate(-4 1.9)" text-anchor="end">
+            <text y="75">25</text>
+            <text y="50">50</text>
+            <text y="25">75</text>
+            <text>100</text>
+        </g>
+    </g>
 </svg>`
 };
