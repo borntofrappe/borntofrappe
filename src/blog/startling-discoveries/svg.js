12798b54cc737e09ffee728febb7d21451f615f8
Gabriele Corti <borntofrappe@gmail.com>
2024-02-28 09:12:25 +0100

    helper graphic
    svg drawing the icon for a fingerprint with one `path` element

---
+++ src/blog/startling-discoveries/svg.js
@@ -0,0 +1,5 @@
+export default {
+	path: `<svg style="display: block;" viewBox="0 0 100 100">
+    <path fill="none" stroke="currentColor" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" d="M 77 84 C 60 87 47.5 78 47.5 63 M 62 94 C 46 94 35 80 35 63 A 12.5 12.5 0 0 1 60 63 M 37 93 C 18 81 20 39 47.5 38 A 25 25 0 0 1 72.5 63 L 72.5 64 M 17 81 C 4 52 25 27 50 27 C 79 28 85 51 85 65 M 10 36 C 25 10 75 10 90 36 M 21 12 C 36 2 66 2 79 12" />
+</svg>`
+};
