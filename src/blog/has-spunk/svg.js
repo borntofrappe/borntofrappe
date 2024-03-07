30764ae48a524fa7c47a4560eadc26bacd883da7
Gabriele Corti <borntofrappe@gmail.com>
2024-03-07 14:11:18 +0100

    intro graphic
    (fix date field in the frontmatter)

---
+++ src/blog/has-spunk/svg.js
@@ -0,0 +1,14 @@
+export default {
+	smiley: `<svg style="display: block;" viewBox="-50 -50 100 100">
+    <defs>
+        <g id="smiley-eye">
+            <ellipse fill="hsl(34 69% 15%)" rx="7" ry="8" />
+            <ellipse fill="hsl(36 60% 34%)" cx="-2" cy="-3" rx="2" ry="2.2" />
+        </g>
+    </defs>
+    <circle fill="hsl(47 94% 57%)" r="50" />
+    <use transform="translate(-17 -11)" href="#smiley-eye" />
+    <use transform="translate(17 -11)" href="#smiley-eye" />
+    <path transform="translate(0 14)" fill="none" stroke="hsl(34 69% 15%)" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M -20 0 A 40 40 0 0 0 20 0 28 28 0 0 1 -20 0" />
+</svg>`
+};
