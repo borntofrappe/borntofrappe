adb913083a913a2f6614707469e1516e4a2c96ad
Gabriele Corti <borntofrappe@gmail.com>
2024-03-07 14:12:58 +0100

    helper graphic
    face details moved with the transform attribute to point in a specific direction

--- src/blog/has-spunk/svg.js
+++ src/blog/has-spunk/svg.js
@@ -10,5 +10,19 @@ export default {
     <use transform="translate(-17 -11)" href="#smiley-eye" />
     <use transform="translate(17 -11)" href="#smiley-eye" />
     <path transform="translate(0 14)" fill="none" stroke="hsl(34 69% 15%)" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M -20 0 A 40 40 0 0 0 20 0 28 28 0 0 1 -20 0" />
+</svg>`,
+	'smiley-ne': `<svg style="display: block;" viewBox="-50 -50 100 100">
+    <defs>
+        <g id="smiley-eye">
+            <ellipse fill="hsl(34 69% 15%)" rx="7" ry="8" />
+            <ellipse fill="hsl(36 60% 34%)" cx="-2" cy="-3" rx="2" ry="2.2" />
+        </g>
+    </defs>
+    <circle fill="hsl(47 94% 57%)" r="50" />
+    <g transform="rotate(45) translate(0 -12) rotate(-45)">
+        <use transform="translate(-17 -11)" href="#smiley-eye" />
+        <use transform="translate(17 -11)" href="#smiley-eye" />
+        <path transform="translate(0 14)" fill="none" stroke="hsl(34 69% 15%)" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M -20 0 A 40 40 0 0 0 20 0 28 28 0 0 1 -20 0" />
+    </g>
 </svg>`
 };
