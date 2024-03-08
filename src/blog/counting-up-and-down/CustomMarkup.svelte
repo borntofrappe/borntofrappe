c589bf84370a964ea6933ec736ffb62ffc8a2ff9
Gabriele Corti <borntofrappe@gmail.com>
2024-03-08 11:17:36 +0100

    custom-markup
    first example of a custom element (without function)

---
+++ src/blog/counting-up-and-down/CustomMarkup.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if (customElements.get('custom-markup')) return;
+
+		const template = document.createElement('template');
+
+		template.innerHTML = `<button>Decrement</button>
+        <span>0</span>
+        <button>Increment</button>`;
+
+		class CustomMarkup extends HTMLElement {
+			constructor() {
+				super();
+
+				const shadowRoot = this.attachShadow({ mode: 'open' });
+				shadowRoot.appendChild(template.content.cloneNode(true));
+			}
+		}
+
+		customElements.define('custom-markup', CustomMarkup);
+	});
+</script>
+
+<custom-markup></custom-markup>
