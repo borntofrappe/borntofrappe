d06d74719046acfa562227cdf3e36926e35a8ca6
Gabriele Corti <borntofrappe@gmail.com>
2024-03-08 11:18:54 +0100

    custom-counter
    functional widget to count up and down

---
+++ src/blog/counting-up-and-down/CustomCounter.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if (customElements.get('custom-counter')) return;
+
+		const template = document.createElement('template');
+
+		template.innerHTML = `<button>Decrement</button>
+        <span>0</span>
+        <button>Increment</button>`;
+
+		class CustomCounter extends HTMLElement {
+			constructor() {
+				super();
+
+				const shadowRoot = this.attachShadow({ mode: 'open' });
+				shadowRoot.appendChild(template.content.cloneNode(true));
+
+				const count = shadowRoot.querySelector('span');
+				const buttonDecrement = shadowRoot.querySelector('button:nth-of-type(1)');
+				const buttonIncrement = shadowRoot.querySelector('button:nth-of-type(2)');
+
+				buttonDecrement.addEventListener('click', () => {
+					count.innerHTML = +count.innerHTML - 1;
+				});
+
+				buttonIncrement.addEventListener('click', () => {
+					count.innerHTML = +count.innerHTML + 1;
+				});
+			}
+		}
+
+		customElements.define('custom-counter', CustomCounter);
+	});
+</script>
+
+<custom-counter></custom-counter>
