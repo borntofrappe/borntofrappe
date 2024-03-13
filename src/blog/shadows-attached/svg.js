dc1782197aad1c1c067b9b49f5b1569a02b149ce
Gabriele Corti <borntofrappe@gmail.com>
2024-03-13 10:09:51 +0100

    intro graphic

---
+++ src/blog/shadows-attached/svg.js
@@ -0,0 +1,9 @@
+export default {
+	'shadow-root': `<svg style="display: block;" viewBox="-45 -21 90 30">
+    <path fill="hsl(249 8% 17%)" d="M -45 -6 C -45 -21 -45 -21 0 -21 45 -21 45 -21 45 -6 45 9 45 9 0 9 -45 9 -45 9 -45 -6" />
+    <g transform="translate(-2 0)" fill="currentColor" font-family="monospace" text-anchor="middle" font-size="18" font-weight="700" style="text-transform: uppercase;" letter-spacing="1">
+        <text fill="hsl(249 11% 5%)" textLength="70" transform="scale(1 0.35) skewX(-30)">Root</text>
+        <text fill="hsl(0 0% 94%)" textLength="70">Shadow</text>
+    </g>
+</svg>`
+};
