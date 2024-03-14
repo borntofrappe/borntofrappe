ceb2321e0e304be22a8ad379e4a38322a30202b6
Gabriele Corti <borntofrappe@gmail.com>
2024-03-14 21:42:06 +0100

    update console
    - reduce the relevance of the circles ancillary to the main graphic
    - draw icon to differentiate _console_ tab from _inspector_ tab
    - fix the gradient to user coordinates to maintain a similar direction

--- src/blog/building-web-blocks/svg.js
+++ src/blog/building-web-blocks/svg.js
@@ -91,7 +91,7 @@ export default {
 	defs: `<svg aria-hidden="true" width="0" height="0" style="inline-size: 0; block-size: 0; position: absolute;">
     <defs>
         <path id="console-arrow" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" d="M -1 -0.5 1 -0.5 0 0.5 Z" />
-        <linearGradient id="console-gradient" x1="0" y1="0" x2="1" y2="0.2">
+        <linearGradient id="console-gradient" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="100" y2="30">
             <stop stop-color="black" stop-opacity="0" offset="0.55" />
             <stop stop-color="black" stop-opacity="0.1" offset="0.55" />
         </linearGradient>
@@ -102,13 +102,13 @@ export default {
     <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
     <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
     <g transform="translate(6 6)">
-        <circle fill="hsl(3 100% 68%)" r="2" />
-        <circle fill="hsl(41 99% 60%)" cx="6" r="2" />
-        <circle fill="hsl(129 62% 49%)" cx="12" r="2" />
+        <circle fill="hsl(3 100% 68%)" r="1.75" />
+        <circle fill="hsl(41 99% 60%)" cx="6" r="1.75" />
+        <circle fill="hsl(129 62% 49%)" cx="12" r="1.75" />
     </g>
     <g transform="translate(30 8)" fill="none" stroke="hsl(0 0% 94%)">
-        <rect stroke-width="0.75" x="-2" y="-2" width="4" height="4" rx="0.5" />
-        <path stroke-width="0.5" d="M 0 -2 0 -3.25 M 2 0 3.25 0 M 0 2 0 3.25 M -2 0 -3.25 0" />
+        <rect stroke-width="0.75" x="-1.75" y="-1.75" width="3.5" height="3.5" rx="0.5" />
+        <path stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" d="M 0 -1.75 0 -2.75 M 1.75 0 2.75 0 M 0 1.75 0 2.75 M -1.75 0 -2.75 0" />
     </g>
     <use transform="translate(6.5 20)" fill="hsl(0 0% 94%)" stroke="hsl(0 0% 94%)" href="#console-arrow" />
     <g font-family="monospace" font-weight="700">
@@ -126,13 +126,13 @@ export default {
     <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
     <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
     <g transform="translate(6 6)">
-        <circle fill="hsl(3 100% 68%)" r="2" />
-        <circle fill="hsl(41 99% 60%)" cx="6" r="2" />
-        <circle fill="hsl(129 62% 49%)" cx="12" r="2" />
+        <circle fill="hsl(3 100% 68%)" r="1.75" />
+        <circle fill="hsl(41 99% 60%)" cx="6" r="1.75" />
+        <circle fill="hsl(129 62% 49%)" cx="12" r="1.75" />
     </g>
     <g transform="translate(30 8)" fill="none" stroke="hsl(0 0% 94%)">
-        <rect stroke-width="0.75" x="-2" y="-2" width="4" height="4" rx="0.5" />
-        <path stroke-width="0.5" d="M 0 -2 0 -3.25 M 2 0 3.25 0 M 0 2 0 3.25 M -2 0 -3.25 0" />
+        <rect stroke-width="0.75" x="-1.75" y="-1.75" width="3.5" height="3.5" rx="0.5" />
+        <path stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" d="M 0 -1.75 0 -2.75 M 1.75 0 2.75 0 M 0 1.75 0 2.75 M -1.75 0 -2.75 0" />
     </g>
     <g fill="hsl(0 0% 94%)" stroke="hsl(0 0% 94%)">
         <use transform="translate(6.5 20)" href="#console-arrow" />
@@ -154,13 +154,13 @@ export default {
     <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
     <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
     <g transform="translate(6 6)">
-        <circle fill="hsl(3 100% 68%)" r="2" />
-        <circle fill="hsl(41 99% 60%)" cx="6" r="2" />
-        <circle fill="hsl(129 62% 49%)" cx="12" r="2" />
+        <circle fill="hsl(3 100% 68%)" r="1.75" />
+        <circle fill="hsl(41 99% 60%)" cx="6" r="1.75" />
+        <circle fill="hsl(129 62% 49%)" cx="12" r="1.75" />
     </g>
     <g transform="translate(30 8)" fill="none" stroke="hsl(0 0% 94%)">
-        <rect stroke-width="0.75" x="-2" y="-2" width="4" height="4" rx="0.5" />
-        <path stroke-width="0.5" d="M 0 -2 0 -3.25 M 2 0 3.25 0 M 0 2 0 3.25 M -2 0 -3.25 0" />
+        <rect stroke-width="0.75" x="-1.75" y="-1.75" width="3.5" height="3.5" rx="0.5" />
+        <path stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" d="M 0 -1.75 0 -2.75 M 1.75 0 2.75 0 M 0 1.75 0 2.75 M -1.75 0 -2.75 0" />
     </g>
     <g fill="hsl(0 0% 94%)" stroke="hsl(0 0% 94%)">
         <use transform="translate(6.5 20)" href="#console-arrow" />
@@ -183,13 +183,13 @@ export default {
     <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
     <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
     <g transform="translate(6 6)">
-        <circle fill="hsl(3 100% 68%)" r="2" />
-        <circle fill="hsl(41 99% 60%)" cx="6" r="2" />
-        <circle fill="hsl(129 62% 49%)" cx="12" r="2" />
+        <circle fill="hsl(3 100% 68%)" r="1.75" />
+        <circle fill="hsl(41 99% 60%)" cx="6" r="1.75" />
+        <circle fill="hsl(129 62% 49%)" cx="12" r="1.75" />
     </g>
     <g transform="translate(30 8)" fill="none" stroke="hsl(0 0% 94%)">
-        <rect stroke-width="0.75" x="-2" y="-2" width="4" height="4" rx="0.5" />
-        <path stroke-width="0.5" d="M 0 -2 0 -3.25 M 2 0 3.25 0 M 0 2 0 3.25 M -2 0 -3.25 0" />
+        <rect stroke-width="0.75" x="-1.75" y="-1.75" width="3.5" height="3.5" rx="0.5" />
+        <path stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" d="M 0 -1.75 0 -2.75 M 1.75 0 2.75 0 M 0 1.75 0 2.75 M -1.75 0 -2.75 0" />
     </g>
     <use fill="hsl(0 0% 94%)" stroke="hsl(0 0% 94%)" transform="translate(6.5 20)" href="#console-arrow" />
     <g font-family="monospace" font-weight="700">
--- src/blog/shadows-attached/svg.js
+++ src/blog/shadows-attached/svg.js
@@ -9,7 +9,7 @@ export default {
 	defs: `<svg aria-hidden="true" width="0" height="0" style="inline-size: 0; block-size: 0; position: absolute;">
     <defs>
         <path id="console-details" fill-rule="evenodd" d="M 2 0 A 2 2 0 0 0 -2 0 A 2 2 0 0 0 2 0 M 0.4 -0.4 0.4 -0.9 A 0.4 0.4 0 0 0 -0.4 -0.9 L -0.4 -0.4 -0.9 -0.4 A 0.4 0.4 0 0 0 -0.9 0.4 L -0.4 0.4 -0.4 0.9 A 0.4 0.4 0 0 0 0.4 0.9 L 0.4 0.4 0.9 0.4 A 0.4 0.4 0 0 0 0.9 -0.4 Z" />
-        <linearGradient id="console-gradient" x1="0" y1="0" x2="1" y2="0.2">
+        <linearGradient id="console-gradient" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="100" y2="30">
             <stop stop-color="black" stop-opacity="0" offset="0.55" />
             <stop stop-color="black" stop-opacity="0.1" offset="0.55" />
         </linearGradient>
@@ -20,13 +20,13 @@ export default {
     <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
     <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
     <g transform="translate(6 6)">
-        <circle fill="hsl(3 100% 68%)" r="2" />
-        <circle fill="hsl(41 99% 60%)" cx="6" r="2" />
-        <circle fill="hsl(129 62% 49%)" cx="12" r="2" />
+        <circle fill="hsl(3 100% 68%)" r="1.75" />
+        <circle fill="hsl(41 99% 60%)" cx="6" r="1.75" />
+        <circle fill="hsl(129 62% 49%)" cx="12" r="1.75" />
     </g>
     <g transform="translate(30 8)" fill="none" stroke="hsl(0 0% 94%)">
         <rect stroke-width="0.75" x="-2" y="-2" width="4" height="4" rx="0.5" />
-        <path stroke-width="0.5" d="M 0 -2 0 -3.25 M 2 0 3.25 0 M 0 2 0 3.25 M -2 0 -3.25 0" />
+        <path stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" d="M -0.75 -0.75 0 0 -0.75 0.75" />
     </g>
     <use fill="hsl(3 100% 68%)" transform="translate(6 20) rotate(45)" href="#console-details" />
     <g font-family="monospace" font-weight="700">
@@ -46,13 +46,13 @@ export default {
     <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
     <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
     <g transform="translate(6 6)">
-        <circle fill="hsl(3 100% 68%)" r="2" />
-        <circle fill="hsl(41 99% 60%)" cx="6" r="2" />
-        <circle fill="hsl(129 62% 49%)" cx="12" r="2" />
+        <circle fill="hsl(3 100% 68%)" r="1.75" />
+        <circle fill="hsl(41 99% 60%)" cx="6" r="1.75" />
+        <circle fill="hsl(129 62% 49%)" cx="12" r="1.75" />
     </g>
     <g transform="translate(30 8)" fill="none" stroke="hsl(0 0% 94%)">
         <rect stroke-width="0.75" x="-2" y="-2" width="4" height="4" rx="0.5" />
-        <path stroke-width="0.5" d="M 0 -2 0 -3.25 M 2 0 3.25 0 M 0 2 0 3.25 M -2 0 -3.25 0" />
+        <path stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" d="M -0.75 -0.75 0 0 -0.75 0.75" />
     </g>
     <use fill="hsl(3 100% 68%)" transform="translate(6 20) rotate(45)" href="#console-details" />
     <g font-family="monospace" font-weight="700">
