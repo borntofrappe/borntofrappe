d889b8def89969a186f77f80a1c55c2aa2f0992b
Gabriele Corti <borntofrappe@gmail.com>
2024-03-15 21:15:32 +0100

    update design
    repeat color scheme from the developer tools visuals
    
    rename title to display

--- src/blog/up-to-date-attributes/RandomNumberGenerator.svelte
+++ src/blog/up-to-date-attributes/RandomNumberGenerator.svelte
@@ -54,17 +54,27 @@ button:active {
 
 button > svg {
     margin-inline-start: 1ch;
-}`;
+	inline-size: 1.25em;
+	block-size: 1.25em;
+}
+button > svg > g {
+	rotate: 90deg;
+    transition: rotate 0.3s cubic-bezier(0.37, 0, 0.63, 1);
+}
+button:hover > svg > g {
+	rotate: -90deg;
+}
+`;
 
-				const title = document.createElement('h1');
-				title.setAttribute('role', 'status');
-				title.setAttribute('aria-live', 'polite');
-				title.textContent = this.number;
+				const display = document.createElement('h1');
+				display.setAttribute('role', 'status');
+				display.setAttribute('aria-live', 'polite');
+				display.textContent = this.number;
 
 				const button = document.createElement('button');
 				button.textContent = 'Generate';
 				button.innerHTML = `<span>Generate</span>
-<svg width="1em" height="1em" viewBox="-5 -5 10 10">
+<svg width="1em" height="1em" viewBox="-6.9 -6.9 13.8 13.8">
     <g transform="rotate(90)" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
         <path transform="translate(2 0)" d="M 0 4.5 A 5.5 5.5 0 0 0 0 -4.5 M 0 -4.5 2.5 -4.5 M 0 -4.5 0 -2" />
         <path transform="scale(-1 -1) translate(2 0)" d="M 0 4.5 A 5.5 5.5 0 0 0 0 -4.5 M 0 -4.5 2.5 -4.5 M 0 -4.5 0 -2" />
@@ -78,7 +88,7 @@ button > svg {
 				});
 
 				shadowRoot.appendChild(style);
-				shadowRoot.appendChild(title);
+				shadowRoot.appendChild(display);
 				shadowRoot.appendChild(button);
 			}
 
@@ -104,8 +114,8 @@ button > svg {
 </script>
 
 <random-number-generator
-	style="--color-foreground: hsl(249 11% 12%); --color-background: hsl(0 0% 94%);"
-	number="-8"
+	style="--color-foreground: hsl(0 0% 94%); --color-background: hsl(249 11% 12%);"
 	min="0"
 	max="100"
+	number="-8"
 ></random-number-generator>
