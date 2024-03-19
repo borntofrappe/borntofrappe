9f719d0c75005dec54daf98c421ad51d867c816a
Gabriele Corti <borntofrappe@gmail.com>
2024-03-19 11:16:31 +0100

    helper graphic

---
+++ src/blog/literal-text/svg.js
@@ -0,0 +1,20 @@
+export default {
+	'js-markup': `<svg style="display: block" viewBox="-20 -37.5 364.33331298828125 94.4000015258789">
+    <rect fill="hsl(210 17% 98%)" x="-20" y="-37.5" width="364.33331298828125" height="94.4000015258789" rx="10" />
+    <g fill="hsl(210 10% 23%)">
+        <rect x="65.83333129882813" y="17" width="48.85" height="19.9" rx="4" />
+        <rect fill="hsl(274 87% 43%)" x="168.21666259765624" y="17" width="48.85" height="19.9" rx="4" />
+        <rect x="240.24998779296874" y="17" width="48.85" height="19.9" rx="4" />
+    </g>
+    <text fill="hsl(210 11% 15%)" font-family="sans-serif" font-size="19" letter-spacing="1" word-spacing="3">
+        <tspan>March 16 9.27 AM</tspan>
+        <tspan> | </tspan>
+        <tspan>0 characters</tspan>
+    </text>
+    <g fill="hsl(210 16% 93%)" font-family="monospace" font-size="11" font-weight="700" text-anchor="middle">
+        <text x="90.13333129882812" y="30">&lt;time&gt;</text>
+        <text x="192.51666259765625" y="30">&lt;text&gt;</text>
+        <text x="264.5499954223633" y="30">&lt;span&gt;</text>
+    </g>
+</svg>`
+};
