8e4183f2fe2f433ae276851217ea7008d0b55e20
Gabriele Corti <borntofrappe@gmail.com>
2024-05-02 20:58:50 +0200

    intro graphic

---
+++ src/blog/open-void/svg.js
@@ -0,0 +1,38 @@
+export default {
+	'open-sign': `<svg style="display: block;" viewBox="-113.87411499023438 -41.0933837890625 225.87411499023438 125.4012451171875">
+    <defs>
+        <filter id="open-sign-filter">
+            <feTurbulence baseFrequency="3 0.125" />
+            <feBlend in2="SourceGraphic" mode="overlay" />
+            <feComposite in2="SourceGraphic" operator="in" />
+        </filter>
+        <rect id="open-sign-background" x="-77.87411117553711" y="-33.59338188171387" width="153.87411499023438" height="110.4012451171875" rx="10" />
+        <clipPath id="open-sign-clip">
+            <use href="#open-sign-background" />
+        <clipPath />
+    </defs>
+    <path fill="none" stroke="hsl(37 35% 66%)" stroke-width="4" stroke-linejoin="round" d="M -77.87411117553711 -23.593381881713867 -107.87411117553711 21.607240676879883 -77.87411117553711 66.80786323547363 M 76.00000381469727 -23.593381881713867 106.00000381469727 21.607240676879883 76.00000381469727 66.80786323547363" />
+    <g fill="hsl(352 60% 20%)">
+        <circle r="6" cx="-107.87411117553711" cy="21.607240676879883" />
+        <circle r="6" cx="106.00000381469727" cy="21.607240676879883" />
+    </g>
+    <use filter="url(#open-sign-filter)" fill="hsl(35 74% 83%)" stroke="hsl(35 74% 83%)" stroke-width="15" href="#open-sign-background" />
+    <g fill="hsl(352 60% 20%)">
+        <circle r="3" cx="-67.87411117553711" cy="-23.593381881713867" /> 
+        <circle r="3" cx="-67.87411117553711" cy="66.80786323547363" /> 
+        <circle r="3" cx="66.00000381469727" cy="66.80786323547363" /> 
+        <circle r="3" cx="66.00000381469727" cy="-23.593381881713867" /> 
+    </g>
+    <path transform="translate(0 21.25)" fill="none" stroke="hsl(352 60% 20%)" stroke-width="0.5" d="M -15 0 -35 0 M 15 0 35 0" />
+    <g fill="hsl(352 60% 20%)" stroke="hsl(352 60% 20%)" stroke-width="0.5" stroke-linejoin="round" text-anchor="middle">
+        <text font-family="serif" font-size="20" font-style="italic">Sorry</text>
+        <text fill="none" y="26" font-family="sans-serif" font-size="14" letter-spacing="0.2" style="text-transform: uppercase;">I'm</text>
+        <text y="56" font-family="sans-serif" font-size="20" font-weight="700" word-spacing="1" style="text-transform: uppercase;" textLength="100" lengthAdjust="spacingAndGlyphs"><tspan stroke-width="1" font-size="24" rotate="-5 3 5">Not</tspan> <tspan>closed</tspan></text>
+    </g>
+    <g style="pointer-events: none;" clip-path="url(#open-sign-clip)" opacity="0.3" fill="hsl(0 0% 100%)">
+        <path d="M 80 -50 60 -50 0 100 20 100" />
+        <path d="M -40 -50 -60 -50 -120 100 -100 100 M -30 -50 -35 -50 -95 100 -90 100" />
+    </g>
+    <use fill="none" stroke="hsl(352 60% 20%)" stroke-width="2" href="#open-sign-background" />
+</svg>`
+};
