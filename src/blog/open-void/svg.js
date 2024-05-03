f39e8357bd049b44e5100b748e3f11f263cce417
Gabriele Corti <borntofrappe@gmail.com>
2024-05-03 10:50:19 +0200

    helper graphic
    instead of a code snippet use an additional visualization of the console and the inspector tab; add the svg definitions in a separate hidden element

--- src/blog/open-void/svg.js
+++ src/blog/open-void/svg.js
@@ -35,7 +35,7 @@ export default {
     </g>
     <use fill="none" stroke="hsl(352 60% 20%)" stroke-width="2" href="#open-sign-background" />
 </svg>`,
-	'inspector-slot': `<svg style="display: block;" viewBox="0 0 100 46">
+	defs: `<svg aria-hidden="true" width="0" height="0" style="inline-size: 0; block-size: 0; position: absolute;">
     <defs>
         <path id="console-arrow" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" d="M -1 -0.5 1 -0.5 0 0.5 Z" />
         <linearGradient id="console-gradient" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="100" y2="30">
@@ -43,6 +43,30 @@ export default {
             <stop stop-color="black" stop-opacity="0.1" offset="0.55" />
         </linearGradient>
     </defs>
+</svg>`,
+	'inspector-markup': `<svg style="display: block;" viewBox="0 0 100 38">
+    <rect fill="hsl(249 11% 12%)" width="100" height="38" rx="5" />
+    <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
+    <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
+    <g transform="translate(6 6)">
+        <circle fill="hsl(3 100% 68%)" r="1.75" />
+        <circle fill="hsl(41 99% 60%)" cx="6" r="1.75" />
+        <circle fill="hsl(129 62% 49%)" cx="12" r="1.75" />
+    </g>
+    <g transform="translate(30 8)" fill="none" stroke="hsl(0 0% 94%)">
+        <rect stroke-width="0.75" x="-1.75" y="-1.75" width="3.5" height="3.5" rx="0.5" />
+        <path stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" d="M 0 -1.75 0 -2.75 M 1.75 0 2.75 0 M 0 1.75 0 2.75 M -1.75 0 -2.75 0" />
+    </g>
+    <g font-family="monospace" font-weight="700">
+        <text fill="hsl(258 97% 74%)" font-size="3.25" x="35" y="9">Inspector</text>
+        <g fill="hsl(0 0% 94%)" font-size="3.75">
+            <text x="10" y="21">Hi there</text>
+            <text x="10" y="29">&lt;span&gt;<tspan dx="1" dy="-0.8">...</tspan><tspan dx="1" dy="0.8">&lt;/span&gt;</tspan></text>
+        </g>
+    </g>
+    <rect style="pointer-events: none;" fill="url(#console-gradient)" width="100" height="38" rx="5" />
+</svg>`,
+	'inspector-slot': `<svg style="display: block;" viewBox="0 0 100 46">
     <rect fill="hsl(249 11% 12%)" width="100" height="46" rx="5" />
     <path fill="hsl(249 8% 17%)" d="M 0 12 0 5 A 5 5 0 0 1 5 0 L 95 0 A 5 5 0 0 1 100 5 L 100 12" />
     <path fill="hsl(249 11% 12%)" d="M 22 12 A 2 2 0 0 0 24 10 L 24 5 A 2 2 0 0 1 26 3 L 62 3 A 2 2 0 0 1 64 5 L 64 10 A 2 2 0 0 0 66 12" />
