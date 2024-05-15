03f34dfeb00751ef60f7be84c7510fa4787e020b
Gabriele Corti <borntofrappe@gmail.com>
2024-05-15 17:35:28 +0200

    refactor grid
    create a separate data structure to keep a one dimensional array for the necessary cells

--- src/blog/light-custom-elements/grids.js
+++ src/blog/light-custom-elements/grids.js
@@ -13,19 +13,22 @@ if (browser && customElements && !customElements.get('top-down-grid')) {
 			const grid = textContent.split('\n').map((row) => row.split(''));
 			const columns = Math.max(...grid.map((row) => row.length));
 			const rows = grid.length;
+			const cells = grid
+				.map((row, y) =>
+					row.map((c, x) => ({
+						x,
+						y,
+						c
+					}))
+				)
+				.flat()
+				.filter(({ c }) => c === 'x');
+
 			this.innerHTML = `<svg viewBox="0 0 ${columns} ${rows}">
-        <g fill="currentColor">
-          ${grid
-						.map((row, y) =>
-							row
-								.map((cell, x) =>
-									cell === 'x' ? `<rect x="${x}" y="${y}" width="1" height="1" />` : ''
-								)
-								.join('')
-						)
-						.join('')}
-      </g>
-  </svg>`;
+			<g fill="currentColor">
+				${cells.map(({ x, y }) => `<rect x="${x}" y="${y}" width="1" height="1" />`)}
+			</g>
+		</svg>`;
 		}
 	}
 
@@ -41,27 +44,29 @@ if (browser && customElements && !customElements.get('top-down-grid')) {
 			const grid = textContent.split('\n').map((row) => row.split(''));
 			const columns = Math.max(...grid.map((row) => row.length));
 			const rows = grid.length;
+			const cells = grid
+				.map((row, y) =>
+					row.map((c, x) => ({
+						x: x / 2 - y / 2,
+						y: y / 4 + x / 4,
+						c
+					}))
+				)
+				.flat()
+				.filter(({ c }) => c === 'x');
+
 			const width = columns / 2 + rows / 2;
 			const height = columns / 4 + rows / 4;
 			const x0 = ((rows - 1) / 2) * -1;
 
 			this.innerHTML = `<svg viewBox="${x0} 0 ${width} ${height}">
-      <g fill="currentColor">
-          ${grid
-						.map((row, y) =>
-							row
-								.map((cell, x) =>
-									cell === 'x'
-										? `<path transform="translate(${x / 2 - y / 2} ${
-												y / 4 + x / 4
-											})" d="M 0 0.25 0.5 0 1 0.25 0.5 0.5" />`
-										: ''
-								)
-								.join('')
-						)
-						.join('')}
-      </g>
-  </svg>`;
+			<defs>
+				<path id="iso-square" d="M 0 0.25 0.5 0 1 0.25 0.5 0.5" />
+			</defs>
+			<g fill="currentColor">
+			${cells.map(({ x, y }) => `<use x="${x}" y="${y}" href="#iso-square" />`)}
+			</g>
+		</svg>`;
 		}
 	}
 
