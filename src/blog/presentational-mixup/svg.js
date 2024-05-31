09e9c924148093db5f31b58e38f8b78ac0d423fc
Gabriele Corti <borntofrappe@gmail.com>
2024-05-31 21:52:07 +0200

    closing graphic

---
+++ src/blog/presentational-mixup/svg.js
@@ -0,0 +1,70 @@
+export default {
+	'scss-cubes': `<svg style="display: block;" viewBox="0.5 -5.75 11 9.5">
+    <defs>
+        <g id="cube">
+            <path d="M 0 0.25 l 0.5 -0.25 0.5 0.25 0 0.5 -0.5 0.25 -0.5 -0.25" />
+            <g fill="black">
+                <path opacity="0.5" d="M 0 0.25 l 0.5 0.25 0 0.5 -0.5 -0.25" />
+                <path opacity="0.25" d="M 0.5 0.5 l 0.5 -0.25 0 0.5 -0.5 0.25" />
+            </g>
+        </g>
+    </defs>
+    <g fill="hsl(187 74% 65%)">
+        <use href="#cube" x="10" y="-2" />
+        <use href="#cube" x="9.5" y="-1.75" />
+        <use href="#cube" x="9" y="-1.5" />
+        <use href="#cube" x="10" y="-2.5" />
+        <use href="#cube" x="10" y="-3" />
+        <use href="#cube" x="10" y="-3.5" />
+        <use href="#cube" x="9.5" y="-3.25" />
+        <use href="#cube" x="9" y="-3" />
+        <use href="#cube" x="9" y="-3.5" />
+        <use href="#cube" x="9" y="-4" />
+        <use href="#cube" x="10" y="-5" />
+        <use href="#cube" x="9.5" y="-4.75" />
+        <use href="#cube" x="9" y="-4.5" />
+        <use href="#cube" x="7.5" y="-0.75" />
+        <use href="#cube" x="7" y="-0.5" />
+        <use href="#cube" x="6.5" y="-0.25" />
+        <use href="#cube" x="7.5" y="-1.25" />
+        <use href="#cube" x="7.5" y="-1.75" />
+        <use href="#cube" x="7.5" y="-2.25" />
+        <use href="#cube" x="7" y="-2" />
+        <use href="#cube" x="6.5" y="-1.75" />
+        <use href="#cube" x="6.5" y="-2.25" />
+        <use href="#cube" x="6.5" y="-2.75" />
+        <use href="#cube" x="7.5" y="-3.75" />
+        <use href="#cube" x="7" y="-3.5" />
+        <use href="#cube" x="6.5" y="-3.25" />
+        <use href="#cube" x="5" y="0.5" />
+        <use href="#cube" x="4.5" y="0.75" />
+        <use href="#cube" x="4" y="1" />
+        <use href="#cube" x="4" y="0.5" />
+        <use href="#cube" x="4" y="0" />
+        <use href="#cube" x="4" y="-0.5" />
+        <use href="#cube" x="4" y="-1" />
+        <use href="#cube" x="4" y="-1.5" />
+        <use href="#cube" x="5" y="-2.5" />
+        <use href="#cube" x="4.5" y="-2.25" />
+        <use href="#cube" x="4" y="-2" />
+        <g fill="hsl(206 96% 72%)">
+            <path opacity="0.45" d="M 11.5 -5 11.5 -1 2 3.725 2 -0.25" />
+            <path opacity="0.9" d="M 11.5 -5 2 -0.25 0.5 -1 10 -5.75" />
+            <path opacity="0.6" d="M 2 3.725 2 -0.25 0.5 -1 0.5 0 1 0.25 1 3.25" />
+        </g>
+        <use href="#cube" x="2.5" y="1.75" />
+        <use href="#cube" x="2" y="2" />
+        <use href="#cube" x="1.5" y="2.25" />
+        <use href="#cube" x="2.5" y="1.25" />
+        <use href="#cube" x="2.5" y="0.75" />
+        <use href="#cube" x="2.5" y="0.25" />
+        <use href="#cube" x="2" y="0.5" />
+        <use href="#cube" x="1.5" y="0.75" />
+        <use href="#cube" x="1.5" y="0.25" />
+        <use href="#cube" x="1.5" y="-0.25" />
+        <use href="#cube" x="2.5" y="-1.25" />
+        <use href="#cube" x="2" y="-1" />
+        <use href="#cube" x="1.5" y="-0.75" />
+    </g>
+</svg>`
+};
