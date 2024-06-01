118add8946032d4750da51236ade52b059fa1651
Gabriele Corti <borntofrappe@gmail.com>
2024-06-01 10:06:27 +0200

    update graphic

--- src/blog/presentational-mixup/svg.js
+++ src/blog/presentational-mixup/svg.js
@@ -1,70 +1,99 @@
 export default {
-	'scss-cubes': `<svg style="display: block;" viewBox="0.5 -5.75 11 9.5">
+	'scss-cubes': `<svg style="display: block;" viewBox="-0.05 -4.8 12.1 10.1">
     <defs>
-        <g id="cube">
+        <g id="c-cube">
             <path d="M 0 0.25 l 0.5 -0.25 0.5 0.25 0 0.5 -0.5 0.25 -0.5 -0.25" />
-            <g fill="black">
+            <g fill="white">
                 <path opacity="0.5" d="M 0 0.25 l 0.5 0.25 0 0.5 -0.5 -0.25" />
                 <path opacity="0.25" d="M 0.5 0.5 l 0.5 -0.25 0 0.5 -0.5 0.25" />
             </g>
         </g>
+        <pattern id="c-pattern" viewBox="-0.4 -0.4 1.8 1.8" patternUnits="userSpaceOnUse" width="1" height="1">
+            <g fill="none" stroke="hsl(185 84% 88%)" stroke-width="0.1">
+            <path>
+                <animate attributeName="d" dur="9s" begin="6s" repeatCount="indefinite" values="M 0.5 0.875 L 0.5 0.875; M 0.5 0.875 L 0.5 0.875; M 0.5 0.875 L 0.5 0.875; M 0.5 0.875 L 0.5 0.975; M 0.5 0.975 L 0.5 0.975; M 0.5 0.975 L 0.5 0.975" />
+            </path>
+            <path>
+                <animate attributeName="d" dur="9s" repeatCount="indefinite" values="M 0 0.5 L 0 0.5; M 0 0.5 L 0 0.5; M 0 0.5 L 0 0.5; M 0 0.5 L 0 0.7; M 0 0.7 L 0 0.7; M 0 0.7 L 0 0.7" />
+            </path>
+            <path>
+                <animate attributeName="d" dur="9s" begin="3s" repeatCount="indefinite" values="M 1 0.1 L 1 0.1; M 1 0.1 L 1 0.1; M 1 0.1 L 1 0.1; M 1 0.1 L 1 0.175; M 1 0.175 L 1 0.175; M 1 0.175 L 1 0.175" />
+            </path>
+            </g>
+        </pattern>
+        <pattern id="c-pattern-1" viewBox="-0.4 -0.4 1.8 1.8" patternUnits="userSpaceOnUse" width="1" height="1">
+            <g fill="none" stroke="hsl(185 84% 88%)" stroke-width="0.1">
+                <path>
+                    <animate attributeName="d" dur="9s" begin="6s" repeatCount="indefinite" values="M 0.4 0.875 L 0.4 0.875; M 0.4 0.875 L 0.4 0.875; M 0.4 0.875 L 0.6 0.975; M 0.6 0.975 L 0.6 0.975; M 0.6 0.975 L 0.6 0.975; M 0.6 0.975 L 0.6 0.975" />
+                </path>
+                <path>
+                    <animate attributeName="d" dur="9s" repeatCount="indefinite" values="M 0 0.5 L 0 0.5; M 0 0.5 L 0 0.5; M 0 0.5 L 0.4 0.7; M 0.4 0.7 L 0.4 0.7; M 0.4 0.7 L 0.4 0.7; M 0.4 0.7 L 0.4 0.7" />
+                </path>
+                <path>
+                    <animate attributeName="d" dur="9s" begin="3s" repeatCount="indefinite" values="M 0.85 0.1 L 0.85 0.1; M 0.85 0.1 L 0.85 0.1; M 0.85 0.1 L 1 0.175; M 1 0.175 L 1 0.175; M 1 0.175 L 1 0.175; M 1 0.175 L 1 0.175" />
+                </path>
+            </g>
+        </pattern>
     </defs>
-    <g fill="hsl(187 74% 65%)">
-        <use href="#cube" x="10" y="-2" />
-        <use href="#cube" x="9.5" y="-1.75" />
-        <use href="#cube" x="9" y="-1.5" />
-        <use href="#cube" x="10" y="-2.5" />
-        <use href="#cube" x="10" y="-3" />
-        <use href="#cube" x="10" y="-3.5" />
-        <use href="#cube" x="9.5" y="-3.25" />
-        <use href="#cube" x="9" y="-3" />
-        <use href="#cube" x="9" y="-3.5" />
-        <use href="#cube" x="9" y="-4" />
-        <use href="#cube" x="10" y="-5" />
-        <use href="#cube" x="9.5" y="-4.75" />
-        <use href="#cube" x="9" y="-4.5" />
-        <use href="#cube" x="7.5" y="-0.75" />
-        <use href="#cube" x="7" y="-0.5" />
-        <use href="#cube" x="6.5" y="-0.25" />
-        <use href="#cube" x="7.5" y="-1.25" />
-        <use href="#cube" x="7.5" y="-1.75" />
-        <use href="#cube" x="7.5" y="-2.25" />
-        <use href="#cube" x="7" y="-2" />
-        <use href="#cube" x="6.5" y="-1.75" />
-        <use href="#cube" x="6.5" y="-2.25" />
-        <use href="#cube" x="6.5" y="-2.75" />
-        <use href="#cube" x="7.5" y="-3.75" />
-        <use href="#cube" x="7" y="-3.5" />
-        <use href="#cube" x="6.5" y="-3.25" />
-        <use href="#cube" x="5" y="0.5" />
-        <use href="#cube" x="4.5" y="0.75" />
-        <use href="#cube" x="4" y="1" />
-        <use href="#cube" x="4" y="0.5" />
-        <use href="#cube" x="4" y="0" />
-        <use href="#cube" x="4" y="-0.5" />
-        <use href="#cube" x="4" y="-1" />
-        <use href="#cube" x="4" y="-1.5" />
-        <use href="#cube" x="5" y="-2.5" />
-        <use href="#cube" x="4.5" y="-2.25" />
-        <use href="#cube" x="4" y="-2" />
-        <g fill="hsl(206 96% 72%)">
-            <path opacity="0.45" d="M 11.5 -5 11.5 -1 2 3.725 2 -0.25" />
-            <path opacity="0.9" d="M 11.5 -5 2 -0.25 0.5 -1 10 -5.75" />
-            <path opacity="0.6" d="M 2 3.725 2 -0.25 0.5 -1 0.5 0 1 0.25 1 3.25" />
+    <g fill="hsl(187 80% 42%)">
+        <g id="c-cube-s">
+            <use x="8" y="1.5" href="#c-cube" />
+            <use x="7.5" y="1.75" href="#c-cube" />
+            <use x="7" y="2" href="#c-cube" />
+            <use x="6.5" y="2.25" href="#c-cube" />
+            <use x="8" y="1" href="#c-cube" />
+            <use x="8" y="0.5" href="#c-cube" />
+            <use x="8" href="#c-cube" />
+            <use x="7.5" y="0.25" href="#c-cube" />
+            <use x="7" y="0.5" href="#c-cube" />
+            <use x="6.5" y="0.75" href="#c-cube" />
+            <use x="6.5" y="0.25" href="#c-cube" />
+            <use x="6.5" y="-0.25" href="#c-cube" />
+            <use x="8" y="-1.5" href="#c-cube" />
+            <use x="7.5" y="-1.25" href="#c-cube" />
+            <use x="7" y="-1" href="#c-cube" />
+            <use x="6.5" y="-0.75" href="#c-cube" />
         </g>
-        <use href="#cube" x="2.5" y="1.75" />
-        <use href="#cube" x="2" y="2" />
-        <use href="#cube" x="1.5" y="2.25" />
-        <use href="#cube" x="2.5" y="1.25" />
-        <use href="#cube" x="2.5" y="0.75" />
-        <use href="#cube" x="2.5" y="0.25" />
-        <use href="#cube" x="2" y="0.5" />
-        <use href="#cube" x="1.5" y="0.75" />
-        <use href="#cube" x="1.5" y="0.25" />
-        <use href="#cube" x="1.5" y="-0.25" />
-        <use href="#cube" x="2.5" y="-1.25" />
-        <use href="#cube" x="2" y="-1" />
-        <use href="#cube" x="1.5" y="-0.75" />
+        <use x="3" y="-1.5" href="#c-cube-s" />
+        <use x="5" y="3" href="#c-cube" />
+        <use x="4.5" y="3.25" href="#c-cube" />
+        <use x="4" y="3.5" href="#c-cube" />
+        <use x="3.5" y="3.25" href="#c-cube" />
+        <use x="3.5" y="2.75" href="#c-cube" />
+        <use x="3.5" y="2.25" href="#c-cube" />
+        <use x="3.5" y="1.75" href="#c-cube" />
+        <use x="3.5" y="1.25" href="#c-cube" />
+        <use x="5" href="#c-cube" />
+        <use x="4.5" y="0.25" href="#c-cube" />
+        <use x="4" y="0.5" href="#c-cube" />
+        <g opacity="0.5">
+            <path d="M 0 -0.25 3 1.25 12 -3.25 9 -4.75 Z" />
+            <path d="M 3 1.25 12 -3.25 12 0.75 3 5.25 Z" />
+            <path d="M 0 -0.25 3 1.25 3 5.25 0 3.75 Z" />
+        </g>
+        <g fill="none" stroke="hsl(185 84% 88%)" stroke-width="0.05" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M 0 -0.25 3 1.25 12 -3.25 9 -4.75 Z" />
+            <path d="M 3 1.25 12 -3.25 12 0.75 3 5.25 Z" />
+            <path d="M 0 -0.25 3 1.25 3 5.25 0 3.75 Z" />
+        </g>
+        <path fill="url(#c-pattern-1)" d="M 0 -0.25 3 1.25 12 -3.25 9 -4.75 Z" />
+        <path fill="url(#c-pattern)" d="M 0 -0.25 3 1.25 12 -3.25 12 0.75 3 5.25 0 3.75 Z" />
+        <use y="3" href="#c-cube" />
+        <use x="0.5" y="3.25" href="#c-cube" />
+        <use x="1" y="3.5" href="#c-cube" />
+        <use x="1.5" y="3.75" href="#c-cube" />
+        <use x="1.5" y="3.25" href="#c-cube" />
+        <use x="1.5" y="2.75" href="#c-cube" />
+        <use y="1.5" href="#c-cube" />
+        <use x="0.5" y="1.75" href="#c-cube" />
+        <use x="1" y="2" href="#c-cube" />
+        <use x="1.5" y="2.25" href="#c-cube" />
+        <use y="1" href="#c-cube" />
+        <use y="0.5" href="#c-cube" />
+        <use href="#c-cube" />
+        <use x="0.5" y="0.25" href="#c-cube" />
+        <use x="1" y="0.5" href="#c-cube" />
+        <use x="1.5" y="0.75" href="#c-cube" />
     </g>
 </svg>`
 };
