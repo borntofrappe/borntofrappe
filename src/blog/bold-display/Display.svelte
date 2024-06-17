608b60d3313dea392d1a2cfec16c777158511d7d
Gabriele Corti <borntofrappe@gmail.com>
2024-06-17 09:41:14 +0200

    dot-matrix-display
    define custom element; use a wrapper to inject the code as the component is mounted

---
+++ src/blog/bold-display/Display.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from 'svelte';
+	import './dot-matrix-display.js';
+
+	export let text = `                         
+ ....................... 
+ ..o.o.ooo.o...o....o... 
+ ..o.o.o...o...o...o.o.. 
+ ..ooo.oo..o...o...o.o.. 
+ ..o.o.o...o...o...o.o.. 
+ ..o.o.ooo.ooo.ooo..o... 
+ ....................... 
+                         `;
+
+	export let inset = '0.2';
+	export let rounding = '0.5';
+
+	let wrapper = null;
+	onMount(() => {
+		wrapper.innerHTML = `<dot-matrix-display inset=${inset} rounding=${rounding}><pre>${text}</pre></dot-matrix-display>`;
+	});
+</script>
+
+<div bind:this={wrapper}>
+	<pre>
+    {text}
+  </pre>
+</div>
