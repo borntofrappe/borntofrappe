18afdcf6ec822343cd57a10b1d07c7ffb9dc9250
Gabriele Corti <borntofrappe@gmail.com>
2024-12-01 18:50:14 +0100

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
 import adapter from "@sveltejs/adapter-netlify";
+import { mdsvex } from "mdsvex";
+
+const mdsvexConfig = {
+  extensions: [".md", ".svx"],
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [mdsvex(mdsvexConfig)],
+  extensions: [".svelte", ...mdsvexConfig.extensions],
   kit: {
     adapter: adapter({
       edge: false,
