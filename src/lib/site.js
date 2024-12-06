b8c0630cbf40c71c4c4107cc5daaa7992913a943
Gabriele Corti <borntofrappe@gmail.com>
2024-12-06 15:46:59 +0100

    remove unused resources
    icons repeated in static and routes upgrated to svelte 5

--- src/lib/site.js
+++ src/lib/site.js
@@ -2,6 +2,7 @@ import { dev } from "$app/environment";
 
 export default {
   title: "borntofrappe",
+  description: "The latest from Gabriele Corti.",
   origin: dev ? "" : "https://borntofrappe.netlify.app",
   author: {
     name: "Gabriele Corti",
