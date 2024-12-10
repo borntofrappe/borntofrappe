6111f9de4979c340b46d7588151725a703482445
Gabriele Corti <borntofrappe@gmail.com>
2024-12-10 21:36:42 +0100

    min styles
    improve readability of blog routes

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,6 +3,24 @@
   let { data } = $props();
 </script>
 
-<h1>{data.title}</h1>
+<main>
+  <h1>{data.title}</h1>
 
-{@render data.content()}
+  {@render data.content()}
+</main>
+
+<style>
+  main {
+    max-inline-size: 70ch;
+    padding-inline: 1rem;
+    line-height: 1.5;
+  }
+
+  main :global(:is(h1, h2, h3)) {
+    line-height: 1.1;
+  }
+
+  main > :global(* + *) {
+    margin-block-start: var(--flow-space, 1em);
+  }
+</style>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,30 +5,63 @@
   let { data } = $props();
 </script>
 
-<h1>blog posts</h1>
+<div class="layout">
+  <main>
+    <h1>Blog</h1>
 
-<div class="articles">
-  {#each data.blog as article}
-    <article>
-      <h2><a href={article.slug}>{article.title}</a></h2>
-      <p>{formatDate(article.date)}</p>
-      <p>{article.description}</p>
-    </article>
-  {/each}
+    {#each data.blog as article}
+      <article>
+        <h2><a href={article.slug}>{article.title}</a></h2>
+        <p>{formatDate(article.date)}</p>
+        <p>{article.description}</p>
+      </article>
+    {/each}
+  </main>
+
+  <nav>
+    {#if data.page > 1}
+      <a href="/blog?page={data.page - 1}">Newer articles</a>
+    {/if}
+
+    {#if data.page < data.pages}
+      <a href="/blog?page={data.page + 1}">Older articles</a>
+    {/if}
+  </nav>
 </div>
 
-{#if data.page > 1}
-  <a href="/blog?page={data.page - 1}">Newer articles</a>
-{/if}
-
-{#if data.page < data.pages}
-  <a href="/blog?page={data.page + 1}">Older articles</a>
-{/if}
-
 <style>
-  .articles {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(25ch, 1fr));
+  .layout {
+    max-inline-size: 70ch;
+    padding-inline: 1rem;
+  }
+
+  main > * + * {
+    margin-block-start: 1.5rem;
+  }
+
+  main > article:first-of-type {
+    margin-block-start: 2rem;
+  }
+
+  article > * + * {
+    margin-block-start: 0.5rem;
+  }
+
+  article {
+    line-height: 1.5;
+  }
+
+  article h2 {
+    line-height: 1.1;
+  }
+
+  nav {
+    display: flex;
     gap: 1rem;
+    justify-content: space-between;
+  }
+
+  nav > :only-child {
+    margin-inline-start: auto;
   }
 </style>
