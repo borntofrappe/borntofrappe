adcccb738f0d422959373193cf72d93ee2c6791d
Gabriele Corti <borntofrappe@gmail.com>
2024-12-18 15:13:38 +0100

    update label

--- src/blog/draw-data/SVG.svelte
+++ src/blog/draw-data/SVG.svelte
@@ -23,6 +23,7 @@
         >{value}
       </text>
     {/each}
-    <text x="0.2" y="0.6" font-size="0.4" font-family="monospace">Data</text>
+    <text x="0.2" y="0.6" font-size="0.4" font-family="monospace">Floaties</text
+    >
   </g>
 </svg>
