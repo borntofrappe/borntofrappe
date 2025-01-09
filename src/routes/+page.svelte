bbdd0e7fd62c993cc1ccef87541566de5c4c28c0
Gabriele Corti <borntofrappe@gmail.com>
2025-01-09 11:20:43 +0100

    update temp landing page
    remove snowfall

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import site from "$lib/site";
-  import SnowFall from "./SnowFall.svelte";
+  import TimePiece from "./TimePiece.svelte";
 </script>
 
 <svelte:head>
@@ -8,61 +8,29 @@
   <meta name="description" content={site.description} />
 </svelte:head>
 
-<SnowFall color="oklch(55% 0.02 270)" background="oklch(21% 0.05 260)" />
-
 <main>
   <h1>Hi there</h1>
-  <p>'tis the season to be redesinin</p>
+  <p>time's pressing for a brand new look</p>
+  <figure>
+    <TimePiece />
+  </figure>
 </main>
 
 <style>
-  ::selection {
-    color: oklch(97% 0.012 280);
-    background: oklch(45% 0.03 250);
-  }
-
   main {
-    color: oklch(97% 0.012 280);
-    /* background: oklch(21% 0.05 260); */
     padding: 1rem;
-    min-block-size: 100svb;
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    justify-content: center;
-    align-items: center;
-    line-height: 1.5;
-    animation: fade-in-bloom 2s 1s both ease-out;
   }
 
-  main > * {
-    color: inherit;
+  main > * + * {
+    margin-block-start: 0.5rem;
   }
 
-  h1 {
-    font-size: clamp(4.2915rem, 3.345rem + 4.7326vi, 7.0128rem);
-    line-height: 1.1;
-    font-family: var(--font-display);
+  figure {
+    margin-block-start: 1.5rem;
+    max-inline-size: 20rem;
   }
 
-  p {
-    font-size: clamp(1.4063rem, 1.3158rem + 0.4522vi, 1.6663rem);
-    font-weight: 700;
-    font-style: italic;
-  }
-
-  @keyframes fade-in-bloom {
-    0% {
-      opacity: 0;
-      filter: brightness(1) blur(20px);
-    }
-    10% {
-      opacity: 1;
-      filter: brightness(2) blur(10px);
-    }
-    100% {
-      opacity: 1;
-      filter: brightness(1) blur(0);
-    }
+  figure :global(svg) {
+    inline-size: 100%;
   }
 </style>
