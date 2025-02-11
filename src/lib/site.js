39b2428a6a150d66d22566c762865c4003f02011
Gabriele Corti <borntofrappe@gmail.com>
2025-02-11 14:29:50 +0100

    update site description

--- src/lib/site.js
+++ src/lib/site.js
@@ -2,7 +2,7 @@ import { dev } from "$app/environment";
 
 export default {
   title: "Borntofrappe",
-  description: "The latest from Gabriele Corti.",
+  description: "Name's Gabriele Corti. Software developer with an inkling and a flair to write delightful code.",
   origin: dev ? "" : "https://borntofrappe.netlify.app",
   author: {
     name: "Gabriele Corti",
