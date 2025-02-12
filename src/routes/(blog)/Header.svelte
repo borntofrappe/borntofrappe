66066f3bbeb6d5523a454cd4d64e22d3969e687f
Gabriele Corti <borntofrappe@gmail.com>
2025-02-12 18:19:01 +0100

    Header component
    h1 + Theme component for the blog pages

---
+++ src/routes/(blog)/Header.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Theme from "./Theme.svelte";
+
+  /** @type {{ title?: string }}*/
+  const { title = "Hello world" } = $props();
+</script>
+
+<header>
+  <h1 class="stroke-text">{title}</h1>
+
+  <Theme />
+</header>
+
+<style>
+   header {
+    --theme-color: #f1f5f8;
+    --theme-foreground: #838ac5;
+    --theme-background: #9cb2ce;
+   }
+   
+  :global([data-time-of-day="morning"]) header {
+    --theme-foreground: #838ac5;
+    --theme-background: #9cb2ce;
+  }
+
+  :global([data-time-of-day="day"]) header {
+    --theme-foreground: #639aef;
+    --theme-background: #73d2e6;
+  }
+
+  :global([data-time-of-day="night"]) header {
+    --theme-foreground: #7369b5;
+    --theme-background: #838ace;
+  }
+
+  header {
+    color: var(--theme-color);
+    background: var(--theme-background);
+    text-align: center;
+    padding: var(--space-m) var(--space-s);
+    padding-block-end: 0;
+  }
+
+  header :global(> *) {
+    max-inline-size: var(--measure-s);
+    margin-inline: auto;
+  }
+
+  h1.stroke-text {
+    --fill: var(--theme-color);
+    --stroke: var(--theme-foreground);
+  }
+
+  header h1 {
+    position: relative;
+  }
+</style>
