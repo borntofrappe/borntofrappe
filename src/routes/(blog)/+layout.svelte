ecbdc4395a43222276fdb3e701ea22af905dc88d
Gabriele Corti <borntofrappe@gmail.com>
2025-02-12 18:46:05 +0100

    re-implement /blog/+layout.svelte
    use the common container to apply color and background properties for the three themes

---
+++ src/routes/(blog)/+layout.svelte
@@ -0,0 +1,63 @@
+<script>
+  import Footer from "$lib/components/Footer.svelte";
+
+  /** @type {{ children: import('svelte').Snippet }} */
+  let { children } = $props();
+</script>
+
+<div class="body">
+  {@render children()}
+</div>
+<div class="footer">
+  <Footer />
+</div>
+
+<style>
+  .body {
+    --color: var(--gray-7);
+		--background: var(--gray-1);
+
+		--selection-color: var(--gray-8);
+		--selection-background: var(--gray-4);
+  }
+
+  .body {
+    color: var(--color);
+		background: var(--background);
+    min-block-size: 100svb;
+    z-index: 1;
+    position: relative;
+    
+  }
+
+  :global([data-time-of-day='morning']) .body {
+		--color: var(--gray-7);
+		--background: var(--gray-1);
+
+		--selection-color: var(--gray-8);
+		--selection-background: var(--gray-4);
+	}
+
+	:global([data-time-of-day='day']) .body {
+		--color: var(--gray-8);
+		--background: var(--gray-0);
+
+		--selection-color: var(--gray-9);
+		--selection-background: var(--gray-4);
+	}
+
+	:global([data-time-of-day='night']) .body {
+		--color: var(--gray-3);
+		--background: var(--gray-9);
+
+		--selection-color: var(--gray-8);
+		--selection-background: var(--gray-5);
+
+    color-scheme: dark;
+	}
+
+  .footer {
+    position: sticky;
+    inset-block-end: 0;
+  }
+</style>
