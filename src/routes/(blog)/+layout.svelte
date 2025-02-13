955dadb4a477b7e4eb5617cd0c997ff044e7179c
Gabriele Corti <borntofrappe@gmail.com>
2025-02-13 09:11:01 +0100

    update selectors
    start with day key-value pairs, switch to night with media preference, switch to theme with data attribute

--- src/routes/(blog)/+layout.svelte
+++ src/routes/(blog)/+layout.svelte
@@ -14,50 +14,61 @@
 
 <style>
   .body {
-    --color: var(--gray-7);
-		--background: var(--gray-1);
+    --color: var(--gray-8);
+    --background: var(--gray-0);
 
-		--selection-color: var(--gray-8);
-		--selection-background: var(--gray-4);
+    --selection-color: var(--gray-9);
+    --selection-background: var(--gray-4);
   }
 
   .body {
     color: var(--color);
-		background: var(--background);
+    background: var(--background);
     min-block-size: 100svb;
     z-index: 1;
     position: relative;
-    
   }
-
-  :global([data-time-of-day='morning']) .body {
-		--color: var(--gray-7);
-		--background: var(--gray-1);
-
-		--selection-color: var(--gray-8);
-		--selection-background: var(--gray-4);
-	}
-
-	:global([data-time-of-day='day']) .body {
-		--color: var(--gray-8);
-		--background: var(--gray-0);
-
-		--selection-color: var(--gray-9);
-		--selection-background: var(--gray-4);
-	}
-
-	:global([data-time-of-day='night']) .body {
-		--color: var(--gray-3);
-		--background: var(--gray-9);
-
-		--selection-color: var(--gray-8);
-		--selection-background: var(--gray-5);
-
-    color-scheme: dark;
-	}
 
   .footer {
     position: sticky;
     inset-block-end: 0;
+  }
+
+  :global(html[data-time-of-day="morning"]) .body {
+    --color: var(--gray-7);
+    --background: var(--gray-1);
+
+    --selection-color: var(--gray-8);
+    --selection-background: var(--gray-4);
+  }
+
+  :global(html[data-time-of-day="day"]) .body {
+    --color: var(--gray-8);
+    --background: var(--gray-0);
+
+    --selection-color: var(--gray-9);
+    --selection-background: var(--gray-4);
+  }
+
+  :global(html[data-time-of-day="night"]) .body {
+    --color: var(--gray-3);
+    --background: var(--gray-9);
+
+    --selection-color: var(--gray-8);
+    --selection-background: var(--gray-5);
+
+    color-scheme: dark;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :global(html:not([data-time-of-day])) .body {
+      --color: var(--gray-3);
+      --background: var(--gray-9);
+
+      --selection-color: var(--gray-8);
+      --selection-background: var(--gray-5);
+
+      color-scheme: dark;
+    }
   }
 </style>
