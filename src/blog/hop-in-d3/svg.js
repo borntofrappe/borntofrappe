959783dfd6413574121fc9f1f917865f6976757d
Gabriele Corti <borntofrappe@gmail.com>
2025-03-04 11:34:43 +0100

    helper graphic

---
+++ src/blog/hop-in-d3/svg.js
@@ -0,0 +1,27 @@
+export default {
+  "svg-g": `<svg style="display: block; --color-surface-1: light-dark(hsl(210 17% 98%), hsl(210 10% 23%)); --color-surface-2: light-dark(hsl(210 17% 95%), hsl(210 11% 15%)); --color-surface-3: light-dark(hsl(210 16% 93%), hsl(214 14% 10%)); --color-text-1: light-dark(hsl(210 10% 23%), hsl(210 14% 89%)); --color-text-2: light-dark(hsl(210 9% 31%), hsl(210 11% 71%));" viewBox="0 0 135 102">
+  <defs>
+    <pattern id="svg-g-pattern" width="10" height="10" patternUnits="userSpaceOnUse" viewBox="0 0 2 2">
+      <rect fill="var(--color-surface-2, hsl(0 0% 95%))" width="2" height="2" />
+      <g fill="var(--color-surface-3, hsl(0 0% 93%))">
+        <rect width="1" height="1" />
+        <rect x="1" y="1" width="1" height="1" />
+      </g>
+    </pattern>
+  </defs>
+  <rect fill="url(#svg-g-pattern)" width="160" height="127" />
+  <g transform="translate(12.5 12.5)">
+    <rect width="110" height="77" fill="var(--color-surface-1, hsl(0 0% 98%))" />
+    <g transform="translate(5 12)">
+      <rect width="100" height="60" fill="none" stroke="var(--color-text-2, hsl(0 0% 31%))" stroke-width="0.4" pathLength="100" stroke-dasharray="0.5 1" stroke-linecap="round" stroke-linejoin="round" />
+    </g>
+    <g fill="var(--color-text-1, hsl(0 0% 24%))" font-family="monospace" font-weight="700">
+      <text x="0" font-size="6"><tspan fill="var(--color-text-2, hsl(0 0% 31%))">&lt;</tspan>svg<tspan fill="var(--color-text-2, hsl(0 0% 31%))">&gt;</tspan></text>
+			<g transform="translate(5 12)">
+				<rect fill="var(--color-surface-1, hsl(0 0% 98%))" x="1" width="12" height="12" y="-6" />
+	      <text x="2" font-size="6"><tspan fill="var(--color-text-2, hsl(0 0% 31%))">&lt;</tspan>g<tspan fill="var(--color-text-2, hsl(0 0% 31%))">&gt;</tspan></text>
+			</g>
+    </g>
+  </g>
+</svg>`,
+};
