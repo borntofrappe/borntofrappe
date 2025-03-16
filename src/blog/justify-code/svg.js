79c0dc466cdeb4d6a54bf72753824b1acb726bee
Gabriele Corti <borntofrappe@gmail.com>
2025-03-16 14:58:16 +0100

    supporting graphic

---
+++ src/blog/justify-code/svg.js
@@ -0,0 +1,152 @@
+export default {
+  "braille-chart": `<svg style="display: block; color: light-dark(hsl(0 0% 10%), hsl(0 0% 92%)); background: light-dark(hsl(0 0% 97%), hsl(0 0% 12%));" viewBox="-1.5 -1 17 29.5">
+  <defs>
+    <circle id="dot-o" r="0.35" fill="currentColor" />
+    <circle id="dot-x" r="0.35" fill="currentColor" fill-opacity="0.2" />
+    <symbol id="letter-a" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-x" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-b" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-c" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-x" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-d" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-x" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-e" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-x" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-f" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-g" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-h" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-i" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-x" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-j" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-x" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-k" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-x" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-l" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-m" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-x" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-n" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-x" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-o" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-x" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-p" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-q" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-r" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-s" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-x" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-t" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-x" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-u" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-x" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-o" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-v" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-o" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-w" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-x" /><use href="#dot-o" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-o" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-x" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-x" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-o" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-y" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-o" x="1" /><use href="#dot-x" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-o" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-z" viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-o" /><use href="#dot-x" x="1" /><use href="#dot-x" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-o" y="2" /><use href="#dot-o" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-." viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-x" /><use href="#dot-x" x="1" /><use href="#dot-o" y="1" /><use href="#dot-o" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-o" x="1" y="2" />
+    </symbol>
+    <symbol id="letter-," viewBox="-0.5 -0.5 2 3">
+      <use href="#dot-x" /><use href="#dot-x" x="1" /><use href="#dot-o" y="1" /><use href="#dot-x" x="1" y="1" /><use href="#dot-x" y="2" /><use href="#dot-x" x="1" y="2" />
+    </symbol>
+  </defs>
+  <g fill="currentColor" font-family="serif" text-anchor="middle" font-size="0.9">
+    <text x="1" y="4">a</text>
+    <text x="4" y="4">b</text>
+    <text x="7" y="4">c</text>
+    <text x="10" y="4">d</text>
+    <text x="13" y="4">e</text>
+    <text x="1" y="8.7">f</text>
+    <text x="4" y="8.7">g</text>
+    <text x="7" y="8.7">h</text>
+    <text x="10" y="8.7">i</text>
+    <text x="13" y="8.7">j</text>
+    <text x="1" y="13.4">k</text>
+    <text x="4" y="13.4">l</text>
+    <text x="7" y="13.4">m</text>
+    <text x="10" y="13.4">n</text>
+    <text x="13" y="13.4">o</text>
+    <text x="1" y="18.1">p</text>
+    <text x="4" y="18.1">q</text>
+    <text x="7" y="18.1">r</text>
+    <text x="10" y="18.1">s</text>
+    <text x="13" y="18.1">t</text>
+    <text x="1" y="22.8">u</text>
+    <text x="4" y="22.8">v</text>
+    <text x="7" y="22.8">w</text>
+    <text x="10" y="22.8">x</text>
+    <text x="13" y="22.8">y</text>
+    <text x="1" y="27.5">z</text>
+    <text x="7" y="27.5">period (.)</text>
+    <text x="13" y="27.5">comma (,)</text>
+  </g>
+  <g style="color: light-dark(hsl(0 0% 20%), hsl(0 0% 60%));">
+    <use href="#letter-a" width="2" height="3" />
+    <use href="#letter-b" width="2" height="3" x="3" />
+    <use href="#letter-c" width="2" height="3" x="6" />
+    <use href="#letter-d" width="2" height="3" x="9" />
+    <use href="#letter-e" width="2" height="3" x="12" />
+    <use href="#letter-f" width="2" height="3" y="4.7" />
+    <use href="#letter-g" width="2" height="3" x="3" y="4.7" />
+    <use href="#letter-h" width="2" height="3" x="6" y="4.7" />
+    <use href="#letter-i" width="2" height="3" x="9" y="4.7" />
+    <use href="#letter-j" width="2" height="3" x="12" y="4.7" />
+    <use href="#letter-k" width="2" height="3" y="9.4" />
+    <use href="#letter-l" width="2" height="3" x="3" y="9.4" />
+    <use href="#letter-m" width="2" height="3" x="6" y="9.4" />
+    <use href="#letter-n" width="2" height="3" x="9" y="9.4" />
+    <use href="#letter-o" width="2" height="3" x="12" y="9.4" />
+    <use href="#letter-p" width="2" height="3" y="14.1" />
+    <use href="#letter-q" width="2" height="3" x="3" y="14.1" />
+    <use href="#letter-r" width="2" height="3" x="6" y="14.1" />
+    <use href="#letter-s" width="2" height="3" x="9" y="14.1" />
+    <use href="#letter-t" width="2" height="3" x="12" y="14.1" />
+    <use href="#letter-u" width="2" height="3" y="18.8" />
+    <use href="#letter-v" width="2" height="3" x="3" y="18.8" />
+    <use href="#letter-w" width="2" height="3" x="6" y="18.8" />
+    <use href="#letter-x" width="2" height="3" x="9" y="18.8" />
+    <use href="#letter-y" width="2" height="3" x="12" y="18.8" />
+    <use href="#letter-z" width="2" height="3" y="23.5" />
+    <use href="#letter-." width="2" height="3" x="6" y="23.5" />
+    <use href="#letter-," width="2" height="3" x="12" y="23.5" />
+  </g>
+</svg>`,
+};
