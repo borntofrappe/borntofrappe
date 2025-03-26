fe4990cf7f3839070fe0998d6de73d95bc510980
Gabriele Corti <borntofrappe@gmail.com>
2025-03-26 09:35:28 +0100

    intro graphic

---
+++ src/blog/getting-rusty/svg.js
@@ -0,0 +1,47 @@
+export default {
+  crust: `<svg style="display: block;" viewBox="-215 -175 430 350">
+  <defs>
+    <linearGradient id="crust-gradient-back" x1="0" y1="-80" x2="0" y2="80" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#af2516" offset="0" />
+      <stop stop-color="#b92e30" offset="0.5" />
+      <stop stop-color="#d02f2a" offset="1" />
+    </linearGradient>
+    <linearGradient id="crust-gradient-body" x1="0" y1="-175" x2="0" y2="175" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#fb8100" offset="0" />
+      <stop stop-color="#fc7202" offset="0.45" />
+      <stop stop-color="#fa5e08" offset="0.7" />
+      <stop stop-color="#f24506" offset="1" />
+    </linearGradient>
+    <linearGradient id="crust-gradient-eye" x1="0.5" y1="0" x2="0.5" y2="1">
+      <stop stop-color="#ef4e14" offset="0" />
+      <stop stop-color="#f85917" offset="0.25" />
+      <stop stop-color="#fa6d13" offset="0.5" />
+      <stop stop-color="#f98a47" offset="0.75" />
+      <stop stop-color="#fd8f3d" offset="1" />
+    </linearGradient>
+  </defs>
+  <g fill="url(#crust-gradient-back)" stroke="url(#crust-gradient-back)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+    <g transform="scale(0.85)">
+      <path transform="translate(140 -8) rotate(-5)" d="M -12.64 -8.52 C 27.25 -6.54 55.16 14.91 57.66 46.31 S 36.23 77.71 -3.15 112.61 -10.64 79.21 -1.17 27.86 -6.15 5.43 -13.14 -7.52 Z" />
+      <path transform="scale(-1 1) translate(140 -8) rotate(-5)" d="M -12.64 -8.52 C 27.25 -6.54 55.16 14.91 57.66 46.31 S 36.23 77.71 -3.15 112.61 -10.64 79.21 -1.17 27.86 -6.15 5.43 -13.14 -7.52" />
+    </g>
+    <path transform="scale(0.95 0.9) translate(0 20)" d="M 169.98 -13.37 C 150 50 30 55 0 55 -30 55 -150 50 -169.98 -13.37 L 0 -" />
+  </g>
+  <g fill="url(#crust-gradient-body)" stroke="url(#crust-gradient-body)" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+    <path transform="translate(140 -8) rotate(-5)" d="M -12.64 -8.52 C 27.25 -6.54 55.16 14.91 57.66 46.31 S 36.23 77.71 -3.15 112.61 -10.64 79.21 -1.17 27.86 -6.15 5.43 -13.14 -7.52 Z" />
+    <path transform="scale(-1 1) translate(140 -8) rotate(-5)" d="M -12.64 -8.52 C 27.25 -6.54 55.16 14.91 57.66 46.31 S 36.23 77.71 -3.15 112.61 -10.64 79.21 -1.17 27.86 -6.15 5.43 -13.14 -7.52" />
+    <path d="M 0 -168.91 C 6.46 -165.87 10.73 -158.04 15.55 -148.96 S 25.35 -149.3 40.5 -165.15 47.61 -156.07 51 -143.07 59.01 -140.04 76.48 -150.72 83.06 -145.39 83.43 -132.55 91.98 -124.19 106.23 -130.77 116.73 -124.89 114.95 -112.25 121.16 -103.69 135.06 -104.77 144.5 -97.64 140.93 -83.2 144.14 -76.09 157.86 -73.41 167.13 -65.22 160.53 -50.42 166.78 -40.62 182.28 -37.07 181.75 -26.2 169.98 -13.37 C 150 50 30 55 0 55 Z" />
+    <path transform="scale(-1 1)" d="M 0 -168.91 C 6.46 -165.87 10.73 -158.04 15.55 -148.96 S 25.35 -149.3 40.5 -165.15 47.61 -156.07 51 -143.07 59.01 -140.04 76.48 -150.72 83.06 -145.39 83.43 -132.55 91.98 -124.19 106.23 -130.77 116.73 -124.89 114.95 -112.25 121.16 -103.69 135.06 -104.77 144.5 -97.64 140.93 -83.2 144.14 -76.09 157.86 -73.41 167.13 -65.22 160.53 -50.42 166.78 -40.62 182.28 -37.07 181.75 -26.2 169.98 -13.37 C 150 50 30 55 0 55 Z" />  
+    <path transform="translate(-5 5)" d="M 50 140 C 49 125 49 115 50 100 C 40 101 25 101 10 100 C 20 70 45 55 70 60 C 80 45 80 45 70 30 L 110 20 C 110 50 110 50 95 85 C 100 110 80 135 50 140" />
+    <path transform="scale(-1 1) translate(-5 5)" d="M 50 140 C 49 125 49 115 50 100 C 40 101 25 101 10 100 C 20 70 45 55 70 60 C 80 45 80 45 70 30 L 110 20 C 110 50 110 50 95 85 C 100 110 80 135 50 140" />
+  </g>
+  <g transform="translate(47 -3)">
+    <ellipse fill="hsl(0 0% 30%)" stroke="url(#crust-gradient-eye)" stroke-width="4" paint-order="stroke" rx="28" ry="31" />
+    <ellipse transform="translate(-9.5 -12.5) rotate(-48)" fill="#fafdfd" rx="11.5" ry="8.5" />
+  </g>
+  <g transform="translate(-47 -3)">
+    <ellipse fill="hsl(0 0% 30%)" stroke="url(#crust-gradient-eye)" stroke-width="4" paint-order="stroke" rx="28" ry="31" />
+    <ellipse transform="translate(-9.5 -12.5) rotate(-48)" fill="#fafdfd" rx="11.5" ry="8.5" />
+  </g>
+</svg>`,
+};
