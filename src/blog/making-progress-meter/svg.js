afd80085fc341fb12eef93b9e66450d8161d2904
Gabriele Corti <borntofrappe@gmail.com>
2025-04-18 11:08:00 +0200

    helper graphic

---
+++ src/blog/making-progress-meter/svg.js
@@ -0,0 +1,14 @@
+export default {
+  "progress-meter": `<svg style="display: block;" viewBox="-0.5 -0.5 43 2.8">
+  <rect fill="lightgreen" width="16.8" height="1" />
+  <rect fill="khaki" x="16.8" width="12.6" height="1" />
+  <g fill="none" stroke="currentColor" stroke-width="0.04" >
+    <path d="M 0 0 42 0 42 1 0 1 Z M 16.8 0 v 1.5 M 29.4 0 v 1.5 M 42 0 v 1.5" />
+  </g>
+  <g fill="currentColor" text-anchor="end" font-family="monospace" font-size="0.8" transform="translate(0 2)">
+    <text x="16.8">complete</text>
+    <text x="29.4">paused</text>
+    <text x="42">left</text>
+  </g>
+</svg>`,
+};
