6e330a0ea24c6f1c7e9478c214a5cfb3eb36c4c6
Gabriele Corti <borntofrappe@gmail.com>
2025-05-22 22:17:16 +0200

    css fade copy

---
+++ src/blog/breaking-style/FadeInCSS.svelte
@@ -0,0 +1,21 @@
+<script>
+  /** @type {{ number: number }}*/
+  let { number } = $props();
+</script>
+
+<svg style="display: block;" viewBox="0 0 1 1">
+  {#key number}
+    <use href="#rune-{number}" />
+  {/key}
+</svg>
+
+<style>
+  use {
+    @media (prefers-reduced-motion: no-preference) {
+      transition: opacity 0.5s ease-out;
+      @starting-style {
+        opacity: 0;
+      }
+    }
+  }
+</style>
