a2cd8a66f7967796b33f899d90608196c002ef93
Gabriele Corti <borntofrappe@gmail.com>
2025-05-22 22:16:57 +0200

    svelte fade in example

---
+++ src/blog/breaking-style/FadeInSvelte.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { fade } from "svelte/transition";
+  /** @type {{ number: number }}*/
+  let { number } = $props();
+</script>
+
+<svg style="display: block;" viewBox="0 0 1 1">
+  {#key number}
+    <use in:fade href="#rune-{number}" />
+  {/key}
+</svg>
+
