09735364d27b23705b2abf384acf8e2dd7b361a6
Gabriele Corti <borntofrappe@gmail.com>
2025-10-26 15:46:14 +0100

    update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,8 +12,26 @@
   <meta name="description" content={site.description} />
 </svelte:head>
 
+<Header title="borntofrappe" />
+
 <main>
-  <Header title="Blog" />
+  {#if data.page === 1}
+    <blockquote>
+      <p>Welcome!</p>
+      <p>
+        This is, or rather was, a happy playground for a software developer, one
+        <b>Gabriele Corti</b> from northern Italy.
+      </p>
+      <p>
+        There are <em>a few</em> articles on software, but let us be honest, despite
+        a couple of musings experimenting with other languages and inspiring tech,
+        the time was ripe to archive the project and let it rest.
+      </p>
+      <p>A testament to a stack deeply enjoyed.</p>
+      <p>Evidence of the insanity and daring hand-writing SVG.</p>
+      <cite>Gabriele Corti, Autumn 2025</cite>
+    </blockquote>
+  {/if}
 
   <div>
     {#each data.blog as { slug, title, date, description }}
@@ -26,13 +44,13 @@
 
     <nav>
       {#if data.page > 1}
-        <a class="link-underline" href="/blog?page={data.page - 1}"
+        <a class="link-underline" href="/?page={data.page - 1}"
           >Newer articles</a
         >
       {/if}
 
       {#if data.page < data.pages}
-        <a class="link-underline" href="/blog?page={data.page + 1}"
+        <a class="link-underline" href="/?page={data.page + 1}"
           >Older articles</a
         >
       {/if}
@@ -41,10 +59,38 @@
 </main>
 
 <style>
-  div {
+  main {
     padding: var(--space-m) var(--space-s);
     max-inline-size: var(--measure-l);
     margin-inline: auto;
+  }
+
+  main {
+    > * + * {
+      margin-block-start: var(--space-xs);
+    }
+
+    div {
+      margin-block-start: var(--space-l);
+    }
+  }
+
+  blockquote {
+    max-inline-size: var(--measure-m);
+    margin-inline: auto;
+    border-radius: var(--space-xs);
+    background: var(--surface-1);
+    padding: var(--space-m) var(--space-s);
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-xs);
+
+    > cite {
+      align-self: end;
+    }
+    > cite::before {
+      content: "— ";
+    }
   }
 
   div > * + * {
@@ -55,17 +101,17 @@
     margin-block-start: var(--space-xl);
   }
 
-  article > * + * {
+  div article > * + * {
+    display: block;
     margin-block-start: var(--space-2xs);
   }
 
-  article > time {
-    display: inline-block;
+  div article > time {
     font-size: var(--step--1);
     font-style: italic;
   }
 
-  nav {
+  div nav {
     display: flex;
     gap: var(--space-s);
     justify-content: space-between;
